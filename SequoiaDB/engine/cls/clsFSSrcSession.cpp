9380eaaf103eee81e9968c966a563d56340f8714
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-01-09 14:09:33 +0000

    解决全量同步时重发数据被误认为日志的bug
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@10175 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsFSSrcSession.cpp
+++ SequoiaDB/engine/cls/clsFSSrcSession.cpp
@@ -182,7 +182,7 @@ namespace engine
       msg.header.header.routeID = req->header.routeID ;
       msg.header.header.requestID = req->header.requestID ;
       msg.packet = req->packet ;
-      msg.type = req->type ;
+      msg.type = _dataType ;
 
       if ( CLS_FS_NOTIFY_TYPE_DOC == _dataType )
       {
