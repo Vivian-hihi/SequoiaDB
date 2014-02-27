fe9929c0a066f2244a7ffb4cae140b1b1917d368
lijianhua <lijianhua@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-02-27 02:02:39 +0000

    【性能优化】创建大量子表，挂到同一主表下时，耗时过长
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@10863 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsShardMgr.cpp
+++ SequoiaDB/engine/cls/clsShardMgr.cpp
@@ -554,32 +554,6 @@ namespace engine
          _catLatch.release () ;
       }
 
-      if ( SDB_OK == rc )
-      {
-         _clsCatalogSet *pClSet = NULL;
-         std::vector< std::string > subCLLst;
-         _pCatAgent->lock_r();
-         pClSet = _pCatAgent->collectionSet( pCollectionName );
-         if ( NULL == pClSet )
-         {
-            _pCatAgent->release_r();
-            rc = SDB_CLS_NO_CATALOG_INFO;
-            goto error;
-         }
-         pClSet->getSubCLList( subCLLst );
-         _pCatAgent->release_r();
-         {
-         // update all sub-collection catalog info               
-         std::vector< std::string >::iterator iterLst
-                                 = subCLLst.begin();
-         while( SDB_OK == rc && iterLst != subCLLst.end() )
-         {
-            rc = syncUpdateCatalog( iterLst->c_str(), millsec );
-            ++iterLst;
-         }
-         }
-      }
-
    done:
       PD_TRACE_EXITRC ( SDB__CLSSHDMGR_SYNCUPDCAT, rc );
       return rc ;
