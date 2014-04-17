647f56296e98a3bfab73fbf82916f18154738778
yunwu <yunwu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-04-17 08:34:20 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@11651 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/ssh2/libssh2_config.h
+++ SequoiaDB/engine/ssh2/libssh2_config.h
@@ -1,53 +1,10 @@
 /* src/libssh2_config.h.  Generated from libssh2_config.h.in by configure.  */
 /* src/libssh2_config.h.in.  Generated from configure.ac by autoheader.  */
 
-#if defined (_WINDOWS)
-#ifndef LIBSSH2_CONFIG_H
-#define LIBSSH2_CONFIG_H
-
-#ifndef WIN32
-#define WIN32
-#endif
-#ifndef _CRT_SECURE_NO_DEPRECATE
-#define _CRT_SECURE_NO_DEPRECATE 1
-#endif /* _CRT_SECURE_NO_DEPRECATE */
-#include <winsock2.h>
-#include <mswsock.h>
-#include <ws2tcpip.h>
-
-#ifdef __MINGW32__
-#define HAVE_UNISTD_H
-#define HAVE_INTTYPES_H
-#define HAVE_SYS_TIME_H
-#define HAVE_GETTIMEOFDAY
-#endif
-
-#define HAVE_WINSOCK2_H
-#define HAVE_IOCTLSOCKET
-#define HAVE_SELECT
-
-#ifdef _MSC_VER
-#define snprintf _snprintf
-#if _MSC_VER < 1500
-#define vsnprintf _vsnprintf
-#endif
-#define strdup _strdup
-#define strncasecmp _strnicmp
-#define strcasecmp _stricmp
-#else
-#define strncasecmp strnicmp
-#define strcasecmp stricmp
-#endif /* _MSC_VER */
-
-/* Enable newer diffie-hellman-group-exchange-sha1 syntax */
-#define LIBSSH2_DH_GEX_NEW 1
-
-#endif /* LIBSSH2_CONFIG_H */
-
-
-#else
 /* Define if building universal (internal helper macro) */
 /* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+#if defined(_LINUX)
 
 /* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
    systems. This function is required for `alloca.c' support on those systems.
@@ -275,4 +232,48 @@
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
 /* #undef size_t */
+
+#else
+#ifndef LIBSSH2_CONFIG_H
+#define LIBSSH2_CONFIG_H
+
+#ifndef WIN32
+#define WIN32
+#endif
+#ifndef _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE 1
+#endif /* _CRT_SECURE_NO_DEPRECATE */
+#include <winsock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+
+#ifdef __MINGW32__
+#define HAVE_UNISTD_H
+#define HAVE_INTTYPES_H
+#define HAVE_SYS_TIME_H
+#define HAVE_GETTIMEOFDAY
+#endif
+
+#define HAVE_WINSOCK2_H
+#define HAVE_IOCTLSOCKET
+#define HAVE_SELECT
+
+#ifdef _MSC_VER
+#define snprintf _snprintf
+#if _MSC_VER < 1500
+#define vsnprintf _vsnprintf
+#endif
+#define strdup _strdup
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#else
+#define strncasecmp strnicmp
+#define strcasecmp stricmp
+#endif /* _MSC_VER */
+
+/* Enable newer diffie-hellman-group-exchange-sha1 syntax */
+#define LIBSSH2_DH_GEX_NEW 1
+
+#endif /* LIBSSH2_CONFIG_H */
+
 #endif
