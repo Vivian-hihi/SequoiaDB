4cab9281c1e077193653a955fb460e9884261e14
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-04-17 06:31:35 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@11642 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ misc/dita2troff/DITA-OT1.8.4/build.bat
@@ -0,0 +1,24 @@
+::collection
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\cl.count.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\cl.insert.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+
+::collection space
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\cs.createCL.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\cs.dropCL.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\cs.getCL.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+
+::cursor
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\cursor.sort.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+
+::sequoiadb
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\db.backupOffline.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\db.createCataRG.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\db.cancelTask.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\db.createCS.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\db.createProcedure.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+
+::replica group
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\rg.createNode.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
+
+::node
+java -jar lib/dost.jar /i:E:\sequoiadb\doc\references\methods\topics_en_v2\node.start.dita /transtype:troff /outdir:E:\sequoiadb\doc\manual
