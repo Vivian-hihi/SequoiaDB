76411245f3201fa95aa3eeb54392a9689490d9b9
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-04-23 10:02:58 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@11741 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/pmd/pmdEnv.cpp
@@ -0,0 +1,314 @@
+/*******************************************************************************
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = pmdEnv.cpp
+
+   Descriptive Name = Process MoDel Main
+
+   When/how to use: this program may be used on binary and text-formatted
+   versions of PMD component. This file contains main function for SequoiaDB,
+   and all other process-initialization code.
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          22/04/2014  XJH Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#include "pmdEnv.hpp"
+#include "ossEDU.hpp"
+#include "pmdSignalHandler.hpp"
+#include "pmd.hpp"
+#include "pdTrace.hpp"
+#include "pmdTrace.hpp"
+
+using namespace bson ;
+
+namespace engine
+{
+
+   pmdSysInfo* pmdGetSysInfo()
+   {
+      static pmdSysInfo s_sysInfo ;
+      return &s_sysInfo ;
+   }
+   SDB_ROLE pmdGetDBRole()
+   {
+      return pmdGetSysInfo()->_dbrole ;
+   }
+   void  pmdSetDBRole( SDB_ROLE role )
+   {
+      pmdGetSysInfo()->_dbrole = role ;
+   }
+   MsgRouteID pmdGetNodeID()
+   {
+      return pmdGetSysInfo()->_nodeID ;
+   }
+   void pmdSetNodeID( MsgRouteID id )
+   {
+      pmdGetSysInfo()->_nodeID = id ;
+   }
+
+   INT32& pmdGetSigNum()
+   {
+      static INT32 s_sigNum = -1 ;
+      return s_sigNum ;
+   }
+
+#if defined (_LINUX)
+
+   // PD_TRACE_DECLARE_FUNCTION ( SDB_PMDSIGHND, "pmdSignalHandler" )
+   void pmdSignalHandler ( INT32 sigNum )
+   {
+      PD_TRACE_ENTRY ( SDB_PMDSIGHND ) ;
+
+      // set signum
+      PMD_SIGNUM = sigNum ;
+
+      if ( sigNum > 0 && sigNum <= OSS_MAX_SIGAL )
+      {
+         // avoid calling PD_LOG since localtime_r is not signal safe
+         //PD_LOG ( PDEVENT, "Recieve signal[%d:%s, %s]",
+         //         sigNum, pmdGetSignalInfo( sigNum )._name,
+         //         pmdGetSignalInfo( sigNum )._handle ? "QUIT" : "IGNORE" ) ;
+         if ( pmdGetSignalInfo( sigNum )._handle ) // quit
+         {
+            PMD_SHUTDOWN_DB( SDB_INTERRUPT ) ;
+         }
+      }
+      PD_TRACE_EXIT ( SDB_PMDSIGHND ) ;
+   }
+
+   // PD_TRACE_DECLARE_FUNCTION ( SDB_PMDEDUUSERTRAPHNDL, "pmdEDUUserTrapHandler" )
+   void pmdEDUUserTrapHandler( OSS_HANDPARMS )
+   {
+      INT32 rc = SDB_OK ;
+      PD_TRACE_ENTRY ( SDB_PMDEDUUSERTRAPHNDL );
+      oss_edu_data * pEduData = NULL ;
+      const CHAR *dumpPath = ossGetTrapExceptionPath () ;
+      if ( !dumpPath )
+      {
+         goto done ;
+      }
+
+      pEduData = _ossEduData.get() ;
+
+      if ( NULL == pEduData )
+      {
+         goto done ;
+      }
+
+      if ( OSS_AM_I_INSIDE_SIGNAL_HANDLER( pEduData ) )
+      {
+         goto done ;
+      }
+      OSS_ENTER_SIGNAL_HANDLER( pEduData ) ;
+
+      if ( signum == OSS_STACK_DUMP_SIGNAL )
+      {
+         PD_LOG ( PDEVENT, "Signal %d is received, "
+                  "prepare to dump stack for all threads", signum ) ;
+         std::set<pthread_t>::iterator it ;
+         std::set<pthread_t> tidList ;
+         pmdGetKRCB()->getEDUMgr()->getEDUThreadID ( tidList ) ;
+         for ( it = tidList.begin(); it != tidList.end(); ++it )
+         {
+            // threadID was initialized to 0 in constructor, and set to real
+            // thread id in pmdEDUEntryPoint
+            if ( 0 == (*it) )
+            {
+               continue ;
+            }
+            rc = ossPThreadKill ( (*it), OSS_STACK_DUMP_SIGNAL_INTERNAL ) ;
+            if ( rc )
+            {
+               PD_LOG ( PDWARNING, "Failed to send signal %d to thread %llu, "
+                        "errno = %d", OSS_STACK_DUMP_SIGNAL_INTERNAL,
+                        (*it), ossGetLastError() ) ;
+            }
+         }
+         ossMemTrace ( dumpPath ) ;
+      }
+      else if ( signum == OSS_STACK_DUMP_SIGNAL_INTERNAL )
+      {
+         PD_LOG ( PDEVENT, "Signal %d is received, "
+                  "prepare to dump stack for %u:%u", signum,
+                  ossGetCurrentProcessID(),
+                  ossGetCurrentThreadID() ) ;
+         ossStackTrace( OSS_HANDARGS, dumpPath ) ;
+      }
+      else
+      {
+         PD_LOG ( PDWARNING, "Unexpected signal is received: %d",
+                  signum ) ;
+      }
+      OSS_LEAVE_SIGNAL_HANDLER( pEduData ) ;
+   done :
+      PD_TRACE1 ( SDB_PMDEDUUSERTRAPHNDL, PD_PACK_INT(rc) );
+      PD_TRACE_EXIT ( SDB_PMDEDUUSERTRAPHNDL ) ;
+      return ;
+   }
+
+   INT32 pmdEnableSignalEvent( const CHAR *filepath )
+   {
+      INT32 rc = SDB_OK ;
+      ossSigSet sigSet ;
+      struct sigaction newact ;
+      ossMemset ( &newact, 0, sizeof(newact)) ;
+      sigemptyset ( &newact.sa_mask ) ;
+
+      // set trap file path
+      if ( filepath )
+      {
+         ossSetTrapExceptionPath ( filepath ) ;
+      }
+
+      // SIGSEGV( 11 )
+      newact.sa_sigaction = ( OSS_SIGFUNCPTR ) ossEDUCodeTrapHandler ;
+      newact.sa_flags |= SA_SIGINFO ;
+      newact.sa_flags |= SA_ONSTACK ;
+      if ( sigaction ( SIGSEGV, &newact, NULL ) )
+      {
+         PD_LOG ( PDERROR, "Failed to setup signal handler for SIGSEGV" ) ;
+         rc = SDB_SYS ;
+         goto error ;
+      }
+      if ( sigaction ( SIGBUS, &newact, NULL ) )
+      {
+         PD_LOG ( PDERROR, "Failed to setup signal handler for SIGBUS" ) ;
+         rc = SDB_SYS ;
+         goto error ;
+      }
+
+      // stack dump signals: SIGURG( 23 )
+      newact.sa_sigaction = ( OSS_SIGFUNCPTR ) pmdEDUUserTrapHandler ;
+      newact.sa_flags |= SA_SIGINFO ;
+      newact.sa_flags |= SA_ONSTACK ;
+      if ( sigaction ( OSS_STACK_DUMP_SIGNAL, &newact, NULL ) )
+      {
+         PD_LOG ( PDERROR, "Failed to setup signal handler for dump signal" ) ;
+         rc = SDB_SYS ;
+         goto error ;
+      }
+      // capture the internal user stack dump signal
+      if ( sigaction ( OSS_STACK_DUMP_SIGNAL_INTERNAL, &newact, NULL ) )
+      {
+         PD_LOG ( PDERROR, "Failed to setup signal handler for dump signal" ) ;
+         rc = SDB_SYS ;
+         goto error ;
+      }
+
+      // other signal
+      sigSet.fillSet () ;
+      sigSet.sigDel ( SIGSEGV ) ;
+      sigSet.sigDel ( SIGBUS ) ;
+      sigSet.sigDel ( SIGALRM ) ;
+      sigSet.sigDel ( SIGPROF ) ;
+      sigSet.sigDel ( OSS_STACK_DUMP_SIGNAL ) ;
+      sigSet.sigDel ( OSS_STACK_DUMP_SIGNAL_INTERNAL ) ;
+      rc = ossRegisterSignalHandle( sigSet, (SIG_HANDLE)pmdSignalHandler ) ;
+      if ( SDB_OK != rc )
+      {
+         PD_LOG ( PDWARNING, "Failed to register signals, rc = %d", rc ) ;
+         // we do not abort startup process if any signal handler can't be
+         // installed
+         rc = SDB_OK ;
+      }
+
+   done :
+      return rc ;
+   error :
+      goto done ;
+   }
+
+#else
+
+   // PD_TRACE_DECLARE_FUNCTION ( SDB_PMDCTRLHND, "pmdCtrlHandler" )
+   BOOL pmdCtrlHandler( DWORD fdwCtrlType )
+   {
+      BOOLEAN ret = FALSE ;
+      PD_TRACE_ENTRY ( SDB_PMDCTRLHND );
+      switch( fdwCtrlType )
+      {
+      // Handle the CTRL-C signal.
+      case CTRL_C_EVENT:
+         PMD_SHUTDOWN_DB( SDB_INTERRUPT ) ;
+         printf( "Ctrl-C event\n\n" ) ;
+         Beep( 750, 300 );
+         ret = TRUE ;
+         goto done ;
+
+      // CTRL-CLOSE: confirm that the user wants to exit.
+      case CTRL_CLOSE_EVENT:
+         Beep( 600, 200 );
+         printf( "Ctrl-Close event\n\n" ) ;
+         ret = TRUE ;
+         goto done ;
+
+      // Pass other signals to the next handler.
+      case CTRL_BREAK_EVENT:
+         Beep( 900, 200 );
+         printf( "Ctrl-Break event\n\n" ) ;
+         ret = FALSE ;
+         goto done ;
+
+      case CTRL_LOGOFF_EVENT:
+         Beep( 1000, 200 );
+         printf( "Ctrl-Logoff event\n\n" ) ;
+         ret = FALSE ;
+         goto done ;
+
+      case CTRL_SHUTDOWN_EVENT:
+         Beep( 750, 500 );
+         printf( "Ctrl-Shutdown event\n\n" ) ;
+         ret = FALSE ;
+         goto done ;
+
+      default:
+         ret = FALSE ;
+         goto done ;
+      }
+   done :
+      PD_TRACE1 ( SDB_PMDCTRLHND, PD_PACK_INT(ret) ) ;
+      PD_TRACE_EXIT ( SDB_PMDCTRLHND ) ;
+      return ret ;
+   }
+
+   INT32 pmdEnableSignalEvent( const CHAR * filepath )
+   {
+      // set trap file path
+      if ( filepath )
+      {
+         ossSetTrapExceptionPath ( filepath ) ;
+      }
+
+      // install ctrl event handler
+      SetConsoleCtrlHandler( (PHANDLER_ROUTINE)pmdCtrlHandler, TRUE ) ;
+
+      return SDB_OK ;
+   }
+
+#endif // _LINUX
+
+}
+
+
