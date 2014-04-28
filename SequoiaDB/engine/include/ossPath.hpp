49bbda4c376b6489c0d70dab6d31f945517f840e
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-04-28 06:18:59 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@11808 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/ossPath.hpp
+++ SequoiaDB/engine/include/ossPath.hpp
@@ -1,4 +1,18 @@
+
 #ifndef OSSPATH_HPP__
 #define OSSPATH_HPP__
+
 #include "ossPath.h"
-#endif
+#include <string>
+#include <map>
+
+using namespace std ;
+
+INT32 ossEnumFiles( const string &dirPath,
+                    map<string, string> &mapFiles,
+                    const CHAR *filter = NULL,
+                    UINT32 deep = 1 ) ;
+
+
+#endif // OSSPATH_HPP__
+
