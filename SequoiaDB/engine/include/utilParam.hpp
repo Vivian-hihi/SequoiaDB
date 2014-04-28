c9e7af50603c0f702fa68645ae0640d2d69d22ec
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-04-28 06:21:50 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@11820 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/include/utilParam.hpp
@@ -0,0 +1,61 @@
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = utilParam.hpp
+
+   Descriptive Name =
+
+   When/how to use: str util
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          26/04/2014  XJH Initial Draft
+
+   Last Changed =
+
+******************************************************************************/
+
+#ifndef UTILPARAM_HPP__
+#define UTILPARAM_HPP__
+
+#include "core.hpp"
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
+
+namespace po = boost::program_options ;
+
+namespace engine
+{
+
+   INT32 utilReadConfigureFile( const CHAR *file,
+                                po::options_description &desc,
+                                po::variables_map &vm ) ;
+
+   INT32 utilReadCommandLine( INT32 argc, CHAR **argv,
+                              po::options_description &desc,
+                              po::variables_map &vm ) ;
+
+}
+
+#endif // UTILPARAM_HPP__
+
