152a6db9f66367f87d1e15e874441827df33cf89
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-04-29 06:34:12 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@11870 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ client/samples/CPP/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "###############building file..."
+./buildApp.sh connect
+./buildApp.sh query
+./buildApp.sh shard
+./buildApp.sh index
+./buildApp.sh sql
+./buildApp.sh insert
+./buildApp.sh update
+
+echo "###############running connect..."
+./build/connect localhost 11810 "" ""
+echo "###############running connect.static..."
+./build/connect.static localhost 11810 "" ""
+
+echo "###############running snap..."
+./build/query localhost 11810 "" ""
+echo "###############running snap.static..."
+./build/query.static localhost 11810 "" ""
+
+echo "###############running shard..."
+./build/shard localhost 11810 "" ""
+echo "###############running shard.static..."
+./build/shard.static localhost 11810 "" ""
+
+echo "###############running index..."
+./build/index localhost 11810 "" ""
+echo "###############running index.static..."
+./build/index.static localhost 11810 "" ""
+
+echo "###############running sql..."
+./build/sql localhost 11810 "" ""
+echo "###############running sql.static..."
+./build/sql.static localhost 11810 "" ""
+
+echo "###############running insert..."
+./build/insert localhost 11810 "" ""
+echo "###############running insert.static..."
+./build/insert.static localhost 11810 "" ""
+
+echo "###############running update..."
+./build/update localhost 11810 "" ""
+echo "###############running update.staic..."
+./build/update.static localhost 11810 "" ""
+
