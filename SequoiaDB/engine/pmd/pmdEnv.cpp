abd542d002658ff75d5bb2267b604dc89f9bd25d
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-05-15 12:10:51 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@12039 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/pmdEnv.cpp
+++ SequoiaDB/engine/pmd/pmdEnv.cpp
@@ -116,6 +116,7 @@ namespace engine
    // PD_TRACE_DECLARE_FUNCTION ( SDB_PMDEDUUSERTRAPHNDL, "pmdEDUUserTrapHandler" )
    void pmdEDUUserTrapHandler( OSS_HANDPARMS )
    {
+#if defined( SDB_ENGINE ) || defined( SDB_OM )
       INT32 rc = SDB_OK ;
       PD_TRACE_ENTRY ( SDB_PMDEDUUSERTRAPHNDL );
       oss_edu_data * pEduData = NULL ;
@@ -180,6 +181,7 @@ namespace engine
    done :
       PD_TRACE1 ( SDB_PMDEDUUSERTRAPHNDL, PD_PACK_INT(rc) );
       PD_TRACE_EXIT ( SDB_PMDEDUUSERTRAPHNDL ) ;
+#endif // SDB_ENGINE || SDB_OM
       return ;
    }
 
