d5fbefead81185f135e7f0bb7ba5fa9d56091040
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-05-19 05:23:56 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@12106 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/pmdOptionsMgr.cpp
+++ SequoiaDB/engine/pmd/pmdOptionsMgr.cpp
@@ -503,8 +503,8 @@ namespace engine
                if ( pEX->hasField( pFieldName ) )
                {
                   _result = SDB_PERM ;
-                  PD_LOG_MSG( PDWARNING, "Field[%s] do not support changing in "
-                              "runtime", pFieldName ) ;
+                  PD_LOG ( PDWARNING, "Field[%s] do not support changing in "
+                           "runtime", pFieldName ) ;
                   goto error ;
                }
                goto done ;
@@ -623,8 +623,8 @@ namespace engine
                if ( pEX->hasField( pFieldName ) )
                {
                   _result = SDB_PERM ;
-                  PD_LOG_MSG( PDWARNING, "Field[%s] do not support changing in "
-                              "runtime", pFieldName ) ;
+                  PD_LOG ( PDWARNING, "Field[%s] do not support changing in "
+                           "runtime", pFieldName ) ;
                   goto error ;
                }
                goto done ;
