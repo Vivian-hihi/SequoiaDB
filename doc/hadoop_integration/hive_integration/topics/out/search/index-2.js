4f41e6295c69ac4f6ab90f0285320a521085ed66
zhangsuonan <zhangsuonan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-05-29 02:22:59 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@12352 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/hadoop_integration/hive_integration/topics/out/search/index-2.js
+++ doc/hadoop_integration/hive_integration/topics/out/search/index-2.js
@@ -1,11 +1,14 @@
 //Auto generated index for searching.
+w["convert"]="0*1";
+w["default.xml.template为"]="0*1";
+w["description"]="0*2";
 w["false"]="0*1";
 w["file"]="0*3";
 w["file:"]="0*4";
 w["handler"]="0*1";
 w["hive"]="0*9,1*4,2*4";
 w["hive-default.xml.template为"]="0*1";
-w["hive-sequoiadb.jar和java"]="0*2";
+w["hive-sequoiadb-"]="0*5";
 w["hive-site"]="0*1";
 w["hive-site.xml文件（如果不存在，可拷贝"]="0*1";
 w["hive-site.xml文件），增加如下属性："]="0*1";
@@ -16,8 +19,5 @@ w["hsequoiadb支持的hive版本列表"]="1*51";
 w["jar"]="0*3";
 w["jars"]="0*1";
 w["join"]="0*1";
-w["lib"]="0*3";
-w["lib安装目录下；"]="0*3";
-w["name"]="0*4";
-w["opt"]="0*3";
+w["lib"]="0*6";
 
--- doc/hadoop_integration/hive_integration/topics/out/search/index-3.js
+++ doc/hadoop_integration/hive_integration/topics/out/search/index-3.js
@@ -1,19 +1,19 @@
 //Auto generated index for searching.
+w["lib安装目录下；"]="0*3";
+w["name"]="0*4";
+w["opt"]="0*3";
 w["path"]="0*1";
 w["property"]="0*4";
-w["sdbdirver"]="0*1";
-w["sdbdirver.jar"]="0*1";
-w["sdbdriver.jar两个文件拷贝到hive"]="0*3";
-w["sequoiadb"]="0*2,1*1,2*1";
+w["sequoiadb"]="0*5,1*1,2*1";
 w["sequoiadb.jar"]="0*1";
-w["sequoiadb.jar和java"]="0*1";
+w["sequoiadb.jar两个文件拷贝到hive"]="0*1";
 w["sequoiadb支持的hive版本列表"]="2*51";
 w["sequoiadb）的hadoop"]="0*3";
 w["site.xml文件（如果不存在，可拷贝"]="0*1";
 w["site.xml文件），增加如下属性："]="0*1";
 w["store"]="0*1";
 w["value"]="0*4";
-w["version"]="0*1,1*1,2*1";
+w["version"]="0*8,1*1,2*1";
 w["修改hive安装目录下的bin"]="0*3";
 w["安装和配置好hadoop"]="0*2";
 w["拷贝sequoiadb安装目录下（默认在"]="0*3";
