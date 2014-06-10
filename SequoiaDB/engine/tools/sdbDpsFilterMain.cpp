37ab39d757519d9af09974937ec9a11b37eb5b6c
liuzhen <liuzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-06-10 11:03:20 +0000

    add GPL Lisence
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@12612 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/sdbDpsFilterMain.cpp
+++ SequoiaDB/engine/tools/sdbDpsFilterMain.cpp
@@ -1,7 +1,44 @@
-#include <iostream>
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = sdbDpsFilterMain.cpp
+
+   Descriptive Name = N/A
+
+   When/how to use: this program may be used on binary and text-formatted
+   versions of data management component. This file contains code logic for
+   data insert/update/delete. This file does NOT include index logic.
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          04/19/2013  LZ  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
 #include "sdbDpsLogFilter.hpp"
 #include "sdbDpsFilter.hpp"
 #include "sdbDpsOption.hpp"
+#include <iostream>
 
 INT32 main(INT32 argc, CHAR** argv)
 {
--- SequoiaDB/engine/tools/sdbDpsOption.hpp
+++ SequoiaDB/engine/tools/sdbDpsOption.hpp
@@ -1,3 +1,40 @@
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = sdbDpsOption.hpp
+
+   Descriptive Name = N/A
+
+   When/how to use: this program may be used on binary and text-formatted
+   versions of data management component. This file contains code logic for
+   data insert/update/delete. This file does NOT include index logic.
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          04/19/2014  LZ  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
 #ifndef _SDB_DPS_FILTER_HPP_
 #define _SDB_DPS_FILTER_HPP_
 
