c77b938551554e759b02df35ba8052ab76934197
liuzhen <liuzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-06-15 05:16:56 +0000

    python driver util.hpp
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@12720 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/python/pysequoiadb/util.hpp
+++ driver/python/pysequoiadb/util.hpp
@@ -8,13 +8,38 @@
 
 #define PARSE_PYTHON_ARGS PyArg_ParseTuple
 
+/*
+ *@brief     macro to cast INT32 to object of Python
+ *@ret_value [in] the exactly value to cast
+ *@return    pointer to PyObject, reference to error code
+ **/
 #define MAKE_RETURN_INT_VALUE( ret_value ) \
    ( PyObject * )Py_BuildValue( "i", ret_value )
 
-#define MAKE_RETURN_OBJECT(cpp_object, dtor_func) \
-   PyObject_FromVoidPtr( cpp_object, dtor_func )
+#define MAKE_RETURN_INT_OBJECT( ret_value, cpp_object ) \
+   ( PyObject * )Py_BuildValue( "iO", ret_value, cpp_object )
 
-#define CAST_PYOBJECT_TO_COBJECT( py_object, tmp, instance, classname ) \
+#define MAKE_PYTHON_VOID_OBJECT( py_object, cpp_object ) \
+   py_object = ( PyObject * )PyCObject_FromVoidPtr( cpp_object, NULL )
+
+/*
+ *@brief      macro to cast C++ object to object of Python, 
+              it will be used in creating a instance of C++ class
+ *@cpp_object [in] the exactly object to cast
+ *@dtor_func  [in] the destructor function for delete C++ object
+ *@return     pointer to PyObject
+ **/
+#define MAKE_RETURN_OBJECT( cpp_object ) \
+   PyCObject_FromVoidPtr( cpp_object, NULL )
+
+/*
+ *@brief     macro to re-cast python object to specified class
+ *@py_object [in] object need to cast
+ *@tmp       [in] a temp pointer
+ *@calssname [in] the class of the instance
+ *@instance  [out] the pointer pointing to real object
+ **/
+#define CAST_PYOBJECT_TO_COBJECT( py_object, tmp, classname, instance ) \
    tmp = PyCObject_AsVoidPtr( obj ) ;                                   \
    if ( NULL == tmp )                                                   \
    {                                                                    \
@@ -29,4 +54,39 @@
       goto done ;                                                       \
    }
 
+/*
+ *@brief     macro to re-cast python object to specified class
+ *@py_object [in] object need to cast
+ *@str_bson  [in] a temp pointer
+ *@calssname [in] the class of the instance
+ *@instance  [out] the pointer pointing to real object
+ **/
+#define CAST_PYBSON_TO_CSTRING( py_object, str )      \
+   str = PyBytes_AsString( py_object ) ;              \
+   if ( NULL == str )                                 \
+   {                                                  \
+      rc = SDB_INVALIDARGS ;                          \
+      goto done ;                                     \
+   }                                                  \
+                                                      \
+   bson_object = SDB_OSS_NEW bson::BSONObj( tmp ) ;   \
+   if ( NULL == bson_object )                         \
+   {                                                  \
+      rc = SDB_INVALIDARGS ;                          \
+      goto done ;                                     \
+   }
+
+#define DEC_PYOBJECT_REF( py_object )  \
+   if ( NULL != client )               \
+   {                                   \
+      Py_DECREF( obj ) ;               \
+   }
+
+#define DELETE_CPPOBJECT( pObject ) \
+   if ( NULL != pObject )           \
+   {                                \
+      SDB_OSS_DEL pObject ;         \
+      pObject = NULL ;              \
+   }
+   
 #endif
