6883fbed2da08f6c98c6f8764e893bcd365c2b19
liuzhen <liuzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-06-25 03:04:03 +0000

    throw exception
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@13089 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/ixm/ixmKey.cpp
+++ SequoiaDB/engine/ixm/ixmKey.cpp
@@ -354,7 +354,7 @@ namespace engine
          default:
             pdLog ( PDERROR, __FUNC__, __FILE__, __LINE__,
                     "Invalid Key is accessed" ) ;
-            throw "Invalid Key is accessed" ;
+            throw std::exception( "Invalid Key is accessed" ) ;
          }
          if ( (bits & cHASMORE) == 0 )
             break ;
