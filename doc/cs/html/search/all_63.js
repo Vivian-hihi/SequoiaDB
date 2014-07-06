d14ec58ca27dde48cf073bfd3001911920c098e3
zhangsuonan <zhangsuonan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-07-06 08:19:32 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@13353 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/cs/html/search/all_63.js
+++ doc/cs/html/search/all_63.js
@@ -1,12 +1,13 @@
 var searchData=
 [
+  ['canceltask',['cancelTask',['../classSequoiaDB_1_1Sequoiadb.html#a9b6f879f0c71ed5aa15541a6f34f8dbc',1,'SequoiaDB::Sequoiadb']]],
   ['changeconnectionoptions',['ChangeConnectionOptions',['../classSequoiaDB_1_1Sequoiadb.html#a27a66181a28ca205a42d9a35f328391a',1,'SequoiaDB::Sequoiadb']]],
   ['close',['Close',['../classSequoiaDB_1_1DBCursor.html#a8d9dc052aa51c2b8d51dd4542af7c526',1,'SequoiaDB::DBCursor']]],
   ['closeallcursors',['closeAllCursors',['../classSequoiaDB_1_1Sequoiadb.html#a0c6ab0fa29c12c3cafc02bddc7343d39',1,'SequoiaDB::Sequoiadb']]],
   ['collectionspace',['CollectionSpace',['../classSequoiaDB_1_1CollectionSpace.html',1,'SequoiaDB']]],
   ['collspace',['CollSpace',['../classSequoiaDB_1_1DBCollection.html#af260a2a0eab053c5702fe7ddc96f13d9',1,'SequoiaDB::DBCollection']]],
   ['configoptions',['ConfigOptions',['../classSequoiaDB_1_1ConfigOptions.html',1,'SequoiaDB']]],
-  ['connect',['Connect',['../classSequoiaDB_1_1Sequoiadb.html#a979b7b8da650b60ca006ef5da84653db',1,'SequoiaDB.Sequoiadb.Connect()'],['../classSequoiaDB_1_1Sequoiadb.html#a3edd6d00dbcdd9eb0878b4bd6aeda54d',1,'SequoiaDB.Sequoiadb.Connect(string username, string password)'],['../classSequoiaDB_1_1Node.html#a753ca37ac212ebf1a08f0bbbd3e268bf',1,'SequoiaDB.Node.Connect()'],['../classSequoiaDB_1_1Node.html#ac3bd7068886ec7ba2d08c1d3661c4c49',1,'SequoiaDB.Node.Connect(string username, string password)']]],
+  ['connect',['Connect',['../classSequoiaDB_1_1Sequoiadb.html#a979b7b8da650b60ca006ef5da84653db',1,'SequoiaDB.Sequoiadb.Connect()'],['../classSequoiaDB_1_1Sequoiadb.html#a3edd6d00dbcdd9eb0878b4bd6aeda54d',1,'SequoiaDB.Sequoiadb.Connect(string username, string password)'],['../classSequoiaDB_1_1Sequoiadb.html#ab38e72edd418c60fea0d83d221b89ccd',1,'SequoiaDB.Sequoiadb.Connect(string username, string password, ConfigOptions options)'],['../classSequoiaDB_1_1Node.html#a753ca37ac212ebf1a08f0bbbd3e268bf',1,'SequoiaDB.Node.Connect()'],['../classSequoiaDB_1_1Node.html#ac3bd7068886ec7ba2d08c1d3661c4c49',1,'SequoiaDB.Node.Connect(string username, string password)']]],
   ['connecttimeout',['ConnectTimeout',['../classSequoiaDB_1_1ConfigOptions.html#acc0196e821f1a04b2af101705a6921e3',1,'SequoiaDB::ConfigOptions']]],
   ['createcollection',['CreateCollection',['../classSequoiaDB_1_1CollectionSpace.html#af2271ff99cb710f2d9b4d46e9941a157',1,'SequoiaDB.CollectionSpace.CreateCollection(string collectionName)'],['../classSequoiaDB_1_1CollectionSpace.html#a36816279c268c6db1f5432565ad1278a',1,'SequoiaDB.CollectionSpace.CreateCollection(string collectionName, BsonDocument options)']]],
   ['createcollectionspace',['CreateCollectionSpace',['../classSequoiaDB_1_1Sequoiadb.html#a9273b33baa0513fc41b0d1f3c1072754',1,'SequoiaDB.Sequoiadb.CreateCollectionSpace(string csName)'],['../classSequoiaDB_1_1Sequoiadb.html#a8b590ed66666bf5c8471798743baaecd',1,'SequoiaDB.Sequoiadb.CreateCollectionSpace(string csName, int pageSize)']]],
