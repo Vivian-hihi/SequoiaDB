8c989ed2fa3db6ec5f156fac6a54de58605b8a5e
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-07-16 07:24:10 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@13595 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/omagent.hpp
+++ SequoiaDB/engine/include/omagent.hpp
@@ -2,23 +2,37 @@
 #define OMAGENT_HPP_
 
 #include "core.hpp"
+#include "../bson/bson.h"
+#include "ossUtil.hpp"
+#include "sptApi.hpp"
+
+using namespace engine ;
+using namespace bson ;
+
+/*
+#define OMA_FIELD_NAME_PING                  "Ping"
+#define OMA_FIELD_NAME_SSH                   "Ssh"
+#define OMA_FIELE_NAME_HOSTNAME              "Hostname"
+#define OMA_FIELD_NMAE_IP                    "Ip"
+#define OMA_FIELD_NAME_SCAN_HOST_RET         "ScanHostRet"
+*/
 
 namespace CLSMGR
 {
-
+/*
    class _omagentObjBuff : public SDBObject
    {
       private:
-         _omagentObjBuff( const _omagentObjBuf &right ) ;
+         _omagentObjBuff( const _omagentObjBuff &right ) ;
 
-         _omagentObjBuff& operator= ( const _omagentObjBuf &right ) ;
+         _omagentObjBuff& operator= ( const _omagentObjBuff &right ) ;
 
       public:
 
          _omagentObjBuff ()
          {
             _pBuff = NULL ;
-            _buffLen = 0 ;
+            _buffSize = 0 ;
             _recordNum = 0 ;
          }
 
@@ -28,34 +42,31 @@ namespace CLSMGR
 
          const CHAR* data () { return _pBuff ; }
          INT32       size () { return _buffSize ; }
-         INT32       recordNum ()( return _recordNum ; }
+         INT32       recordNum () { return _recordNum ; }
 
       private:
-         CHAR                *_pBuff ;
+         CHAR                 *_pBuff ;
          INT32                _buffSize ;
          INT32                _recordNum ;
    } ;
 
    typedef _omagentObjBuff omagentObjBuff ;
-
-/*
-   omagent control func
 */
+   // get spider monkey engine
+   _sptScope* getSptScope () ;
 
-   BOOLEAN omagentIsCommand ( const CHAR *name ) ;
+   // get bson field
+   INT32 omagentGetIntElement ( const BSONObj &obj, const CHAR *fieldName,
+                                INT32 &value ) ;
 
-   INT32 omagentParseCommand ( const CHAR *name,
-                               _omagentCommand **ppCommand ) ;
+   INT32 omagentGetStringElement ( const BSONObj &obj, const CHAR *fieldName,
+                                   const CHAR **value ) ;
 
-   INT32 omagentInitCommand ( _omagentCommand *pCommand ,INT32 flags,
-                              INT64 numToSkip,
-                              INT64 numToReturn, const CHAR *pMatcherBuff,
-                              const CHAR *pSelectBuff, const CHAR *pOrderByBuff,
-                              const CHAR *pHintBuff ) ;
+   INT32 omagentGetObjElement ( const BSONObj &obj, const CHAR *fieldName,
+                                BSONObj &value ) ;
 
-   INT32 omagentRunCommand ( _omagentCommand *pCommand, omagentObjBuff &objBuff ) ;
-
-   INT32 omagentReleaseCommand ( _omagentCommand **ppCommand ) ;
+   INT32 omagentGetBooleanElement ( const BSONObj &obj, const CHAR *fieldName,
+                                    BOOLEAN &value ) ;
 
 }
 
