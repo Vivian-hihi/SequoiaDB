fc606c6ff596a4e824952633fd1be0499015a23a
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-08-01 05:54:45 +0000

    omagent相关
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@13849 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/include/omagentDef.hpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = omagentDef.hpp
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          07/29/2014  XJH Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#ifndef OMAGENT_DEF_HPP__
+#define OMAGENT_DEF_HPP__
+
+#include "core.hpp"
+#include "oss.hpp"
+
+namespace engine
+{
+
+}
+
+#endif // OMAGENT_DEF_HPP__
+
