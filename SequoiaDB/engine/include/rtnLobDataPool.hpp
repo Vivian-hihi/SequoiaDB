7113ce3e24b7d0381cea8943321245140a2ca972
yunwu <yunwu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-08-18 02:08:58 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@14245 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/include/rtnLobDataPool.hpp
@@ -0,0 +1,132 @@
+/*******************************************************************************
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = rtnLobDataPool.hpp
+
+   Descriptive Name =
+
+   When/how to use: this program may be used on binary and text-formatted
+   versions of data management component. This file contains structure for
+   DMS storage unit and its methods.
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          05/08/2014  YW Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#ifndef RTN_LOBDATAPOOL_HPP_
+#define RTN_LOBDATAPOOL_HPP_
+
+#include "core.hpp"
+#include "oss.hpp"
+#include <vector>
+#include <list>
+
+namespace engine
+{
+   class _rtnLobDataPool : public SDBObject
+   {
+   public:
+      _rtnLobDataPool() ;
+      virtual ~_rtnLobDataPool() ;
+
+   public:
+      struct tuple
+      {
+         SINT64 offset ;
+         UINT32 len ;
+         const CHAR *data ;
+
+         tuple( SINT64 o,
+                UINT32 l,
+                const CHAR *d )
+         :offset( o ),
+          len( l ),
+          data( d )
+         {
+
+         }
+      } ;
+
+   public:
+      /// buf will be freed by pool
+      /// the last buf allocated will be invalid when allocate mem.
+      INT32 allocate( UINT32 len, CHAR **buf ) ;
+
+      /// realBuf will be freed when do clear
+      INT32 push( const CHAR *data, UINT32 len,
+                  SINT64 offset ) ;
+
+      void pushDown() ;
+
+      void entrust( CHAR *buf ) ;
+
+      BOOLEAN empty() const
+      {
+         return 0 == _dataSz ;
+      }
+
+      UINT32 getDataSize() const
+      {
+         return _dataSz ;
+      }
+
+      BOOLEAN match( SINT64 offset ) const
+      {
+         if ( !_pool.empty() )
+         {
+            return offset == _pool[_current].offset ;
+         }
+         else
+         {
+            return FALSE ;
+         }
+      }
+
+      BOOLEAN next( UINT32 len, const CHAR **data, UINT32 &read ) ;
+
+      void clear() ;
+
+   private:
+      struct compare
+      {
+         BOOLEAN operator()( const tuple &l, const tuple &r )
+         {
+            return l.offset < r.offset ;
+         }
+      } ;
+
+      CHAR *_buf ;
+      UINT32 _bufSz ;
+      std::vector<tuple> _pool ;
+      std::list<CHAR *> _toBeFreed ;
+      UINT32 _dataSz ;
+      SINT32 _current ;
+      friend class _rtnLobStream ;
+   } ;
+   typedef class _rtnLobDataPool rtnLobDataPool ;
+}
+
+#endif
+
---
+++ SequoiaDB/engine/rtn/rtnLobDataPool.cpp
@@ -0,0 +1,174 @@
+/*******************************************************************************
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = rtnLobDataPool.cpp
+
+   Descriptive Name =
+
+   When/how to use: this program may be used on binary and text-formatted
+   versions of data management component. This file contains structure for
+   DMS storage unit and its methods.
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          05/08/2014  YW Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#include "rtnLobDataPool.hpp"
+#include "pd.hpp"
+#include <algorithm>
+
+namespace engine
+{
+   _rtnLobDataPool::_rtnLobDataPool()
+   :_buf( NULL ),
+    _bufSz( 0 ),
+    _dataSz( 0 ),
+    _current( -1 )
+   {
+
+   }
+
+   _rtnLobDataPool::~_rtnLobDataPool()
+   {
+      clear() ;
+
+      if ( NULL != _buf )
+      {
+         SDB_OSS_FREE( _buf ) ;
+         _buf = NULL ;
+         _bufSz = 0 ;
+      }
+   }
+
+   INT32 _rtnLobDataPool::allocate( UINT32 len, CHAR **buf )
+   {
+      INT32 rc = SDB_OK ;
+      SDB_ASSERT( NULL != buf, "can not be null" ) ;
+      
+      if ( len <= _bufSz )
+      {
+         *buf = _buf ;
+         goto done ;
+      }
+      else if ( NULL != _buf )
+      {
+         SDB_OSS_FREE( _buf ) ;
+         _bufSz = 0 ;
+      }
+
+      _buf = ( CHAR * )SDB_OSS_MALLOC( len ) ;
+      if ( NULL == _buf )
+      {
+         PD_LOG( PDERROR, "failed to allocate mem." ) ; 
+         rc = SDB_OOM ;
+         goto error ;
+      }
+      _bufSz = len ;
+      *buf = _buf ;
+   done:
+      return rc ;
+   error:
+      goto done ;
+   }
+
+   void _rtnLobDataPool::entrust( CHAR *buf )
+   {
+      _toBeFreed.push_back( buf ) ;
+      return ;
+   }
+
+   BOOLEAN _rtnLobDataPool::next( UINT32 len, const CHAR **buf, UINT32 &read )
+   {
+      BOOLEAN res = FALSE ;
+
+      if ( 0 <= _current )
+      {
+         SDB_ASSERT( _pool.size() > ( UINT32 )_current, "impossible" ) ;
+         tuple &t = _pool[_current] ;
+         UINT32 realLen = len <= t.len ? len : t.len ;
+         *buf = t.data ;
+         read = realLen ;
+         t.len -= realLen ;
+         _dataSz -= realLen ;
+         res = TRUE ;
+
+         if ( 0 == t.len )
+         {
+            ++_current ;
+            if ( _pool.size() == ( UINT32 )_current )
+            {
+               _current = -1 ;
+               SDB_ASSERT( 0 == _dataSz, "must be zero" ) ;
+            }
+         }
+         else
+         {
+            t.data += realLen ;
+            t.offset += realLen ;
+         }
+      }
+
+      return res ;
+   }
+
+   INT32 _rtnLobDataPool::push( const CHAR *data, UINT32 len,
+                                SINT64 offset )
+   {
+      INT32 rc = SDB_OK ;
+      SDB_ASSERT( NULL != data, "can not be null" ) ;
+      _pool.push_back( tuple( offset, len, data ) ) ;
+      _dataSz += len ;
+   done:
+      return rc ;
+   error:
+      goto done ;
+   }
+
+   void _rtnLobDataPool::pushDown()
+   {
+      if ( 1 < _pool.size() )
+      {
+         std::sort( _pool.begin(), _pool.end(), compare() ) ;
+      }
+
+      _current = 0 ;
+      return ;
+   }
+
+   void _rtnLobDataPool::clear()
+   {
+      _pool.clear() ;
+      _dataSz = 0 ;
+      _current = -1 ;
+      std::list<CHAR *>::iterator itr = _toBeFreed.begin() ;
+      for ( ; itr != _toBeFreed.end(); ++itr )
+      {
+         SDB_OSS_FREE( *itr ) ;
+      }
+      _toBeFreed.clear() ;
+      return ;
+   }
+}
+
