c13476d32fdbdbf087c446b344d156b77d43552c
liuwuxing <liuwuxing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-08-20 06:50:19 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@14351 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testprocess/3-programs/sdbinspect/create_maincl.js
+++
@@ -1,9 +0,0 @@
-var db = new Sdb('sdbserver3',11810)
-db.inspect_cs.createCL('maincl',{ShardingKey:{id:1},ShardingType:'range',ReplSize:0,Compressed:true,IsMainCL:true})
-db.inspect_cs.createCL('cl1',{ShardingKey:{id:1},ShardingType:'range',ReplSize:0,Compressed:true,IsMainCL:false})
-db.inspect_cs.createCL('cl2',{ShardingKey:{id:1},ShardingType:'range',ReplSize:0,Compressed:true,IsMainCL:false})
-db.inspect_cs.maincl.attachCL('inspect_cs.cl1',{LowBound:{id:0},UpBound:{id:50}})
-db.inspect_cs.maincl.attachCL('inspect_cs.cl2',{LowBound:{id:50},UpBound:{id:100}})
-
-
-
