e80c46f3e6d6d94409b5dfa851af3c0d68157784
liuzhen <liuzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-08-20 07:07:20 +0000

    word
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@14352 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/inspect/sdbInspect.cpp
+++ SequoiaDB/engine/tools/inspect/sdbInspect.cpp
@@ -2976,7 +2976,7 @@ INT32 _sdbCi::handle( const po::options_description &desc,
 
    if ( vm.empty() || vm.count( CONSISTENCY_INSPECT_HELP ) )
    {
-      std::cout << "This tool is used to inpect data among nodes under the "
+      std::cout << "This tool is used to inspect data among nodes under the "
                 << "same group to make sure all data in node are the same."
                 << "It will scan all records, and save record if record is "
                 << "different among nodes, and a report will be producted."
