d0b6f05e83311c164c6d7e46a99150eda0d327f3
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-08-20 11:40:49 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@14382 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/include/omagentNodeCmd.hpp
@@ -0,0 +1,46 @@
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = omagentNodeCmd.hpp
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          20/08/2014  Xu Jianhui  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#ifndef OMAGENT_NODECMD_HPP__
+#define OMAGENT_NODECMD_HPP__
+
+#include "omagentCommand.hpp"
+
+using namespace bson ;
+
+namespace engine
+{
+
+}
+
+#endif //OMAGENT_NODECMD_HPP__
+
---
+++ SequoiaDB/engine/omagent/omagentNodeCmd.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+
+
+   Copyright (C) 2011-2014 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = omagentNodeCmd.cpp
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          20/08/2014  Xu Jianhui  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+
+#include "omagentNodeCmd.hpp"
+
+using namespace bson ;
+
+namespace engine
+{
+
+}
+
