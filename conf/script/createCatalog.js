ff97d60ffa4e28412492162cac2794b406e98baa
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-09-10 03:01:10 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@14909 6121f079-4a18-41d3-8c30-8870a322f2c2

--- conf/script/createCatalog.js
+++ conf/script/createCatalog.js
@@ -21,7 +21,7 @@
    2014-7-26 Zhaobo Tan  Init
 */
 if ( typeof(COORD_HOSTNAME) == "undefined" ) { COORD_HOSTNAME = "127.0.0.1" ; }
-if ( typeof(COORD_SERVICE) == "undefined" ) { COORD_SERVICE = "13579" ; }
+if ( typeof(COORD_SERVICE) == "undefined" ) { COORD_SERVICE = "10000" ; }
 if ( typeof(DB_USERNAME) == "undefined" ) { DB_USERNAME = "" ; }
 if ( typeof(DB_PASSWORD) == "undefined" ) { DB_PASSWORD = "" ; }
 if ( typeof(INSTALL_HOSTNAME) == "undefined" ) {}
@@ -37,14 +37,74 @@ var objRet = new Object() ;
 objRet.Rc = 0 ;
 objRet.Detail = "" ;
 
+function waitCatalogRGAvailable( db )
+{
+   // TODO: tanzhaobo
+   // it may loop forever
+   while ( true )
+   {
+      try
+      {
+         db.list(SDB_LIST_GROUPS) ;
+      }
+      catch ( e )
+      {
+         if ( -71 == e )
+         {
+            continue ;
+         }
+         else
+         {
+            throw e ;
+         }
+      }
+      break ;
+   }
+}
+
+function createCatalogNode( db )
+{
+   // try to get system catalog group
+   var rg = null ;
+   try
+   {
+      rg = db.getRG("SYSCatalogGroup") ;
+   }
+   // catalog has not been created
+   catch ( e )
+   {
+      if ( -180 == e )
+      {
+         try
+         {
+            rg = db.createCataRG( INSTALL_HOSTNAME, INSTALL_SERVICE,
+                                  INSTALL_PATH, CONFIG ) ;
+         }
+         catch ( e )
+         {
+            throw e ;
+         }
+         return objRet ;
+      }
+      else
+      {
+         throw e ;
+      }
+   }
+   // catalog has been created
+   var node = rg.createNode( INSTALL_HOSTNAME, INSTALL_SERVICE,
+                             INSTALL_PATH, CONFIG ) ;
+   node.start() ;
+}
+
 function main()
 {
    try
    {
       // check arguments
       if ( typeof(COORD_HOSTNAME) == "undefined" ||
-           typeof(COORD_SERVICE) == "undefined" ||
-           typeof(DB_USERNAME) == "undefined" ||
+           typeof(COORD_SERVICE) == "undefined"  ||
+           typeof(DB_USERNAME) == "undefined"    ||
            typeof(DB_PASSWORD) == "undefined" )
       {
          objRet.Rc = -6 ;
@@ -52,8 +112,8 @@ function main()
                          + " username or password to connect to database" ;
       }
       if ( typeof(INSTALL_HOSTNAME) == "undefined" ||
-           typeof(INSTALL_SERVICE) == "undefined" ||
-           typeof(INSTALL_PATH) == "undefined" ||
+           typeof(INSTALL_SERVICE) == "undefined"  ||
+           typeof(INSTALL_PATH) == "undefined"     ||
            typeof(CONFIG) == "undefined" )
       {
          objRet.Rc = -6 ;
@@ -61,52 +121,17 @@ function main()
                          " are need for create catalog" ;
          return objRet ;
       }
-print("COORD_HOSTNAME is: " + COORD_HOSTNAME + '\n') ;
-print("COORD_SERVICE is: " + COORD_SERVICE + '\n') ;
-print("DB_USERNAME is: " + DB_USERNAME + '\n') ;
-print("DB_PASSWORD is: " + DB_PASSWORD + '\n') ;
-
       // connect to virtual coord
       var db = new Sdb( COORD_HOSTNAME, COORD_SERVICE, DB_USERNAME, DB_PASSWORD ) ;
-print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n") ;
-      // try to get system catalog group
-      var rg = null ;
-      try
-      {
-         rg = db.getRG("SYSCatalogGroup") ;
-      }
-      // catalog has not been created
-      catch ( e )
-      {
-         if ( -180 == e )
-         {
-            try
-            {
-               rg = db.createCataRG( INSTALL_HOSTNAME, INSTALL_SERVICE,
-                                     INSTALL_PATH, CONFIG ) ;
-            }
-            catch ( e )
-            {
-               throw e ;
-            }
-            return objRet ;
-         }
-         else
-         {
-            throw e ;
-         }
-      }
-      // catalog has been created
-      var node = rg.createNode( INSTALL_HOSTNAME, INSTALL_SERVICE,
-                                INSTALL_PATH, CONFIG ) ;
-      node.start() ;
+      // create catalog node
+      createCatalogNode( db ) ;
+      // wait catalog to be available
+      waitCatalogRGAvailable( db ) ;
 
-print("hahahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ok !!!\n") ;
       return objRet ;
    }
    catch ( e )
    {
-print("error is e = " + e + "\n" ) ;
       if ( typeof(e) != "number" )
       {
          objRet.Rc = -10 ;
@@ -122,7 +147,6 @@ print("error is e = " + e + "\n" ) ;
             objRet.Detail = eval( '(' + errMsg + ')' ) ;
          }
       }
-print("Errmsg is: " + objRet.Detail + "\n") ;
       return objRet ;
    }
 }
