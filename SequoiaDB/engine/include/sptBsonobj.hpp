5c50b20a3fe5c43b4990bb1ed3c9650214745eef
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-09-15 07:00:55 +0000

    增加bson的toString方法
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15065 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/sptBsonobj.hpp
+++ SequoiaDB/engine/include/sptBsonobj.hpp
@@ -56,6 +56,10 @@ namespace engine
                     _sptReturnVal &rval,
                     bson::BSONObj &detail ) ;
 
+      INT32 toString( const _sptArguments &arg,
+                      _sptReturnVal &rval,
+                      bson::BSONObj &detail ) ;
+
       INT32 destruct() ;
 
    private:
