725485b21c529ce7e47c2343ce73b4ce1d4260fb
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-09-16 08:21:36 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15097 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ conf/script/rollbackCoord.js
@@ -0,0 +1,101 @@
+/*******************************************************************************
+
+   Copyright (C) 2012-2014 SequoiaDB Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*******************************************************************************/
+/*
+@description: remove the newly created catalog group
+@modify list:
+   2014-7-26 Zhaobo Tan  Init
+*/
+if ( typeof(COORD_HOSTNAME) == "undefined" ) {}
+if ( typeof(COORD_SERVICE) == "undefined" ) {}
+if ( typeof(DB_USERNAME) == "undefined" ) { DB_USERNAME = "" ; }
+if ( typeof(DB_PASSWORD) == "undefined" ) { DB_PASSWORD = "" ; }
+
+var objRet = new Object() ;
+
+objRet.Rc = 0 ;
+objRet.detail = "" ;
+
+function removeCoordNodes( db )
+{
+   var rg = null ;
+
+   try
+   {
+      rg = db.getCoordRG ;
+   }
+   catch ( e )
+   {
+      if ( -154 == e )
+      {
+         return ;
+      }
+      else
+      {
+         throw e ;
+      }
+   }
+   db.removeCoordRG() ;
+}
+
+function main()
+{
+   var db =  null ;
+   try
+   {
+      // check arguments
+      if ( typeof(COORD_HOSTNAME) == "undefined" ||
+           typeof(COORD_SERVICE) == "undefined" )
+      {
+         objRet.Rc = -6 ;
+         objRet.detail = "virtual coord hostname and svcname are need"
+         return objRet ;
+      }
+
+      // connect to virtual coord
+      db = new Sdb( COORD_HOSTNAME, COORD_SERVICE, DB_USERNAME, DB_PASSWORD ) ;
+      // remove coord nodes
+      removeCoordNodes( db ) ;
+
+      return objRet ;
+   }
+   catch ( e )
+   {
+// TODO:
+//print("error is e = " + e + "\n") ;
+      if ( typeof(e) != "number" )
+      {
+         objRet.Rc = -10 ;
+         objRet.detail = "system error" ;
+      }
+      else
+      {
+         var errMsg = "" ;
+         objRet.Rc = e ;
+         errMsg = getLastErrMsg() ;
+         if ( "" != errMsg && null != errMsg && undefined != errMsg )
+         {
+            objRet.detail = eval( '(' + errMsg + ')' ) ;
+         }
+      }
+      return objRet ;
+   }
+}
+
+// execute
+   main() ;
+
---
+++ conf/script/rollbackDataNode.js
@@ -0,0 +1,124 @@
+/*******************************************************************************
+
+   Copyright (C) 2012-2014 SequoiaDB Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*******************************************************************************/
+/*
+@description: remove the newly created data group
+@modify list:
+   2014-7-26 Zhaobo Tan  Init
+*/
+if ( typeof(COORD_HOSTNAME) == "undefined" ) {}
+if ( typeof(COORD_SERVICE) == "undefined" ) {}
+// TODO: username and password are need
+if ( typeof(DB_USERNAME) == "undefined" ) { DB_USERNAME = "" ; }
+if ( typeof(DB_PASSWORD) == "undefined" ) { DB_PASSWORD = "" ; }
+if ( typeof(CREATED_DATA_GROUP) == "undefined" ) {}
+
+var objRet = new Object() ;
+
+objRet.Rc = 0 ;
+objRet.detail = "" ;
+
+function removeGroup( db, name )
+{
+   var rg = null ;
+   
+   try
+   {
+      rg = db.getRG( name ) ;
+   }
+   catch ( e )
+   {
+      if ( -154 == e )
+      {
+         return ;
+      }
+      else
+      {
+         throw e ;
+      }
+   }
+   // stop all the data node in this group
+   rg.stop() ;
+   // remove data group
+   db.removeRG( name ) ;
+}
+
+function removeDataGroup( db, groups )
+{
+   var arr = null ;
+
+   arr = groups["GroupName"] ;
+   for ( var i = 0; i < arr.length; i++ )
+   {
+      removeGroup( db, arr[i] ) ;
+   }
+}
+
+function main()
+{
+   var db = null ;
+   var groups = null ;
+   try
+   {
+      // check arguments
+      if ( typeof(COORD_HOSTNAME) == "undefined" ||
+           typeof(COORD_SERVICE) == "undefined" )
+      {
+         objRet.Rc = -6 ;
+         objRet.detail = "virtual coord hostname and svcname are need"
+         return objRet ;
+      }
+      if ( typeof(CREATED_DATA_GROUP) == "undefined" )
+      {
+         objRet.Rc = -6 ;
+         objRet.detail = "not specified data group to remove"
+         return objRet ;
+      }
+
+      // connect to virtual coord
+      var db = new Sdb( COORD_HOSTNAME, COORD_SERVICE, DB_USERNAME, DB_PASSWORD ) ;
+      // get groups info
+      groups = eval( '(' + CREATED_DATA_GROUP + ')' ) ;
+      // remove data group
+      removeDataGroup( db, groups ) ;
+
+      return objRet ;
+   }
+   catch ( e )
+   {
+      if ( typeof(e) != "number" )
+      {
+         objRet.Rc = -10 ;
+         objRet.detail = "system error" ;
+      }
+      else
+      {
+         var errMsg = "" ;
+         objRet.Rc = e ;
+         errMsg = getLastErrMsg() ;
+         if ( "" != errMsg && null != errMsg && undefined != errMsg )
+         {
+            objRet.detail = eval( '(' + errMsg + ')' ) ;
+         }
+      }
+      return objRet ;
+   }
+}
+
+// execute
+   main() ;
+
