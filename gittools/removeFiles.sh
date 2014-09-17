87faac641b2b33eb6706b58c40358d876e680aa9
taoewang <taoewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-09-17 09:43:16 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15126 6121f079-4a18-41d3-8c30-8870a322f2c2

--- gittools/removeFiles.sh
+++ gittools/removeFiles.sh
@@ -13,19 +13,17 @@ DESTPATH=$2
 
 # loop through the file
 while read line ;do
-   SOURCEPATH=$ROOTPATH/$line
    TARGETPATH=$DESTPATH/$line
-   TARGETDIR=$DESTPATH/$(dirname $line)
-   if [[ -f $SOURCEPATH ]]; then
+   if [[ -f $TARGETPATH ]]; then
       # if the source is a file, let's just remove file
       echo "remove file $TARGETPATH"
       rm -f $TARGETPATH
-   elif [[ -d $SOURCEPATH ]]; then
+   elif [[ -d $TARGETPATH ]]; then
       # if the source is a directory, let's remove dir
-      echo "remove directory $TARGETDIR"
-      rm -rf $TARGETDIR
+      echo "remove directory $TARGETPATH"
+      rm -rf $TARGETPATH
    else
-      echo "$SOURCEPATH is not a file nor dir"
+      echo "$TARGETPATH is not a file nor dir"
       exit 1
    fi
 done < $1
