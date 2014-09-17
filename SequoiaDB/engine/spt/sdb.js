1bbe1bbaff73a1bb779b9b5e907e9893ed3ba153
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-09-17 06:08:52 +0000

    rollback
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15108 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/sdb.js
+++ SequoiaDB/engine/spt/sdb.js
@@ -71,25 +71,6 @@ Bson.prototype.toString = function() {
 }
 // end Bson
 
-// BSONObj
-BSONObj.prototype.toObj = function() {
-   return JSON.parse( this.toJson() ) ;
-}
-
-BSONObj.prototype.toString = function() {
-   try
-   {
-      var obj = this.toObj() ;
-      var str = JSON.stringify( obj, undefined, 2 ) ;
-      return str ;
-   }
-   catch( e )
-   {
-      return this.toJson() ;
-   	}
-}
-// end BSONObj
-
 // SdbCursor
 SdbCursor.prototype.toArray = function() {
    if ( this._arr )
