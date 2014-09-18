03b2a53041f32a0a8820c79c047f5bc861f7be5d
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-09-18 10:00:34 +0000

    spt代码调整
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15177 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/fmpMain.cpp
+++ SequoiaDB/engine/pmd/fmpMain.cpp
@@ -1,78 +1,64 @@
+
 #include "fmpController.hpp"
 #include <string>
+#include "pmdOptions.h"
+#include "pmdDef.hpp"
+#include "utilParam.hpp"
 
 using namespace std ;
 
+#define COMMANDS_OPTIONS \
+    ( PMD_COMMANDS_STRING (PMD_OPTION_HELP, ",h"), "help" ) \
+    ( PMD_OPTION_VERSION, "version" )
+
+// initialize options
+void init ( po::options_description &desc )
+{
+   PMD_ADD_PARAM_OPTIONS_BEGIN ( desc )
+      COMMANDS_OPTIONS
+   PMD_ADD_PARAM_OPTIONS_END
+}
+
+void displayArg ( po::options_description &desc )
+{
+   std::cout << "Usage:  sdbfmp [OPTION]" <<std::endl;
+   std::cout << desc << std::endl ;
+}
+
 INT32 main( INT32 argc, CHAR **argv )
 {
-/*
-   INT32 rc = SDB_OK ;
-   _fmpJSVM vm ;
-   if ( !vm.ok() )
-   {
-      return 0 ;
-   }
-
-   {
-   BSONObj res ;
-   BSONObjBuilder builder ;
-//   BSONObjBuilder builder1, builder2, builder3 ;
-//   builder.appendCode("func", argv[1]) ;
-//   builder1.appendCode( "func", "function sum1(x,y){return sum2(x,y);}" ) ;
-//   rc = vm.compile( builder1.obj().firstElement() , "sum1") ;
-//   rc = vm.eval( builder1.obj(), res ) ;
-//   if ( SDB_OK != rc )
-//   {
-//      cout << "compile sum1 failed" << endl ;
-//      return 0 ;
-//   }
-
-//   builder2.appendCode( "func", "function sum2(x,y){return x+y;}" ) ;
-//   rc = vm.compile( builder2.obj().firstElement() , "sum2") ;
-//   rc = vm.eval( builder2.obj(), res ) ;
-//   if ( SDB_OK != rc )
-//   {
-//      cout << "compile sum2 failed" << endl ;
-//      return 0 ;
-//   }
-
-//   builder3.appendCode("func", "sum1(1,2);") ;
-   rc = vm.eval( builder.obj(), res ) ;
-   BSONElement type = res.getField( FMP_RES_TYPE ) ;
-   if ( type.eoo() || NumberInt != type.type() )
-   {
-      cout << "sth wrong happened:"<< res.toString() << endl ;
-   }
-   else if ( FMP_RES_TYPE_RECORDSET != type.Int() )
-   {
-      cout << "normal res:" << res.toString() << endl ;
-   }
-   else
-   {
-      while (TRUE )
-      {
-         rc = vm.fetch( res ) ;
-         if ( SDB_DMS_EOC == rc )
-         {
-            break ;
-         }
-         else if ( SDB_OK == rc )
-         {
-            cout << "fetch:" << res.toString() << endl ;
-         }
-         else
-         {
-            cout << "sth wrong happened:"<< res.toString() << endl ;
-            break ;
-         }
-      }
-   }
-   }
-*/
-
    INT32 rc = SDB_OK ;
    fmpController controller ;
+   po::options_description desc ( "Command options" ) ;
+   po::variables_map vm ;
+
+   init ( desc ) ;
+
+   // validate arguments
+   rc = engine::utilReadCommandLine( argc, argv, desc, vm, FALSE ) ;
+   if ( rc )
+   {
+      PD_LOG( PDERROR, "Invalid arguments, rc: %d", rc ) ;
+      displayArg ( desc ) ;
+      goto done ;
+   }
+   /// read cmd first
+   if ( vm.count( PMD_OPTION_HELP ) )
+   {
+      displayArg( desc ) ;
+      rc = SDB_PMD_HELP_ONLY ;
+      goto done ;
+   }
+   if ( vm.count( PMD_OPTION_VERSION ) )
+   {
+      ossPrintVersion( "Sdb fmp version" ) ;
+      rc = SDB_PMD_VERSION_ONLY ;
+      goto done ;
+   }
+
    rc = controller.run() ;
 
-   return rc ;
+done:
+   return SDB_OK == rc ? 0 : 127 ;
 }
+
