ab2cb292f0930d648897fa83e78e0445b9ac51e4
longyang <longyang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-10-16 13:39:26 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15571 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ script/install_om.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+###########################################
+# script parameter description:
+# $1 for rootPath, ex: /opt/sequoiadb
+# $2 for cm port
+# $3 om svcname
+# $4 om dbpath
+###########################################
+
+rootPath=$1
+cmPort=$2
+svcname=$3
+dbpath=$4
+
+sdbFile=$1/bin/sdb
+
+# first to start sdbcm
+$rootPath/bin/sdbcmart
+if [ [ $? != 0 ] ] ; then
+   echo "Start sdbcm failed"
+   exit 1
+fi
+
+# second to create om
+$sdbFile -s " var oma = new Oma('localhost', '${cmPort}' ); \
+             try { \
+                 oma.createOM( '${svcname}', '${dbpath}' ) ; \
+              } catch( e ) { \
+                 if ( e != SDBCM_NODE_EXISTED ) { \
+                    throw e ; } \
+              } \
+              try { \
+                 oma.startNode( '${svcname}' ) ; \
+              } catch( e ) { \
+                 oma.removeOM( '${svcname}' ) ; \
+                 throw e ; \
+              } "
+if [ [ $? != 0 ] ] ; then
+   echo "Create OM failed"
+   exit 1
+else
+   echo "Create OM succeed"
+   exit 0
+fi
