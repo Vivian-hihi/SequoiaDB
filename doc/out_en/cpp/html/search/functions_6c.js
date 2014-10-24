b211eed0404356e096e96afd410ff1bade318388
zhangsuonan <zhangsuonan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-10-24 06:08:38 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@15904 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out_en/cpp/html/search/functions_6c.js
+++ doc/out_en/cpp/html/search/functions_6c.js
@@ -6,6 +6,7 @@ var searchData=
   ['listcollectionspaces',['listCollectionSpaces',['../classsdbclient_1_1sdb.html#a6df92cd59b5a2f633b9066da31ee0532',1,'sdbclient::sdb']]],
   ['listcollectionspacesindomain',['listCollectionSpacesInDomain',['../classsdbclient_1_1sdbDomain.html#a9386a9a833b1d37bd37bef097f63f461',1,'sdbclient::sdbDomain']]],
   ['listdomains',['listDomains',['../classsdbclient_1_1sdb.html#a507853027ef45972e5dffdd1c5d53621',1,'sdbclient::sdb']]],
+  ['listlobs',['listLobs',['../classsdbclient_1_1sdbCollection.html#ad9256ad1c6815a3941e4f7d2178588eb',1,'sdbclient::sdbCollection']]],
   ['listprocedures',['listProcedures',['../classsdbclient_1_1sdb.html#a5a2fa83aaae147419194df4cd9df9a86',1,'sdbclient::sdb']]],
   ['listreplicagroups',['listReplicaGroups',['../classsdbclient_1_1sdb.html#a6e6d3ba5eef8738e6b92c82d386aa465',1,'sdbclient::sdb']]],
   ['listtasks',['listTasks',['../classsdbclient_1_1sdb.html#ab6cfedca719dce5d3b2c2362bb1dc42e',1,'sdbclient::sdb']]]
--- doc/out_en/cpp/html/search/functions_71.js
+++ doc/out_en/cpp/html/search/functions_71.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['query',['query',['../classsdbclient_1_1sdbCollection.html#a0b094c874ea53c019da6f5b4ad62a37f',1,'sdbclient::sdbCollection']]]
+  ['query',['query',['../classsdbclient_1_1sdbCollection.html#a5b5dae5cc3d1db97a86c6910be9ef259',1,'sdbclient::sdbCollection']]]
 ];
