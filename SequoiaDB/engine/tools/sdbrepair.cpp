10b0c0656dcd6faf28a71110c17d2a7cf1fcc4c5
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-11-26 08:44:29 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@16378 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/sdbrepair.cpp
+++ SequoiaDB/engine/tools/sdbrepair.cpp
@@ -304,7 +304,7 @@ void inspectCollections ( OSSFILE &file )
    // repair flag
    for ( UINT32 i = 0 ; i < DMS_MME_SLOTS ; ++i )
    {
-      mb = (( dmsMB** )&gMMEBuff)[ i ] ;
+      mb = (dmsMB*)( (CHAR*)gMMEBuff + (i*DMS_MB_SIZE) ) ;
 
       if ( DMS_MB_FLAG_FREE != mb->_flag &&
            !OSS_BIT_TEST ( mb->_flag, DMS_MB_FLAG_DROPED ) )
