a9413270fba23f185263f13851e3497c2f6aff5f
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2014-12-31 09:27:48 +0000

    优化：给cmd.run增加useShell参数，默认为TRUE
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@16706 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/usrdef/sptUsrCmd.cpp
+++ SequoiaDB/engine/spt/usrdef/sptUsrCmd.cpp
@@ -297,7 +297,8 @@ namespace engine
       ss << "Cmd functions:" << endl
          << " var cmd = new Cmd()" << endl
          << "   run( cmd, [args], [timeout], [useShell] )  timeout(ms), default 0: never timeout" << endl
-         << "   start( cmd, [args], [useShell] )" << endl
+         << "        useShell 0/1, default 1" << endl ;
+         << "   start( cmd, [args], [useShell] )  useShell 0/1, default 1" << endl
          << "   getLastRet()" << endl
          << "   getLastOut()" << endl ;
       rval.setStringVal( "", ss.str().c_str() ) ;
