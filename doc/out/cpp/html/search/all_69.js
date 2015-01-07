532be954a628378acafb961094f55840d29cea90
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-01-07 10:15:23 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@16769 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/cpp/html/search/all_69.js
+++ doc/out/cpp/html/search/all_69.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['insert',['insert',['../classsdbclient_1_1sdbCollection.html#a4f1f71eb57d42dd766ef43d90702f837',1,'sdbclient::sdbCollection']]],
   ['iscatalog',['isCatalog',['../classsdbclient_1_1sdbReplicaGroup.html#a10e5653a1e1962abd1f092bc5a030e09',1,'sdbclient::sdbReplicaGroup']]],
-  ['isclosed',['isClosed',['../classsdbclient_1_1sdbLob.html#a14d3eba0e6fea8c8a3dd90519cce5391',1,'sdbclient::sdbLob']]],
-  ['isvalid',['isValid',['../classsdbclient_1_1sdb.html#a587e82a600dfefaab2872df189e08ca4',1,'sdbclient::sdb']]]
+  ['isclosed',['isClosed',['../classsdbclient_1_1sdbLob.html#a14d3eba0e6fea8c8a3dd90519cce5391',1,'sdbclient::sdbLob::isClosed(BOOLEAN &amp;flag)'],['../classsdbclient_1_1sdbLob.html#a09b96d2ab7b1af9b8855b1ba8e6e1990',1,'sdbclient::sdbLob::isClosed()']]],
+  ['isvalid',['isValid',['../classsdbclient_1_1sdb.html#a587e82a600dfefaab2872df189e08ca4',1,'sdbclient::sdb::isValid(BOOLEAN *result)'],['../classsdbclient_1_1sdb.html#a88eb7a1f8f9687024b3803fa130b8445',1,'sdbclient::sdb::isValid()']]]
 ];
