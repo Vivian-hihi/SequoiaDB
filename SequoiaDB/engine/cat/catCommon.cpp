bb096d562640d8168b8ec8e3aa1f3bcc52a4e858
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-01-17 10:56:50 +0000

    fix bug: when cs have a main cl and main cl attach sub cl, dropCS report error -6
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@16888 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cat/catCommon.cpp
+++ SequoiaDB/engine/cat/catCommon.cpp
@@ -1174,7 +1174,10 @@ namespace engine
                }
                BSONObj cataItemObj = e.embeddedObject() ;
                BSONElement eleGrpID = cataItemObj.getField( CAT_GROUPID_NAME ) ;
-               groupSet.insert( eleGrpID.numberInt() ) ;
+               if ( eleGrpID.isNumber() )
+               {
+                  groupSet.insert( eleGrpID.numberInt() ) ;
+               }
             }
          }
          catch( std::exception &e )
