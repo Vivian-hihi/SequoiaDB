63e1a52cc88370dbd427548b9bf6d9b9bc00150b
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-02-05 07:15:03 +0000

    增加在用户不鉴权的情况下做自动无用户名和密码的隐式鉴权
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@17165 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/pmdProcessor.cpp
+++ SequoiaDB/engine/pmd/pmdProcessor.cpp
@@ -70,10 +70,8 @@ namespace engine
       if ( MSG_AUTH_VERIFY_REQ == msg->opCode )
       {
          rc = getClient()->authenticate( msg ) ;
-         goto done ;
       }
-
-      if ( MSG_BS_INTERRUPTE == msg->opCode )
+      else if ( MSG_BS_INTERRUPTE == msg->opCode )
       {
          rc = _onInterruptMsg( msg, getDPSCB() ) ;
       }
@@ -85,79 +83,81 @@ namespace engine
       {
          rc = _onDisconnectMsg() ;
       }
-      else if ( !getClient()->isAuthed() )
+      else
       {
-         rc = getClient()->authenticate( "", "" ) ;
-      }
+         if ( !getClient()->isAuthed() )
+         {
+            rc = getClient()->authenticate( "", "" ) ;
+            if ( rc )
+            {
+               goto done ;
+            }
+         }
 
-      if ( rc )
-      {
-         goto done ;
-      }
-
-      switch( msg->opCode )
-      {
-         case MSG_BS_MSG_REQ :
-            rc = _onMsgReqMsg( msg ) ;
-            break ;
-         case MSG_BS_UPDATE_REQ :
-            rc = _onUpdateReqMsg( msg, getDPSCB() ) ;
-            break ;
-         case MSG_BS_INSERT_REQ :
-            rc = _onInsertReqMsg( msg ) ;
-            break ;
-         case MSG_BS_QUERY_REQ :
-            rc = _onQueryReqMsg( msg, getDPSCB(), contextBuff, contextID ) ;
-            break ;
-         case MSG_BS_DELETE_REQ :
-            rc = _onDelReqMsg( msg, getDPSCB() ) ;
-            break ;
-         case MSG_BS_GETMORE_REQ :
-            rc = _onGetMoreReqMsg( msg, contextBuff, contextID ) ;
-            break ;
-         case MSG_BS_KILL_CONTEXT_REQ :
-            rc = _onKillContextsReqMsg( msg ) ;
-            break ;
-         case MSG_BS_SQL_REQ :
-            rc = _onSQLMsg( msg, contextID ) ;
-            break ;
-         case MSG_BS_TRANS_BEGIN_REQ :
-            rc = _onTransBeginMsg() ;
-            break ;
-         case MSG_BS_TRANS_COMMIT_REQ :
-            rc = _onTransCommitMsg( getDPSCB() ) ;
-            break ;
-         case MSG_BS_TRANS_ROLLBACK_REQ :
-            rc = _onTransRollbackMsg( getDPSCB() ) ;
-            break ;
-         case MSG_BS_AGGREGATE_REQ :
-            rc = _onAggrReqMsg( msg, contextID ) ;
-            break ;
-         case MSG_BS_LOB_OPEN_REQ :
-            rc = _onOpenLobMsg( msg, getDPSCB(), contextID, contextBuff ) ;
-            break ;
-         case MSG_BS_LOB_WRITE_REQ:
-            rc = _onWriteLobMsg( msg ) ;
-            break ;
-         case MSG_BS_LOB_READ_REQ:
-            rc = _onReadLobMsg( msg, contextBuff ) ;
-            break ;
-         case MSG_BS_LOB_CLOSE_REQ:
-            rc = _onCloseLobMsg( msg ) ;
-            break ;
-         case MSG_BS_LOB_REMOVE_REQ:
-            rc = _onRemoveLobMsg( msg, getDPSCB() ) ;
-            break ;
-         default :
-            PD_LOG( PDWARNING, "Session[%s] recv unknow msg[type:[%d]%d, "
-                    "len: %d, tid: %d, routeID: %d.%d.%d, reqID: %lld]",
-                    getSession()->sessionName(), IS_REPLY_TYPE(msg->opCode),
-                    GET_REQUEST_TYPE(msg->opCode), msg->messageLength,
-                    msg->TID, msg->routeID.columns.groupID,
-                    msg->routeID.columns.nodeID,
-                    msg->routeID.columns.serviceID, msg->requestID ) ;
-            rc = SDB_INVALIDARG ;
-            break ;
+         switch( msg->opCode )
+         {
+            case MSG_BS_MSG_REQ :
+               rc = _onMsgReqMsg( msg ) ;
+               break ;
+            case MSG_BS_UPDATE_REQ :
+               rc = _onUpdateReqMsg( msg, getDPSCB() ) ;
+               break ;
+            case MSG_BS_INSERT_REQ :
+               rc = _onInsertReqMsg( msg ) ;
+               break ;
+            case MSG_BS_QUERY_REQ :
+               rc = _onQueryReqMsg( msg, getDPSCB(), contextBuff, contextID ) ;
+               break ;
+            case MSG_BS_DELETE_REQ :
+               rc = _onDelReqMsg( msg, getDPSCB() ) ;
+               break ;
+            case MSG_BS_GETMORE_REQ :
+               rc = _onGetMoreReqMsg( msg, contextBuff, contextID ) ;
+               break ;
+            case MSG_BS_KILL_CONTEXT_REQ :
+               rc = _onKillContextsReqMsg( msg ) ;
+               break ;
+            case MSG_BS_SQL_REQ :
+               rc = _onSQLMsg( msg, contextID ) ;
+               break ;
+            case MSG_BS_TRANS_BEGIN_REQ :
+               rc = _onTransBeginMsg() ;
+               break ;
+            case MSG_BS_TRANS_COMMIT_REQ :
+               rc = _onTransCommitMsg( getDPSCB() ) ;
+               break ;
+            case MSG_BS_TRANS_ROLLBACK_REQ :
+               rc = _onTransRollbackMsg( getDPSCB() ) ;
+               break ;
+            case MSG_BS_AGGREGATE_REQ :
+               rc = _onAggrReqMsg( msg, contextID ) ;
+               break ;
+            case MSG_BS_LOB_OPEN_REQ :
+               rc = _onOpenLobMsg( msg, getDPSCB(), contextID, contextBuff ) ;
+               break ;
+            case MSG_BS_LOB_WRITE_REQ:
+               rc = _onWriteLobMsg( msg ) ;
+               break ;
+            case MSG_BS_LOB_READ_REQ:
+               rc = _onReadLobMsg( msg, contextBuff ) ;
+               break ;
+            case MSG_BS_LOB_CLOSE_REQ:
+               rc = _onCloseLobMsg( msg ) ;
+               break ;
+            case MSG_BS_LOB_REMOVE_REQ:
+               rc = _onRemoveLobMsg( msg, getDPSCB() ) ;
+               break ;
+            default :
+               PD_LOG( PDWARNING, "Session[%s] recv unknow msg[type:[%d]%d, "
+                       "len: %d, tid: %d, routeID: %d.%d.%d, reqID: %lld]",
+                       getSession()->sessionName(), IS_REPLY_TYPE(msg->opCode),
+                       GET_REQUEST_TYPE(msg->opCode), msg->messageLength,
+                       msg->TID, msg->routeID.columns.groupID,
+                       msg->routeID.columns.nodeID,
+                       msg->routeID.columns.serviceID, msg->requestID ) ;
+               rc = SDB_INVALIDARG ;
+               break ;
+         }
       }
 
    done:
@@ -900,6 +900,12 @@ namespace engine
       {
          rc = SDB_COORD_UNKNOWN_OP_REQ ;
          goto done ;
+      }
+      else if ( MSG_BS_INTERRUPTE == msg->opCode ||
+                MSG_BS_INTERRUPTE_SELF == msg->opCode ||
+                MSG_BS_DISCONNECT == msg->opCode )
+      {
+         // don't need auth
       }
       else if ( !getClient()->isAuthed() )
       {
