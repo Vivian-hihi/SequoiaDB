2b69ddd6d9ef432d07d9d551ba5766ee04a96d50
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-04-07 02:48:52 +0000

    SEQUOIADBMAINSTREAM-663: add DBCollection.QueryAndUpdate() & DBCollection.QueryAndRemove() to C# driver API doc
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@18156 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/cs/html/search/variables_66.js
+++ doc/out/cs/html/search/variables_66.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['flg_5fquery_5fexplain',['FLG_QUERY_EXPLAIN',['../classSequoiaDB_1_1DBQuery.html#a059c1cef62008e33cb8cd2311a4ff27a',1,'SequoiaDB::DBQuery']]],
   ['flg_5fquery_5fforce_5fhint',['FLG_QUERY_FORCE_HINT',['../classSequoiaDB_1_1DBQuery.html#ad98e9048b66addb6b0decdf057042432',1,'SequoiaDB::DBQuery']]],
+  ['flg_5fquery_5fmodify',['FLG_QUERY_MODIFY',['../classSequoiaDB_1_1DBQuery.html#a68d90ff25a68e2ae2c0a7fd4f367feb6',1,'SequoiaDB::DBQuery']]],
   ['flg_5fquery_5fparalled',['FLG_QUERY_PARALLED',['../classSequoiaDB_1_1DBQuery.html#a4792133a9a9656ddd662f3eb3fc11635',1,'SequoiaDB::DBQuery']]],
   ['flg_5fquery_5fstringout',['FLG_QUERY_STRINGOUT',['../classSequoiaDB_1_1DBQuery.html#a919102e00170e8feb865392c1a50a9d1',1,'SequoiaDB::DBQuery']]],
   ['flg_5fquery_5fwith_5freturndata',['FLG_QUERY_WITH_RETURNDATA',['../classSequoiaDB_1_1DBQuery.html#ad702fb9a09995ccb9801dedcb52ba854',1,'SequoiaDB::DBQuery']]]
