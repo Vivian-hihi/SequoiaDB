f952abeb46861ef8ea89c039873ee9f48d18dbe0
linyoubin <linyoubin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-04-15 06:45:57 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@18446 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ doc/out/java/html/search/all_74.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['truncate',['truncate',['../classcom_1_1sequoiadb_1_1base_1_1DBCollection.html#a6fd38f003b6e52167a7941fadb696669',1,'com::sequoiadb::base::DBCollection']]]
+];
