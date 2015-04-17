359a19c756a4c81ee0e42af8d00fa76219c0444b
linyoubin <linyoubin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-04-17 07:26:20 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@18558 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/java/html/search/functions_61.js
+++ doc/out/java/html/search/functions_61.js
@@ -4,5 +4,6 @@ var searchData=
   ['aggregate',['aggregate',['../classcom_1_1sequoiadb_1_1base_1_1DBCollection.html#ac8dc51878789c63f070923de9f496cda',1,'com::sequoiadb::base::DBCollection']]],
   ['altercollection',['alterCollection',['../classcom_1_1sequoiadb_1_1base_1_1DBCollection.html#a7003ce6587cbdc6abcb9e0934531a192',1,'com::sequoiadb::base::DBCollection']]],
   ['alterdomain',['alterDomain',['../classcom_1_1sequoiadb_1_1base_1_1Domain.html#ad9efa3df5bb6d455c9a7ac718bd95737',1,'com::sequoiadb::base::Domain']]],
-  ['attachcollection',['attachCollection',['../classcom_1_1sequoiadb_1_1base_1_1DBCollection.html#afc6a716ed9c3afc2382c9c3d385c286c',1,'com::sequoiadb::base::DBCollection']]]
+  ['attachcollection',['attachCollection',['../classcom_1_1sequoiadb_1_1base_1_1DBCollection.html#afc6a716ed9c3afc2382c9c3d385c286c',1,'com::sequoiadb::base::DBCollection']]],
+  ['attachnode',['attachNode',['../classcom_1_1sequoiadb_1_1base_1_1ReplicaGroup.html#a7e27d1d73be1979530beaf79e03a5e3d',1,'com::sequoiadb::base::ReplicaGroup']]]
 ];
