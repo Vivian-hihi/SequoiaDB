c1d62307dcea03fa7fe6d4341b4c7b7548fa3c06
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-05-13 02:08:37 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@18830 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/web/data/js/public/Frame/sdbspark/append.js
@@ -0,0 +1,47 @@
+var sdbjs = {
+	_layout: null,
+	resize: function(){
+		var obj = this ;
+		obj._layout._onResize() ;
+		//修正集合空间列表的高度
+		var layoutLeftHeight = $( '.l-layout-left' ).height() ;
+		var layoutLeftTitleHeight = $( '.l-layout-left > .l-layout-header' ).outerHeight() ;
+		var layoutLeftContentHeight = layoutLeftHeight - layoutLeftTitleHeight ;
+		$( '#operatePanel > iframe' ).height( layoutLeftContentHeight ) ;
+		//修改数据库总览的高度
+		var layoutCenterHeight = $( '.l-layout-center' ).height() ;
+		var layoutCenterTitleHeight = $( '#tab > .l-tab-links' ).height() ;
+		var layoutCenterContentHeight = layoutCenterHeight - layoutCenterTitleHeight ;
+		$( '#masterList > iframe' ).height( layoutCenterContentHeight ) ;
+	},
+	check: function(){
+		return true ;
+	},
+	render: function(){
+		var obj = this ;
+		//设置标题
+		$( '#layout > [position="left"]' ).attr( 'title', '添加Master' ) ;
+        $( '#layout > [position="center"]' ).attr( 'title', 'Master列表' ) ;
+		//创建布局
+		obj._layout = $( '#layout' ).ligerLayout( { leftWidth: 400, allowLeftCollapse: true, allowLeftResize: true, heightDiff: -5 } ) ;
+		//加载控制面板
+		var csList = $( '#operatePanel' ).ligerSdbIframe( { url: M( 'sdbspark/operate.html' ) } ) ;
+        //加载master表格
+        var csList = $( '#masterList' ).ligerSdbIframe( { url: M( '' ) } ) ;
+		obj.resize() ;
+	},
+	synData: function(){
+		return ;
+	}
+} ;
+
+$(document).ready( function(){
+	if( sdbjs.check() ){
+		sdbjs.render() ;
+		sdbjs.synData() ;
+	}
+} ) ;
+
+$( window ).resize( function(){
+	sdbjs.resize() ;
+} ) ;
---
+++ SequoiaDB/web/data/js/public/appendspark.js
@@ -0,0 +1,59 @@
+function openAboutus(){
+    $.ligerDialog.open( { height: 240,
+                          width: 630,
+                          title: '关于SAC',
+                          url: M('public/aboutus.html'),
+                          showMax: false,
+                          showToggle: false,
+                          showMin: false,
+                          isResize: false,
+                          slide: false,
+                          show: false,
+                          cls: 'ext-modal' } ) ;
+}
+
+var sdbjs = {
+	_tab: null,
+	addCSTab: function( tabId, tabTitle, url, showClose, options ){
+		var obj = this ;
+		obj._tab.addTabItem( { tabid: tabId, text: tabTitle, url: F( url ), showClose: showClose } ) ;
+	},
+	resize: function(){
+		var obj = this ;
+		var height = $( window ).height() ;
+		var menuHeight = $( '#menu' ).height() ;
+		var footHeight = $( '#foot' ).height() ;
+		obj._tab.setHeight( height - menuHeight - footHeight - 3 ) ;
+	},
+	check: function(){
+		return true ;
+	},
+	render: function(){
+		var obj = this ;
+		$( '#menu' ).ligerSdbMenuBar( { items: [{ text: '首页', click: function(){ window.location = '../index.html' ; } },
+                                                { text: '帮助', menu: { items: [ { text: '关于SAC系统', click: function(){ openAboutus(); } } ] } } ] } ) ;
+		obj._tab = $( '#tab' ).ligerTab( { onAfterSelectTabItem: function( targetID ){
+			var sdbjsObj = window.frames[ targetID ].sdbjs ;
+			if( typeof( sdbjsObj ) != 'undefined' )
+			{
+				sdbjsObj.resize() ;
+			}
+		} } ) ;
+		obj._tab.addTabItem( { tabid: 'database', text: 'Spark', url: F( 'sdbspark/append.html' ), showClose: false } ) ;
+		obj.resize() ;
+	},
+	synData: function(){
+		return ;
+	}
+} ;
+
+$(document).ready( function(){
+	if( sdbjs.check() ){
+		sdbjs.render() ;
+		sdbjs.synData() ;
+	}
+} ) ;
+
+$( window ).resize( function(){
+	sdbjs.resize() ;
+} ) ;
