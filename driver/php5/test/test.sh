3c897f6d6aba623a905dc22c19d89f4bf0114a76
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-06-03 08:35:35 +0000

    更新php驱动本地测试的代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19085 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/php5/test/test.sh
+++ driver/php5/test/test.sh
@@ -5,3 +5,4 @@ INSTALL_DIR=../../../
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${INSTALL_DIR}/tools/server/php_linux/libxml2/lib/
 
 ${INSTALL_DIR}/tools/server/php_linux/bin/php-bin -c ./php.ini -f ./test.php
+echo $?
