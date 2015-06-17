e2d495d2a73cb6609859e2773d26285670196be8
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-06-17 02:41:17 +0000

    修复支持展示主表
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19271 6121f079-4a18-41d3-8c30-8870a322f2c2

--- client/admin/admintpl/php/ajax/a-data_l.php
+++ client/admin/admintpl/php/ajax/a-data_l.php
@@ -5,6 +5,7 @@ $isfirst = true ;
 
 $array_1 = array() ;
 $array_2 = array() ;
+$array_3 = array() ;
 
 $cursor = $db -> getList ( SDB_LIST_COLLECTIONSPACES ) ;
 if ( !empty ( $cursor ) )
@@ -26,7 +27,32 @@ if ( !empty ( $cursor ) )
 	}
 }
 
+$cursor = $db -> getSnapshot ( 8 ) ;
+if ( !empty ( $cursor ) )
+{
+	while ( $arr = $cursor -> getNext() )
+	{
+        if( array_key_exists( 'IsMainCL', $arr ) && $arr['IsMainCL'] == true )
+        {
+            $tmpCSCL = explode( '.', $arr['Name'] ) ;
+            array_push( $array_3, $tmpCSCL  ) ;
+        }
+	}
+}
+
 $cscl_list = arrayMerges( $array_1, $array_2 ) ;
+
+foreach( $cscl_list as $key => $csInfo )
+{
+    foreach( $array_3 as $masterInfo )
+    {
+        if( $csInfo['Name'] == $masterInfo[0] )
+        {
+            array_push( $cscl_list[ $key ]['Collection'], array( 'Name' => $masterInfo[1] ) ) ;
+        }
+    }
+}
+
 $cscl_list = json_encode( array( 'name' => '数据库', 'child' => array( 'name' => '集合空间', 'child' => $cscl_list ) ), true ) ;
 
 $smarty -> assign( "cscl_list", $cscl_list ) ;
