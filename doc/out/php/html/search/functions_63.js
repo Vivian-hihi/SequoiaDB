7cb4dbbbb55c70df7b4ebf20918b638f298cd78d
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-06-26 09:20:59 +0000

    php驱动增加新接口，更新文档
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19478 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/php/html/search/functions_63.js
+++ doc/out/php/html/search/functions_63.js
@@ -4,7 +4,9 @@ var searchData=
   ['connect',['connect',['../classSequoiaDB.html#aa5d8ac5b06d383d0a74357e25aa5efcc',1,'SequoiaDB::connect()'],['../classSequoiaNode.html#a585df8009f7a27a0a7e9cfee3d63e32f',1,'SequoiaNode::connect()']]],
   ['count',['count',['../classSequoiaCL.html#aed703c2e2dabfe8361829bd366036689',1,'SequoiaCL']]],
   ['createcatagroup',['createCataGroup',['../classSequoiaDB.html#afe7ad660a46c30661d9e124913b2c027',1,'SequoiaDB']]],
-  ['createindex',['createIndex',['../classSequoiaCL.html#a31e2929f49d02dd7e96c75745cda38e4',1,'SequoiaCL']]],
+  ['createidindex',['createIdIndex',['../classSequoiaCL.html#aea325953ed8ae327669dbf7177a3d891',1,'SequoiaCL']]],
+  ['createindex',['createIndex',['../classSequoiaCL.html#ad03a2a4b4e379c79ece0d2ed710ef19b',1,'SequoiaCL']]],
+  ['createindexoffline',['createIndexOffline',['../classSequoiaCL.html#a269767ef9d14f92bfe1f60a2a11db6b3',1,'SequoiaCL']]],
   ['createnode',['createNode',['../classSequoiaGroup.html#a1d2278e3bedcd500009e79a13a56dcc2',1,'SequoiaGroup']]],
   ['current',['current',['../classSequoiaCursor.html#a384b05fd98b8a15f89cf26522d13fe03',1,'SequoiaCursor']]]
 ];
