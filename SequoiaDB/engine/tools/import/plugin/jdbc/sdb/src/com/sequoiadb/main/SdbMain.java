ffc9b2da4619f70818f377fa8cc9e0f5c524251b
liuchuankai <liuchuankai@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-07-29 12:05:19 +0000

    reset sdbimport project
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19858 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/tools/import/plugin/jdbc/sdb/src/com/sequoiadb/main/SdbMain.java
@@ -0,0 +1,179 @@
+package com.sequoiadb.main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.sequoiadb.tasks.ReadSdb;
+
+public class SdbMain {
+	
+
+public static void main(String[] args) {
+	
+	/**
+	 * @param corePoolSize
+	 * @param maximumPoolSize
+	 * @param keepAliveTime
+	 * @param unit
+	 * @param workQueue
+	 * */
+	ThreadPoolExecutor executor = new ThreadPoolExecutor(12, 20, 200, TimeUnit.MILLISECONDS,
+            new ArrayBlockingQueue<Runnable>(12));
+	PropertyConfigurator.configure("lib/log4j.properties");
+	
+	Map<String,Object> map=paraseCommand(args);
+	if(map != null){
+	List<String> obj = (List)map.get("sqlList");
+	for(String sql: obj){
+	    ReadSdb myTask = new ReadSdb(map,sql);
+        executor.execute(myTask);
+    }
+    executor.shutdown();
+	}
+}
+
+   private static Map<String,Object> paraseCommand(String[] args){
+	   
+//	    command = "--connect jdbc:db2://192.168.30.223:50110/sequoia --username db2inst1 --password liuck_2015 --table sedb10 --sumrow 10 --spilt-by column1 --threads 1";
+//		String command = command;
+	   Options opt = new Options();
+       opt.addOption("connect", true, "JDBC URL.");
+       opt.addOption("username", true, "DataBase UserName.");
+       opt.addOption("password",true,"DataBase password.");
+       opt.addOption("table",true,"table name which you want to select.");
+       opt.addOption("sumrow",true,"Rows which you want to select.");
+       opt.addOption("threads",true,"Concurrent number");
+       opt.addOption("version",false,"information about sdbJdbc");
+       opt.addOption("h", "help",  false, "print help for the command.");
+       String formatstr = "gmkdir [--connect][--username][password][--table][--sumrow][--threads][-h/--help] DirectoryName";
+       String versionstr = "version  v1.1";
+       HelpFormatter formatter = new HelpFormatter();
+       CommandLineParser parser = new PosixParser();
+       CommandLine cl = null;
+       Logger logger = Logger.getLogger(SdbMain.class);
+       List<String> list = new ArrayList<String>();
+       Map<String,Object> map = new HashMap<String,Object>();
+       try {
+           // 处理Options和参数
+           cl = parser.parse( opt, args );
+       } catch (ParseException e) {
+           formatter.printHelp( formatstr, opt ); // 如果发生异常，则打印出帮助信息
+           logger.error(e.getStackTrace(),e);
+       }
+       //-h或--help
+       if(cl == null){
+    	   logger.error("CommandLine error");
+    	   return null;
+       }
+       if (null !=cl && cl.hasOption("h")) {
+           HelpFormatter hf = new HelpFormatter();
+           hf.printHelp(formatstr, "", opt, "");
+           return null;
+       }
+       if(null != cl && cl.hasOption("version")){
+    	   System.out.println(versionstr);
+    	   return null;
+       }
+       //--connect
+       if (null !=cl && cl.hasOption("connect")) {
+    	   String conStr = cl.getOptionValue("connect");
+    	   int mysqlIndex = conStr.indexOf("oracle");
+           int DB2Index = conStr.indexOf("db2");
+           if(mysqlIndex >0)
+        	   map.put("dbType", "oracle");
+           if(DB2Index > 0)
+               map.put("dbType", "db2");
+       	   map.put("url", cl.getOptionValue("connect"));
+       }
+        //--username
+       if (null !=cl && cl.hasOption("username")) {
+           map.put("user", cl.getOptionValue("username"));
+       }
+       //--password
+       if(null !=cl && cl.hasOption("password")) {
+       	map.put("password", cl.getOptionValue("password"));
+       }
+       //--table
+       if(null !=cl && cl.hasOption("table")) {
+       	map.put("table", cl.getOptionValue("table"));
+       }
+     //--sumrow
+       if(null !=cl && cl.hasOption("sumrow")) {
+       	map.put("sumRow", cl.getOptionValue("sumrow"));
+       }
+      //--threads
+       if(null !=cl && cl.hasOption("threads")) {
+       	map.put("threads", cl.getOptionValue("threads"));
+       }  
+          if(map.get("url") == null){
+        	  logger.error("could not found --connect option");
+        	  return null;
+          }
+          if(map.get("user") == null){
+        	  logger.error("could not found --username option");
+        	  return null;
+          }
+          if(map.get("password") == null){
+        	  logger.error("could not found --password option");
+        	  return null;
+          }
+          if(map.get("sumRow") == null)
+          {
+        	  logger.error("could not found --sumRow option");
+        	  return null;
+          }
+          if(map.get("threads") == null){
+        	  logger.error("could not found --threads option");
+        	  return null;
+          }
+          if(map.get("table") == null){
+        	  logger.error("could not found --table option");
+        	  return null;
+          }
+          if(map.get("dbType") == null){
+        	  logger.error("could not found --dbType option");
+        	  return null;
+          }
+          int sumRow = Integer.parseInt(map.get("sumRow").toString());
+          int threads = Integer.parseInt(map.get("threads").toString());
+          int avg = sumRow/threads;
+          int start = 0;
+          int end = avg;
+          String sql = null;
+          String table = map.get("table").toString();
+          for(int i=1;i<=threads;i++){
+       	   if(i == threads)
+       	   {   
+       		   if(map.get("dbType").toString() == "db2")
+       		   sql = "select * from (select s.*,rownumber() over() as rn from (select * from "+table+") as s ) as s1 where s1.rn between "+start+" and "+sumRow;   
+       	       if(map.get("dbType").toString() == "oracle")
+       	       sql = "select * from (select rownum,* from "+table+" where rownum <="+sumRow+") where rownum >"+start;	   
+       	   }else{
+       		   if(map.get("dbType").toString() == "db2")
+       	       sql = "select * from (select s.*,rownumber() over() as rn from (select * from "+table+") as s ) as s1 where s1.rn between "+start+" and "+end;
+       	       if(map.get("dbType").toString() == "oracle")
+       	    	sql = "select * from (select rownum,* from "+table+" where rownum <="+sumRow+") where rownum >"+start;   
+       	   }
+       	   start = i*avg+1;
+       	   end = (i+1)*avg;
+       	   list.add(sql);
+          }
+          map.put("sqlList", list);
+          logger.info("args:"+map);
+          return map;
+   }
+}
---
+++ SequoiaDB/engine/tools/import/plugin/jdbc/sdb/src/com/sequoiadb/tasks/ReadSdb.java
@@ -0,0 +1,174 @@
+package com.sequoiadb.tasks;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.log4j.Logger;
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+
+import com.sequoiadb.service.ConnectDataBase;
+
+
+public class ReadSdb implements Runnable{
+	
+	private int taskNum;
+	
+	private Map<String,Object> map;
+	
+	private String sql =null;
+	
+	Logger logger = Logger.getLogger(ReadSdb.class);
+	private static ConcurrentLinkedQueue<BSONObject> queue = new ConcurrentLinkedQueue<BSONObject>();
+	private static int count = 2;
+	private static CountDownLatch lath = new CountDownLatch(count); //finished when lath==0
+	
+	public  ReadSdb(int num) {
+		this.taskNum = num;
+	}
+	public ReadSdb(){
+		
+	}
+	public ReadSdb(Map<String,Object> map,String sql){
+		
+		this.map = map;
+		this.sql = sql;
+	}
+   Connection conn = null;
+	
+	PreparedStatement pstmt = null;
+	
+	ResultSet rs = null;
+	
+
+	@Override
+	public void run() {
+        try {
+        	
+        	read(map.get("dbType").toString(),map.get("url").toString(),map.get("user").toString(),map.get("password").toString(),sql);
+        } catch (InterruptedException e) {
+        	logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+		
+	}
+	//readDB
+	
+	public  void read(String dbType,String url,String user,String password,String sql) throws InterruptedException{
+		
+		ConnectDataBase cdb = new ConnectDataBase(dbType,url,user,password);
+		  conn = cdb.getConnection();
+		  
+          long startTime = 	System.currentTimeMillis();
+          logger.info("dbType="+dbType+" url="+url+" user="+user+" sql"+sql);
+//          System.out.println("dbType="+dbType+" url="+url+" user="+user+" sql"+sql);
+		  try {
+		   pstmt = conn.prepareStatement(sql);
+		   rs = pstmt.executeQuery();
+		   ExecutorService es = Executors.newFixedThreadPool(4);
+		     
+		   while (rs.next()) {
+			   ResultSetMetaData  rsmd=rs.getMetaData();
+			   BSONObject bson =  new BasicBSONObject();
+			   
+			   int i = -1;
+			   int type = -1;
+			   try{
+				   for(i = 1;i<=rsmd.getColumnCount();i++){
+					   String name = rsmd.getColumnName(i);
+					   type = rsmd.getColumnType(i);
+					   switch(type){
+					   case Types.BIGINT:
+						   bson.put(name, rs.getLong(name));break;
+					   case Types.BOOLEAN:
+						   bson.put(name, rs.getBoolean(name));break;
+					   case Types.DATE:
+						   bson.put(name, rs.getDate(name));break;
+					   case Types.DOUBLE:
+						   bson.put(name, rs.getDouble(name));break;
+					   case Types.FLOAT:
+						   bson.put(name, rs.getFloat(name));break;
+					   case Types.INTEGER:
+						   bson.put(name, rs.getInt(name));break;  
+					   case Types.SMALLINT:
+						   bson.put(name, rs.getInt(name));break;
+					   case Types.TIME:
+						   bson.put(name, rs.getTime(name));break;	
+					   case Types.TIMESTAMP:
+						   bson.put(name, rs.getTimestamp(name));break;	
+					   case Types.TINYINT:
+						   bson.put(name, rs.getShort(name));break;
+					   case Types.VARCHAR:
+						   bson.put(name, rs.getString(name));break;	  
+					   case Types.NCHAR:
+						   bson.put(name, rs.getNString(name));break;  
+					   case Types.NVARCHAR:
+						   bson.put(name, rs.getNString(name));break; 
+					   case Types.BIT:
+						   bson.put(name, rs.getByte(name));break;
+					   case Types.BLOB:
+						   bson.put(name, rs.getBlob(name));break;
+					   case Types.NUMERIC:
+						   bson.put(name, rs.getBigDecimal(name));
+					   default :
+						   logger.error("could not found this type on metadata");
+						   break;
+					   }
+				   }
+			   }catch(Exception e){
+				   logger.error(e.getMessage());
+			   }
+			   ReadSdb.offer(bson);
+			   for(int k=0;k<count;k++){
+					es.submit(new Poll());
+				}
+		   }
+		   lath.await();
+			es.shutdown();
+		  } catch (SQLException e) {
+			  logger.error(e.getMessage());  
+		   e.printStackTrace();
+		  } finally {
+		   try {
+			   
+		    if (pstmt != null)
+		     pstmt.close();
+		    if (conn != null)
+		     conn.close();
+		    logger.info("costTime"+(System.currentTimeMillis() - startTime));
+//		    System.err.println("duringTime :"+(System.currentTimeMillis() - startTime));
+		   } catch (SQLException e) {
+			   logger.error(e.getMessage());  
+		    e.printStackTrace();
+		   }
+		  }
+	}
+	
+	public static void offer(BSONObject bon){
+			
+			queue.offer(bon);
+	}
+	public static class Poll implements Runnable{
+
+		@Override
+		public void run() {
+			while(!queue.isEmpty()){
+//				queue.poll();
+				System.out.println(queue.poll());
+			}
+			lath.countDown();
+			
+		}
+		
+	}
+	
+}
