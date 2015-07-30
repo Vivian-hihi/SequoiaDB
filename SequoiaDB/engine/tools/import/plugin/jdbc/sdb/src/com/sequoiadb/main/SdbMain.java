f6dfe3f53685ced3568ed4a2bcbb1bf559d85b9b
liuchuankai <liuchuankai@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-07-30 01:24:27 +0000

    adjustment format
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19863 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/import/plugin/jdbc/sdb/src/com/sequoiadb/main/SdbMain.java
+++ SequoiaDB/engine/tools/import/plugin/jdbc/sdb/src/com/sequoiadb/main/SdbMain.java
@@ -50,130 +50,131 @@ public static void main(String[] args) {
 	   
 //	    command = "--connect jdbc:db2://192.168.30.223:50110/sequoia --username db2inst1 --password liuck_2015 --table sedb10 --sumrow 10 --spilt-by column1 --threads 1";
 //		String command = command;
-	   Options opt = new Options();
-       opt.addOption("connect", true, "JDBC URL.");
-       opt.addOption("username", true, "DataBase UserName.");
-       opt.addOption("password",true,"DataBase password.");
-       opt.addOption("table",true,"table name which you want to select.");
-       opt.addOption("sumrow",true,"Rows which you want to select.");
-       opt.addOption("threads",true,"Concurrent number");
-       opt.addOption("version",false,"information about sdbJdbc");
-       opt.addOption("h", "help",  false, "print help for the command.");
-       String formatstr = "gmkdir [--connect][--username][password][--table][--sumrow][--threads][-h/--help] DirectoryName";
-       String versionstr = "version  v1.1";
-       HelpFormatter formatter = new HelpFormatter();
-       CommandLineParser parser = new PosixParser();
-       CommandLine cl = null;
-       Logger logger = Logger.getLogger(SdbMain.class);
-       List<String> list = new ArrayList<String>();
-       Map<String,Object> map = new HashMap<String,Object>();
-       try {
-           // 处理Options和参数
-           cl = parser.parse( opt, args );
-       } catch (ParseException e) {
-           formatter.printHelp( formatstr, opt ); // 如果发生异常，则打印出帮助信息
-           logger.error(e.getStackTrace(),e);
-       }
-       //-h或--help
-       if(cl == null){
-    	   logger.error("CommandLine error");
-    	   return null;
-       }
-       if (null !=cl && cl.hasOption("h")) {
-           HelpFormatter hf = new HelpFormatter();
-           hf.printHelp(formatstr, "", opt, "");
-           return null;
-       }
-       if(null != cl && cl.hasOption("version")){
-    	   System.out.println(versionstr);
-    	   return null;
-       }
-       //--connect
-       if (null !=cl && cl.hasOption("connect")) {
-    	   String conStr = cl.getOptionValue("connect");
-    	   int mysqlIndex = conStr.indexOf("oracle");
-           int DB2Index = conStr.indexOf("db2");
-           if(mysqlIndex >0)
-        	   map.put("dbType", "oracle");
-           if(DB2Index > 0)
-               map.put("dbType", "db2");
-       	   map.put("url", cl.getOptionValue("connect"));
-       }
-        //--username
-       if (null !=cl && cl.hasOption("username")) {
-           map.put("user", cl.getOptionValue("username"));
-       }
-       //--password
-       if(null !=cl && cl.hasOption("password")) {
-       	map.put("password", cl.getOptionValue("password"));
-       }
-       //--table
-       if(null !=cl && cl.hasOption("table")) {
-       	map.put("table", cl.getOptionValue("table"));
-       }
-     //--sumrow
-       if(null !=cl && cl.hasOption("sumrow")) {
-       	map.put("sumRow", cl.getOptionValue("sumrow"));
-       }
-      //--threads
-       if(null !=cl && cl.hasOption("threads")) {
-       	map.put("threads", cl.getOptionValue("threads"));
-       }  
-          if(map.get("url") == null){
-        	  logger.error("could not found --connect option");
-        	  return null;
-          }
-          if(map.get("user") == null){
-        	  logger.error("could not found --username option");
-        	  return null;
-          }
-          if(map.get("password") == null){
-        	  logger.error("could not found --password option");
-        	  return null;
-          }
-          if(map.get("sumRow") == null)
-          {
-        	  logger.error("could not found --sumRow option");
-        	  return null;
-          }
-          if(map.get("threads") == null){
-        	  logger.error("could not found --threads option");
-        	  return null;
-          }
-          if(map.get("table") == null){
-        	  logger.error("could not found --table option");
-        	  return null;
-          }
-          if(map.get("dbType") == null){
-        	  logger.error("could not found --dbType option");
-        	  return null;
-          }
-          int sumRow = Integer.parseInt(map.get("sumRow").toString());
-          int threads = Integer.parseInt(map.get("threads").toString());
-          int avg = sumRow/threads;
-          int start = 0;
-          int end = avg;
-          String sql = null;
-          String table = map.get("table").toString();
-          for(int i=1;i<=threads;i++){
-       	   if(i == threads)
-       	   {   
-       		   if(map.get("dbType").toString() == "db2")
-       		   sql = "select * from (select s.*,rownumber() over() as rn from (select * from "+table+") as s ) as s1 where s1.rn between "+start+" and "+sumRow;   
-       	       if(map.get("dbType").toString() == "oracle")
-       	       sql = "select * from (select rownum,* from "+table+" where rownum <="+sumRow+") where rownum >"+start;	   
-       	   }else{
-       		   if(map.get("dbType").toString() == "db2")
-       	       sql = "select * from (select s.*,rownumber() over() as rn from (select * from "+table+") as s ) as s1 where s1.rn between "+start+" and "+end;
-       	       if(map.get("dbType").toString() == "oracle")
-       	    	sql = "select * from (select rownum,* from "+table+" where rownum <="+sumRow+") where rownum >"+start;   
-       	   }
-       	   start = i*avg+1;
-       	   end = (i+1)*avg;
-       	   list.add(sql);
-          }
-          map.put("sqlList", list);
-          logger.info("args:"+map);
-          return map;
+		Options opt = new Options();
+		opt.addOption("connect", true, "JDBC URL.");
+		opt.addOption("username", true, "DataBase UserName.");
+		opt.addOption("password", true, "DataBase password.");
+		opt.addOption("table", true, "table name which you want to select.");
+		opt.addOption("sumrow", true, "Rows which you want to select.");
+		opt.addOption("threads", true, "Concurrent number");
+		opt.addOption("version", false, "information about sdbJdbc");
+		opt.addOption("h", "help", false, "print help for the command.");
+		String formatstr = "gmkdir [--connect][--username][password][--table][--sumrow][--threads][-h/--help] DirectoryName";
+		String versionstr = "version  v1.1";
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLineParser parser = new PosixParser();
+		CommandLine cl = null;
+		Logger logger = Logger.getLogger(SdbMain.class);
+		List<String> list = new ArrayList<String>();
+		Map<String, Object> map = new HashMap<String, Object>();
+		try {
+			cl = parser.parse(opt, args);
+		} catch (ParseException e) {
+			formatter.printHelp(formatstr, opt);
+			logger.error(e.getStackTrace(), e);
+		}
+		// -h or --help
+		if (cl == null) {
+			logger.error("CommandLine error");
+			return null;
+		}
+		if (null != cl && cl.hasOption("h")) {
+			HelpFormatter hf = new HelpFormatter();
+			hf.printHelp(formatstr, "", opt, "");
+			return null;
+		}
+		if (null != cl && cl.hasOption("version")) {
+			System.out.println(versionstr);
+			return null;
+		}
+		// --connect
+		if (null != cl && cl.hasOption("connect")) {
+			String conStr = cl.getOptionValue("connect");
+			int mysqlIndex = conStr.indexOf("oracle");
+			int DB2Index = conStr.indexOf("db2");
+			if (mysqlIndex > 0)
+				map.put("dbType", "oracle");
+			if (DB2Index > 0)
+				map.put("dbType", "db2");
+			map.put("url", cl.getOptionValue("connect"));
+		}
+		// --username
+		if (null != cl && cl.hasOption("username")) {
+			map.put("user", cl.getOptionValue("username"));
+		}
+		// --password
+		if (null != cl && cl.hasOption("password")) {
+			map.put("password", cl.getOptionValue("password"));
+		}
+		// --table
+		if (null != cl && cl.hasOption("table")) {
+			map.put("table", cl.getOptionValue("table"));
+		}
+		// --sumrow
+		if (null != cl && cl.hasOption("sumrow")) {
+			map.put("sumRow", cl.getOptionValue("sumrow"));
+		}
+		// --threads
+		if (null != cl && cl.hasOption("threads")) {
+			map.put("threads", cl.getOptionValue("threads"));
+		}
+		if (map.get("url") == null) {
+			logger.error("could not found --connect option");
+			return null;
+		}
+		if (map.get("user") == null) {
+			logger.error("could not found --username option");
+			return null;
+		}
+		if (map.get("password") == null) {
+			logger.error("could not found --password option");
+			return null;
+		}
+		if (map.get("sumRow") == null) {
+			logger.error("could not found --sumRow option");
+			return null;
+		}
+		if (map.get("threads") == null) {
+			logger.error("could not found --threads option");
+			return null;
+		}
+		if (map.get("table") == null) {
+			logger.error("could not found --table option");
+			return null;
+		}
+		if (map.get("dbType") == null) {
+			logger.error("could not found --dbType option");
+			return null;
+		}
+		int sumRow = Integer.parseInt(map.get("sumRow").toString());
+		int threads = Integer.parseInt(map.get("threads").toString());
+		int avg = sumRow / threads;
+		int start = 0;
+		int end = avg;
+		String sql = null;
+		String table = map.get("table").toString();
+		for (int i = 1; i <= threads; i++) {
+			if (i == threads) {
+				if (map.get("dbType").toString() == "db2")
+					sql = "select * from (select s.*,rownumber() over() as rn from (select * from " + table
+							+ ") as s ) as s1 where s1.rn between " + start + " and " + sumRow;
+				if (map.get("dbType").toString() == "oracle")
+					sql = "select * from (select rownum,* from " + table + " where rownum <=" + sumRow
+							+ ") where rownum >" + start;
+			} else {
+				if (map.get("dbType").toString() == "db2")
+					sql = "select * from (select s.*,rownumber() over() as rn from (select * from " + table
+							+ ") as s ) as s1 where s1.rn between " + start + " and " + end;
+				if (map.get("dbType").toString() == "oracle")
+					sql = "select * from (select rownum,* from " + table + " where rownum <=" + sumRow
+							+ ") where rownum >" + start;
+			}
+			start = i * avg + 1;
+			end = (i + 1) * avg;
+			list.add(sql);
+		}
+		map.put("sqlList", list);
+		logger.info("args:" + map);
+		return map;
    }
 }
