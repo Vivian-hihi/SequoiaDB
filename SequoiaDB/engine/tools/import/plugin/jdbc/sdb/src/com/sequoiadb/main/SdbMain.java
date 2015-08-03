294cb572d64abd99a8c97ae027a5d39cbf902d45
liuchuankai <liuchuankai@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-08-03 10:45:31 +0000

    add user defined sql import data from DB
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19919 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/tools/import/plugin/jdbc/sdb/src/com/sequoiadb/main/SdbMain.java
+++ SequoiaDB/engine/tools/import/plugin/jdbc/sdb/src/com/sequoiadb/main/SdbMain.java
@@ -1,5 +1,9 @@
 package com.sequoiadb.main;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,10 +21,12 @@ import org.apache.commons.cli.PosixParser;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import com.sequoiadb.service.ConnectDataBase;
 import com.sequoiadb.tasks.ReadSdb;
 
 public class SdbMain {
 	
+public	static Logger logger = Logger.getLogger(SdbMain.class);
 
 public static void main(String[] args) {
 	
@@ -48,7 +54,7 @@ public static void main(String[] args) {
 
    private static Map<String,Object> paraseCommand(String[] args){
 	   
-//	    command = "--connect jdbc:db2://192.168.30.223:50110/sequoia --username db2inst1 --password liuck_2015 --table sedb10 --sumrow 10 --spilt-by column1 --threads 1";
+//	    command = "--connect jdbc:db2://192.168.30.223:50110/sequoia --username db2inst1 --password liuck_2015 --table sedb10 --sumrow 1000000 --threads 4";
 //		String command = command;
 		Options opt = new Options();
 		opt.addOption("connect", true, "JDBC URL.");
@@ -58,13 +64,13 @@ public static void main(String[] args) {
 		opt.addOption("sumrow", true, "Rows which you want to select.");
 		opt.addOption("threads", true, "Concurrent number");
 		opt.addOption("version", false, "information about sdbJdbc");
+		opt.addOption("sql", true, "sql");
 		opt.addOption("h", "help", false, "print help for the command.");
 		String formatstr = "gmkdir [--connect][--username][password][--table][--sumrow][--threads][-h/--help] DirectoryName";
 		String versionstr = "version  v1.1";
 		HelpFormatter formatter = new HelpFormatter();
 		CommandLineParser parser = new PosixParser();
 		CommandLine cl = null;
-		Logger logger = Logger.getLogger(SdbMain.class);
 		List<String> list = new ArrayList<String>();
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
@@ -119,33 +125,41 @@ public static void main(String[] args) {
 			map.put("threads", cl.getOptionValue("threads"));
 		}
 		if (map.get("url") == null) {
+			System.out.println("could not found --connect option");
 			logger.error("could not found --connect option");
-			return null;
-		}
-		if (map.get("user") == null) {
-			logger.error("could not found --username option");
-			return null;
-		}
-		if (map.get("password") == null) {
-			logger.error("could not found --password option");
-			return null;
-		}
-		if (map.get("sumRow") == null) {
-			logger.error("could not found --sumRow option");
-			return null;
-		}
-		if (map.get("threads") == null) {
-			logger.error("could not found --threads option");
-			return null;
-		}
-		if (map.get("table") == null) {
-			logger.error("could not found --table option");
 			return null;
 		}
 		if (map.get("dbType") == null) {
 			logger.error("could not found --dbType option");
 			return null;
 		}
+		if (map.get("user") == null) {
+			System.out.println("could not found --username option");
+			logger.error("could not found --username option");
+			return null;
+		}
+		if (map.get("password") == null) {
+			System.out.println("could not found --password option");
+			logger.error("could not found --password option");
+			return null;
+		}
+		if(cl != null && !cl.hasOption("sql")){
+		if (map.get("table") == null) {
+				System.out.println("could not found --table option");
+				logger.info("could not found --table option");
+				return null;
+		}	
+		if (map.get("sumRow") == null) {
+			int sumrow = queryRowCount(map);
+			map.put("sumRow",sumrow);
+			logger.info("could not found --sumrow option");
+			logger.info("default query all rows from"+map.get("table"));
+		}
+		if (map.get("threads") == null) {
+			map.put("threads", 1);
+			logger.info("could not found --threads option");
+		}
+		
 		int sumRow = Integer.parseInt(map.get("sumRow").toString());
 		int threads = Integer.parseInt(map.get("threads").toString());
 		int avg = sumRow / threads;
@@ -153,6 +167,7 @@ public static void main(String[] args) {
 		int end = avg;
 		String sql = null;
 		String table = map.get("table").toString();
+		
 		for (int i = 1; i <= threads; i++) {
 			if (i == threads) {
 				if (map.get("dbType").toString() == "db2")
@@ -173,8 +188,37 @@ public static void main(String[] args) {
 			end = (i + 1) * avg;
 			list.add(sql);
 		}
+		}
+		if(cl != null && cl.hasOption("sql")){
+			String sqlList = cl.getOptionValue("sql");
+			String sqlStr[] = sqlList.split(";");
+			for(String sql : sqlStr){
+			list.add(sql);
+			}
+		}
 		map.put("sqlList", list);
 		logger.info("args:" + map);
 		return map;
+   }
+   private static int queryRowCount(Map<String,Object> map){
+	   ConnectDataBase cdb = new ConnectDataBase(map.get("dbType").toString(), map.get("url").toString(),map.get("user").toString(),map.get("password").toString());
+	   Connection conn = null;
+	   PreparedStatement pstmt = null;
+	   ResultSet rs = null;
+	   conn = cdb.getConnection();
+	   int rowCount = 0;
+	   String queryRowSql = "select count(1) from "+map.get("table");
+	   try {
+		pstmt = conn.prepareStatement(queryRowSql);
+		rs = pstmt.executeQuery();
+		while(rs.next()){
+			rowCount = Integer.parseInt(rs.getString(1));
+		}
+	} catch (SQLException e) {
+		logger.error("query sumRows from table error");
+		logger.error(e.getMessage());
+	}
+		
+	   return rowCount;
    }
 }
