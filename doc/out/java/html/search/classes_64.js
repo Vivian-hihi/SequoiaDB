baa93c84a6fc7c0e7380373808c9a4b58048e574
linyoubin <linyoubin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-08-05 08:15:40 +0000

    SEQUOIADBMAINSTREAM-988 增加dc接口说明
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@19965 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/java/html/search/classes_64.js
+++ doc/out/java/html/search/classes_64.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['dbcollection',['DBCollection',['../classcom_1_1sequoiadb_1_1base_1_1DBCollection.html',1,'com::sequoiadb::base']]],
   ['dbcursor',['DBCursor',['../classcom_1_1sequoiadb_1_1base_1_1DBCursor.html',1,'com::sequoiadb::base']]],
+  ['dbdatacenter',['DBDataCenter',['../interfacecom_1_1sequoiadb_1_1base_1_1DBDataCenter.html',1,'com::sequoiadb::base']]],
   ['dblob',['DBLob',['../interfacecom_1_1sequoiadb_1_1base_1_1DBLob.html',1,'com::sequoiadb::base']]],
   ['dbquery',['DBQuery',['../classcom_1_1sequoiadb_1_1base_1_1DBQuery.html',1,'com::sequoiadb::base']]],
   ['domain',['Domain',['../classcom_1_1sequoiadb_1_1base_1_1Domain.html',1,'com::sequoiadb::base']]]
--- doc/out/java/html/search/functions_65.js
+++ doc/out/java/html/search/functions_65.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['enableimage',['enableImage',['../interfacecom_1_1sequoiadb_1_1base_1_1DBDataCenter.html#a6ca2e6e65ff0e3cc76ddc29d7cba55d8',1,'com::sequoiadb::base::DBDataCenter']]],
+  ['enablereadonly',['enableReadonly',['../interfacecom_1_1sequoiadb_1_1base_1_1DBDataCenter.html#a4c0b6dbd60235b01ccc3916d5cb9d142',1,'com::sequoiadb::base::DBDataCenter']]],
   ['evaljs',['evalJS',['../classcom_1_1sequoiadb_1_1base_1_1Sequoiadb.html#a23153434de0ea4c30ad3afd412a029c4',1,'com::sequoiadb::base::Sequoiadb']]],
   ['exec',['exec',['../classcom_1_1sequoiadb_1_1base_1_1Sequoiadb.html#a6f0060abf50c06b5027a8b8efaa08c53',1,'com::sequoiadb::base::Sequoiadb']]],
   ['execupdate',['execUpdate',['../classcom_1_1sequoiadb_1_1base_1_1Sequoiadb.html#ae1e44d555c1f45a8813458bd7a9fa07c',1,'com::sequoiadb::base::Sequoiadb']]],
