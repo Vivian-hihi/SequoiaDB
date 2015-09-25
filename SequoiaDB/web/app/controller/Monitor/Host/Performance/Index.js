4168f776fab8f7192130eff1c4bb0a7544d30646
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-09-25 01:43:37 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@20469 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/web/app/controller/Monitor/Host/Performance/Index.js
@@ -0,0 +1,9 @@
+(function(){
+   var sacApp = window.SdbSacManagerModule ;
+   //控制器
+   sacApp.controllerProvider.register( 'Monitor.Performance.Index.Ctrl', function( $scope, SdbRest, SdbFunction, InheritSize, FormModal ){
+
+   } ) ;
+   //记录视图
+
+}());
---
+++ SequoiaDB/web/app/controller/Monitor/Host/Preview/Index.js
@@ -0,0 +1,9 @@
+(function(){
+   var sacApp = window.SdbSacManagerModule ;
+   //控制器
+   sacApp.controllerProvider.register( 'Monitor.Preview.Index.Ctrl', function( $scope, SdbRest, SdbFunction, InheritSize, FormModal ){
+
+   } ) ;
+   //记录视图
+
+}());
