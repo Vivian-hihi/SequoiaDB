a7aa040a72d999827d5ac6e1da5e56f3511067aa
zhangsuonan <zhangsuonan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-09-26 05:49:20 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@20479 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/python/html/search/all_61.js
+++ doc/out/python/html/search/all_61.js
@@ -1,7 +1,10 @@
 var searchData=
 [
+  ['activate',['activate',['../classpysequoiadb_1_1datacenter_1_1datacenter.html#a2dea906bd2dcc5118e2919cc8551f50f',1,'pysequoiadb::datacenter::datacenter']]],
   ['aggregate',['aggregate',['../classpysequoiadb_1_1collection_1_1collection.html#acb1ad419c09f99b7053724b27fd69795',1,'pysequoiadb::collection::collection']]],
   ['as_5fdatetime',['as_datetime',['../classbson_1_1timestamp_1_1Timestamp.html#ae7f50ab8fcc084eb5639837b50e33e21',1,'bson::timestamp::Timestamp']]],
   ['as_5fdoc',['as_doc',['../classbson_1_1dbref_1_1DBRef.html#ac665852d185cb2b127e304cd3bfa6562',1,'bson::dbref::DBRef']]],
-  ['attach_5fcollection',['attach_collection',['../classpysequoiadb_1_1collection_1_1collection.html#adfb42a07a5fd91c726024c26ccb2e46e',1,'pysequoiadb::collection::collection']]]
+  ['attach_5fcollection',['attach_collection',['../classpysequoiadb_1_1collection_1_1collection.html#adfb42a07a5fd91c726024c26ccb2e46e',1,'pysequoiadb::collection::collection']]],
+  ['attach_5fgroups',['attach_groups',['../classpysequoiadb_1_1datacenter_1_1datacenter.html#a56cea477b19d1479281d42c39b4d3c2f',1,'pysequoiadb::datacenter::datacenter']]],
+  ['attach_5fnode',['attach_node',['../classpysequoiadb_1_1replicagroup_1_1replicagroup.html#a16c14624da00f1be2d5dcae8cf99f8ab',1,'pysequoiadb::replicagroup::replicagroup']]]
 ];
