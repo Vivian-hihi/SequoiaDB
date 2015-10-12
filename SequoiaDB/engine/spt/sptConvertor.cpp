b11364ade100af0ac0343aef1b3beeeafb83aa54
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-10-12 02:57:12 +0000

    windows下编译
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@20535 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/sptConvertor.cpp
+++ SequoiaDB/engine/spt/sptConvertor.cpp
@@ -1134,9 +1134,9 @@ BOOLEAN sptConvertor::_isValidOid( const CHAR *value )
       return FALSE ;
    for ( UINT32 i = 0; i < 24; ++i )
    {
-      if ( ! ( ( value[i] > '0' && value[i] < '9' ) ||
-               ( value[i] > 'a' && value[i] < 'f' ) ||
-               ( value[i] > 'A' && value[i] < 'F' ) ) )
+      if ( ! ( ( value[i] >= '0' && value[i] <= '9' ) ||
+               ( value[i] >= 'a' && value[i] <= 'f' ) ||
+               ( value[i] >= 'A' && value[i] <= 'F' ) ) )
       {
          return FALSE ;
       }
