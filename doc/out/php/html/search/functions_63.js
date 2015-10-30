6a6b56a5aedea827f4a81ca005b0593d63f52c23
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-10-30 09:28:07 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@20739 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/out/php/html/search/functions_63.js
+++ doc/out/php/html/search/functions_63.js
@@ -5,8 +5,7 @@ var searchData=
   ['count',['count',['../classSequoiaCL.html#aed703c2e2dabfe8361829bd366036689',1,'SequoiaCL']]],
   ['createcatagroup',['createCataGroup',['../classSequoiaDB.html#afe7ad660a46c30661d9e124913b2c027',1,'SequoiaDB']]],
   ['createidindex',['createIdIndex',['../classSequoiaCL.html#aea325953ed8ae327669dbf7177a3d891',1,'SequoiaCL']]],
-  ['createindex',['createIndex',['../classSequoiaCL.html#ad03a2a4b4e379c79ece0d2ed710ef19b',1,'SequoiaCL']]],
-  ['createindexoffline',['createIndexOffline',['../classSequoiaCL.html#a269767ef9d14f92bfe1f60a2a11db6b3',1,'SequoiaCL']]],
+  ['createindex',['createIndex',['../classSequoiaCL.html#aa76379a2bbc2e90f69d9ffcb6dcf40f0',1,'SequoiaCL']]],
   ['createnode',['createNode',['../classSequoiaGroup.html#a1d2278e3bedcd500009e79a13a56dcc2',1,'SequoiaGroup']]],
   ['current',['current',['../classSequoiaCursor.html#a384b05fd98b8a15f89cf26522d13fe03',1,'SequoiaCursor']]]
 ];
---
+++ doc/out/php/html/search/functions_74.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['truncate',['truncate',['../classSequoiaCL.html#a631c35754b9f079346b2a4bf3f6aa38d',1,'SequoiaCL']]]
+];
