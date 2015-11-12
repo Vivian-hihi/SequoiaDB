e8626efd7dd6421058c2293815650f01c2e26564
longyang <longyang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-11-12 01:33:26 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@20953 6121f079-4a18-41d3-8c30-8870a322f2c2

--- script/install_om.sh
+++ script/install_om.sh
@@ -4,14 +4,14 @@
 # script parameter description:
 # $1 for rootPath, ex: /opt/sequoiadb
 # $2 for cm port
-# $3 om svcname
-# $4 om dbpath
+# $3 om svcName
+# $4 om dbPath
 ###########################################
 
 rootPath=$1
 cmPort=$2
-svcname=$3
-dbpath=$4
+svcName=$3
+dbPath=$4
 restPort=$5
 installer_pathname=$6
 
@@ -26,34 +26,61 @@ sdbFile=$1/bin/sdb
 
 echo "rootPath=" $1
 echo "cmPort=" $2
-echo "svcname=" $3
-echo "dbpath=" $4
+echo "svcName=" $3
+echo "dbPath=" $4
 echo "restPort=" $5
 echo "installer_pathname=" $6
 
 # second to create om
-$sdbFile -s " var oma = new Oma('localhost', '${cmPort}' ); \
-             try { \
-                 oma.createOM( '${svcname}', '${dbpath}' ,{httpname: '${restPort}'}
-) ; \
-              } catch( e ) { \
-                 if ( e != SDBCM_NODE_EXISTED ) { \
-                    throw e ; } \
-              } \
-              try { \
-                 oma.startNode( '${svcname}' ) ; \
-              } catch( e ) { \
-                 oma.removeOM( '${svcname}' ) ; \
-                 throw e ; \
+$sdbFile -s " var _svcName = '${svcName}' ;                                              \
+              var _dbPath = '${dbPath}' ;                                                \
+              var _restPort = '${restPort}' ;                                            \
+              var _cmPort = '${cmPort}' ;                                                \
+              var arr = [] ;                                                             \
+              var num = 0 ;                                                              \
+              var canRemove = true ;                                                     \
+              arr = Sdbtool.listNodes( {type:'om', mode:'local', expand:true} ) ;        \
+              num = arr.size() ;                                                         \
+              if ( num == 0 ) {                                                          \
+                 arr = Sdbtool.listNodes( {type:'om', mode:'run', expand:true} ) ;       \
+                 num = arr.size() ;                                                      \
+              }                                                                          \
+              if ( num < 0 || num >= 2 ) {                                               \
+                 println( 'Error: there are ' + num + ' sdbom exist in localhost' ) ;    \
+                 throw SDB_SYS ;                                                         \
+              }                                                                          \
+              if ( num == 1 ) {                                                          \
+                 var obj = eval( '(' + arr.pos() + ')' ) ;                               \
+                 _svcName = obj['svcname'] ;                                             \
+                 _dbPath = obj['dbpath'] ;                                               \
+                 _restPort = obj['omname'] ;                                             \
+                 canRemove = false ;                                                     \
+              }                                                                          \
+              var oma = new Oma( '127.0.0.1', _cmPort ) ;                                \
+              try {                                                                      \
+                 oma.createOM( _svcName, _dbPath, {httpname: _restPort} ) ;              \
+              } catch( e ) {                                                             \
+                 if ( e == SDBCM_NODE_EXISTED ) {                                        \
+                    println( 'Warning: sdbom has existed in localhost' ) ;               \
+                 }                                                                       \
+                 throw e ;                                                               \
+              }                                                                          \
+              try {                                                                      \
+                  oma.startNode( _svcName ) ;                                            \
+              } catch( e ) {                                                             \
+                 if ( canRemove ) {                                                      \
+                    oma.removeOM( _svcName ) ;                                           \
+                 }                                                                       \
+                 throw e ;                                                               \
               } "
-#copy installer file to packet dir
-cp $6  $1/packet
 
+#check whether om is ok or not
 if [  $? != 0  ] ; 
 then
    echo "Create OM failed"
-   exit 1
 else
    echo "Create OM succeed"
-   exit 0
 fi
+
+#copy installer file to packet dir
+cp $6  $1/packet
