415024b165cd6a632ac3e8e4802e44e4f259c0fa
liuzhen <liuzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-05-21 04:55:57 +0000

    add sample of decimal using python driver
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@23197 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ client/samples/Python/sequoiadb/decimal.py
@@ -0,0 +1,59 @@
+import pysequoiadb
+from pysequoiadb import client
+from pysequoiadb.error import SDBEndOfCursor
+
+import bson
+from bson import Decimal
+
+
+if '__main__' == __name__:
+
+   d = Decimal( 100, 30)
+   d.parse("12345.678909876543212345678909877654321")
+
+   dmx = Decimal()
+   dmx.set_max()
+
+   dmn = Decimal()
+   dmn.set_min()
+
+   dzero = Decimal()
+   dzero.set_zero()
+
+   dn = Decimal()
+
+   doc = { "name":"hello", "key": d }
+
+   docs = [
+      { "name":"world", "key": dmx },
+      { "name":"fuck", "key": dmn },
+      { "name":"the", "key": dzero },
+      { "name":"decimal", "key": dn },
+   ]
+
+   db = client("localhost", 50000)
+
+   db.create_collection_space("foo")
+
+   db.foo.create_collection("bar")
+
+   db.foo.bar.insert(doc)
+   print 'insert a doc: %r' % doc
+   db.foo.bar.bulk_insert(0, docs)
+   print 'insert: %r' % docs
+
+
+   cr = db.foo.bar.query()
+
+   try:
+      while True:
+         record = cr.next()
+         print record
+         dd = record['key']
+         print 'is max : %r'  % dd.is_max() 
+         print 'is min : %r'  % dd.is_min() 
+         print 'is zero : %r' % dd.is_zero() 
+   except SDBEndOfCursor:
+      print "find all record"
+
+   db.drop_collection_space("foo")
