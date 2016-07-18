d386f5039c1624cca36b34c72f90b2446f718015
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-07-18 06:33:17 +0000

    fix: lob write, backup offline三者会产生死等  lob write拿了dms write lock, 并等待backup结束，而backup offline又等待lob write结束
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@23951 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/cls/clsShardSession.cpp
+++ SequoiaDB/engine/cls/clsShardSession.cpp
@@ -568,10 +568,7 @@ namespace engine
       rc = _reply ( &_replyHeader, buffObj.data(), buffObj.size() ) ;
 
    done:
-      if ( 0 == eduCB()->getLockItem(SDB_LOCK_DMS)->lockCount() )
-      {
-         eduCB()->writingDB( FALSE ) ;
-      }
+      eduCB()->writingDB( FALSE ) ;
       MON_END_OP( _pEDUCB->getMonAppCB() ) ;
       PD_TRACE_EXITRC ( SDB__CLSSHDSESS__ONOPMSG, rc ) ;
       return rc ;
