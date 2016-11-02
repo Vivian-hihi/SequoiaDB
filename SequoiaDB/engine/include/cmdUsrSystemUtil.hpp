16e3c61e2dae31b9ecc786a7474a861a9ca6a85c
wujiaming <wujiaming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-11-02 06:07:30 +0000

    SEQUOIADBMAINSTREAM-2034 更正 copyright 信息
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@25490 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/cmdUsrSystemUtil.hpp
+++ SequoiaDB/engine/include/cmdUsrSystemUtil.hpp
@@ -14,7 +14,7 @@
    You should have received a copy of the GNU Affero General Public License
    along with this program. If not, see <http://www.gnu.org/license/>.
 
-   Source File Name = usrSystemUtil.hpp
+   Source File Name = cmdUsrSystemUtil.hpp
 
    Descriptive Name =
 
