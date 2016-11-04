02389fd3a9c2bf80cd717295e619c8a108501a38
linyoubin <linyoubin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-11-04 05:17:52 +0000

    SEQUOIADBMAINSTREAM-2031 增加regex的说明， $type支持字符串格式
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@25521 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/mth/mthCommon.cpp
+++ SequoiaDB/engine/mth/mthCommon.cpp
@@ -69,6 +69,7 @@ namespace engine
       { "int32",                      NumberInt },
       { "timestamp",                  Timestamp },
       { "int64",                      NumberLong },
+      { "regex",                      RegEx },
       { "decimal",                    NumberDecimal },
       { "maxkey",                     MaxKey },
    } ;
