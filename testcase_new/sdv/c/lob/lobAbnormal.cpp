7c8c4df736a17358f5716290cb75f7e938a7368f
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-11-21 06:50:34 +0000

    更新lob用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@25840 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/sdv/c/lob/lobAbnormal.cpp
@@ -0,0 +1,175 @@
+﻿/************************************************************************
+*@Description : Test lob abnormal use:not exist lob,not closed lob,write
+*               lob with read mode.
+*@Modify List :
+*               2016-11-21   Liang xuewang   Init
+************************************************************************/
+
+#include <gtest/gtest.h>
+#include <client.h>
+#include <malloc.h>
+
+#define HOST      "localhost"
+#define SVCNAME   "11810"
+#define USR       ""
+#define PASSWD    ""
+
+TEST( lobAbnormalTest, NotExistLob )
+{
+   sdbConnectionHandle db = SDB_INVALID_HANDLE ;
+   sdbCSHandle cs         = SDB_INVALID_HANDLE ;
+   sdbCSHandle cl         = SDB_INVALID_HANDLE ;
+   sdbLobHandle lob       = SDB_INVALID_HANDLE ;
+   const char* CsName      = "lobAbnormalTestCs" ;
+   const char* ClName      = "lobAbnormalTestCl" ;
+   int rc = SDB_OK ;
+   
+   // connect sdb,create cs cl
+   rc = sdbConnect( HOST, SVCNAME, USR, PASSWD, &db ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to connect sdb" ;
+   rc = sdbCreateCollectionSpace( db, CsName, SDB_PAGESIZE_4K, &cs ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to create cs" ;
+   rc = sdbCreateCollection( cs, ClName, &cl ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to create cl" ; 
+   
+   // read not exist lob
+   bson_oid_t oid ;
+   bson_oid_gen( &oid ) ;
+   rc = sdbOpenLob( cl, &oid, SDB_LOB_READ, &lob ) ;
+   ASSERT_EQ( SDB_FNE, rc ) << "fail to test read not exist lob" ;
+   
+   // get not exist lob size
+   int size ;
+   rc = sdbGetLobSize( lob, &size ) ;
+   ASSERT_EQ( SDB_INVALIDARG, rc ) << "fail to test get not exist lob size" ;
+   
+   // get not exist lob create time
+   int mills ;
+   rc = sdbGetLobCreateTime( lob, &mills ) ;
+   ASSERT_EQ( SDB_INVALIDARG, rc ) << "fail to test get not exist lob create time" ;
+   
+   // close not exist lob
+   rc = sdbCloseLob( &lob ) ;
+   ASSERT_EQ( SDB_INVALIDARG, rc ) << "fail to test close not exist lob" ;
+   
+   // remove not exist lob
+   rc = sdbRemoveLob( lob, &oid ) ;
+   ASSERT_EQ( SDB_INVALIDARG, rc ) << "fail to remove not exist lob" ;
+   
+   // drop cs,release handle
+   rc = sdbDropCollectionSpace( db, CsName ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to drop cs in the end" ;
+   sdbReleaseCollection( cl ) ;
+   sdbReleaseCS( cs ) ;
+   sdbReleaseConnection( db ) ;
+}
+
+TEST( lobAbnormalTest, NotClosedLob )
+{
+   sdbConnectionHandle db = SDB_INVALID_HANDLE ;
+   sdbCSHandle cs         = SDB_INVALID_HANDLE ;
+   sdbCSHandle cl         = SDB_INVALID_HANDLE ;
+   sdbLobHandle lob       = SDB_INVALID_HANDLE ;
+   const char* CsName      = "lobAbnormalTestCs" ;
+   const char* ClName      = "lobAbnormalTestCl" ;
+   int rc = SDB_OK ;
+   
+   // connect sdb,create cs cl
+   rc = sdbConnect( HOST, SVCNAME, USR, PASSWD, &db ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to connect sdb" ;
+   rc = sdbCreateCollectionSpace( db, CsName, SDB_PAGESIZE_4K, &cs ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to create cs" ;
+   rc = sdbCreateCollection( cs, ClName, &cl ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to create cl" ; 
+   
+   // create lob buffer
+   int lobSize = 1024*1024*2 ;
+   char* lobWriteBuffer = ( char* )malloc( lobSize ) ;
+   if( !lobWriteBuffer )
+	{
+		printf( "out of memory for lob buffer.\n" ) ;
+		return ;
+	}
+	memset( lobWriteBuffer, 'a', lobSize ) ;
+	
+	// open lob,write lob,without close lob
+   bson_oid_t oid ;
+   bson_oid_gen( &oid ) ;
+   rc = sdbOpenLob( cl, &oid, SDB_LOB_CREATEONLY, &lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to open lob with create mode" ;
+   rc = sdbWriteLob( lob, lobWriteBuffer, lobSize ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to write lob"  ;
+   
+   // read lob when lob is not closed
+   rc = sdbOpenLob( cl, &oid, SDB_LOB_READ, &lob ) ;
+   ASSERT_EQ( SDB_LOB_IS_NOT_AVAILABLE, rc ) << "fail to test open lob when lob is not closed" ;
+   
+   // remove lob when lob is not closed
+   rc = sdbRemoveLob( cl, &oid ) ;
+   ASSERT_EQ( SDB_LOB_IS_NOT_AVAILABLE, rc ) << "fail to test remove lob when lob is not closed" ;
+   
+   // close lob
+   rc = sdbCloseLob( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to close lob in the end" ;
+   
+   // free buffer
+   free( lobWriteBuffer ) ;
+   
+   // drop cs,release handle
+   rc = sdbDropCollectionSpace( db, CsName ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to drop cs in the end" ;
+   sdbReleaseCollection( cl ) ;
+   sdbReleaseCS( cs ) ;
+   sdbReleaseConnection( db ) ;
+}
+
+TEST( lobAbnormalTest, WriteLobWithReadMode )
+{
+   sdbConnectionHandle db = SDB_INVALID_HANDLE ;
+   sdbCSHandle cs         = SDB_INVALID_HANDLE ;
+   sdbCSHandle cl         = SDB_INVALID_HANDLE ;
+   sdbLobHandle lob       = SDB_INVALID_HANDLE ;
+   const char* CsName     = "lobAbnormalTestCs" ;
+   const char* ClName     = "lobAbnormalTestCl" ;
+   int rc = SDB_OK ;
+   
+   // connect sdb,create cs cl
+   rc = sdbConnect( HOST, SVCNAME, USR, PASSWD, &db ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to connect sdb" ;
+   rc = sdbCreateCollectionSpace( db, CsName, SDB_PAGESIZE_4K, &cs ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to create cs" ;
+   rc = sdbCreateCollection( cs, ClName, &cl ) ;
+   ASSERT_EQ( rc, SDB_OK ) << "fail to create cl" ;
+   
+   // create lob buffer
+   int lobSize = 1024*1024*2 ;
+   char* lobWriteBuffer = ( char* )malloc( lobSize ) ;
+   if( !lobWriteBuffer )
+	{
+		printf( "out of memory for lob buffer.\n" ) ;
+		return ;
+	}
+	memset( lobWriteBuffer, 'a', lobSize ) ; 
+   
+   // write lob with read mode
+   bson_oid_t oid ;
+   bson_oid_gen( &oid ) ;
+   rc = sdbOpenLob( cl, &oid1, SDB_LOB_READ, &lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to open lob with read mode" ;
+   rc = sdbWriteLob( lob, lobWriteBuffer, lobSize ) ;
+   ASSERT_EQ( SDB_INVALIDARG, rc ) << "fail to test write lob with read mode" ;
+   
+   // close lob
+   rc = sdbCloseLob( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to close lob in the end" ;
+   
+   // free buffer
+   free( lobWriteBuffer ) ;
+   
+   // drop cs,release handle
+   rc = sdbDropCollectionSpace( db, CsName ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to drop cs in the end" ;
+   sdbReleaseCollection( cl ) ;
+   sdbReleaseCS( cs ) ;
+   sdbReleaseConnection( db ) ;
+}
