ce16c0ef35f7829a7bd730fa8b1b71e5218e190a
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-11-25 05:57:40 +0000

    新增匹配符随机200万数据，随机选取匹配符自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@25992 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/mthMatch_large_data/commlib.js
@@ -0,0 +1,221 @@
+﻿/************************************
+*@Description: insert data
+*@author:      zhaoyu
+*@createDate:  2015.5.20
+**************************************/
+function insertData( dbcl, insertData )
+{  
+   try
+   {
+      dbcl.insert(insertData);
+      println( "--insert data success" ) ;
+   }
+   catch(e)
+   {
+      throw buildException("insertData()",e,"insert", "insert success","insert fail");
+   }
+}
+
+/**********************************************************************
+@Description:  generate all kinds of types data randomly
+@author:       Ting YU
+@usage:        var rd = new commDataGenerator();
+               1.var recs = rd.getRecords( 300, "int", ['a','b'] );
+               2.var recs = rd.getRecords( 3, ["int", "string"] );
+               3.rd.getValue("string");
+***********************************************************************/
+function dataGenerator()
+{
+   this.getRecords =
+   function( recNum, dataTypes, fieldNames )
+   {
+      return getRandomRecords( recNum, dataTypes, fieldNames );
+   }
+   
+   this.getValue = 
+   function( dataType )
+   {
+      return getRandomValue( dataType );
+   }
+}
+
+function getRandomRecords( recNum, dataTypes, fieldNames )
+{
+  if( fieldNames === undefined ) { fieldNames = getRandomFieldNames(); }
+  if( dataTypes.constructor !== Array ) { dataTypes = [ dataTypes ]; }
+  
+  var recs = [];
+  var i = 0;
+  for( var i = 0; i < recNum; i++ )
+  {          
+	 // generate 1 record
+	 var rec = {};
+	 rec["No"] = i;
+	 for( var j in fieldNames )
+	 {  
+		// generate 1 filed
+		var rdn = parseInt( Math.random() * dataTypes.length );
+		if( rdn % 2 == 0){
+			var filedName = fieldNames[j];
+			var dataType = dataTypes[rdn];  //randomly get 1 data type
+			var filedVal =  getRandomValue( dataType );
+		}
+		if( filedVal !== undefined ) rec[filedName] = filedVal;
+	 } 
+	 
+	 recs.push( rec );
+  }   
+  return recs;
+}
+
+function getRandomFieldNames( minNum, maxNum )
+{
+  if( minNum == undefined ) { minNum = 0; }
+  if( maxNum == undefined ) { maxNum = 16; }
+  
+  var fieldNames = [];
+  var fieldNum = getRandomInt( minNum, maxNum );
+  
+  for( var i = 0; i < fieldNum; i++ )
+  {
+	 //get 1 field name
+	 var fieldName = "";
+	 var fieldNameLen = getRandomInt( 1, 9 );
+	 for( var j = 0; j < fieldNameLen; j++ )
+	 {
+		//get 1 char
+		var ascii = getRandomInt( 97, 123 ); // 'a'~'z'
+		var c = String.fromCharCode( ascii );
+		fieldName += c;
+	 }
+	 fieldNames.push( fieldName );
+  }
+  
+  return fieldNames;
+}
+
+/**********************************************************************
+@Description:  generate all kinds of types data randomly
+@author:       Ting YU
+@usage:        var rd = new commDataGenerator();
+               1.var recs = rd.getRecords( 300, "int", ['a','b'] );
+               2.var recs = rd.getRecords( 3, ["int", "string"] );
+               3.rd.getValue("string");
+***********************************************************************/
+function getRandomValue( dataType )
+{
+  var value = undefined;
+  
+  switch( dataType )
+  {
+	 case "int":
+		value = getRandomInt( -2147483648, 2147483647 );
+		break;
+	 case "long":
+		value = getRandomLong( -922337203685477600, 922337203685477600 );
+		break;
+	 case "float":
+		value = getRandomFloat( -999999, 999999 );
+		break;
+	 case "array":
+		value = getRandomArray();
+		break; 
+	 case "non-existed":
+		break;                 
+  }
+  
+  return value;
+}
+
+function getRandomInt( min, max ) // [min, max)
+   {
+      var range = max - min;
+      var value = min + parseInt( Math.random() * range );
+      return value;
+   }
+   
+function getRandomLong( min, max )
+{
+  var value = getRandomInt( min, max );
+  return NumberLong(value);
+}
+
+function getRandomFloat( min, max )
+{
+  var range = max - min;
+  var value = min + Math.random() * range;
+  return value;
+}
+
+function getRandomArray()
+{
+  var arr = [];   
+  var dataTypes= [ "int", "long", "float" ];
+				   
+  var arrLen = getRandomInt( 1, 5 );  
+  for( var i = 0; i < arrLen; i++ )
+  {
+	 var dataType = dataTypes[ parseInt( Math.random() * dataTypes.length ) ];  //randomly get 1 data type 
+	 
+	 var elem = getRandomValue( dataType );
+	 arr.push( elem );
+  }
+  
+  return arr;
+}
+ 
+function genRandomFindCondition( matches, fieldNames )
+{
+	var recs = [];
+	// generate 1 record     
+	for( var j in fieldNames )
+	{  	
+		var filedName = fieldNames[j];
+		var rec = {}; 
+		// generate 1 filed
+		var rdn = parseInt( Math.random() * matches.length );
+		if( rdn % 2 === 0){
+			var match = matches[rdn];
+			rec[filedName] = match;
+			recs.push( rec ); 
+		}
+	}   
+  return recs;
+}
+
+function getFindCondition( recNum, dataTypes, matches )
+{
+  if( dataTypes.constructor !== Array ) { dataTypes = [ dataTypes ]; }
+  
+  var recs = [];
+  for( var i = 0; i < recNum; i++ )
+  {          
+	 // generate 1 record
+	 var rec = {};      
+	 for( var j in matches )
+	 {  
+		// generate 1 filed
+		var match = matches[j];
+
+		var dataType = dataTypes[ parseInt( Math.random() * dataTypes.length ) ];  //randomly get 1 data type
+		var filedVal =  getRandomValue( dataType );
+		
+		if( filedVal !== undefined ) rec[match] = filedVal;       
+	 } 
+	 
+	 recs.push( rec );
+  }   
+  return recs;
+}
+
+function mergeObj(left,right)
+{
+	var obj ={}
+	for(var k in left)
+		obj[k] = left[k];
+	
+	for (var k in right)
+		obj[k] = right[k];
+	
+	return obj;
+}
---
+++ testcase_new/story/js/mthMatch_large_data/randomFind_9246.js
@@ -0,0 +1,160 @@
+/************************************
+*@Description: random data,random matches,find data,only check count
+*@author:      zhaoyu
+*@createdate:  2016.11.3
+*@testlinkCase:seqDB-9246 
+**************************************/
+function main()
+{
+   //clean environment before test
+   commDropCL( db, COMMCSNAME, COMMCLNAME, true, true,"drop CL in the beginning" ) ;
+   
+   //create cl
+   var dbcl = commCreateCL( db, COMMCSNAME, COMMCLNAME, 0);
+   
+   try
+   {
+      //insert random numberical data
+      var fieldNames = ['fieldName1','fieldName2','fieldName3','fieldName4','fieldName5','fieldName6'];
+      
+      for(var i= 0; i< 200;i++){
+         var rd = new dataGenerator();
+         var recs = rd.getRecords( 10000, ["int", "long", "float", "array"], fieldNames );
+         insertData(dbcl, recs);
+      }
+      
+      var loopNum = 10;
+      for(var i= 0; i< loopNum;i++){
+   	   //create index
+   	   commCreateIndex( dbcl, "fieldName1Index", {fieldName1:1});
+   	   commCreateIndex( dbcl, "fieldName2Index", {fieldName2:-1});
+   	   commCreateIndex( dbcl, "fieldName3Index", {fieldName3:1});
+   	   commCreateIndex( dbcl, "fieldName4Index", {fieldName4:-1});
+   	   commCreateIndex( dbcl, "fieldName5Index", {fieldName5:1});
+   	   commCreateIndex( dbcl, "fieldName6Index", {fieldName6:-1});
+   	   
+   	   //set all matches support index scan
+   	   matches1 = [ "$et", "$gt", "$gte", "$lt", "$lte", "$ne" ];
+   	   matches2 = [ "$in", "$all" ];
+   	   
+   	   var findCondition1 = getFindCondition( 1, ["int", "long", "float" ], matches1 );
+   	   var findValue2 = getRandomArray();
+   	   var findCondition2 = getFindCondition( 1, "array", matches2 );
+   	   
+   	   findCondition3 = [ {$exists:0}, {$isnull:1} ];
+   	   
+   	   //convert array to object
+   	   findConditionObj1 = arrToObj( findCondition1 );
+   	   findConditionObj2 = arrToObj( findCondition2 );
+   	   findConditionObj3 = arrToObj( findCondition3 );
+   	   
+   	   var obj1 = mergeObj(findConditionObj1, findConditionObj2);
+   	   var obj2 = mergeObj(obj1, findConditionObj3);
+   	   
+   	   var findConditions = [];
+   	   var cnt = 0;
+   	   for( var j in obj2 ){ 
+   		  var subcond = {};
+   		  subcond[j] = obj2[j]
+   		  findConditions.push(subcond);
+   	   }
+   	   
+   	   //generate random find condition
+   	   var randomCondition = genRandomFindCondition( findConditions, fieldNames );
+   	   var randomConditionObj = {$and:randomCondition};
+   	   println("randomCondition:"+ JSON.stringify(randomConditionObj));
+   		
+   	   //get index scan result
+   	   var ixScanCursor = dbcl.find( randomConditionObj, null ).sort( { _id: 1 } );
+   	   
+   	   //get index scan explain
+   	   var ixScanExplain = dbcl.find(randomConditionObj,null).explain();
+   	   var ixScanExplainRecs = [];
+   	   while( ixScanExplain.next() )
+   	   {
+   			ixScanExplainRecs.push( ixScanExplain.current().toObj() );
+   	   }
+   	   
+   	   //drop index
+   	   commDropIndex(dbcl, "fieldName1Index");
+   	   commDropIndex(dbcl, "fieldName2Index");
+   	   commDropIndex(dbcl, "fieldName3Index");
+   	   commDropIndex(dbcl, "fieldName4Index");
+   	   commDropIndex(dbcl, "fieldName5Index");
+   	   commDropIndex(dbcl, "fieldName6Index");
+   	   
+   	   //get table scan result
+   	   var tbScanCursor = dbcl.find( randomConditionObj, null ).sort( { _id: 1 } );
+   	   
+   	   //get table scan explain
+   	   var tbScanExplain = dbcl.find(randomConditionObj,null).explain();
+   	   var tbScanExplainRecs = [];
+   	   while( tbScanExplain.next() )
+   	   {
+   			tbScanExplainRecs.push( tbScanExplain.current().toObj() );
+   	   }
+   	   
+   	   //check count
+         if( ixScanCursor.count().toString() !== tbScanCursor.count().toString() )
+         {
+            println("ixScanCursor.count():"+ixScanCursor.count());
+            println("tbScanCursor.count():"+tbScanCursor.count());
+         	println("\n\nixScanExplainRecs:"+JSON.stringify(ixScanExplainRecs));
+         	println("\n\ntbScanExplainRecs:"+JSON.stringify(tbScanExplainRecs));
+         	throw buildException("check count", null, "", ixScanCursor.count(), tbScanCursor.count());
+         }
+         
+         //check every records every fields,tbScanRecs as compare source
+         /*while( ixScanCursor.next() && tbScanCursor.next() )
+         {
+      		var ixScanRec = ixScanCursor.current().toObj();
+      		var tbScanRec = tbScanCursor.current().toObj();
+      		println("\nixScanRec = "+JSON.stringify(ixScanRec)+"\n\ntbScanRec = "+JSON.stringify(tbScanRec));
+      		if( JSON.stringify(ixScanRec) !== JSON.stringify(tbScanRec) )
+   			{
+   				//println("\n\nallDataRecs:" + JSON.stringify(allDataRecs));
+   				println("\n\nixScanExplainRecs:"+JSON.stringify(ixScanExplainRecs));
+   				println("\n\ntbScanExplainRecs:"+JSON.stringify(tbScanExplainRecs));
+   				println("\nixScanRec = "+JSON.stringify(ixScanRec)+"\n\ntbScanRec = "+JSON.stringify(tbScanRec));
+   				throw buildException("check record", "rec ERROR");
+   			}
+         }*/
+      }
+   }catch(e){
+      throw e;
+   }
+}
+main()
+ 
+ /************************************
+*@Description: unset the order for arr elements.
+*@author:      zhaoyu 
+*@createDate:  2016/7/19
+*@parameters:               
+**************************************/
+function getRdmDataFromArr(arr)
+{
+   //unset the arr order
+   var newArr = arr.sort(function(){return Math.random()-0.5});
+   
+   //convert array to object
+   var obj = {};
+   for(var i in newArr){
+      for (var k in newArr[i])
+      {
+         obj[k] = newArr[i][k];
+      } 
+   }
+   return obj;
+}
+
+function arrToObj( arr ){
+   var obj = {};
+   for(var i in arr){
+      for (var k in arr[i])
+      {
+         obj[k] = arr[i][k];
+      } 
+   }
+   return obj;
+}
