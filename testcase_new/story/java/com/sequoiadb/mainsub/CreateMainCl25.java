d43ccb859474e4721909d96d16b67cc9db47c5ff
zengxianquan <zengxianquan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-12-14 01:39:25 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@26224 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/com/sequoiadb/mainsub/CreateMainCl25.java
@@ -0,0 +1,148 @@
+package com.sequoiadb.mainsub;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.SdbTestBase;
+
+/**
+ * FileName: CreateMainCl25.java
+ * test content:  创建子表时指定hash分区时的ShardingKey与主表相同 
+ * testlink case: seqDB-25
+ * @author zengxianquan
+ * @date 2016年12月9日
+ * @version 1.00
+ */
+public class CreateMainCl25 extends SdbTestBase{
+		
+	private Sequoiadb db = null;
+	private String csName = null;
+	private CollectionSpace cs = null;
+	private String mainclName = "maincl_25";
+	private String subclName = "subcl_25";
+	
+	@BeforeClass
+	public void setUp(){
+		System.out.println(this.getClass().getName()+" begin at "
+				+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+		try{
+			db=new Sequoiadb(SdbTestBase.coordUrl,"","");
+		}catch(BaseException e){
+			Assert.assertTrue(false, "connect  failed,"+SdbTestBase.coordUrl+e.getMessage());
+		}	
+	}
+	@AfterClass
+	public void tearDown(){
+		try{
+			cs.dropCollection(mainclName);
+		}catch(BaseException e){
+			Assert.fail("failed to drop maincl "+"ErrorMsg:\n" +e.getMessage());
+		}finally{
+			System.out.println("End to run " + this.getClass().getName() 
+						+ ", end in: " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+			db.disconnect();
+		}
+	}
+	
+	@Test
+	public void testMainclByAppointSameShardingKey(){
+		csName=SdbTestBase.csName;
+		try{
+			cs=db.getCollectionSpace(csName);
+		}catch(BaseException e){
+			Assert.fail("failed to get collectionspace"+ "ErrorMsg:\n" +e.getMessage());
+		}
+		createMaincl();
+		attachSubcl();
+		checkCUID();		
+	}
+	
+	public void createMaincl(){
+		BSONObject options=new BasicBSONObject();
+		options.put("IsMainCL", true);
+		BSONObject opt=new BasicBSONObject();
+		opt.put("time", 1);
+		options.put("ShardingKey", opt);
+		try{
+			cs.createCollection(mainclName,options);			
+		}catch(BaseException e){
+			Assert.fail("failed to create maincl "+e.getMessage());
+		}
+	}
+	
+	public void attachSubcl(){
+		BSONObject subclOptions=new BasicBSONObject();
+		BSONObject subOpt=new BasicBSONObject();
+		subOpt.put("time", 1);
+		subclOptions.put("ShardingType","hash");
+		subclOptions.put("ShardingKey", subOpt);
+		try{
+			cs.createCollection(subclName,subclOptions);			
+		}catch(BaseException e){
+			Assert.fail("failed to create subclName "+e.getMessage());
+		}
+		BSONObject attachOpt=new BasicBSONObject();
+		BSONObject lowBound=new BasicBSONObject();
+		BSONObject upBound=new BasicBSONObject();
+		lowBound.put("time", 0);
+		upBound.put("time", 100);
+		attachOpt.put("LowBound", lowBound);
+		attachOpt.put("UpBound", upBound);
+		try{
+			cs.getCollection(mainclName).attachCollection(csName+"."+subclName, attachOpt);			
+		}catch(BaseException e){
+			Assert.fail("failed to attach "+e.getMessage());
+		}
+	}
+	
+	public void checkCUID(){
+		DBCollection maincl=null;
+		try{
+			maincl=cs.getCollection(mainclName);			
+		}catch(BaseException e){
+			Assert.fail("failed to get collection");
+		}
+		try{		
+			maincl.insert("{time:10,no:10,test:'test'}");
+			
+			DBCursor res=maincl.query();
+			while(res.hasNext()){
+				BSONObject dataRes=res.getNext();
+				if((!dataRes.get("time").equals(10)) 
+						||(!dataRes.get("no").equals(10))
+						||(!dataRes.get("test").equals("test"))){
+					Assert.fail("failed to query data ");
+				}
+			}
+			
+			maincl.update("{time:10}","{$set:{'test':'update'}}",null);
+			DBCursor updateRes=maincl.query();
+			while(updateRes.hasNext()){
+				BSONObject dataRes=updateRes.getNext();
+				if((!dataRes.get("time").equals(10)) 
+						||(!dataRes.get("no").equals(10))
+						||(!dataRes.get("test").equals("update"))){
+					Assert.fail("failed to query data ");
+				}
+			}
+			
+			maincl.delete("{time:10}");
+			Assert.assertEquals(maincl.getCount(),0,"failed to delete data");
+			
+		}catch(BaseException e){
+				Assert.fail("failed to CUID :"+e.getMessage());
+		}
+	}
+}
---
+++ testcase_new/story/java/com/sequoiadb/mainsub/CreateMainCl26.java
@@ -0,0 +1,148 @@
+package com.sequoiadb.mainsub;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.SdbTestBase;
+/**
+ * FileName: CreateMainCl26.java
+ * test content: 创建子表时指定range分区时的ShardingKey与主表相同 
+ * testlink case: seqDB-26
+ * @author zengxianquan
+ * @date 2016年12月9日
+ * @version 1.00
+ */
+public class CreateMainCl26 extends SdbTestBase{
+		
+	private Sequoiadb db = null;
+	private String csName = null;
+	private String mainclName = "maincl_26";
+	private String subclName = "subcl_26";
+	private CollectionSpace cs = null;
+
+	@BeforeClass
+	public void setUp(){
+		System.out.println(this.getClass().getName()+" begin at "
+				+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+		try{
+			db=new Sequoiadb(SdbTestBase.coordUrl,"","");
+		}catch(BaseException e){
+			Assert.assertTrue(false, "connect  failed,"+SdbTestBase.coordUrl+e.getMessage());
+		}	
+	}
+	@AfterClass
+	public void tearDown(){
+		try{
+			cs.dropCollection(mainclName);
+		}catch(BaseException e){
+			Assert.fail("failed to drop maincl "+"ErrorMsg:\n" +e.getMessage());
+		}finally{
+			System.out.println("End to run " + this.getClass().getName() 
+						+ ", end in: " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+			db.disconnect();
+		}
+	}
+	@Test
+	public void testCreateMainRangSubclBySameShardingKey(){
+		csName=SdbTestBase.csName;
+		try{
+			cs=db.getCollectionSpace(csName);			
+		}catch(BaseException e){
+			Assert.fail("failed to get collectionSpace "+e.getMessage());
+		}
+		createMaincl();
+		attachSubcl();
+		checkCUID();	
+	}
+	
+	public void createMaincl(){	
+		BSONObject options=new BasicBSONObject();
+		options.put("IsMainCL", true);
+		BSONObject opt=new BasicBSONObject();
+		opt.put("time", 1);
+		options.put("ShardingKey", opt);
+		try{
+			cs.createCollection(mainclName,options);
+		}catch(BaseException e){
+			Assert.fail("Failed to create maincl " + "ErrorMsg:\n"+e.getMessage());
+		}
+	}
+	public void attachSubcl(){	
+		BSONObject subclOptions=new BasicBSONObject();
+		BSONObject subOpt=new BasicBSONObject();
+		subOpt.put("time", 1);
+		subclOptions.put("ShardingType","range");
+		subclOptions.put("ShardingKey", subOpt);
+		try{
+			cs.createCollection(subclName, subclOptions);
+		}catch(BaseException e){
+			Assert.fail("Failed to create subcl  "
+					+ "ErrorMsg:\n" +e.getMessage());
+		}
+		BSONObject attachOpt=new BasicBSONObject();
+		BSONObject lowBound=new BasicBSONObject();
+		BSONObject upBound=new BasicBSONObject();
+		lowBound.put("time", 0);
+		upBound.put("time", 100);
+		attachOpt.put("LowBound", lowBound);
+		attachOpt.put("UpBound", upBound);
+		try{
+			cs.getCollection(mainclName).attachCollection(csName+"."+subclName, attachOpt);			
+		}catch(BaseException e){
+			Assert.fail("failed to attach "+ "ErrorMsg:\n"+e.getMessage());
+		}
+	}
+	public void checkCUID(){
+		DBCollection maincl=null;
+		try{
+			maincl=cs.getCollection(mainclName);			
+		}catch(BaseException e){
+			Assert.fail("failed to get collection");
+		}
+		try{		
+			maincl.insert("{time:10,no:10,test:'test'}");
+			
+			DBCursor res=maincl.query();
+			while(res.hasNext()){
+				BSONObject dataRes=res.getNext();
+				if((!dataRes.get("time").equals(10)) 
+						||(!dataRes.get("no").equals(10))
+						||(!dataRes.get("test").equals("test"))){
+					Assert.fail("failed to query data ");
+				}
+			}
+			
+			maincl.update("{time:10}","{$set:{'test':'update'}}",null);
+			DBCursor updateRes=maincl.query();
+			while(updateRes.hasNext()){
+				BSONObject dataRes=updateRes.getNext();
+				if((!dataRes.get("time").equals(10)) 
+						||(!dataRes.get("no").equals(10))
+						||(!dataRes.get("test").equals("update"))){
+					Assert.fail("failed to query data ");
+				}
+			}
+			
+			maincl.delete("{time:10}");
+			Assert.assertEquals(maincl.getCount(),0,"failed to delete data");
+			
+		}catch(BaseException e){
+				Assert.fail("failed to CUID :"+e.getMessage());
+		}
+	}
+	
+	
+}
