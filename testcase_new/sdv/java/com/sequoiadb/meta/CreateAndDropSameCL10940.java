d29678eb4126a4b8781d669d276b328839169011
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-01-05 09:42:41 +0000

    新增元数据操作用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@26848 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/sdv/java/com/sequoiadb/meta/CreateAndDropSameCL10940.java
@@ -0,0 +1,141 @@
+package com.sequoiadb.meta;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.CommLib;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbThreadBase;
+
+/**
+* FileName: CreateAndDropCL10940.java
+* test content:concurrent creation and deletion of same cl
+* testlink case:seqDB-10940
+* @author wuyan
+    * @Date    2016.9.12
+* @version 1.00
+*/
+public class CreateAndDropSameCL10940 extends SdbTestBase {	
+	
+	private String clName = "cl10940";
+	private static Sequoiadb sdb = null;
+	private Random random = new Random();	
+	String clGroupName = null;
+	
+	@BeforeClass
+	public void setUp( ){
+		System.out.println(this.getClass().getName()+" begin at "
+				+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+		try{
+			sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		}catch(BaseException e){			
+			Assert.assertTrue(false,"connect %s failed,"+SdbTestBase.coordUrl+e.getMessage());
+		}			
+		
+		//randomly take a group name to create cl
+		CommLib commlib = new CommLib();
+		ArrayList<String> groupsName = commlib.getDataGroupNames(sdb);
+		clGroupName = groupsName.get(random.nextInt(3));
+	}
+	
+	@Test
+	public void createAndDropCL10940(){
+		CreateCLThread createCLThread = new CreateCLThread();
+		DropCLThread dropCLThread = new DropCLThread();		
+		
+		createCLThread.start();
+		dropCLThread.start();
+		
+		 if(!(createCLThread.isSuccess() && dropCLThread.isSuccess())){
+	            List<Exception> exceptions = new ArrayList<Exception>();
+	            exceptions.addAll(createCLThread.getExceptions());
+	            exceptions.addAll(dropCLThread.getExceptions());
+	            
+	            String errMsg = "";
+	            for(int i = 0; i < exceptions.size(); i++){
+	                exceptions.get(i).printStackTrace(); 
+	                errMsg += exceptions.get(i).getMessage() + "\n";
+	            }
+	            Assert.fail(errMsg);
+	        }		
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void tearDown(){
+		try{
+			CollectionSpace cs = sdb.getCollectionSpace(SdbTestBase.csName);
+			if(cs.isCollectionExist(clName)){
+				cs.dropCollection(clName);
+			}			
+			sdb.disconnect();
+			System.out.println(this.getClass().getName()+" end at "
+					+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+		}catch(BaseException e){			
+			Assert.assertTrue(false,"clean up failed:"+e.getMessage());
+		}
+	}	
+
+	
+	class CreateCLThread extends SdbThreadBase {
+        @Override
+        public void exec() throws BaseException{
+            Sequoiadb db1 = null;
+            CollectionSpace cs1 = null;
+            DBCollection dbcl = null;
+            
+            try{
+                db1 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+                cs1 = db1.getCollectionSpace(SdbTestBase.csName);
+                dbcl = cs1.createCollection(clName); 
+                checkCreateCl(dbcl);
+            }catch(BaseException e){
+            	Assert.assertEquals(-23,e.getErrorCode(),e.getMessage());
+            }finally{
+            	if(db1 != null){
+            		db1.disconnect();            		
+            	}                
+            }
+        }
+    }
+	
+	class DropCLThread extends SdbThreadBase {
+        @Override
+        public void exec() throws BaseException{
+            Sequoiadb db2 = null;
+            CollectionSpace cs2 = null;     
+            try{
+                db2 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+                cs2 = db2.getCollectionSpace(SdbTestBase.csName);    
+                cs2.dropCollection(clName);   
+                Assert.assertNull(cs2.getCollection(clName));
+            }catch(BaseException e){
+            	Assert.assertEquals(-23,e.getErrorCode(),"drop cl fail "+e.getMessage());
+            }finally{
+            	if(db2 != null){
+            		db2.disconnect();            		
+            	}                
+            }
+        }
+    }
+		
+	public void checkCreateCl(DBCollection cl){
+		try{
+			cl.insert("{a:1}");	
+			Assert.assertEquals(cl.getCount(), 1);
+		}catch(BaseException e){
+			Assert.assertTrue(false,"insert fail "+e.getMessage());
+		}
+	}
+}
