cf2f57270777c4102964afab88cda7ad62564921
wangwenjing <wangwenjing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-01-12 09:24:27 +0000

    SEQUOIADBMAINSTREAM-2148
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27087 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/php/sdb/php_sdb_forcesession_7635_Test.php
+++ testcase_new/driver/php/sdb/php_sdb_forcesession_7635_Test.php
@@ -9,61 +9,103 @@ define('Cur_Path', dirname(__FILE__));
 include_once Cur_Path.'/../global.php';
 class forceSessionTest extends PHPUnit_Framework_TestCase
 {
-    protected $db;
-    protected $testdb;
-    protected function setUp()
-    {
-       $this->db = new Sequoiadb();
-       $err = $this->db->connect( globalParameter::getHostName() , 
-                                globalParameter::getCoordPort() ) ;     
-       $this->assertEquals( 0, $err['errno'] , '连接coord失败') ;
-       
-       $cursor = $this->db->list( SDB_LIST_GROUPS );
-       $err = $this->db->getError() ;
-       if ( $err['errno'] != -159 ){
-          while( $record = $cursor->next() ){
-             if ( $record['GroupName'] == "SYSCatalogGroup" ||
-                  $record['GroupName'] == "SYSCoord" )
-             {
-                continue;
-             }
-             var_dump( $record['Group'][0]['HostName'] );
-             var_dump( $record['Group'][0]['Service'][0]["Name"] );
-             
-             $this->testdb = new Sequoiadb();
-             $err = $this->testdb->connect( $record['Group'][0]['HostName'].":".$record['Group'][0]['Service'][0]["Name"] );
-             $this->assertEquals( 0, $err['errno'], 
-                   '连接'.$record['Group'][0]['HostName'].':'.$record['Group'][0]['Service'][0]["Name"].'失败' ) ;
-             break;
-          }
-       }else{
-          $this->assertEquals( -159, $err['errno'], 'list(SDB_LIST_GROUPS) 失败' );
-          $this->testdb = $this->db;
-       }
-    }
+   protected static $db ;
+   protected static $err ;
+   protected static $nodes ;
+   protected $testdb ;
 
-    public function testForceSession()
-    {
-        $sessionID = -1 ;
-        $cursor = $this->testdb->list( SDB_LIST_SESSIONS_CURRENT ) ;
-        $err = $this->testdb->getError() ;
-        $this->assertEquals( 0, $err['errno'], 'list( SDB_LIST_SESSIONS_CURRENT ) 失败' ) ;
-        $find = False;
-        while( $record = $cursor->next() )
-        {
-            $sessionID = $record['SessionID'] ;
-            $find = True;
-        }
-        $this->assertEquals( $find, True, 'list(SDB_LIST_SESSIONS_CURRENT) 返回为空' ) ;
- 
-        $err = $this->testdb->forceSession( $sessionID ) ;
-        $this -> assertEquals( -16, $err['errno'], 'forceSession错误' );
-    }
+   public static function setUpBeforeClass()
+   {
+      self::$db = new Sequoiadb() ;
+      self::$err = self::$db->connect( globalParameter::getHostName() ,
+                                       globalParameter::getCoordPort() ) ;
+      if ( self::$err['errno'] != 0 )
+      {
+         return ;
+      }
+      
+      $cursor = self::$db->list( SDB_LIST_GROUPS );
+      self::$err = self::$db->getError() ;
+      self::$nodes = array() ;
+      if ( self::$err['errno'] == 0 )
+      {
+         while( $record = $cursor->next() )
+         {
+            if ( $record['GroupName'] == "SYSCatalogGroup" ||
+                 $record['GroupName'] == "SYSCoord" )
+            {
+               continue;
+            }
+            array_push( self::$nodes, 
+                $record['Group'][0]['HostName'].":".$record['Group'][0]['Service'][0]["Name"] ) ;
+         }
+      }
+   }
+   
+   protected function setUp()
+   {
+      if ( self::$err['errno'] != 0 )
+      {
+         $this->markTestSkipped( self::$err['errno'] ) ;
+      }
+      
+      $this->testdb = new Sequoiadb();
+      $err = $this->testdb->connect( self::$nodes[0] );
+      $this->assertEquals( 0, $err['errno'],
+                   '连接'.self::$nodes[0].'失败' ) ;
+   }
+    
+   public function testForceSession()
+   {
+      $sessionID = -1 ;
+      $cursor = $this->testdb->list( SDB_LIST_SESSIONS_CURRENT ) ;
+      $curerr = $this->testdb->getError() ;
+      $this->assertEquals( 0, $curerr['errno'], 'list( SDB_LIST_SESSIONS_CURRENT ) 失败' ) ;
+      $find = False;
+      while( $record = $cursor->next() )
+      {
+         $sessionID = $record['SessionID'] ;
+         $find = True;
+      }
+      $this->assertEquals( $find, True, 'list(SDB_LIST_SESSIONS_CURRENT) 返回为空' ) ;
+      $curerr = $this->testdb->forceSession( $sessionID ) ;
+      $this -> assertEquals( -16, $curerr['errno'], 'forceSession错误' );
+   }
+   
+   public function testForceSessionWithOption()
+   {
+      $cursor = $this->testdb -> execSQL( 'select * from $SNAPSHOT_SESSION where Status="Running" and Type="SyncClockWorker"' ) ;
+      $isFind = false ;
+      $nodename = '' ;
 
-    protected function tearDown()
-    {
-        $err = $this->db->close();
-        $this->assertEquals( 0, $err['errno'] ) ;
-    }
-}
+      while( $record = $cursor -> next() )
+      {
+         $nodename  = $record['NodeName'] ;
+         $sessionID = $record['SessionID'] ;
+         $isFind = true ;
+         break ;
+      }
+
+      if( $isFind )
+      {
+         //有session
+         $hostname = explode( ':', $nodename ) ;
+         $svcname  = $hostname[1] ;
+         $hostname = $hostname[0] ;
+         $err = $this->testdb -> forceSession( $sessionID, array( 'HostName' => $hostname, 'svcname' => $svcname ) ) ;
+         $this -> assertEquals( -63, $err['errno'], 'forceSession错误' ) ;
+      }
+   }
+   
+   protected function tearDown()
+   {
+      $err = $this->testdb->close();
+   }
+   
+   public static function tearDownAfterClass()
+   {
+      $err = self::$db->close();
+   }
+};
 ?>
+
