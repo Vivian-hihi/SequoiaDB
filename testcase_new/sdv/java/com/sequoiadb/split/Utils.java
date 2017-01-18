d2942c4077e6349af447e762f61d154ad9ffca87
huangqiaohui <huangqiaohui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-01-18 06:09:21 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27201 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/com/sequoiadb/split/Utils.java
+++ testcase_new/sdv/java/com/sequoiadb/split/Utils.java
@@ -1,4 +1,4 @@
-package com.sequoiadb.split;
+package com.sequoiadb.serial;
 
 import java.util.ArrayList;
 
@@ -21,7 +21,6 @@ import com.sequoiadb.testcommon.CommLib;
  * @version 1.00
  *
  */
-
 
 public class Utils {
 
@@ -132,7 +131,11 @@ public class Utils {
 			}
 		}
 		String str = stackBuffer.toString();
-		return str.substring(0, str.length() - 2);
+		if (str.length() >= 2) {
+			return str.substring(0, str.length() - 2);
+		} else {
+			return str;
+		}
 	}
 
 	public static ArrayList<String> getGroupName(Sequoiadb sdb, String csName, String clName) throws BaseException {
