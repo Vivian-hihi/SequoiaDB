fa71b8a8b2ae3252a3eb53fb457cd03be3dcbf82
wangwenjing <wangwenjing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-02-10 04:58:52 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27435 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/performance/tpcc/run/misc/test1.py
+++
@@ -1,7 +0,0 @@
-#!/bin/env python
-import os
-from os.path import join, getsize
-for root, dirs, files in os.walk('/data/disk6/testdata'):
-   a = root
-   print root, "consumes",
-   print "a=%s"%a
