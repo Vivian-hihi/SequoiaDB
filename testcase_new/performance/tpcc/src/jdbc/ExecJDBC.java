1cfee28ec8e134cf50236a8771c66196aab51c24
wangwenjing <wangwenjing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-02-17 06:29:38 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27537 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/performance/tpcc/src/jdbc/ExecJDBC.java
+++ testcase_new/performance/tpcc/src/jdbc/ExecJDBC.java
@@ -131,7 +131,7 @@ public class ExecJDBC {
             sql += "\'" ;
             sql += ini.getProperty( iniKey ) ;
             sql += "\'" ;
-            sql += ");" ;
+            sql += ",preferedinstance \'M\');" ;
         }
 
         return sql ;
