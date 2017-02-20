e4670438bf6895a0f206a30e3baeabdfe956db5a
huangqiaohui <huangqiaohui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-02-20 06:39:42 +0000

    问题是由主备节点暂时不一致导致的，修改检查结果从主节点读取，并且在检查压缩过程中加入等待压缩字典的建立代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27588 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/com/sequoiadb/lzw/Sdv6665.java
+++ testcase_new/sdv/java/com/sequoiadb/lzw/Sdv6665.java
@@ -2,7 +2,6 @@ package com.sequoiadb.lzw;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 
 import org.bson.BSONObject;
@@ -15,129 +14,128 @@ import org.testng.annotations.Test;
 
 import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
+
 /**
- * FileName: Sdv6665
- * test content: hash范围切分，分区键为单个字段_st.compress.03.024
- * testlink case: SeqDB-6665
+ * FileName: Sdv6661 test content: query查询记录_st.compress.03.020 testlink case:
+ * SeqDB-6661
+ * 
  * @author zengxianquan
- * @date 2016年12月30日
+ * @date 2016年12月29日
  * @version 1.00
  */
-public class Sdv6665 extends SdbTestBase{
+public class Sdv6661 extends SdbTestBase {
 	private Sequoiadb sdb = null;
-    private String clName = "cl6665";
-    private List<String> dataGroupNames =null;
-    private  DBCollection cl = null;
-    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
-    
-    @BeforeClass
-    public void setUp() {
-        System.out.println(this.getClass().getName()+" begin at "+sdf.format(new Date()));
-        try{
-            sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-        }catch(BaseException e){
-            Assert.fail(e.getMessage());
-        }
-        if (Commlib.isStandAlone(sdb)){
-            throw new SkipException("is standalone skip testcase");
-        }
-        dataGroupNames = Commlib.getDataGroups(sdb);
-        createCL();
-    }
-    
-   @AfterClass
-    public void tearDown(){
-        try{
-            CollectionSpace cs = sdb.getCollectionSpace(csName);  
-            if(cs.isCollectionExist(clName)){
-                cs.dropCollection(clName);
-            }
-        }catch(BaseException e){
-            Assert.fail(e.getMessage());
-        }finally{
-            sdb.disconnect();
-            System.out.println(this.getClass().getName()+" end at "+sdf.format(new Date()));
-        }
-    }
-    
-    @Test
-    public void test() {
-    	
-    	String sourceGroupName = dataGroupNames.get(0);
-    	String destGroupName = dataGroupNames.get(1);
-    	try {
-	        int dataCount = 600;
-            int strLength =  1024*1024;          
-            insertData(cl, dataCount, strLength);  
-			Commlib.checkCompressed(cl, sourceGroupName);
-			split(sourceGroupName, destGroupName);
-			Commlib.checkCompressed(cl, destGroupName);
-			checkSplit(sdb, sourceGroupName, 100);
-			checkSplit(sdb, destGroupName, 100);
-		} catch (BaseException e) {
-			e.printStackTrace();
-		}
-    }
-   
-    private void createCL(){     
-        BSONObject option = new BasicBSONObject();
-        BSONObject shardingKey = new BasicBSONObject();
-        try{
+	private String clName = "cl6661";
+	private String dataGroupName = null;
+	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
 
-            option.put("Group", dataGroupNames.get(0));
-            option.put("Compressed", true);
-            option.put("CompressionType", "lzw");
-            option.put("ShardingType", "hash");
-            shardingKey.put("_id", 1);
-            option.put("ShardingKey", shardingKey);
-            
-            CollectionSpace cs = sdb.getCollectionSpace(SdbTestBase.csName);
-            cl = cs.createCollection(clName, option);
-        }catch(BaseException e){
-        	e.printStackTrace();
-            Assert.fail(e.getMessage());
-        }
-    }
-    
-    public String insertData(DBCollection cl, int dataCount, int strLength){
-    	String strRec = getRandomString(strLength);
-    	for(int i = 0; i < dataCount; i++){
-    		cl.insert("{_id:"+i+",key:'"+strRec+i+"'}");
-    	}
-    	return strRec;
-    }
-    
-    private String getRandomString(int length){
-        String base = "abc";
-        Random random = new Random();
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0; i < length; i++){
-            int index = random.nextInt(base.length());
-            sb.append(base.charAt(index));
-        }
-        return sb.toString();
-    }
-    
-    private void split(String sourceGroupName, String destGroupName ){
-    	try{
-    		double percent = 50;
-    		cl.split(sourceGroupName, destGroupName, percent);    		
-    	}catch(BaseException e){
-    		Assert.fail(e.getMessage());
-    	}
-    }    
-    
-    private void checkSplit(Sequoiadb db, String dataGroupName, long expectDataCount){
-    	DBCollection splitCL = null;
-    	Sequoiadb dataDB = Commlib.getDataDB(db, dataGroupName);
-    	splitCL = dataDB.getCollectionSpace(SdbTestBase.csName).getCollection(clName);
-    	int count = (int)splitCL.getCount();
-    	int offSet = (int)(0.3 * 600);
-    	if(Math.abs(count - 300) > offSet){
-             Assert.fail("the split result is wrong");
-        }
-    }   
+	@BeforeClass
+	public void setUp() {
+		System.out.println(this.getClass().getName() + " begin at " + sdf.format(new Date()));
+		try {
+			sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		} catch (BaseException e) {
+			Assert.fail(e.getMessage());
+		}
+		if (Commlib.isStandAlone(sdb)) {
+			throw new SkipException("is standalone skip testcase");
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		try {
+			CollectionSpace cs = sdb.getCollectionSpace(csName);
+			if (cs.isCollectionExist(clName)) {
+				cs.dropCollection(clName);
+			}
+		} catch (BaseException e) {
+			Assert.fail(e.getMessage());
+		} finally {
+			sdb.disconnect();
+			System.out.println(this.getClass().getName() + " end at " + sdf.format(new Date()));
+		}
+	}
+
+	@Test
+	public void test() {
+		try {
+			DBCollection cl = createCL();
+			int dataCount = 600;
+			int strLength = 512 * 1024;
+			String rec = insertData(cl, dataCount, strLength);
+			Commlib.checkCompressed(cl, dataGroupName);
+			checkQuery(dataCount, rec);
+		} catch (BaseException e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	private DBCollection createCL() {
+		DBCollection cl = null;
+		BSONObject option = new BasicBSONObject();
+		try {
+			dataGroupName = Commlib.getDataGroups(sdb).get(0);
+			option.put("Group", dataGroupName);
+			option.put("Compressed", true);
+			option.put("CompressionType", "lzw");
+			CollectionSpace cs = sdb.getCollectionSpace(csName);
+			cl = cs.createCollection(clName, option);
+		} catch (BaseException e) {
+			Assert.fail(e.getMessage());
+		}
+		return cl;
+	}
+
+	public String insertData(DBCollection cl, int dataCount, int strLength) {
+		String strRec = getRandomString(strLength);
+		for (int i = 0; i < dataCount / 2; i++) {
+			cl.insert("{_id:" + i + ",key:'" + strRec + i + "'}");
+		}
+		
+		Commlib.waitCreateDict(cl, dataGroupName);  //等待压缩字典的建立,最多等待60分钟
+		
+		for (int i = dataCount / 2; i < dataCount; i++) {
+			cl.insert("{_id:" + i + ",key:'" + strRec + i + "'}");
+		}
+		return strRec;
+	}
+
+	private String getRandomString(int length) {
+		String base = "abc";
+		Random random = new Random();
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			int index = random.nextInt(base.length());
+			sb.append(base.charAt(index));
+		}
+		return sb.toString();
+	}
+
+	private void checkQuery(int dataCount, String strRec) {
+		DBCursor cursor = null;
+		try {
+			DBCollection cl = sdb.getCollectionSpace(csName).getCollection(clName);
+			cursor = cl.query(null, null, "{_id:1}", null, 10, 10);
+			int i = 10;
+			while (cursor.hasNext()) {
+				String actRec = cursor.getNext().toString();
+				String exptRec = "{ \"_id\" : " + i + " , \"key\" : \"" + strRec + i + "\"}";
+				if (!exptRec.equals(actRec)) {
+					Assert.fail("The data is error");
+				}
+				i++;
+			}
+		} catch (BaseException e) {
+			Assert.fail(e.getMessage());
+		} finally {
+			if (cursor != null) {
+				cursor.close();
+			}
+		}
+	}
 }
