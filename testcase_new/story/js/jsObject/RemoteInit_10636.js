d0bb95a472f0a40a5a50b4673f629055b4afb667
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-02-23 02:54:53 +0000

    修改remote使用非cm端口时的报错
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27659 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/jsObject/RemoteInit_10636.js
+++ testcase_new/story/js/jsObject/RemoteInit_10636.js
@@ -4,38 +4,13 @@
 *                          10637 初始化Remote对象，端口不是cm端口
 *@author      : Liang XueWang
 ******************************************************************************/
-
-// 测试使用非cm端口初始化remote创建system对象后，调用type方法出错
-RemoteTest.prototype.testInitWithWrongSvc = function()
-{
-   this.testInit() ;
-   var system = this.remote.getSystem() ;
-   
-   // 测试调用type方法时报错
-   try
-   {
-      system.type() ;
-      throw "illegal system call type should be failed" ;
-   }
-   catch( e )
-   {
-      if( e !== -6 )
-      {
-         throw buildException( "testInitWithWrongSvc", e, 
-               "check init remote with COORDSVCNAME " + this, -6, e ) ;
-      }   
-   }
-   
-   this.remote.close() ;
-}
-
 function main()
 {
    // 获取远程主机
    var remotehost = toolGetRemotehost() ;
    
    // 测试使用不存在的主机初始化
-   var rt = new RemoteTest( "IllegalHost", CMSVCNAME, false ) ;
+   var rt = new RemoteTest( "IllegalHost", CMSVCNAME, false, true ) ;
    rt.testInit() ;
    
    // 获取空闲端口
@@ -50,9 +25,19 @@ function main()
    rt = new RemoteTest( remotehost, svcname, true, false ) ;
    rt.testInit() ;
    
-   // 测试使用非cm端口初始化（初始化及创建对象时不报错，调用方法时报错）
-   rt = new RemoteTest( remotehost, COORDSVCNAME ) ;
-   rt.testInitWithWrongSvc() ;
+   // 测试使用非cm端口初始化
+   try
+   {
+      var remote = new Remote( remotehost, COORDSVCNAME ) ;
+      throw "init remote with coord svcname should be failed" ;
+   }
+   catch( e )
+   {
+      if( e !== -6 )
+      {
+         throw buildException( "main", e, "init remote with coord svcname", -6, e ) ;
+      }
+   }
 }
 
 main()
