6326703ead179cc5d8959e35679bc1e3efd9bb40
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-03-06 05:38:28 +0000

    针对jira-1425问题单新增自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27846 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/query/query_hint_count_11201.js
@@ -0,0 +1,62 @@
+﻿/****************************************************
+@description: seqDB-11201:query.hint.count查询
+@author:
+              2017-3-7 huangxiaoni init
+****************************************************/
+main();
+
+function main()
+{  
+   try
+   {
+      db.setSessionAttr( { PreferedInstance: "M" } );
+      
+      var clName  = COMMCLNAME+"_11201";
+      var idxName = "idx";
+      
+      var cl = readyCL( clName );
+      cl.createIndex( idxName, {a:-1} );
+      insertRecs( cl );
+      var rc = queryRecs( cl, idxName );
+      checkResult( rc );
+   
+      cleanCL( clName );
+   }
+      catch(e)
+   {
+   	throw e;
+   }
+}
+
+function insertRecs( cl )
+{
+   println("\n---Begin to insert records.");
+   
+   for( i = 0 ; i < 50 ; i++ )
+   {
+      cl.insert({a:i,b:i});
+   }
+}
+
+function queryRecs( cl, idxName )
+{
+   println("\n---Begin to exec[query.hint.count].");
+   
+   var rc = cl.find({b:{$gte:10}}).hint({"": idxName }).sort({a:1}).skip(10).limit(20).count();
+   
+   return rc ;
+}
+
+function checkResult( rc )
+{
+   println("\n---Begin to check result.");
+   
+   var expCnt = 40;
+   var actCnt = Number( rc );
+   if( expCnt !== actCnt )
+   {
+      throw buildException("checkResult", null, "[compare the records]", 
+                          "[count:"+ expCnt +"]",
+                          "[count:"+ actCnt +"]");
+   }
+}
