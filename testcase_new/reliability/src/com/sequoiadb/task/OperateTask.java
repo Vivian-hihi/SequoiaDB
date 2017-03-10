3571489916c40e28e16db417b623fbbed989b160
huangqiaohui <huangqiaohui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-03-10 06:04:39 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27908 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/src/com/sequoiadb/task/OperateTask.java
+++ testcase_new/reliability/src/com/sequoiadb/task/OperateTask.java
@@ -1,11 +1,9 @@
 /**
- * Copyright (c) 2017, SequoiaDB Ltd.
- * File Name:OperateTask.java
+ * Copyright (c) 2017, SequoiaDB Ltd. File Name:OperateTask.java
  * 
  *
- *  @author wenjingwang
- * Date:2017-2-21下午4:54:48
- *  @version 1.00
+ * @author wenjingwang Date:2017-2-21下午4:54:48
+ * @version 1.00
  */
 package com.sequoiadb.task;
 
@@ -13,61 +11,70 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import org.bson.BSONObject;
+
+import com.sequoiadb.exception.FaultException;
 import com.sequoiadb.exception.ReliabilityException;
 
 public abstract class OperateTask extends Task {
-	public enum faultStatus {
-		INIT, MAKESUCCESS, MAKEFAILURE, RESTORESUCESS, RESTOREFAILURE, EXCEPTION
-	};
+    public enum faultStatus {
+        INIT,
+        MAKESUCCESS,
+        MAKEFAILURE,
+        RESTORESUCESS,
+        RESTOREFAILURE,
+        EXCEPTION
+    };
 
-	private TaskMgr mgr = null;
-	private static final int defaultDuration = 5;
+    private TaskMgr mgr = null;
+    private static final int defaultDuration = 5;
 
-	public OperateTask(String name) {
-		super(name, defaultDuration);
-	}
+    public OperateTask(String name) {
+        super(name, defaultDuration);
+    }
 
-	public void setMgr(TaskMgr mgr) {
-		this.mgr = mgr;
-	}
+    public void setMgr(TaskMgr mgr) {
+        this.mgr = mgr;
+    }
 
-	/**
-	 * 当故障make或restore完毕将会调用此函数，通知结果
-	 * 
-	 * @param status
-	 *            key:FaultMakeTask.MAKE_RESULT,FaultMakeTask.RESTORE_RESULT
-	 *            value:OperateTask.status
-	 */
-	public abstract void faultNotify(BSONObject status);
+    /**
+     * 当故障make或restore完毕将会调用此函数，通知结果
+     * 
+     * @param status
+     *            key:FaultMakeTask.MAKE_RESULT,FaultMakeTask.RESTORE_RESULT
+     *            value:OperateTask.status
+     */
+    public abstract void faultNotify(BSONObject status) throws FaultException;
 
-	public abstract void exec() throws Exception;
+    public abstract void exec() throws Exception;
 
-	public void run() {
-		setStatus(Task.TaskStatus.TASKSTART);
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date date = Calendar.getInstance().getTime();
-		System.out.println("Thread '" + this.getName() + "' run at " + sdf.format(date));
-		try {
-			exec();
-		} catch (Exception e) {
-			if (e instanceof InterruptedException) {
-				setStatus(Task.TaskStatus.TASKINTERRUPT);
-			} else {
-				setStatus(Task.TaskStatus.TASKTHROWEXCEPTION);
-				exception = new ReliabilityException(e);
-				exception.setStackTrace(e.getStackTrace());
-			}
-		}
-		if (exception == null) {
-			setStatus(Task.TaskStatus.TASKSTOP);
-		}
-		date = Calendar.getInstance().getTime();
-		System.out.println("Thread '" + this.getName() + "' end at " + sdf.format(date));
-		// mgr.Done(this);
-	}
+    public void run() {
+        setStatus(Task.TaskStatus.TASKSTART);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = Calendar.getInstance().getTime();
+        System.out.println("Thread '" + this.getName() + "' run at " + sdf.format(date));
+        try {
+            exec();
+        }
+        catch (Exception e) {
+            if (e instanceof InterruptedException) {
+                setStatus(Task.TaskStatus.TASKINTERRUPT);
+            }
+            else {
+                setStatus(Task.TaskStatus.TASKTHROWEXCEPTION);
+                exception = new ReliabilityException(e);
+                exception.setStackTrace(e.getStackTrace());
+            }
+        }
+        if (exception == null) {
+            setStatus(Task.TaskStatus.TASKSTOP);
+        }
+        date = Calendar.getInstance().getTime();
+        System.out.println("Thread '" + this.getName() + "' end at " + sdf.format(date));
+        // mgr.Done(this);
+    }
 
-	public Task getTaskByName(String name) {
-		return mgr.getTaskByName(name);
-	}
+    public Task getTaskByName(String name) {
+        return mgr.getTaskByName(name);
+    }
 
 }
