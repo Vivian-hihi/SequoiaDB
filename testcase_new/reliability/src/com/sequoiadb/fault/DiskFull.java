ff6ccbb871dcfa1c50f096c6534961cb9c568e42
huangqiaohui <huangqiaohui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-03-13 08:23:53 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27918 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/src/com/sequoiadb/fault/DiskFull.java
+++ testcase_new/reliability/src/com/sequoiadb/fault/DiskFull.java
@@ -1,11 +1,9 @@
 /**
- * Copyright (c) 2017, SequoiaDB Ltd.
- * File Name:DiskFull.java
+ * Copyright (c) 2017, SequoiaDB Ltd. File Name:DiskFull.java
  * 
  *
- *  @author wenjingwang
- * Date:2017-2-21下午4:54:48
- *  @version 1.00
+ * @author wenjingwang Date:2017-2-21下午4:54:48
+ * @version 1.00
  */
 package com.sequoiadb.fault;
 
@@ -19,160 +17,183 @@ import com.sequoiadb.exception.ReliabilityException;
 import com.sequoiadb.task.FaultMakeTask;
 
 public class DiskFull extends Fault {
-	private String hostName;
-	private String user;
-	private String passwd;
-	private String padPath;
-	private int port;
-	private List<String> padFileList = new ArrayList<String>();
-	private Ssh ssh;
-	private String remotePath;
+    private String hostName;
+    private String user;
+    private String passwd;
+    private String padPath;
+    private int port;
+    private List<String> padFileList = new ArrayList<String>();
+    private Ssh ssh;
+    private String remotePath;
+    private int presetPercent;
 
-	private final String localScriptPath = "./script";
-	private final String scriptName = "fillUpDisk.sh";
+    private final String localScriptPath = "./script";
+    private final String scriptName = "fillUpDisk.sh";
 
-	/**
-	 * 使用root jenkins 22端口登录目标机
-	 * 
-	 * @param hostName
-	 * @param padPath
-	 *            填充文件的路径
-	 */
-	public DiskFull(String hostName, String padPath) {
-		super("diskFull");
-		this.hostName = hostName;
-		this.user = "root";
-		this.passwd = SdbTestBase.rootPwd;
-		this.padPath = padPath;
-		this.remotePath = SdbTestBase.workDir;
-		this.port = 22;
-	}
+    /**
+     * 
+     * @param hostName
+     * @param padPath
+     *            填充文件的路径
+     */
+    public DiskFull(String hostName, String padPath) {
+        super("diskFull");
+        this.hostName = hostName;
+        this.user = "root";
+        this.passwd = SdbTestBase.rootPwd;
+        this.padPath = padPath;
+        this.remotePath = SdbTestBase.workDir;
+        this.port = 22;
+        this.presetPercent = 0;
+    }
 
-	// for debug
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public DiskFull(String hostName, String padPath, int presetPercent) {
+        super("diskFull");
+        this.hostName = hostName;
+        this.user = "root";
+        this.passwd = SdbTestBase.rootPwd;
+        this.padPath = padPath;
+        this.remotePath = SdbTestBase.workDir;
+        this.port = 22;
+        this.presetPercent = presetPercent;
+    }
 
-	// for debug
-	public void setUser(String user) {
-		this.user = user;
-	}
+    // for debug
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	// for debug
-	public void setPwd(String passwd) {
-		this.passwd = passwd;
-	}
+    // for debug
+    public void setUser(String user) {
+        this.user = user;
+    }
 
-	// for debug
-	public void setRemotePath(String remotePath) {
-		this.remotePath = remotePath;
-	}
+    // for debug
+    public void setPwd(String passwd) {
+        this.passwd = passwd;
+    }
 
-	public void make() throws FaultException {
-		fillUpDisk(100);
-	}
+    // for debug
+    public void setRemotePath(String remotePath) {
+        this.remotePath = remotePath;
+    }
 
-	public boolean checkMakeResult() throws FaultException {
-		try {
-			ssh.exec("df " + padPath + " | sed '1d' |awk '{print $4}'");
-			String stdOut = ssh.getStdout();
-			if (stdOut.length() <= 0) {
-				throw new FaultException("can not find this path:" + padPath);
-			}
-			int padSize = Integer.parseInt(stdOut.substring(0, stdOut.length() - 1));
-			if (padSize > 0) {
-				return false;
-			} else {
-				return true;
-			}
-		} catch (ReliabilityException e) {
-			FaultException e1 = new FaultException(e);
-			e1.setStackTrace(e.getStackTrace());
-			throw e1;
-		}
-	}
+    public void make() throws FaultException {
+        fillUpDisk(100);
+    }
 
-	public void restore() throws FaultException {
-		for (int i = 0; i < padFileList.size();) {
-			try {
-				ssh.exec("rm -f " + padFileList.get(i));
-			} catch (ReliabilityException e) {
-				FaultException e1 = new FaultException(e);
-				e1.setStackTrace(e.getStackTrace());
-				throw e1;
-			}
-			padFileList.remove(i);
-		}
-	}
+    public boolean checkMakeResult() throws FaultException {
+        try {
+            ssh.exec("df " + padPath + " | sed '1d' |awk '{print $4}'");
+            String stdOut = ssh.getStdout();
+            if (stdOut.length() <= 0) {
+                throw new FaultException("can not find this path:" + padPath);
+            }
+            int padSize = Integer.parseInt(stdOut.substring(0, stdOut.length() - 1));
+            if (padSize > 0) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        catch (ReliabilityException e) {
+            FaultException e1 = new FaultException(e);
+            e1.setStackTrace(e.getStackTrace());
+            throw e1;
+        }
+    }
 
-	public boolean checkRestoreResult() {
-		return padFileList.size() == 0 ? true : false;
-	}
+    public void restore() throws FaultException {
+        for (int i = 0; i < padFileList.size();) {
+            try {
+                ssh.exec("rm -f " + padFileList.get(i));
+            }
+            catch (ReliabilityException e) {
+                FaultException e1 = new FaultException(e);
+                e1.setStackTrace(e.getStackTrace());
+                throw e1;
+            }
+            padFileList.remove(i);
+        }
+    }
 
-	public void fillUpDisk(int percent) throws FaultException {
-		try {
-			ssh.exec(remotePath + "/" + scriptName + " " + padPath + " " + percent);
-			if (ssh.getStdout().length() > 0) {
-				String padFileName = ssh.getStdout().substring(0, ssh.getStdout().length() - 1);
-				padFileList.add(padFileName);
-			}
-		} catch (ReliabilityException e) {
-			FaultException e1 = new FaultException(e);
-			e1.setStackTrace(e.getStackTrace());
-			throw e1;
-		}
-	}
+    public boolean checkRestoreResult() {
+        return padFileList.size() == 0 ? true : false;
+    }
 
-	@Override
-	public boolean init() throws FaultException {
-		try {
-			ssh = new Ssh(hostName, user, passwd, port);
-			try {
-				ssh.exec("mkdir " + SdbTestBase.workDir);
-			} catch (Exception e) {
-			}
-			ssh.scpTo(localScriptPath + "/" + scriptName, remotePath + "/");
-			ssh.exec("chmod 777 " + remotePath + "/" + scriptName);
-		} catch (ReliabilityException e) {
-			FaultException e1 = new FaultException(e);
-			e1.setStackTrace(e.getStackTrace());
-			throw e1;
-		}
-		return true;
-	}
+    public void fillUpDisk(int percent) throws FaultException {
+        try {
+            ssh.exec(remotePath + "/" + scriptName + " " + padPath + " " + percent);
+            if (ssh.getStdout().length() > 0 && !ssh.getStdout().equals("NothingToDo\n")) {
+                String padFileName = ssh.getStdout().substring(0, ssh.getStdout().length() - 1);
+                padFileList.add(padFileName);
+            }
+        }
+        catch (ReliabilityException e) {
+            FaultException e1 = new FaultException(e);
+            e1.setStackTrace(e.getStackTrace());
+            throw e1;
+        }
+    }
 
-	@Override
-	public boolean fini() throws FaultException {
-		try {
-			if (ssh != null) {
-				ssh.exec("rm -rf " + remotePath + "/" + scriptName);
-				ssh.close();
-				ssh = null;
-			}
-		} catch (ReliabilityException e) {
-			FaultException e1 = new FaultException(e);
-			e1.setStackTrace(e.getStackTrace());
-			throw e1;
-		}
-		return true;
-	}
+    @Override
+    public boolean init() throws FaultException {
+        try {
+            ssh = new Ssh(hostName, user, passwd, port);
+            try {
+                ssh.exec("mkdir " + SdbTestBase.workDir);
+            }
+            catch (Exception e) {
+                // Ignore exception
+            }
+            ssh.scpTo(localScriptPath + "/" + scriptName, remotePath + "/");
+            ssh.exec("chmod 777 " + remotePath + "/" + scriptName);
+            fillUpDisk(presetPercent);
+        }
+        catch (ReliabilityException e) {
+            FaultException e1 = new FaultException(e);
+            e1.setStackTrace(e.getStackTrace());
+            throw e1;
+        }
+        return true;
+    }
 
-	/**
-	 * 
-	 * @param hostName
-	 * @param padPath
-	 *            填充路徑
-	 * @param maxDelay
-	 *            最大延迟启动时间s
-	 * @param duration
-	 *            持续时间s
-	 * @return
-	 */
-	public static FaultMakeTask getFaultMakeTask(String hostName, String padPath, int maxDelay, int duration) {
-		FaultMakeTask task = null;
-		DiskFull df = new DiskFull(hostName, padPath);
-		task = new FaultMakeTask(df, maxDelay, duration, 3);
-		return task;
-	}
+    @Override
+    public boolean fini() throws FaultException {
+        try {
+            if (ssh != null) {
+                ssh.exec("rm -rf " + remotePath + "/" + scriptName);
+                ssh.close();
+                ssh = null;
+            }
+        }
+        catch (ReliabilityException e) {
+            FaultException e1 = new FaultException(e);
+            e1.setStackTrace(e.getStackTrace());
+            throw e1;
+        }
+        return true;
+    }
 
+    /**
+     * 
+     * @param hostName
+     * @param padPath
+     *            填充路徑
+     * @param maxDelay
+     *            最大延迟启动时间s
+     * @param duration
+     *            持续时间s
+     * @param presetPercent
+     *            线程启动之前，希望磁盘占用率达到的数值
+     * @return
+     */
+    public static FaultMakeTask getFaultMakeTask(String hostName, String padPath, int maxDelay,
+            int duration, int presetPercent) {
+        FaultMakeTask task = null;
+        DiskFull df = new DiskFull(hostName, padPath, presetPercent);
+        task = new FaultMakeTask(df, maxDelay, duration, 3);
+        return task;
+    }
 }
