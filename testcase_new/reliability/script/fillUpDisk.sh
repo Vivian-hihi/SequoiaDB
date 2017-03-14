c8af4f56fcf75ac6c776c305ea52b65a25347f99
huangqiaohui <huangqiaohui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-03-14 01:11:45 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27921 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/script/fillUpDisk.sh
+++ testcase_new/reliability/script/fillUpDisk.sh
@@ -3,6 +3,12 @@ all=$(df -l  $1 | sed '1d' | awk '{print $3,$4,$5}')
 used=$(echo $all | awk '{print $1}')
 available=$(echo $all | awk '{print $2}')
 percent=$(echo $all | awk '{print $3}')
+percent=${percent%%\%}
+if [ $percent -ge $2 ]
+then
+  echo "NothingToDo"
+  exit 0
+fi
 #echo $used $available $percent
 total=$[$used+$available]
 padSize=$(expr $total \* $2 / 100 - $used)
