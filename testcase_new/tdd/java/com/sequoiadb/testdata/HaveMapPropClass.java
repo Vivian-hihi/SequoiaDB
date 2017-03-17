5a246bd2846a57a95cff7f232568b7937ce2cf90
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-03-17 05:40:13 +0000

    update java driver testcases
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@27993 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/tdd/java/com/sequoiadb/testdata/HaveMapPropClass.java
+++ testcase_new/tdd/java/com/sequoiadb/testdata/HaveMapPropClass.java
@@ -2,35 +2,54 @@ package com.sequoiadb.testdata;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class HaveMapPropClass {
-	private Map<String, String> mapProp = null;
-	private Map<String,User> userMap=null;
-	
-	public HaveMapPropClass() {
-		mapProp = new HashMap<String, String>();
-	}
+    private Map<String, String> mapProp = null;
+    private Map<String, User> userMap = null;
 
-	public Map<String, String> getMapProp() {
-		return mapProp;
-	}
+    public HaveMapPropClass() {
+        mapProp = new HashMap<String, String>();
+    }
 
-	public void setMapProp(Map<String, String> value) {
-		this.mapProp = value;
-	}
-	
-	public Map<String,User> getUserMap(){
-		return this.userMap;
-	}
-	
-	public void setUserMap(Map<String,User> userMap){
-		this.userMap=userMap;
-	}
+    public Map<String, String> getMapProp() {
+        return mapProp;
+    }
 
-	@Override
-	public String toString() {
-		return "HaveMapPropClass [mapProp=" + mapProp + ", userMap=" + userMap
-				+ "]";
-	}
-	
+    public void setMapProp(Map<String, String> value) {
+        this.mapProp = value;
+    }
+
+    public Map<String, User> getUserMap() {
+        return this.userMap;
+    }
+
+    public void setUserMap(Map<String, User> userMap) {
+        this.userMap = userMap;
+    }
+
+    @Override
+    public String toString() {
+        return "HaveMapPropClass [mapProp=" + mapProp + ", userMap=" + userMap
+            + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mapProp, userMap);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof HaveMapPropClass) {
+            if (this == obj) {
+                return true;
+            }
+
+            HaveMapPropClass other = (HaveMapPropClass) obj;
+            return this.mapProp.equals(other.mapProp) && this.userMap.equals(other.userMap);
+        } else {
+            return false;
+        }
+    }
 }
--- testcase_new/tdd/java/com/sequoiadb/testdata/User.java
+++ testcase_new/tdd/java/com/sequoiadb/testdata/User.java
@@ -1,54 +1,71 @@
 package com.sequoiadb.testdata;
 
 import java.util.Map;
+import java.util.Objects;
 
-public class User{
-	public int age;
-	public String name;
-	public Map<String,Student> student;
-	
-	public void setAge(int age){
-		this.age=age;
-	}
-	
-	public int   getAge(){
-		return this.age;
-	}
-	
-	public void  setName(String name){
-		this.name=name;
-	}
-	
-	public User(int age, String name, Map<String, Student> student) {
-		super();
-		this.age = age;
-		this.name = name;
-		this.student = student;
-	}
+public class User {
+    public int age;
+    public String name;
+    public Map<String, Student> student;
 
-	public String getName(){
-		return this.name;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	
-	
-	public Map<String, Student> getStudent() {
-		return student;
-	}
+    public int getAge() {
+        return this.age;
+    }
 
-	public void setStudent(Map<String, Student> student) {
-		this.student = student;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(int age, String name, Map<String, Student> student) {
+        super();
+        this.age = age;
+        this.name = name;
+        this.student = student;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
 
-	public User() {
-	}
+    public Map<String, Student> getStudent() {
+        return student;
+    }
 
-	@Override
-	public String toString() {
-		return "User [age=" + age + ", name=" + name + ", student=" + student
-				+ "]";
-	}
-	
-	
+    public void setStudent(Map<String, Student> student) {
+        this.student = student;
+    }
+
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User [age=" + age + ", name=" + name + ", student=" + student
+            + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, name, student);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            if (this == obj) {
+                return true;
+            }
+
+            User other = (User) obj;
+            return this.age == other.age && this.name.equals(other.name) && this.student.equals(other.student);
+        } else {
+            return false;
+        }
+    }
 }
