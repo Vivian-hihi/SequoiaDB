98f9d837be777aa94ee3cbaa62c00ff168b60dd3
linsuqiang <linsuqiang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-04-07 07:15:42 +0000

    断网端口改为动态获取
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@28491 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/script/brokenNetwork.sh
+++ testcase_new/reliability/script/brokenNetwork.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 trap "" 1 2 3 24
-ifconfig eth0 down
+eth=`ifconfig | grep 'eth' | awk '{print $1}'`
+ifconfig ${eth} down
 sleep $1 &
 wait
-ifconfig eth0 up
+ifconfig ${eth} up
 service network restart
 /etc/init.d/networking restart
