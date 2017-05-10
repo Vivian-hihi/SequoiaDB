ee0839618cb540cc8f72cfe5228226418af5b413
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-10 07:02:23 +0000

    fix: upsert loadPatter not make predicate
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@28956 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/coord/coordUpdateOperator.cpp
+++ SequoiaDB/engine/coord/coordUpdateOperator.cpp
@@ -345,7 +345,7 @@ namespace engine
 
          coordInsertOperator insertOpr ;
 
-         rc = matcherTree.loadPattern ( matcher ) ;
+         rc = matcherTree.loadPattern ( matcher, FALSE ) ;
          PD_RC_CHECK ( rc, PDERROR, "Failed to load matcher[%s], rc: %d",
                        matcher.toString().c_str(), rc ) ;
 
