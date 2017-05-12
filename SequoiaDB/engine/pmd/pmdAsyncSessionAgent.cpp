b3ee1e2ae8e62cae59d464d4e06f86290ee5d827
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-12 16:31:26 +0000

    opt: 重置interrupt在waitEvent之后
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@29026 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/pmdAsyncSessionAgent.cpp
+++ SequoiaDB/engine/pmd/pmdAsyncSessionAgent.cpp
@@ -71,6 +71,8 @@ namespace engine
 
          if ( cb->waitEvent( event, OSS_ONE_SEC, TRUE ) )
          { 
+            /// reset again to avoid set interrupt self
+            cb->resetInterrupt() ;
             if ( PMD_EDU_EVENT_TERM == event._eventType )
             {
                PD_LOG ( PDDEBUG, "EDU[%lld, %s] is terminated", cb->getID(),
