8b52e507c30e22ca148c78764f84e3db006578f8
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-13 02:45:18 +0000

    修改php测试findAndRemove用例 因为regex的bson匹配方式改变了
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@29029 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/php/crud/php_findAndRemove_allType_paramArray_7665_Test.php
+++ testcase_new/driver/php/crud/php_findAndRemove_allType_paramArray_7665_Test.php
@@ -92,7 +92,11 @@ class DataOperator04 extends BaseOperator
       $updateReturnArray = array();
       for( $i = 0; $i < count( $recsArray ) - 3 ; $i++ )  //filter 3 records after find
       {
-         if( $i < 12 || $i > 13 ) //last record is minKey/maxKey, can not match the records to remove 
+         if( $i === 10 ) //regex, match regex bson, must add $et
+         {
+            $condition = array( 'b' => array( '$et' => $recsArray[$i]['b'] ) ) ;
+         }
+         else if( $i < 12 || $i > 13 ) //last record is minKey/maxKey, can not match the records to remove 
          {
             $condition = array( 'b' => $recsArray[$i]['b'] );
          }
