dca46fe473d0ce7e5abb217ecf8267ef460eaa65
laojingtang <laojingtang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-05-16 04:44:42 +0000

    增加lob自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@29080 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/reliability/src/com/sequoiadb/lob/LobUtil.java
@@ -0,0 +1,41 @@
+package com.sequoiadb.lob;
+
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.Sequoiadb;
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.bson.util.JSON;
+
+import static com.sequoiadb.metaopr.commons.MyUtil.*;
+
+/**
+ * @FileName
+ * @Author laojingtang
+ * @Date 17-5-11
+ * @Version 1.00
+ */
+public class LobUtil {
+    public static final String csName = "lobcs", clName = "lobcl";
+
+
+    public static void createLobCsAndCl() {
+        Sequoiadb db = getSdb();
+        if (isCsExisted(csName) == true) {
+            if (db.getCollectionSpace(csName).getCollection(clName) != null)
+                return;
+        }
+        createCS(csName);
+        BSONObject option = (BSONObject) JSON.parse("{ ShardingKey: { \"age\": 1 }," +
+                " ShardingType: \"hash\", " +
+                "Partition: 1024, ReplSize: 1," +
+                " Compressed: true ," +
+                "Group:\"group1\"}");
+        createCl(csName, clName, option);
+        DBCollection cl = db.getCollectionSpace(csName)
+                .getCollection(clName);
+
+        cl.split("group1", "group2", 50);
+        db.close();
+    }
+
+}
