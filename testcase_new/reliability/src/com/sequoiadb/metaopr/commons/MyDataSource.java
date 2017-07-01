4d832e9f3a6b21b6333867b65140eed99df6333a
laojingtang <laojingtang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-07-01 05:31:03 +0000

    新建链接时，指定keepalive=true
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@29882 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/src/com/sequoiadb/metaopr/commons/MyDataSource.java
+++ testcase_new/reliability/src/com/sequoiadb/metaopr/commons/MyDataSource.java
@@ -1,8 +1,12 @@
 package com.sequoiadb.metaopr.commons;
 
+import com.sequoiadb.base.ConfigOptions;
 import com.sequoiadb.commlib.SdbTestBase;
 import com.sequoiadb.datasource.DatasourceOptions;
 import com.sequoiadb.datasource.SequoiadbDatasource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @FileName
@@ -24,7 +28,12 @@ public class MyDataSource {
         dsOpt.setDeltaIncCount(20);
         dsOpt.setMaxIdleCount(20);
         dsOpt.setCheckInterval(60 * 1000);
-        _ds = new SequoiadbDatasource(SdbTestBase.coordUrl, "", "", dsOpt);
+        ConfigOptions configOptions=new ConfigOptions();
+        //不打开keepalive，某一端断网后，另一端无法感知。
+        configOptions.setSocketKeepAlive(true);
+        List<String> urls=new ArrayList<>(10);
+        urls.add(SdbTestBase.coordUrl);
+        _ds=new SequoiadbDatasource(urls,"","",configOptions,dsOpt);
         return _ds;
     }
 }
