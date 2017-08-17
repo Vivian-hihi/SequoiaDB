8ce2c3b539b5d42256fdc161186172cf879faf3f
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-08-17 00:34:06 +0000

    添加固定集合AutoIndexId参数用例和pop并发用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@30736 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/cappedCL/cappedCL_createCL_AutoIndexId_12383.js
@@ -0,0 +1,68 @@
+/************************************
+*@Description: Create CappedCL , check Parameter AutoIndexId
+*@author:      liuxiaoxuan
+*@createdate:  2017.8.15
+*@testlinkCase:seqDB-12383
+**************************************/
+
+main()
+
+function main()
+{
+   var csName = COMMCSNAME + "_12383";
+   commDropCS( db, csName, true, "drop CS in the beginning" );
+
+   var csOption = {Capped:true};
+   commCreateCS( db, csName, false, "", csOption );
+
+   var clName = COMMCLNAME + "_12383";
+   
+   //Not Exist Parameter AutoIndexId
+   var option1 = {Capped:true, Size:1024};
+   checkCreateCLOptions(csName,clName,option1);
+   
+   // AutoIndexId true
+   var option2 = {Capped:true, Size:1024, AutoIndexId:true};
+   checkCreateCLOptions(csName,clName,option2);
+   
+   // AutoIndexId is int
+   var option3 = {Capped:true, Size:1024, AutoIndexId:123};
+   checkCreateCLOptions(csName,clName,option3);
+   
+   // AutoIndexId is String
+   var option4 = {Capped:true, Size:1024, AutoIndexId:"a"};
+   checkCreateCLOptions(csName,clName,option4);
+   
+   // AutoIndexId is null
+   var option5 = {Capped:true, Size:1024, AutoIndexId:null};
+   checkCreateCLOptions(csName,clName,option5);
+   
+   // Valid AutoIndexId
+   var isValid = true;
+   var option6 = {Capped:true, Size:1024, AutoIndexId:false};
+   checkCreateCLOptions(csName,clName,option6,isValid);
+   
+   commDropCS( db, csName, true, "drop CS in the end" );
+
+}
+
+function checkCreateCLOptions( csName, clName, options, isValid)
+{
+    try
+    {
+       var dbcl = commCreateCLByOption( db, csName, clName, options, true, true );
+	   if ( isValid == undefined ) { throw "NEED_CREATE_FAIL_ERROR"; } 
+	   println("Create CL with option: " + JSON.stringify(options) + " success!");
+    }
+   catch(e)
+   {
+      if( e !== -6)
+      {
+          throw buildException("Invalid parameter is not -6,error msg is: " + e);
+      }
+      else
+      {
+         println("check result success!");
+      }
+   }
+}
