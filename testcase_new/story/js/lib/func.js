16cac6ce884a5bfff9f8fb608b28cd5cbcd7021b
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-08-17 00:36:13 +0000

    增加Date类型格式化函数
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@30737 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/lib/func.js
+++ testcase_new/story/js/lib/func.js
@@ -1694,6 +1694,25 @@ function commDataGenerator()
    }
 }
 
+Date.prototype.Format = function(fmt)   
+    { //author: meizz   
+      var o = {   
+        "M+" : this.getMonth()+1,                 //month   
+        "d+" : this.getDate(),                    //date  
+        "h+" : this.getHours(),                   //hour  
+        "m+" : this.getMinutes(),                 //minute 
+        "s+" : this.getSeconds(),                 //second   
+        "q+" : Math.floor((this.getMonth()+3)/3), //quarter   
+        "S"  : this.getMilliseconds()             //millisecond   
+      };   
+      if(/(y+)/.test(fmt))   
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));   
+      for(var k in o)   
+        if(new RegExp("("+ k +")").test(fmt))   
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));   
+      return fmt;   
+    }
+
 /**********************************************************************
 @Description:  make dir in host( can be used to make WORKDIR )
 @author:       Liangxw
