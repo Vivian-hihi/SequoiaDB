64d7e8c0af273fb99755da387a9a0bce74802d88
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-01 08:04:10 +0000

    CI调整该执行脚本，适用CI跑串行和配置
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@30996 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/python/run_all_case.py
+++ testcase_new/driver/python/run_all_case.py
@@ -2,20 +2,23 @@ import unittest
 import os
 import xmlrunner
 
-case_path = os.path.join(os.getcwd())
+case_path = os.path.join(os.getcwd(),"")
 report_path = os.path.join(os.getcwd(), "report")
-
+tmp_case_path0 = os.path.split(case_path)[0]
+tmp_case_path1 = os.path.split(case_path)[1]
+pattern_param='*.py'
+if tmp_case_path1.endswith('.py'):
+   pattern_param = tmp_case_path1
+   case_path = tmp_case_path0
+   
+print (pattern_param)
+print (case_path)
 
 def all_case():
-   discover = unittest.defaultTestLoader.discover(case_path, pattern="test*.py", top_level_dir=None)
+   discover = unittest.defaultTestLoader.discover(case_path, pattern=pattern_param, top_level_dir=None)
    print(discover)
    return discover
 
 if __name__ == "__main__":
    runner = xmlrunner.XMLTestRunner(output=report_path)
    test_result = runner.run(all_case())
-   """
-   runner = unittest.TextTestRunner()
-   test_result = runner.run(all_case())
-   """
-   
