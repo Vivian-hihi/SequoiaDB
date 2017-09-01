c30ab4fd6d8c6f710c68676bb6a0e773cd5a0acd
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-01 09:19:37 +0000

    修改python基本数据操作用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@31005 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/python/dataopeartion/index/test_index_create_drop_query_12476.py
@@ -0,0 +1,147 @@
+# @decription: create/drop/query common index
+# @author:     liuxiaoxuan 2017-8-30
+
+import unittest
+import datetime
+from pysequoiadb import client
+from pysequoiadb import collectionspace
+from pysequoiadb.error import (SDBTypeError, SDBBaseError, SDBEndOfCursor,SDBError)
+from lib.config import *
+
+cs_name = "cs_12476"
+cl_name = "cl_12476"
+insert_nums = 100
+class TestIndex12476(unittest.TestCase):
+   def setUp(self):
+      try:
+         print(datetime.datetime.now())
+         config = Config()
+         self.db = client( config.host_name, config.service )
+         self.create_cl()
+         self.insert_datas()         
+      except SDBBaseError as e:
+         print(e.detail)
+         raise e        
+ 
+   def testIdIndex12476(self):
+      try:  
+         aIndex = {'a':1}
+         aIdxName = 'a'
+         bIndex = {'b':1}
+         bIdxName = 'b'     
+          
+         isOption = True          
+         self.create_index(aIndex,aIdxName,isOption)
+         self.create_index(bIndex,bIdxName,not isOption)
+         self.query_datas(aIdxName)
+          
+         self.check_indexes()
+         self.check_one_index(aIdxName)
+         self.check_one_index(bIdxName)
+          
+         self.drop_index(aIdxName)
+         self.drop_index(bIdxName)
+         self.query_datas("")
+      except SDBBaseError as e:
+         print(e.detail) 
+         assert False
+		 
+   def tearDown(self):
+      try:
+         print(datetime.datetime.now())
+         self.db.drop_collection_space(cs_name)
+         self.db.disconnect()
+      except SDBBaseError as e:
+         if(-34 != e.code):
+            print(e.detail)
+            raise e      	 
+		  
+   def create_cl(self):
+      try:
+         print( '---begin to create cs---')
+         self.cs = self.db.create_collection_space(cs_name)            
+         
+         self.cl = self.cs.create_collection(cl_name)
+         print( '---create cl success---' )   
+      except SDBError as e:
+         print(e.detail) 
+         raise e    
+  
+   def insert_datas(self):   
+      print( '---begin to insert records---' )
+      for i in range(1,insert_nums):
+         try:
+            self.cl.insert({"_id":i,"a":i, "b":"test" + str(i)})  
+         except SDBError as e:
+            print(e.detail) 
+            raise e        
+    
+   def check_one_index(self,index_name):
+      print( '---check one index---' )
+      try:
+         cursor = self.cl.get_indexes(index_name);  
+         rec = cursor.next()
+         act_idx_name = rec['IndexDef']['name']
+         exp_idx_name = index_name
+         self.assertEqual(exp_idx_name, act_idx_name,'index name is not exist')
+      except SDBBaseError as e:
+         print(e.detail) 
+         raise e             
+    
+   def check_indexes(self):
+      print( '---check all indexes---' )
+      try:
+         cursor = self.cl.get_indexes(); 
+         idxs = []
+         while True:
+            try:
+               rec = cursor.next()
+               index_name = rec['IndexDef']['name']
+               idxs.append(index_name)
+               print(index_name)
+            except SDBEndOfCursor:
+               break
+         if('a' not in idxs or 'b' not in idxs):
+            assert False        
+      except SDBBaseError as e:
+         print(e.detail) 
+         raise e             
+       
+   def drop_index(self,index_name):   
+      print( '---drop index---' )
+      try:
+         self.cl.drop_index(index_name)  
+      except SDBBaseError as e:
+         print(e.detail) 
+         raise e      
+      
+   def create_index(self,index,index_name,isOption):   
+      print( '---create index---' )
+      try:
+         isUnique = False
+         isEnforced = False
+         buffer_size = 0
+         if(isOption):
+            isUnique = True
+            isEnforced = True
+            buffer_size = 128
+         self.cl.create_index(index, index_name, isUnique, isEnforced, buffer_size)
+      except SDBBaseError as e:
+         print(e.detail) 
+         raise e       
+      
+   def query_datas(self,index_name):   
+      print( '---check data with index---' )
+      try:
+         rec = self.cl.query(condition={"a":{'$et':10}},\
+                             order_by={"_id":1},\
+                             hint={"":index_name},\
+                             flags=1).next()
+         exprec = {"_id":10,"a":10, "b":"test" + str(10)}
+         self.assertEqual( rec,exprec)                     
+      except SDBBaseError as e:
+         print(e.detail) 
+         raise e                              
+                        
+if __name__ == "__main__":
+    unittest.main() 
---
+++ testcase_new/driver/python/dataopeartion/insert/test_save_12502.py
@@ -0,0 +1,139 @@
+# @decription: save records 
+# @author:     liuxiaoxuan 2017-8-29
+
+import unittest
+import datetime
+from pysequoiadb import client
+from pysequoiadb import collectionspace
+from pysequoiadb.error import (SDBTypeError, SDBBaseError, SDBEndOfCursor,SDBError)
+from lib.config import *
+from bson.objectid import ObjectId
+
+cs_name = "cs_12502"
+cl_name = "cl_12502"
+class TestSave12502(unittest.TestCase):
+   def setUp(self):
+      try:
+         print(datetime.datetime.now())
+         config = Config()
+         self.db = client( config.host_name, config.service ) 
+         if(self.is_stand_alone()):
+            self.skipTest('current environment less than tow groups')
+         if(self.one_group_mode()):
+            self.skipTest('need at least 2 data groups')  
+             
+         dataGroupNames = self.get_data_groupnames()
+         srcGroupName = dataGroupNames[0]
+         destGroupName = dataGroupNames[1]
+          
+         self.create_cl(srcGroupName)
+         self.insert_datas()    
+         self.split_cl(srcGroupName,destGroupName)
+      except SDBBaseError as e:
+         print(e.detail)
+         raise e        
+      
+   def testSaveFields(self):   
+      print( '---begin to save records---' )
+      try:
+         doc_commNoMatchId = {"a":"newA_withNoMatchId"}
+         doc_commNotexistId = {"a":"newA_withNotExistId","_id":ObjectId("66bb5667c5d061d6f579d000")}
+         doc_commExistId = {"a":"newA_withExistId","_id":ObjectId("53bb5667c5d061d6f579d0bb")}
+         doc_commNewField = {"newField":"upsertNewField","_id":ObjectId("53bb5667c5d061d6f579d0bc")}
+         
+         self.cl.save(doc_commNoMatchId)  
+         self.cl.save(doc_commNotexistId)
+         self.cl.save(doc_commExistId)
+         self.cl.save(doc_commNewField)
+          
+         doc_shardNoMatchId = {"no":9999}
+         doc_shardNotexistId = {"no":1001,"_id":ObjectId("92bb5667c5d061d6f580d0ab")}
+         doc_shardExistId = {"no":2002,"_id":ObjectId("53bb5667c5d061d6f579d0bb")}
+         doc_shardNewField = {"newNo":"newNo","_id":ObjectId("53bb5667c5d061d6f579d0bd")}
+          
+         self.cl.save(doc_shardNoMatchId)  
+         self.cl.save(doc_shardNotexistId)
+         self.cl.save(doc_shardExistId)
+         self.cl.save(doc_shardNewField)           
+          
+      except SDBError as e:
+         print(e.detail) 
+         raise e                    
+                
+   def tearDown(self):
+      try:
+         print(datetime.datetime.now())
+         self.db.drop_collection_space(cs_name)
+         self.db.disconnect()
+      except SDBBaseError as e:
+         if(-34 != e.code):
+            print(e.detail)
+            raise e              
+ 
+   def is_stand_alone(self):
+      try:
+         cursor = self.db.list_replica_groups()
+      except SDBBaseError as e:
+         if(-159 == e.code):
+            return True
+      return False           
+   
+   def one_group_mode(self):
+      size = len(self.get_data_groupnames())
+      if (2 > size):
+         return True; 
+      return False;
+
+   def get_data_groupnames(self):
+      print( '---begin to get data group names' )
+      groupNames = []
+       
+      cr = self.db.get_list(7)   
+      while True:
+         try:
+            rec = cr.next()
+            groupNames.append(rec['GroupName'])
+         except SDBEndOfCursor:
+            break
+         except Exception as e:
+            raise e
+      groupNames.remove("SYSCatalogGroup")
+      groupNames.remove("SYSCoord")         
+      return groupNames   
+ 
+   def create_cl(self,srcGroupName):
+      try:
+         print( '---begin to create cs---')
+         self.cs = self.db.create_collection_space(cs_name)            
+         #create cl
+         option = {"ShardingKey":{'no':1},"ShardingType":'hash',"Group":srcGroupName}
+         self.cl = self.cs.create_collection(cl_name , option)
+         print( '---create cl success---' )   
+      except SDBError as e:
+         print(e.detail) 
+         raise e    
+  
+   def insert_datas(self):   
+      print( '---begin to insert records---' )
+      objectIds = [ObjectId("53bb5667c5d061d6f579d0bb"),\
+                   ObjectId("53bb5667c5d061d6f579d0bc"),\
+                   ObjectId("53bb5667c5d061d6f579d0bd"),\
+                   ObjectId("53bb5667c5d061d6f579d0be"),\
+                   ObjectId("53bb5667c5d061d6f579d0bf")]
+      for i in range(1,len(objectIds)):
+         try:
+            self.cl.insert({"_id":objectIds[i],"no":i,"a":"test" + str(i)})  
+         except SDBError as e:
+            print(e.detail) 
+            raise e  
+         
+   def split_cl(self,srcGroupName,destGroupName):   
+      print( '---begin to split---' )
+      try:
+         self.cl.split_async_by_percent(srcGroupName,destGroupName,50.0)
+      except (SDBTypeError, SDBBaseError) as e:
+         print(e) 
+         raise e               
+                             
+if __name__ == "__main__":
+    unittest.main() 
