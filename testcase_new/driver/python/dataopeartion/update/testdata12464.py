a417caf0e8bee9b10c414851111c3062cb6430b2
laojingtang <laojingtang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-05 02:43:04 +0000

    add testdata12464.py
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@31037 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/python/dataopeartion/update/testdata12464.py
@@ -0,0 +1,83 @@
+# @decription: data  opeartion
+# @testlink:   seqDB-12464
+# @author:     LaoJingTang 2017-8-30
+import unittest
+
+from lib import testlib
+from pysequoiadb.error import (SDBBaseError)
+
+
+class Data12464(unittest.TestCase):
+   def setUp(self):
+      testlib.print_setup_msg(self)
+      self.db = testlib.default_db()
+      self.create_cs_cl()
+
+   def update_test(self, list__expect, update, **kwargs):
+      for i in self.original_list:
+         self.cl.insert(i)
+
+      cur = self.cl.update(update, **kwargs)
+      list1 = self.get_records()
+      self.check_result_equal(list__expect, list1)
+
+      self.cl.delete()
+
+   def test(self):
+      self.original_list = ({"a": 0, "b": 0}, {"a": 1, "b": 1}, {"a": 2, "b": 2})
+      original_list = self.original_list
+
+      update = {"$inc": {"a": 1}}
+
+      # condition+update
+      condition = {"a": {"$et": 0}}
+      update_list = ({"a": 1, "b": 0}, {"a": 1, "b": 1}, {"a": 2, "b": 2})
+      self.update_test(update_list, update, condition=condition)
+
+      # hint+update
+      hint = {"": "index"}
+      l = ({"a": 1, "b": 0}, {"a": 2, "b": 1}, {"a": 3, "b": 2})
+      self.update_test(l, update, hint=hint)
+
+      # flags+update
+      QUERY_FLG_FORCE_HINT = 128
+      try:
+         self.update_test(l, update, flagss=QUERY_FLG_FORCE_HINT, hint=hint)
+      except SDBBaseError as e:
+         pass
+
+   def check_result_equal(self, expected, actual):
+      msg = "\nexpected: " + str(expected) + "\nactual: " + str(actual)
+      for x in actual:
+         self.assertIn(x, expected, msg)
+      for x in expected:
+         self.assertIn(x, actual, msg)
+
+   def get_records(self, cur=None):
+      if cur == None:
+         cur = self.cl.query()
+      items = list()
+      while True:
+         try:
+            item = cur.next()
+            item.pop('_id')
+            items.append(item)
+         except BaseException as e:
+            break
+      cur.close()
+      return items
+
+   def tearDown(self):
+      testlib.print_teardown_msg(self)
+      self.db.drop_collection_space(self.cs_name)
+      self.db.disconnect()
+
+   def create_cs_cl(self):
+      self.cs_name = self.__class__.__name__ + "_cs"
+      self.cl_name = self.__class__.__name__ + "_cl"
+      try:
+         self.db.drop_collection_space(self.cs_name)
+      except BaseException as e:
+         pass
+      self.cs = self.db.create_collection_space(self.cs_name)
+      self.cl = self.cs.create_collection(self.cl_name)
