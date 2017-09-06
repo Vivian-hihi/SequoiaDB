8ceafb80717847e83173dbd023f09741998d0a22
laojingtang <laojingtang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-06 04:43:25 +0000

    M       lib/sdbconfig.py
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@31071 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/python/lib/sdbconfig.py
+++ testcase_new/driver/python/lib/sdbconfig.py
@@ -1,14 +1,12 @@
-import os.path
 import json
 import os
-from pysequoiadb import client
+import os.path
 
 class SdbConfig(object):
-
    def __init__(self):
       self.get_config()
-      
-   def get_config(self): 
+
+   def get_config(self):
       config_file = os.path.join(os.getcwd(), "config.json")
       fp = open(config_file)
       configs = json.load(fp)
@@ -20,21 +18,8 @@ class SdbConfig(object):
       self.rsrv_node_dir = configs['RSRVNODEDIR']
       self.work_dir = configs['WORKDIR']
       self.break_on_failure = configs['BREAK_ON_FAILURE']
-      """
-      print(self.host_name)
-      print(self.service)
-      print(self.changed_prefix)
-      print(self.rsrv_port_begin)
-      print(self.rsrv_port_end)
-      print(self.rsrv_node_dir)
-      print(self.work_dir)
-      print(self.break_on_failure)
-      """
+
       fp.close()
 
-"""
-if __name__ == "__main__":
-   print str(id(default_db))
-   print str(id(default_db))
-   print str(id(default_db))
-"""
+sdb_config = SdbConfig()
+
