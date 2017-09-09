57abe6c91b28b58dec77364959559fc99d52b6e6
zhaowenbo <zhaowenbo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-09 07:26:31 +0000

    SEQUOIADBMAINSTREAM-2776:【Task】研究配置动态生效并将结果反映到官网 修改代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@31141 6121f079-4a18-41d3-8c30-8870a322f2c2

--- misc/autogen/dbConfForWeb.h
+++ misc/autogen/dbConfForWeb.h
@@ -20,6 +20,7 @@
 class OptEle
 {
 public:
+	std::string nametag ;
     std::string longtag ;
     std::string shorttag ;
     std::string typeofwebtag ;
@@ -57,11 +58,9 @@ class OptGenForWeb
     const char *language ;
     std::vector<OptOtherInfoEle*> optOtherInfo ;
     std::vector<OptEle*> optlist ;
-	boost::property_tree::ptree defaultTagValue ;
     void loadOtherInfoFromXML () ;
     void loadFromXML () ;
-	INT32 parseOptListTag( boost::property_tree::ptree::value_type &v,
-							   OptEle *newele ) ;
+	INT32 parseOptListTag( boost::property_tree::ptree::value_type &v ) ;
     std::string genOptions () ;
 	std::string genSupplement() ;
     void gendoc () ;
