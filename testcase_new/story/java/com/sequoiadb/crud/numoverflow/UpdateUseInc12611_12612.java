ff5b48c76ce96c0094d20a9a6dac19da7770e7d7
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-09-18 08:24:01 +0000

    新增数值溢出优化测试用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@31366 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/com/sequoiadb/crud/numoverflow/UpdateUseInc12611_12612.java
@@ -0,0 +1,140 @@
+package com.sequoiadb.crud.numoverflow;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.SdbTestBase;
+
+/**
+* FileName: UpdateUseInc12611_12612.java
+* test content:Numeric value overflow for single character using $inc operation,
+* 				and the $inc is used as a update.
+* testlink case:seqDB-12611
+* @author luweikang
+    * @Date    2017.9.15
+* @version 1.00
+*/
+
+public class UpdateUseInc12611_12612 extends SdbTestBase{
+	
+	private String clName = "inc_update12611_12612";
+	private Sequoiadb sdb = null;
+	private CollectionSpace cs = null;
+	private static DBCollection cl = null; 
+	
+	
+	String []records = {"{'no':-2147483648,'long':{'$numberLong':'-9223372036854775808'},'test':1}",
+			"{'no':{a:{b:1073741823}},'string':'123','test':2}",
+			"{'no':[-2147483648,[2147483647],{'$numberLong':'9223372036854775807'}],test:3}",
+			"{'no':2147483647,'long':{'$numberLong':'9223372036854775807'},'test':4}",
+			"{'no':2147483647,'long':{'$numberLong':'9223372036854775807'},'test':5}"};
+	
+	
+	@DataProvider(name = "operData")
+	public Object[][] generateIntDatas(){
+		String []expRecords1 = {"{'no':-2147493648,'long':{'$numberLong':'-9223372036854775808'},'test':1}"};
+		String []expRecords2 = {"{'no':-1073746824,'long':{'$numberLong':'-9223372036854775808'},'test':1}"};
+		String []expRecords3 = {"{'no':-1073746824,'long':{'$decimal':'-9223372037928522632'},'test':1}"};
+		String []expRecords4 = {"{'no':{a:{b:2147493647}},'string':'123','test':2}"};
+		String []expRecords5 = {"{'no':[-2147483648,[3221225471],{'$numberLong':'9223372036854775807'}],test:3}"};
+		String []expRecords6 = {"{'no':[-2147483648,[3221225471],{'$decimal':'9223372036854775808'}],test:3}"};
+		String []expRecords7 = {"{'no':2147483646.5,'long':9223372036854775807},'test':4}"};
+		String []expRecords8 = {"{'no':2147483646.5,'long':9223372036854775806.5},'test':4}"};
+		String []expRecords9 = {"{'no':2147483647.5,'long':9223372036854775807},'test':5}"};
+		String []expRecords10 = {"{'no':2147483647.5,'long':9223372036854775807.5},'test':5}"};
+		
+		String expJavaLong = "class java.lang.Long";
+		String expJavaDouble = "class java.lang.Double";
+		String expJavaDecimal = "class org.bson.types.BSONDecimal";
+		String expLongType = "int64";
+		String expDoubleType = "double";
+		String expDecimalType = "decimal";
+		
+		return new Object[][]{
+			//the parameters: int matcherValue,String updateName, String updateValue, String []expRecords,String expTypeToSdb,Boolean isVerifyTypeToJava, String expTypeToJava
+			new Object[]{1,"no",new Long(-10000),expRecords1,expLongType,true,expJavaLong},
+			new Object[]{1,"no",new Long(1073746824),expRecords2,expLongType,true,expJavaLong},
+			new Object[]{1,"long",new Integer(-1073746824),expRecords3,expDecimalType,true,expJavaDecimal},
+			new Object[]{2,"no.a.b",new Long(1073751824),expRecords4,expLongType,false,null},
+			
+			//SEQUOIADBMAINSTREAM-2795
+//			new Object[]{3,"no.1.0",new Long(1073741824),expRecords5,expLongType,false,null},
+//			new Object[]{3,"no.2",new Integer(1),expRecords6,expDecimalType,false,null},
+			//SEQUOIADBMAINSTREAM-2820
+//			new Object[]{4,"no",new Double(-0.5),expRecords7,expJavaDouble,true,expDoubleType},
+//			new Object[]{4,"long",new Double(-0.5),expRecords8,expJavaDouble,true,expDoubleType},
+//			new Object[]{5,"no",new Double(0.5),expRecords9,expJavaDouble,true,expDoubleType},
+//			new Object[]{5,"long",new Double(0.5),expRecords10,expJavaDouble,true,expDoubleType},
+//			
+		};
+	}
+	
+	@BeforeClass
+	public void setUp(){
+		System.out.println(this.getClass().getName()+" begin at "
+					+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));
+		try{
+			sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		}catch(BaseException e){			
+			Assert.assertTrue(false,"connect %s failed,"+coordUrl+e.getMessage());
+		}
+		
+		cs = sdb.getCollectionSpace(SdbTestBase.csName);
+		cl = Commlib.createCL(cs, clName);
+		String []records = {"{'no':-2147483648,'long':{'$numberLong':'-9223372036854775808'},'test':1}",
+		        			"{'no':{a:{b:1073741823}},'string':'123','test':2}",
+		        			"{'no':[-2147483648,[2147483647],{'$numberLong':'9223372036854775807'}],test:3}",
+		        			"{'no':2147483647,'long':{'$numberLong':'9223372036854775807'},'test':4}",
+		        			"{'no':2147483647,'long':{'$numberLong':'9223372036854775807'},'test':5}"};
+
+		Commlib.insert(cl, records);
+	}
+	
+	@Test(dataProvider = "operData")
+	public void testInc(int matcherValue,String updateName, Object updateValue, String []expRecords,String expTypeToSdb,Boolean isVerifyTypeToJava, String expTypeToJava){
+		try{			
+			BSONObject uValue = new BasicBSONObject();
+			uValue.put(updateName, updateValue);
+			Commlib.updateOper(cl, matcherValue, uValue, "update"); 
+			Commlib.checkUpdateResult(cl, matcherValue, expRecords);
+			try {
+				Commlib.checkUpdateDataType(cl, matcherValue, updateName, expTypeToSdb, isVerifyTypeToJava, expTypeToJava);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}catch(BaseException e){			
+			Assert.assertTrue(false,"upsert numeric value overflowoper failed,"+e.getMessage());
+		}		
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		try{
+			System.out.println(this.getClass().getName()+" end at "
+					 +new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date()));			
+			CollectionSpace cs = sdb.getCollectionSpace(SdbTestBase.csName);
+			if(cs.isCollectionExist(clName)){
+				cs.dropCollection(clName);
+			}			
+		}catch(BaseException e){
+			Assert.fail("clear env failed, errMsg:" + e.getMessage());
+		}finally{
+			if(sdb != null){
+				sdb.close();
+			}
+		}
+	}
+}
