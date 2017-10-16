faae4d00dd4c2cc949373589e1d327aaa70283f4
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-16 11:45:04 +0000

    回归问题单2689，新增固定集合可靠性用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@31888 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/src/com/sequoiadb/cappedCL/Utils.java
+++ testcase_new/reliability/src/com/sequoiadb/cappedCL/Utils.java
@@ -6,12 +6,18 @@ import java.util.Date;
 import java.util.List;
 
 import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
 
+import java.util.Random;
+
+import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.commlib.GroupWrapper;
+import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.exception.ReliabilityException;
 import org.bson.types.ObjectId;
+import org.testng.Assert;
 
 public class Utils {
 
@@ -53,6 +59,84 @@ public class Utils {
                 }
             }
         }
+    }
+	
+	/*
+	 * insert records
+	 * @param cl
+	 * @param insertNums
+	 * @param strLength
+	 */
+	public static void insertRecords(DBCollection cl,int insertNums,int strLength) {
+		BSONObject insertObj = new BasicBSONObject();
+		insertObj.put("a", getRandomString(strLength));
+		for(int i = 0; i < insertNums; i++) {
+			try {
+				cl.insert(insertObj);
+			}catch (BaseException e) {
+				throw e;
+			}   	
+		}
+    }
+	
+	/*
+	 * pop records
+	 * @param cl
+	 * @param logicalID
+	 * @param direction
+	 */
+	public static void pop(DBCollection cl,long logicalID,int direction) {
+		BSONObject popObj = new BasicBSONObject();
+		popObj.put("LogicalID", logicalID);
+		popObj.put("Direction", direction);
+		try {
+			cl.pop(popObj);
+		}catch (BaseException e) {
+			throw e;
+		}   	
+    }
+	
+	/*
+	 * get one logicalID in capped CL
+	 * @param cl
+	 * @param skip
+	 */
+	public static long getLogicalID(DBCollection cl,long skip) {
+		long logicalID = -1;
+		BSONObject orderBy = new BasicBSONObject();
+		orderBy.put("_id", 1);
+		DBCursor query = null;
+		try {
+			long returnOne = 1;
+			query = cl.query(null,null,orderBy,null,skip,returnOne);
+			while(query.hasNext()) {
+				logicalID = (long)query.getNext().get("_id");
+			}
+		}catch (BaseException e) {
+			throw e;
+		}finally {
+			if(query != null) {
+				query.close();
+			}
+			System.out.println("logicalID: " + logicalID);
+		} 
+		return logicalID;
+    }
+	
+	/*
+	 * get random string for records
+	 * @param length
+	 */
+	public static String getRandomString(int length) {
+		Random random = new Random();
+        String baseStr = "abcdefghijklmnopqrstuvwxyz";
+        int baseLength = baseStr.length();
+        StringBuffer strBuf = new StringBuffer();
+        for (int i = 0; i < length; ++i) {
+            int index = random.nextInt(baseLength);
+            strBuf.append(baseStr.charAt(index));
+        }
+        return strBuf.toString();
     }
 
     public static void printBeginTime(Object object) {
