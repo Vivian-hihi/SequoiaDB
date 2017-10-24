98af4825388de418d1ac3a06f57515e124bf6e41
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-10-24 01:58:30 +0000

    补充replace用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32038 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/updateShardingKey/updateShardingKey_replace_13052.js
@@ -0,0 +1,42 @@
+/************************************
+*@Description: update ShardingKey use operator $replace,set the flag is true,update the shardingKey;
+*@author:      wuyan
+*@createdate:  2017.7.20
+**************************************/
+var clName = CHANGEDPREFIX + "_updateShardingKey_13052";
+function main()
+{
+   if( true == commIsStandalone( db ) )
+   {
+      println( "run mode is standalone" );
+      return;
+   }
+   
+   //clean environment before test
+   commDropCL( db, COMMCSNAME, clName, true, true,"drop CL in the beginning" ) ;
+   
+   //create cl
+   var options = {ShardingKey:{no:1}, ShardingType:"range",ReplSize:0,Compressed:true};
+   var dbcl = commCreateCLByOption( db, COMMCSNAME, clName, options, true, true );
+   
+   //insert data 	
+   var doc = [{no:"testno",a:1},{no:["test2"],a:2},
+	            {no:2,a:["test1","test2"]}];
+   insertData(dbcl, doc);
+	
+	//updateShardingKey use $replace
+	var updateCondition = {$replace:{no:5,a:{$date:"2017-07-20"}}};
+	updateData( dbcl, updateCondition )
+	
+	//check the update result
+	var expRecs = [{no:5,a:{$date:"2017-07-20"}},
+	                  {no:5,a:{$date:"2017-07-20"}},
+	                  {no:5,a:{$date:"2017-07-20"}}];;
+   checkResult( dbcl, null,null, expRecs, {_id:1} ); 	
+   
+   // drop collectionspace in clean
+   commDropCL( db, COMMCSNAME, clName, false, false,
+             "drop colleciton in the end" );
+   
+}
+main();
