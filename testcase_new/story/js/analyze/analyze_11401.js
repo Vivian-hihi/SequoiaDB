5fa10a431d6a1a87ac08d389e90821c402c45603
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-09 15:44:09 +0000

    上传analyze用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32377 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/analyze/analyze_11401.js
@@ -0,0 +1,99 @@
+/************************************
+*@Description: alter cl更新统计信息  
+*@author:      liuxiaoxuan
+*@createdate:  2017.11.09
+*@testlinkCase: seqDB-11401
+**************************************/
+function main()
+{
+	if(commIsStandalone(db))
+	{
+		println("skip standalone environment");
+		return;
+	}
+	
+   var csName = COMMCSNAME + "11401";
+	commDropCS( db, csName, true, "drop CS in the beginning" );
+	
+   commCreateCS( db, csName, false, "" );
+		
+	//create cl	
+	var clName = COMMCLNAME + "11401";
+	var dbcl = commCreateCL( db, csName, clName );
+	
+	//create index
+	commCreateIndex( dbcl, "b", {b : 1}, false )
+	
+	//insert
+	var insertNums = 3000;
+   insertDatas( dbcl, insertNums );
+	
+	//check before invoke analyze
+	checkStat( db, csName, clName, "b", false, false );
+	
+	//check the query explain of master/slave nodes 
+	var findConf = {b : 9000};
+   var expExplains = [{ScanType:"ixscan", IndexName:"b", ReturnNum:insertNums}];
+   
+   db.setSessionAttr( { PreferedInstance: "m" } );
+   checkExplain( db, csName, clName, findConf, null, null, expExplains )
+	
+   db.setSessionAttr( { PreferedInstance: "s" } );
+   checkExplain( db, csName, clName, findConf, null, null, expExplains )
+	
+	println("check result before analyze success!");
+	
+	//invoke analyze
+	var options = {CollectionSpace: csName};
+	analyze( db, options );
+	
+   //check after analyze
+	checkStat( db, csName, clName, "b", true, true );
+   
+   //check the query explain of master/slave nodes 
+	var findConf1 = {b : 9000};
+	var findConf2 = {a : 9000};
+   var expExplains = [{ScanType:"tbscan", IndexName:"", ReturnNum:insertNums}];
+	
+   db.setSessionAttr( { PreferedInstance: "m" } );
+   checkExplain( db, csName, clName, findConf1, null, null, expExplains )
+	checkExplain( db, csName, clName, findConf2, null, null, expExplains )
+   
+   db.setSessionAttr( { PreferedInstance: "s" } );
+   checkExplain( db, csName, clName, findConf1, null, null, expExplains )
+	checkExplain( db, csName, clName, findConf2, null, null, expExplains )
+
+	//alter cl
+	alterCL(csName, clName);
+	
+	//check after alter
+	var findConf1 = {b : 9000};
+   var expExplains1 = [{ScanType:"tbscan", IndexName:"", ReturnNum:insertNums}];
+	
+	var findConf2 = {a : 9000};
+   var expExplains2 = [{ScanType:"ixscan", IndexName:"$shard", ReturnNum:insertNums}];
+	
+   db.setSessionAttr( { PreferedInstance: "m" } );
+   checkExplain( db, csName, clName, findConf1, null, null, expExplains1 )
+	checkExplain( db, csName, clName, findConf2, null, null, expExplains2 )
+   
+   db.setSessionAttr( { PreferedInstance: "s" } );
+   checkExplain( db, csName, clName, findConf1, null, null, expExplains1 )
+	checkExplain( db, csName, clName, findConf2, null, null, expExplains2 )
+	
+   println("check result after analyze success!");
+}
+
+function alterCL( csName, clName )
+{
+   try
+   {
+		var cl = db.getCS(csName).getCL(clName);
+      cl.alter( {ShardingKey: {a : 1}, ShardingType: "hash"} );
+   }
+	catch(e)
+   {
+      throw buildException("alter CL", e, "alter", "alter success", e);
+   }
+}
+main();
