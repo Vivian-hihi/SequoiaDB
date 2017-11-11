b2662c552bc9e445ce0406aff49286542afff67f
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-11 02:30:06 +0000

    新增公共方法
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32411 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/analyze/commlib.js
+++ testcase_new/story/js/analyze/commlib.js
@@ -134,6 +134,8 @@ function checkStat( db, csName, clName, indexName, clExistStat, indexExistStat )
       while(!lsnFlag)
       {
          lsnFlag = checkLSN(db, groups[i]);
+         //println("check primary and slave node lsn flag:" + lsnFlag);
+         sleep(500);
       }
       
       var rg = db.getRG(groups[i]);
@@ -243,4 +245,170 @@ function checkExplain( dbcl, findConf, sortConf, hintConf, expExplains )
    	}
    }
    
+}
+
+/************************************
+*@Description: get SrcGroup and TargetGroup info,the groups information
+               include GroupName,HostName and svcname
+*@author:      wuyan
+*@createdate:  2015.10.14
+**************************************/
+function getSplitGroups(csName,clName,targetGrMaxNums)
+{
+   var allGroupInfo =  getGroupName(db, true);    
+   var srcGroupName = getSrcGroup(csName, clName );
+   var splitGroups = new Array();
+   if( targetGrMaxNums >= allGroupInfo.length-1 ) 
+   {
+      targetGrMaxNums = allGroupInfo.length-1;      
+   }
+   var index =1;
+   
+   for( var i = 0 ; i != allGroupInfo.length ; ++i )
+   {
+      if( srcGroupName == allGroupInfo[i][0] )
+      {
+         splitGroups[0] = new Object();
+			splitGroups[0].GroupName = allGroupInfo[i][0];
+			splitGroups[0].HostName = allGroupInfo[i][1];
+			splitGroups[0].svcname = allGroupInfo[i][2];	
+      }
+      else 
+      {
+         if (index > targetGrMaxNums)
+         {
+            continue;
+         }      
+         splitGroups[index] = new Object();
+			splitGroups[index].GroupName = allGroupInfo[i][0];
+			splitGroups[index].HostName = allGroupInfo[i][1];
+			splitGroups[index].svcname = allGroupInfo[i][2];			          
+         index++;                  
+      }
+   }   
+   return splitGroups;
+   
+}
+
+/************************************
+*@Description: get the informations of the srcGroups and targetGroups,then split cl with different options,
+               only split 1 times
+               return the informations of the srcGroups and targetGroups
+*@author:      wuyan
+*@createdate:  2015.10.14
+**************************************/
+function ClSplitOneTimes( csName, clName, startCondition, endCondition )
+{
+	try
+	{
+	   var targetGroupNums = 1;
+      var groupsInfo = getSplitGroups(csName,clName,targetGroupNums);
+      var srcGrName = groupsInfo[0].GroupName;    
+      var tarGrName = groupsInfo[1].GroupName;
+      println(csName+"."+clName+"'s target group: "+tarGrName);
+      var CL = db.getCS(csName).getCL(clName);
+      println("--begin split") 
+		if ( typeof(startCondition) === "number" ) //percentage split
+		{
+			CL.split( srcGrName, tarGrName, startCondition );
+		}
+		else if ( typeof(startCondition) === "object" && endCondition === undefined ) //range split without end condition
+		{
+			CL.split( srcGrName, tarGrName, startCondition );
+			println("startCondition="+startCondition)
+		}
+		else if ( typeof(startCondition) === "object" && typeof(endCondition) === "object" ) //range split with end condition
+		{
+			CL.split( srcGrName, tarGrName, startCondition, endCondition );
+		}	
+		println("--end split")
+	}
+	catch ( e )
+	{
+		throw e;
+	}
+	return groupsInfo;
+}
+
+/************************************
+*@Description: get Group name and Service name
+*@author：wuyan 2015/10/20
+**************************************/
+function getGroupName(db, mustBePrimary)
+{
+   var RGname = null ;
+   try
+   {
+      RGname = db.listReplicaGroups().toArray();
+   }
+   catch (e)
+   {
+      throw e;
+   }
+   var j = 0;
+   var arrGroupName = Array();
+   for (var i=1 ; i != RGname.length ; ++i )
+   {
+      var eRGname = eval('('+RGname[i]+')') ;   
+      if( 1000 <= eRGname["GroupID"] )
+      {
+         arrGroupName[j] = Array();
+         var primaryNodeID = eRGname["PrimaryNode"] ;
+         var groups = eRGname["Group"] ;
+         for ( var m = 0; m < groups.length; m++ )
+         {  
+            if ( true == mustBePrimary )
+            {
+               var nodeID = groups[m]["NodeID"] ;
+               if ( primaryNodeID != nodeID )
+                  continue ;
+            }               
+            arrGroupName[j].push(eRGname["GroupName"]) ;
+            arrGroupName[j].push(groups[m]["HostName"]) ;
+            arrGroupName[j].push(groups[m]["Service"][0]["Name"]) ;
+            break ;
+         }
+         ++j;
+      }
+   }
+   return arrGroupName;
+}
+
+/************************************
+*@Description: get SrcGroup name,update getPG to getSrcGroup
+*@author:      wuyan
+*@createdate:  2015.10.14
+**************************************/
+function getSrcGroup( csName, clName )
+{
+   try
+   {
+      if( undefined == csName || undefined == clName )
+      {
+         println( "cs name: " + csName + ", clName: " + clName ) ;
+         throw "cs or cl name is undefined" ;
+      }
+      var tableName = csName + "." + clName ;
+      var cataMaster = db.getCatalogRG().getMaster().toString().split(":");
+      var catadb = new Sdb( cataMaster[0],cataMaster[1] ) ;
+      var Group = catadb.SYSCAT.SYSCOLLECTIONS.find().toArray() ;
+      var srcGroupName ;
+      for( var i = 0 ; i < Group.length ; ++i )
+      {
+         var eachID = eval("("+Group[i]+")") ;
+         if( tableName == eachID["Name"] )
+         {
+            srcGroupName = eachID["CataInfo"][0]["GroupName"] ;
+            println( csName + "." + clName + "'s source group: " + srcGroupName );
+            break ;
+         }
+      }
+      return srcGroupName ;
+   }
+   catch( e )
+   {
+      println( "failed to get source group, cs name: " + csName +
+               ", cl name: " + clName ) ;
+      throw e ;
+   }
 }
