990b2364072bede703b90751c7bf8ceb129d7f81
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-13 09:17:56 +0000

    新增analyze自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32457 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/analyze/analyze_commCL_11623.js
@@ -0,0 +1,151 @@
+﻿/************************************
+*@Description: 指定普通cl生成默认统计信息并手工修改统计信息再清空 
+*@author:      zhaoyu
+*@createdate:  2017.11.13
+*@testlinkCase:seqDB-11623
+**************************************/
+function main()
+{
+   var clName = COMMCLNAME + "_11623";
+   var insertNum = 2000;
+	var sameValues = 9000;
+   
+   //清理环境
+   commDropCL( db, COMMCSNAME, clName, true, true,"drop CL in the beginning" ) ;
+   
+   //创建cl
+   var dbcl = commCreateCL( db, COMMCSNAME, clName);
+   
+   //创建索引
+   commCreateIndex( dbcl, "a", {a:1});
+   
+   //插入记录
+	insertDiffDatas( dbcl, insertNum );
+	insertSameDatas( dbcl, insertNum, sameValues );
+	
+   //执行统计
+   analyze( db, {Collection: COMMCSNAME + "." + clName} );
+   
+   //获取主备节点
+   db.setSessionAttr( { PreferedInstance: "m" } );
+   var dbclPrimary = db.getCS(COMMCSNAME).getCL(clName);
+   db.setSessionAttr( { PreferedInstance: "s" } );
+   var dbclSlave = db.getCS(COMMCSNAME).getCL(clName);
+   
+   //检查统计信息
+   checkStat( db, COMMCSNAME, clName, "a", true, true );
+   
+   //检查主备节点访问计划
+   var findConf = {a:sameValues};
+   var expExplains = [{ScanType:"tbscan", IndexName:"", ReturnNum:insertNum}];
+   
+   var actExplains = getCommonExplain( dbclPrimary, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   var actExplains = getCommonExplain( dbclSlave, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   println("check result after analyze success!");
+   
+   //生成默认统计信息
+   analyze( db, {Mode:3, Collection: COMMCSNAME + "." + clName} );
+   
+   //检查统计信息
+   checkStat( db, COMMCSNAME, clName, "a", true, false );
+   
+   //检查主备节点访问计划
+   var findConf = {a:sameValues};
+   var expExplains = [{ScanType:"ixscan", IndexName:"a", ReturnNum:insertNum}];
+   
+   var actExplains = getCommonExplain( dbclPrimary, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   var actExplains = getCommonExplain( dbclSlave, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   println("check result analyze mode set 3 success!");
+   
+   //手工修改主节点统计信息
+   var mcvValues = [{a:8000},{a:9000},{a:9001}];
+   var fracs = [50,5000,50];
+   updateIndexStateInfo( db, COMMCSNAME, clName, "a", mcvValues, fracs );
+   
+   //统计信息加载至缓存
+   analyze( db, {Mode:4, Collection: COMMCSNAME + "." + clName} );
+   
+   //检查统计信息
+   checkStat( db, COMMCSNAME, clName, "a", true, true );
+   
+   //检查主备节点访问计划
+   var findConf = {a:sameValues};
+   var expExplains = [{ScanType:"tbscan", IndexName:"", ReturnNum:insertNum}];
+   
+   var actExplains = getCommonExplain( dbclPrimary, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   var actExplains = getCommonExplain( dbclSlave, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   println("check result analyze mode set 4 success!");
+   
+   //清空统计信息
+   analyze( db, {Mode:5, Collection: COMMCSNAME + "." + clName} );
+   
+   //检查统计信息
+   checkStat( db, COMMCSNAME, clName, "a", true, true );
+   
+   //检查主备节点访问计划
+   var findConf = {a:sameValues};
+   var expExplains = [{ScanType:"tbscan", IndexName:"", ReturnNum:insertNum}];
+   
+   var actExplains = getCommonExplain( dbclPrimary, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   var actExplains = getCommonExplain( dbclSlave, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   println("check result analyze mode set 5 success!");
+   
+   //再次更新统计信息
+   var mcvValues = [{a:8000},{a:9000},{a:9001}];
+   var fracs = [50,50,50];
+   updateIndexStateInfo( db, COMMCSNAME, clName, "a", mcvValues, fracs );
+   
+   //检查统计信息
+   checkStat( db, COMMCSNAME, clName, "a", true, true );
+   
+   //检查主备节点访问计划
+   var findConf = {a:sameValues};
+   var expExplains = [{ScanType:"tbscan", IndexName:"", ReturnNum:insertNum}];
+   
+   var actExplains = getCommonExplain( dbclPrimary, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   var actExplains = getCommonExplain( dbclSlave, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   println("check result after update index stat but no analyze success!");
+   
+   //再次清空缓存
+   analyze( db, {Mode:5, Collection: COMMCSNAME + "." + clName} );
+   
+   //检查统计信息
+   checkStat( db, COMMCSNAME, clName, "a", true, true );
+   
+   //检查主备节点访问计划
+   var findConf = {a:sameValues};
+   var expExplains = [{ScanType:"ixscan", IndexName:"a", ReturnNum:insertNum}];
+   
+   var actExplains = getCommonExplain( dbclPrimary, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   var actExplains = getCommonExplain( dbclSlave, findConf);
+   checkExplain( actExplains, expExplains )
+   
+   println("check result after update index stat and analyze mode set 5 success!");
+   
+   //清理环境
+   //commDropCL( db, COMMCSNAME, clName, true, true,"drop CL in the end" );
+  
+ }
+ main()
