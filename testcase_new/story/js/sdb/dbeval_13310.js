bb6e13a4c315da8f9502b3f139223e504939dd82
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-13 05:22:29 +0000

    回归问题单2930，增加用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32443 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/sdb/dbeval_13310.js
@@ -0,0 +1,31 @@
+/******************************************************************************
+*@Description : test get collection with eval
+*               TestLink : seqDB-13310:执行eval获取集合后插入查询数据
+*@auhor       : Liang XueWang
+******************************************************************************/
+function main( db )
+{
+   var clName = CHANGEDPREFIX + "_cl13310" ;
+   commCreateCL( db, COMMCSNAME, clName ) ;
+    
+   try
+   {
+      var code = "db." + COMMCSNAME + "." + clName ;
+      var cl = db.eval( code ) ;
+      cl.insert( { a: 1 } ) ;
+   }
+   catch( e )
+   {
+      throw buildException( "main", e, "eval cl " + code, 0, e ) ;
+   }
+   
+   var aVal = cl.find( {}, { a: "" } ).next().toObj()["a"] ;
+   if( aVal !== 1 )
+   {
+      throw buildException( "main", null, "check find result", 1, aVal ) ;
+   } 
+    
+   commDropCL( db, COMMCSNAME, clName ) ;
+}
+
+main( db ) ;
