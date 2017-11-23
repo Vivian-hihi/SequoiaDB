4ef51d8bcd447c15ec3f4a9c777e1d0c9cf34857
laojingtang <laojingtang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-23 11:35:41 +0000

    update TimePrinterListener
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32713 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/testcommon/TimePrinterListener.java
+++ testcase_new/story/java/com/sequoiadb/testcommon/TimePrinterListener.java
@@ -10,35 +10,32 @@ import java.util.Date;
  * Created by laojingtang on 17-11-23.
  */
 public class TimePrinterListener extends TestListenerAdapter {
-    // if you want some package use this listener,
-    // you should add the package name to packageArray.
-    private String[] packageArray = {"com.sequoiadb.lob.randomwrite"};
 
     @Override
     public void onConfigurationSuccess(ITestResult itr) {
         super.onConfigurationSuccess(itr);
-        if (itr.getMethod().isAfterClassConfiguration() && isPrint(itr))
+        if (itr.getMethod().isAfterClassConfiguration())
             printEndTime(itr);
     }
 
     @Override
     public void onConfigurationFailure(ITestResult itr) {
         super.onConfigurationFailure(itr);
-        if (itr.getMethod().isAfterClassConfiguration() && isPrint(itr))
+        if (itr.getMethod().isAfterClassConfiguration())
             printEndTime(itr);
     }
 
     @Override
     public void onConfigurationSkip(ITestResult itr) {
         super.onConfigurationSkip(itr);
-        if (itr.getMethod().isAfterClassConfiguration() && isPrint(itr))
+        if (itr.getMethod().isAfterClassConfiguration())
             printEndTime(itr);
     }
 
     @Override
     public void beforeConfiguration(ITestResult tr) {
         super.beforeConfiguration(tr);
-        if (tr.getMethod().isBeforeClassConfiguration() && isPrint(tr))
+        if (tr.getMethod().isBeforeClassConfiguration())
             printBeginTime(tr);
     }
 
@@ -48,15 +45,6 @@ public class TimePrinterListener extends TestListenerAdapter {
 
     private void printEndTime(ITestResult tr) {
         System.out.println(getTestMethodName(tr) + " end: " + getCurTimeStr());
-    }
-
-    private boolean isPrint(ITestResult tr) {
-        String s = tr.getTestClass().getRealClass().getPackage().getName();
-        for (String s1 : packageArray) {
-            if (s.equals(s1))
-                return true;
-        }
-        return false;
     }
 
     private String getTestMethodName(ITestResult tr) {
