64b291489625b554ac031bfc57c248d9106caa20
laojingtang <laojingtang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-24 02:14:54 +0000

    catch -219 error code
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32731 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/python/task/testtask.py
+++ testcase_new/driver/python/task/testtask.py
@@ -4,10 +4,11 @@
  @testlink:   测试用例 seqDB-12495 :: 版本: 1 :: 列出/取消/等待任务
  @author:     laojingtang
 """
-from pysequoiadb.errcode import SDB_CAT_TASK_NOTFOUND
+from pysequoiadb import SDBError
+from pysequoiadb.errcode import SDB_CAT_TASK_NOTFOUND, SDB_TASK_ALREADY_FINISHED
 
 from lib import testlib
-from pysequoiadb import SDBError
+
 
 class TestTask12495(testlib.SdbTestBase):
    def setUp(self):
@@ -26,23 +27,23 @@ class TestTask12495(testlib.SdbTestBase):
       cl_option = {"ShardingKey": {"a": 1}, "ShardingType": "hash", "Group": self.g1_name}
       testlib.drop_cs(self.db, self.cs_name, ignore_not_exist=True)
       self.cs = self.db.create_collection_space(self.cs_name)
-      self.cl = self.cs.create_collection(self.cl_name,cl_option)
+      self.cl = self.cs.create_collection(self.cl_name, cl_option)
 
    def tearDown(self):
       if self.should_clean_env():
          self.db.drop_collection_space(self.cs_name)
 
    def test_task(self):
-      list=[{"a":i} for i in range(10000)]
-      self.cl.bulk_insert(0,list)
-      id=self.cl.split_async_by_percent(self.g1_name,self.g2_name,50.0)
+      list = [{"a": i} for i in range(10000)]
+      self.cl.bulk_insert(0, list)
+      id = self.cl.split_async_by_percent(self.g1_name, self.g2_name, 50.0)
       import sys
       if sys.version_info[0] != 3:
-         id=long(id);
+         id = long(id);
       self.db.list_tasks()
       try:
-         self.db.cancel_task(task_id=id,is_async=True)
+         self.db.cancel_task(task_id=id, is_async=True)
       except SDBError as e:
-         if e.errcode!=SDB_CAT_TASK_NOTFOUND:
+         if e.errcode != SDB_CAT_TASK_NOTFOUND or e.errcode != SDB_TASK_ALREADY_FINISHED:
             raise e
-      self.db.wait_task([id],1)
+      self.db.wait_task([id], 1)
