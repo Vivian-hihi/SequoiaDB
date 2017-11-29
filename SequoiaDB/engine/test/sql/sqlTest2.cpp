6efda4bde6807b0f37bb6eac16dd1a285929fd61
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2017-11-29 05:44:58 +0000

    opt: 优化sql测试程序输入
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@32876 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/test/sql/sqlTest2.cpp
+++ SequoiaDB/engine/test/sql/sqlTest2.cpp
@@ -159,6 +159,12 @@ TEST(sqlTest, parse_1)
       ossMemset( line, 0, SQL_READLINE_LEN ) ;
       ossMemset( dumpBuf, 0, SQL_DUMPBUFF_LEN ) ;
 
+      if ( qgm )
+      {
+         SDB_OSS_DEL qgm ;
+         qgm = NULL ;
+      }
+
       rc = readInput( "sql", 0, line, SQL_READLINE_LEN ) ;
       if ( rc )
       {
@@ -199,31 +205,39 @@ TEST(sqlTest, parse_1)
 
             qgmOptiTreeNode *e = NULL ;
             rc = qgm->extend( e ) ;
+            if ( e )
+            {
+               qgm = e ;
+            }
+
             if ( SDB_OK != rc )
             {
-               cout << "rc = " << rc << endl ;
-               break ;
+               cout << "***ERROR*** Extend failed, rc = " << rc << endl ;
+               continue ;
             }
             else
             {
                cout << "After extent, tree dump:" << endl ;
                e->dump() ;
-               qgm = e ;
             }
 
             // optimizer
             qgmOptTree tree( qgm ) ;
             optQgmOptimizer optimizer ;
             rc = optimizer.adjust( tree ) ;
+            if ( qgm != tree.getRoot() )
+            {
+               qgm = tree.getRoot() ;
+            }
+
             if ( SDB_OK != rc )
             {
-               cout << "opt failed, rc = " << rc << endl ;
-               break ;
+               cout << "***ERROR*** Optimize failed, rc = " << rc << endl ;
+               continue ;
             }
             else
             {
                cout << "After optimizer, tree dump:" << endl ;
-               qgm = tree.getRoot() ;
                qgm->dump() ;
             }
 
@@ -234,12 +248,15 @@ TEST(sqlTest, parse_1)
                container.plan() = plan ;
                rc = qgmDump( &container, dumpBuf, 1024*1024*10) ;
                if ( SDB_OK == rc )
-               cout << dumpBuf << endl ;
+               {
+                  cout << dumpBuf << endl ;
+               }
             }
             else
             {
-               cout << "rc:" << rc << endl ;
-               break ;
+               cout << "***ERROR*** Build plan tree failed, rc = "
+                    << rc << endl ;
+               continue ;
             }
 
             SDB_OSS_DEL qgm ;
