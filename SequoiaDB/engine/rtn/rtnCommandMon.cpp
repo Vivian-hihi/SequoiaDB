1d3e55d79b71468faf40d34e0fa48603b0362783
heguoming <heguoming@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-01-29 07:05:53 +0000

    SEQUOIADBMAINSTREAM-3237 : snapshot和list中用户定义的agg提示判断order是否存在有误
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34031 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnCommandMon.cpp
+++ SequoiaDB/engine/rtn/rtnCommandMon.cpp
@@ -286,7 +286,7 @@ namespace engine
          }
 
          /// order by
-         if ( orderBy.isEmpty() )
+         if ( !orderBy.isEmpty() )
          {
             rc = appendObj( BSON( AGGR_SORT_PARSER_NAME << orderBy ),
                             pOutBuff, buffSize, buffUsedSize, buffObjNum ) ;
