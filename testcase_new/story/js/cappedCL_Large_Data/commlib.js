6b5eee4fe17eed26abbedcfcbdafcd259c3e6a7d
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-02-08 07:49:24 +0000

    固定集合新增自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34249 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/cappedCL_Large_Data/commlib.js
+++ testcase_new/story/js/cappedCL_Large_Data/commlib.js
@@ -257,3 +257,168 @@ function checkRec( rc, expRecs )
    	}
    }
 }
+
+
+/************************************
+*@Description: 获取所有组的数据节点
+*@author:      zhaoyu 
+*@createDate:  2017.11.8
+**************************************/
+function getNodesInGroups(db, groups)
+{
+   var datas = new Array();
+   
+   //standalone
+   if(true === commIsStandalone(db))
+   {
+      datas[0] = Array();
+      datas[0][0] = db;
+   }else{
+      for (var i = 0 ; i < groups.length; ++i)
+      {
+         datas[i] = Array();
+         
+         var rg = db.getRG(groups[i]);
+         var rgDetail = eval( "(" + rg.getDetail().toArray()[0] + ")");
+         var nodesInGroup = rgDetail.Group;
+         for(var j = 0; j < nodesInGroup.length; ++j)
+         {
+            var hostName = nodesInGroup[j].HostName;
+            var serviceName = nodesInGroup[j].Service[0].Name;
+            datas[i][j] = new Sdb(hostName, serviceName);                                                                                                                                 
+         }
+         
+      }
+   }
+   return datas;
+}
+
+/************************************
+*@Description: 获取主节点的lsn
+*@author:      liuxiaoxuan
+*@createDate:  2017.01.29
+**************************************/
+function getPrimaryNodeLSNs(db, groups)
+{
+   
+   var datas = getNodesInGroups(db, groups);
+   
+   var LSNs = new Array();
+   for(var i = 0; i < datas.length; ++i)
+   {
+      var nodesInGroup = datas[i];
+      LSNs[i] = Array();
+      for(var j = 0; j < nodesInGroup.length; ++j)
+      { 
+         var getSnapshot6 = eval( "(" + nodesInGroup[j].snapshot(6).toArray()[0] + ")" );
+         
+         var completeLSN = getSnapshot6.CompleteLSN;
+         var isPrimary = getSnapshot6.IsPrimary;
+         if(isPrimary)
+         {
+            LSNs[i][0] = completeLSN;
+            break;
+         }   
+      }
+   }
+   
+   return LSNs;
+}
+
+/************************************
+*@Description: 获取备节点的lsn
+*@author:      liuxiaoxuan
+*@createDate:  2017.01.29
+**************************************/
+function getSlaveNodeLSNs(db, groups)
+{
+   var datas = getNodesInGroups(db, groups);
+   
+   var LSNs = new Array();
+   for(var i = 0; i < datas.length; ++i)
+   {
+      var nodesInGroup = datas[i];
+      LSNs[i] = Array();
+      var f = 0;
+      for(var j = 0; j < nodesInGroup.length; ++j)
+      { 
+         var getSnapshot6 = eval( "(" + nodesInGroup[j].snapshot(6).toArray()[0] + ")" );
+         
+         var completeLSN = getSnapshot6.CompleteLSN;
+         var isPrimary = getSnapshot6.IsPrimary;
+         if(!isPrimary)
+         {
+            LSNs[i][f++] = completeLSN;
+         }   
+      }
+   }
+   
+   return LSNs;
+}
+
+/************************************
+*@Description: 检查cl所在组的主备节点lsn是否一致(超时600s)
+*@author:      liuxiaoxuan
+*@createDate:  2017.01.29
+**************************************/
+function checkConsistency(db, csName, clName, groups)
+{
+   if(csName == null) { var csName = "UNDEFINED"; }
+   if(clName == null) { var clName = "UNDEFINED"; }
+   if(groups == undefined) { var groups = commGetCLGroups( db, csName + "." + clName ); }
+   
+   //the longest waiting time is 600S
+   var lsnFlag = false;
+   var timeout = 600;
+   var doTimes = 0; 
+   
+   //get primary nodes
+   var primaryNodeLSNs = getPrimaryNodeLSNs(db, groups);
+   while(true)
+   {
+      lsnFlag = checkLSN(db, groups, primaryNodeLSNs);
+      if(!lsnFlag)
+      {
+         if(doTimes < timeout)
+         {
+            ++doTimes;
+            sleep(1000);
+         }
+         else
+         {
+            throw "check lsn time out";
+         }     
+      }
+      else 
+      {
+         break;
+      }
+   }
+   
+}
+
+/************************************
+*@Description: 获取当前主备节点lsn是否一致的状态
+*@author:      zhaoyu
+*@createDate:  2017.11.8
+**************************************/
+function checkLSN(db, groups, primaryNodeLSNs)
+{
+   var slaveNodeLSNs = getSlaveNodeLSNs(db, groups);
+ 
+   var checkLSN = true;
+   //比较主备节点lsn
+   for(var i = 0; i < slaveNodeLSNs.length; ++i)
+   {
+      for(var j = 0; j < slaveNodeLSNs[i].length; ++j)
+      {
+         if(primaryNodeLSNs[i][0] > slaveNodeLSNs[i][j])
+         {
+            checkLSN = false;
+            return checkLSN;
+         }
+      }
+   }
+   
+   return checkLSN;
+}
