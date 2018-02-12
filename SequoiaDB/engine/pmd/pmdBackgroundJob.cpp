c780e55f251c4e695d374c851d8e416d54efd549
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-02-12 09:01:03 +0000

    SEQUOIADBMAINSTREAM-3031 pmd和net重构，阶段1 - background task增加reuseEDU的接口，对于不参reuse的需要销毁
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34305 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/pmdBackgroundJob.cpp
+++ SequoiaDB/engine/pmd/pmdBackgroundJob.cpp
@@ -48,6 +48,7 @@ namespace engine
       pmdEDUMgr *pEDUMgr = pmdGetKRCB()->getEDUMgr() ;
       rtnBaseJob *job = (rtnBaseJob*)pData ;
       INT32 rc = SDB_OK ;
+      BOOLEAN reuseEDU = job->reuseEDU() ;
 
       PD_LOG( PDINFO, "Start a background job[%s]", job->name() ) ;
 
@@ -70,6 +71,11 @@ namespace engine
 
       // remove from job mgr
       jobMgr->_removeJob ( cb->getID(), rc ) ;
+      // force edu
+      if ( !reuseEDU )
+      {
+         pEDUMgr->forceUserEDU( cb->getID() ) ;
+      }
 
       PD_TRACE_EXITRC ( SDB_PMDBGJOBENTPNT, rc );
       return SDB_OK ;
