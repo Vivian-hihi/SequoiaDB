76da1eb938a2c2220ee7f1501847b58aa6d8cd5f
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-06 11:11:26 +0000

    fix bug: SEQUOIADBMAINSTREAM-3284 解决线程异常退出导致background job卡住的问题
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34623 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/rtn/rtnBackgroundJobBase.cpp
+++ SequoiaDB/engine/rtn/rtnBackgroundJobBase.cpp
@@ -181,7 +181,15 @@ namespace engine
       }
 
       // wait edu attach in
-      pJob->waitAttach () ;
+      while( SDB_OK != pJob->waitAttach( OSS_ONE_SEC ) )
+      {
+         if ( !_eduMgr->getEDUByID( newEDUID ) )
+         {
+            /// The edu has terminate due to some exception
+            rc = SDB_SYS ;
+            goto error ;
+         }
+      }
       // add to map
       _mapJobs[newEDUID] = pJob ;
       //_mapResult[newEDUID] = SDB_OK ;
@@ -253,48 +261,41 @@ namespace engine
    _rtnBaseJob::_rtnBaseJob ()
    {
       _pEDUCB = NULL ;
-      _latchIn.try_get () ;
+      _evtIn.reset() ;
+      _evtOut.signal() ;
    }
 
    _rtnBaseJob::~_rtnBaseJob ()
    {
-      _latchIn.release () ;
    }
 
    INT32 _rtnBaseJob::attachIn ( pmdEDUCB * cb )
    {
       _pEDUCB = cb ;
-      _latchOut.try_get () ;
-      _latchIn.release () ;
+      _evtOut.reset() ;
+      _evtIn.signal() ;
       return SDB_OK ;
    }
 
    INT32 _rtnBaseJob::attachOut ()
    {
-      _latchOut.release () ;
+      _evtOut.signal() ;
       _pEDUCB = NULL ;
       return SDB_OK ;      
    }
 
-   INT32 _rtnBaseJob::waitAttach ()
+   INT32 _rtnBaseJob::waitAttach ( INT64 millsec )
    {
-      _latchIn.get () ;
-      return SDB_OK ;
+      return _evtIn.wait( millsec ) ;
    }
 
-   INT32 _rtnBaseJob::waitDetach ()
+   INT32 _rtnBaseJob::waitDetach ( INT64 millsec )
    {
-      _latchOut.get () ;
-      _latchOut.release () ;
-      return SDB_OK ;
+      return _evtOut.wait( millsec ) ;
    }
 
    pmdEDUCB * _rtnBaseJob::eduCB ()
    {
-      if ( !_pEDUCB )
-      {
-         return pmdGetThreadEDUCB() ;
-      }
       return _pEDUCB ;
    }
 
