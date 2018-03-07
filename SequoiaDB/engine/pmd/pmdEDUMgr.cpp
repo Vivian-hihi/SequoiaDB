3ef254dc4ae23b3243f4b86554288ba42a06b307
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-07 09:09:51 +0000

    fix bug: 修正 ioService 的 reset 放置到 resetDB 中
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34637 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/pmd/pmdEDUMgr.cpp
+++ SequoiaDB/engine/pmd/pmdEDUMgr.cpp
@@ -797,6 +797,22 @@ namespace engine
       return _getWritingEDUCount( eduTypeFilter, idThreshold ) ;
    }
 
+   void _pmdEDUMgr::resetIOService()
+   {
+      VEC_IOSERVICE::iterator itIO ;
+      IIOService *pIOService = NULL ;
+
+      ossScopedLock lock( &_latch, SHARED ) ;
+
+      itIO = _vecIOServices.begin() ;
+      while( itIO != _vecIOServices.end() )
+      {
+         pIOService = *itIO ;
+         pIOService->resetMon() ;
+         ++itIO ;
+      }
+   }
+
    void _pmdEDUMgr::resetMon( EDUID eduID )
    {
       MAP_EDUCB_IT it ;
@@ -829,20 +845,6 @@ namespace engine
          {
             cb->resetMon() ;
             goto done ;
-         }
-      }
-
-      /// reset io service
-      if ( PMD_INVALID_EDUID == eduID )
-      {
-         VEC_IOSERVICE::iterator itIO ;
-         IIOService *pIOService = NULL ;
-         itIO = _vecIOServices.begin() ;
-         while( itIO != _vecIOServices.end() )
-         {
-            pIOService = *itIO ;
-            pIOService->resetMon() ;
-            ++itIO ;
          }
       }
 
