c9eefd31d3f0ababaca88b9d29117049a7719354
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-09 09:56:50 +0000

    featrue: SEQUOIADBMAINSTREAM-3079 民生资源管控项目代码移值
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34681 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/omsvc/omStrategyMgr.hpp
+++ SequoiaDB/engine/omsvc/omStrategyMgr.hpp
@@ -53,6 +53,7 @@ namespace engine
    {
       public:
          _omStrategyMgr() ;
+         _omStrategyMgr( const omStrategyMgr &others ) ;
          ~_omStrategyMgr() ;
 
          INT32 init( pmdEDUCB *cb ) ;
