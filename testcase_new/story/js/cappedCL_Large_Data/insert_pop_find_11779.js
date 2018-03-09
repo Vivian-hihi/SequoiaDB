61443cba6dc9e87bf56396ed023e09c09a0e07c9
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-09 04:47:07 +0000

    调整用例最大的Size为100G
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34669 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/cappedCL_Large_Data/insert_pop_find_11779.js
+++ testcase_new/story/js/cappedCL_Large_Data/insert_pop_find_11779.js
@@ -13,7 +13,7 @@ function main()
    commCreateCS( db, csName, false, "", csOption );
    
    var clName = COMMCLNAME + "_11779";
-   var clOption = {Capped:true, Size:1024, AutoIndexId:false};
+   var clOption = {Capped:true, Size:102400, AutoIndexId:false};
    var dbcl = commCreateCLByOption( db, csName, clName, clOption, true, true );
    
    //获取主备节点
