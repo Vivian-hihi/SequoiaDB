5d96aedeee6e547f7bebc49167acdb3c95b2daf3
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-13 08:22:17 +0000

    上个版本代码提交错误，修正
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34728 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/php/newType/php_decimal_specialValue_14015_Test.php
+++ testcase_new/driver/php/newType/php_decimal_specialValue_14015_Test.php
@@ -1,16 +1,16 @@
 /****************************************************
 @description:      insert decimal, by __construct
-@testlink cases:   seqDB-8911
-@input:        1 insert, data type: decimal
+@testlink cases:   seqDB-14015
+@input:        1 insert, data type: decimal, special value: MAX/MIN/NAN/-INF/INF
 @output:     success
 @modify list:
-        2016-7-7 XiaoNi Huang init
+        2018-3-13 XiaoNi Huang init
 ****************************************************/
 <?php
 define('Cur_Path', dirname(__FILE__));
 include_once Cur_Path.'/../func.php';
 
-class DecimalType03 extends BaseOperator 
+class DecimalType14015 extends BaseOperator 
 {  
    public function __construct()
    {
@@ -30,15 +30,22 @@ class DecimalType03 extends BaseOperator
    }
    
    function insertRecs( $clDB )
-   {
-      $recs0 = array( 'a' => 0, 'b' => new SequoiaDecimal( '1.123' ) );
-      $recs1 = array( 'a' => 1, 'b' => new SequoiaDecimal( '1.123', -1 ) );
-      $recs2 = array( 'a' => 2, 'b' => new SequoiaDecimal( '1.123', 1000, 999 ) );
-      $recs3 = array( 'a' => 3, 'b' => new SequoiaDecimal( '1.123', 1, 0 ) );
+   {  
+      $recs0 = array( 'a' => 0, 'b' => new SequoiaDecimal( 'MAX' ) );
+      $recs1 = array( 'a' => 1, 'b' => new SequoiaDecimal( 'MIN' ) );
+      $recs2 = array( 'a' => 2, 'b' => new SequoiaDecimal( 'NAN' ) );
+      $recs3 = array( 'a' => 3, 'b' => new SequoiaDecimal( 'INF' ) );
+      $recs4 = array( 'a' => 4, 'b' => new SequoiaDecimal( '-INF' ) );
+      $recs5 = array( 'a' => 5, 'b' => new SequoiaDecimal( 'max' ) );
+      $recs6 = array( 'a' => 6, 'b' => new SequoiaDecimal( 'min' ) );
+      $recs7 = array( 'a' => 7, 'b' => new SequoiaDecimal( 'nan' ) );
+      $recs8 = array( 'a' => 8, 'b' => new SequoiaDecimal( 'inf' ) );
+      $recs9 = array( 'a' => 9, 'b' => new SequoiaDecimal( '-inf' ) );
       
-      $recsArray = array( $recs0, $recs1, $recs2, $recs3 );
+      $recsArray = array( $recs0, $recs1, $recs2, $recs3, $recs4, $recs5, $recs6, $recs7, $recs8, $recs9 );
       for( $i = 0; $i < count( $recsArray ); $i++ )
-      {
+      {  
+         //var_dump("i = " + $i);
          $clDB -> insert( $recsArray[$i] );
       }
    }
@@ -62,7 +69,7 @@ class DecimalType03 extends BaseOperator
    
 }
 
-class TestDecimal03 extends PHPUnit_Framework_TestCase
+class TestDecimal14015 extends PHPUnit_Framework_TestCase
 {
    protected static $dbh;
    private static $csName;
@@ -71,7 +78,7 @@ class TestDecimal03 extends PHPUnit_Framework_TestCase
    
    public static function setUpBeforeClass()
    {
-      self::$dbh = new DecimalType03();
+      self::$dbh = new DecimalType14015();
       
       echo "\n---Begin to ready parameter.\n";
       self::$csName = self::$dbh -> COMMCSNAME;
@@ -95,31 +102,40 @@ class TestDecimal03 extends PHPUnit_Framework_TestCase
    
    function test_find()
    {
-      echo "\n---Begin to find records by type[100].\n";
+      echo "\n---Begin to find records.\n";
       
       $recsArray = self::$dbh -> findRecs( self::$clDB );
       
       $errno = self::$dbh -> getErrno();
       $this -> assertEquals( -29, $errno );
       
-      $this -> assertCount( 4, $recsArray );
-      $expDecimal0 = "1.123"; 
-      $expDecimal1 = "1.123"; 
-      $expDecimal2 = "1.123000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"; 
-      $expDecimal3 = "1";
+      $this -> assertCount( 10, $recsArray );
+      $expDecimal0 = "MAX"; 
+      $expDecimal1 = "MIN"; 
+      $expDecimal2 = "NaN"; 
       $this -> assertEquals( $expDecimal0, $recsArray[0]['b'] );
+      $this -> assertEquals( $expDecimal0, $recsArray[3]['b'] );
+      $this -> assertEquals( $expDecimal0, $recsArray[5]['b'] );
+      $this -> assertEquals( $expDecimal0, $recsArray[8]['b'] );
+      
       $this -> assertEquals( $expDecimal1, $recsArray[1]['b'] );
+      $this -> assertEquals( $expDecimal1, $recsArray[4]['b'] );
+      $this -> assertEquals( $expDecimal1, $recsArray[6]['b'] );
+      $this -> assertEquals( $expDecimal1, $recsArray[9]['b'] );
+      
       $this -> assertEquals( $expDecimal2, $recsArray[2]['b'] );
-      $this -> assertEquals( $expDecimal3, $recsArray[3]['b'] );
+      $this -> assertEquals( $expDecimal2, $recsArray[7]['b'] );
    }
    
-   function test_dropCL()
+   public static function tearDownAfterClass()
    {
       echo "\n---Begin to drop cl in the end.\n";
       
       self::$dbh -> dropCL( self::$csName, self::$clName, false );
       $errno = self::$dbh -> getErrno();
-      $this -> assertEquals( 0, $errno );
+      if ($errno != 0) {
+          throw new Exception("failed to drop cl, errno=".$errno);
+      }
    }
    
 }
