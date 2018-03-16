4963361b5eaed71b76db296e4a98c0e68680bd11
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-16 09:11:07 +0000

    增加打印时间的信息
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34842 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/CSharp/Crud/Delete/DeleteMatcherRecord14540.cs
+++ testcase_new/driver/CSharp/Crud/Delete/DeleteMatcherRecord14540.cs
@@ -28,6 +28,7 @@ namespace CSharp.Crud.Delete
         [TestInitialize()]
         public void SetUp()
         {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:fff") + " begin: " + this.GetType().ToString());
             sdb = new Sequoiadb(SdbTestBase.coordUrl);
             sdb.Connect();
             cs = sdb.GetCollecitonSpace(SdbTestBase.csName);
@@ -112,6 +113,7 @@ namespace CSharp.Crud.Delete
                 {
                     sdb.Disconnect();
                 }
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:fff") + " end: " + this.GetType().ToString());
             }
         }
 
