dfa3ca94a2396d227153a19e3cadd7d178247911
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-16 00:22:20 +0000

    新增用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34809 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/CSharp/Crud/DataType/TimestampData14588.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SequoiaDB;
+using CSharp.TestCommon;
+using SequoiaDB.Bson;
+
+namespace CSharp.Crud.DataType
+{
+    /**
+    * description: insert Timestamp data, then update / query / delete                   
+    * testcase:    14588
+    * author:      wuyan
+    * date:        2018/3/15
+    */
+    [TestClass]
+    public class TimestampData14588
+    {
+        private Sequoiadb sdb = null;
+        private CollectionSpace cs = null;
+        private DBCollection cl = null;
+        private const string clName = "TimestampData_14588";
+
+        [TestInitialize()]
+        public void SetUp()
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:fff") + " begin: " + this.GetType().ToString());
+            sdb = new Sequoiadb(SdbTestBase.coordUrl);
+            sdb.Connect();
+            cs = sdb.GetCollecitonSpace(SdbTestBase.csName);
+            cl = cs.CreateCollection(clName);
+        }
+
+        [TestMethod()]
+        public void TestTimestampData14588()
+        {
+            InsertAndQueryTimestampData();
+            UpdateTimestampData();
+            DeleteTimestampData();
+        }
+
+        [TestCleanup()]
+        public void TearDown()
+        {
+            try
+            {
+                cs.DropCollection(clName);
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss:fff") + " end  : " + this.GetType().ToString());
+            }
+            finally
+            {
+                if (sdb != null)
+                {
+                    sdb.Disconnect();
+                }
+            }
+        }
+
+        private void InsertAndQueryTimestampData()
+        {
+            //insert date           
+            
+            long timestamp1 = (-6847833601L << 32) + 0;           
+            long timestamp2= (253402271999L << 32) + 0; 
+            //timestamp : 1752-12-31 23:59:59  
+            BsonDocument insertor1 = new BsonDocument { { "a", new BsonTimestamp(timestamp1) }, { "b", 1 } };
+            //timestamp : 9999-12-31
+            BsonDocument insertor2 = new BsonDocument { { "a", new BsonTimestamp(timestamp2) }, { "b", 2 } };
+            //timestamp : 1902-01-01 00:00:00
+            BsonDocument insertor3 = new BsonDocument { { "a", new BsonTimestamp(-2145945600, 0)}, { "b", 3 } };
+            //timestamp:  2037-12-31 23:59:59.999999
+            BsonDocument insertor4 = new BsonDocument { { "a", new BsonTimestamp(2145887999, 999999) }, { "b", 4 } };
+            //timestamp:  1970-01-01            
+            BsonDocument insertor5 = new BsonDocument { { "a", new BsonTimestamp(-28801,0) }, { "b", 5 } }; 
+            cl.Insert(insertor1);
+            cl.Insert(insertor2);
+            cl.Insert(insertor3);
+            cl.Insert(insertor4);
+            cl.Insert(insertor5);
+          
+            //test query        
+            BsonDocument matcherConf1 = new BsonDocument { { "a", new BsonTimestamp(timestamp1) } };
+            QueryAndCheckResult(matcherConf1, insertor1);
+            BsonDocument matcherConf2 = new BsonDocument { { "a", new BsonTimestamp(timestamp2) } };
+            QueryAndCheckResult(insertor2, insertor2);
+            BsonDocument matcherConf3 = new BsonDocument { { "a", new BsonTimestamp(2145887999, 999999) } };
+            QueryAndCheckResult(insertor3, insertor3);
+            BsonDocument matcherConf4 = new BsonDocument { { "a", new BsonTimestamp(-28801, 0)} };
+            QueryAndCheckResult(insertor4, insertor4);
+
+            //check insert records num 
+            long count = cl.GetCount(null);
+            Assert.AreEqual(5, count);
+        }
+
+        private void UpdateTimestampData()
+        {
+            // update timestamp
+            BsonDocument modifier = new BsonDocument { { "$set", new BsonDocument { { "a", new BsonTimestamp(145887999, 999999) } } } };
+            BsonDocument matcher = new BsonDocument { { "b", 4 } };
+            cl.Update(matcher, modifier, null);
+            long updateCount = cl.GetCount(new BsonDocument { { "a", new BsonTimestamp(145887999, 999999) }, { "b", 4 } });
+            Assert.AreEqual(1, updateCount);
+
+            //update timestamp to  other type            
+            BsonDocument modifier1 = new BsonDocument { { "$set", new BsonDocument { { "a", true } } } };
+            BsonDocument matcher1 = new BsonDocument { { "b", 5 } };
+            cl.Update(matcher1, modifier1, null);
+            long updateCount1 = cl.GetCount(new BsonDocument { { "a", true }, { "b", 5 } });
+            Assert.AreEqual(1, updateCount1);
+            Assert.AreEqual(5, cl.GetCount(null));
+        }
+
+        private void DeleteTimestampData()
+        {
+            //delete  data 
+            BsonDocument matcher = new BsonDocument { { "a", new BsonTimestamp(-2145945600, 0) } };
+            cl.Delete(matcher);
+            //check result
+            long deleteCount = cl.GetCount(matcher);
+            Assert.AreEqual(0, deleteCount);
+            long existNum = 4;
+            Assert.AreEqual(existNum, cl.GetCount(null));            
+        }
+
+        private void QueryAndCheckResult(BsonDocument cond, BsonDocument expRecord)
+        {
+            DBQuery query = new DBQuery();
+            query.Matcher = cond;
+            DBCursor cursor = cl.Query(query);
+            BsonDocument actObj = null;
+            while (cursor.Next() != null)
+            {
+                actObj = cursor.Current();
+            }
+            cursor.Close();
+            Console.WriteLine("expRecord="+expRecord.ToJson());
+            Console.WriteLine("actRecord=" + actObj.ToJson());
+            Assert.AreEqual(expRecord, actObj);
+        }
+    }
+}
