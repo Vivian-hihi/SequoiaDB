4c66c503b5ac70397491b660d6f8f725af3140de
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-19 08:21:00 +0000

    featrue: SEQUOIADBMAINSTREAM-3079 民生资源管控项目代码移值 增加 om 与 coord 的通信代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34874 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/omsvc/omManager.cpp
+++ SequoiaDB/engine/omsvc/omManager.cpp
@@ -2203,7 +2203,7 @@ namespace engine
 
          rc = _reply( handle, &_replyHeader, buffObj.data(),
                       buffObj.size() ) ;
-         if ( rc == SDB_OK )
+         if ( rc )
          {
             PD_LOG ( PDERROR, "failed to send reply, rc: %d", rc ) ;
          }
