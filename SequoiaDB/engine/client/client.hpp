e3122dcad3f162cf5ea338db02ea2f3e6816db66
zhaowenbo <zhaowenbo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-26 07:45:36 +0000

    SEQUOIADBMAINSTREAM-3056:【Story】提供节点配置集中变更修改的能力 C/C++ 驱动描述修正
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34999 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/client/client.hpp
+++ SequoiaDB/engine/client/client.hpp
@@ -4973,8 +4973,9 @@ namespace sdbclient
 
       /** \fn INT32 updateConfig(const bson::BSONObj &configs, 
                                  const bson::BSONObj &options)
-          \brief Force the node to reload config from file and take effect.
-          \param [in] configs the specific configuration parameters to update
+          \brief Update node config and take effect.
+          \param [in] configs the specific configuration parameters to update.
+                { diaglevel:3 } Modify diaglevel as 3.
           \param [in] options The control options:(Only take effect in coordinate nodes)
                 GroupID:INT32,
                 GroupName:String,
@@ -4995,8 +4996,9 @@ namespace sdbclient
 
       /** \fn INT32 deleteConfig(const bson::BSONObj &configs, 
                                  const bson::BSONObj &options)
-          \brief Force the node to reload config from file and take effect.
-          \param [in] configs the specific configuration parameters to delete
+          \brief Delete node config and take effect.
+          \param [in] configs the specific configuration parameters to delete.
+                { diaglevel:1 } Delete diaglevel config and restore to default value.
           \param [in] options The control options:(Only take effect in coordinate nodes)
                 GroupID:INT32,
                 GroupName:String,
