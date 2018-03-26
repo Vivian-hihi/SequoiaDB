cd908c58131683e8e07c637210240e92b70a4ada
zhaowenbo <zhaowenbo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-03-26 00:32:11 +0000

    SEQUOIADBMAINSTREAM-3056:【Story】提供节点配置集中变更修改的能力 SDB_SNAP_CONFIG 修正为 SDB_SNAP_CONFIGS
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@34984 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/client/clientDef.h
+++ SequoiaDB/engine/client/clientDef.h
@@ -40,7 +40,7 @@
 #define SDB_SNAP_TRANSACTIONS_CURRENT 10
 #define SDB_SNAP_ACCESSPLANS      11
 #define SDB_SNAP_HEALTH           12
-#define SDB_SNAP_CONFIG           13
+#define SDB_SNAP_CONFIGS          13
 
 // define value for list
 #define SDB_LIST_CONTEXTS         0
