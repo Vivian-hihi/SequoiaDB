fc8199e24046c89eac03e973e2bb3c3edfb52a9f
liangxuewang <liangxuewang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-04-02 06:46:27 +0000

    修改用例，新建节点后更新删除配置
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35090 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/c/misc/configs14875.cpp
+++ testcase_new/driver/c/misc/configs14875.cpp
@@ -13,15 +13,18 @@
 class confTest14875 : public testBase
 {
 protected:
-   sdbConnectionHandle data ;
+   sdbReplicaGroupHandle rg ;
    CHAR host[ MAX_NAME_SIZE+1 ] ;
    CHAR svc[ MAX_NAME_SIZE+1 ] ;
+   CHAR dbPath[ MAX_NAME_SIZE+1 ] ;
    
    void SetUp()
    {
       testBase::SetUp() ;
+      rg = SDB_INVALID_HANDLE ;
       memset( host, 0, MAX_NAME_SIZE+1 ) ;
       memset( svc, 0, MAX_NAME_SIZE+1 ) ;
+      memset( dbPath, 0, MAX_NAME_SIZE+1 ) ;
    }
    void TearDown()
    {
@@ -32,25 +35,42 @@ protected:
    {
       INT32 rc = SDB_OK ;
       vector<string> groups ;
-      vector<string> nodes ;
       const CHAR* rgName ;
-      string nodename ;
-      string hostname ;
-      string svcname ;
-      INT32 pos ;
+      sdbNodeHandle node ;
 
       rc = getGroups( db, groups ) ;
       CHECK_RC( SDB_OK, rc, "fail to get groups" ) ;
       rgName = groups[0].c_str() ;
-      rc = getGroupNodes( db, rgName, nodes ) ;
-      CHECK_RC( SDB_OK, rc, "fail to group nodes" ) ;
-      pos = nodes[0].find_first_of( ":" ) ;
-      hostname = nodes[0].substr( 0, pos ) ;
-      svcname = nodes[0].substr( pos+1 ) ;
-      strcpy( host, hostname.c_str() ) ;
-      strcpy( svc, svcname.c_str() ) ;
-      printf( "node: %s:%s\n", host, svc ) ;
+      rc = sdbGetReplicaGroup( db, rgName, &rg ) ;
+      CHECK_RC( SDB_OK, rc, "fail to get group" ) ;
+     
+      rc = getLocalHost( host, MAX_NAME_SIZE ) ;
+      CHECK_RC( SDB_OK, rc, "fail to get local hostname" ) ;
+      strcpy( svc, ARGS->rsrvPortBegin() ) ;
+      sprintf( dbPath, "%s%s%s", ARGS->rsrvNodeDir(), "data/", svc ) ;
+      printf( "create node: %s:%s, dbPath: %s\n", host, svc, dbPath ) ;
+      
+      rc = sdbCreateNode( rg, host, svc, dbPath, NULL ) ;
+      CHECK_RC( SDB_OK, rc, "fail to create node" ) ;
+      rc = sdbGetNodeByHost( rg, host, svc, &node ) ;
+      CHECK_RC( SDB_OK, rc, "fail to get node" ) ;
+      rc = sdbStartNode( node ) ;
+      CHECK_RC( SDB_OK, rc, "fail to start node" ) ;
+      sdbReleaseNode( node ) ;
+   done:
+      return rc ;
+   error:
+      goto done ;
+   }
 
+   INT32 fini()
+   {
+      INT32 rc = SDB_OK ;
+      
+      printf( "remove node: %s:%s\n", host, svc ) ;
+      rc = sdbRemoveNode( rg, host, svc, NULL ) ;
+      CHECK_RC( SDB_OK, rc, "fail to remove node" ) ;
+      sdbReleaseReplicaGroup( rg ) ;
    done:
       return rc ;
    error:
@@ -132,5 +152,8 @@ TEST_F( confTest14875, upDelConf )
    // check delete
    rc = getNodeWeight( &weight ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
-   ASSERT_EQ( 10, weight ) << "fail to check delete" ; 
+   ASSERT_EQ( 10, weight ) << "fail to check delete" ;
+
+   rc = fini() ;
+   ASSERT_EQ( SDB_OK, rc ) ;
 }
