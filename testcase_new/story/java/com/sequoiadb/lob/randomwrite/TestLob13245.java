e20b2d3df012b2ff3f0ebee61bbae1b5c3591f10
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-04-09 00:52:58 +0000

    修正 上一次的错误提交，用例13245不支持独立模式，13252支持
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35195 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/lob/randomwrite/TestLob13245.java
+++ testcase_new/story/java/com/sequoiadb/lob/randomwrite/TestLob13245.java
@@ -6,11 +6,13 @@ import com.sequoiadb.base.DBLob;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.exception.SDBError;
+import com.sequoiadb.testcommon.CommLib;
 import com.sequoiadb.testcommon.SdbTestBase;
 import org.bson.BSONObject;
 import org.bson.types.ObjectId;
 import org.bson.util.JSON;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -37,6 +39,10 @@ public class TestLob13245 extends SdbTestBase {
         csName = SdbTestBase.csName;
         clName = "cl_" + this.getClass().getSimpleName();
         db = new Sequoiadb(coordUrl, "", "");
+        CommLib comm = new CommLib();
+        if (comm.isStandAlone(db)) {
+            throw new SkipException("can not support standalone");
+        }
         cs = db.getCollectionSpace(csName);
         List<String> groupNames = RandomWriteLobUtil.getDataGroups(db);
 
