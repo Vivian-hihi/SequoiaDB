7780e32dab6f49d5aa390bf794bc14ba33182f15
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-04-26 07:52:47 +0000

    alter 测试用例提交
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35416 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/alter/alter14945.js
@@ -0,0 +1,40 @@
+/************************************
+*@Description: 分区表关闭切分功能后执行切分
+*@author:      luweikang
+*@createdate:  2018.4.25
+*@testlinkCase:seqDB-14945
+**************************************/
+
+main();
+
+function main()
+{
+   println("---begin test---");
+   var csName = COMMCSNAME;
+   var clName = CHANGEDPREFIX + "_14945";
+   
+   var options = { ShardingType: 'hash', ShardingKey: {a: 1}};
+   var cl = commCreateCLByOption( db, csName, clName, options, true, false, "create CL in the begin");
+   
+   
+   //关闭分区功能
+   println("---close sharding---");
+   cl.disableSharding();
+   
+   println("---split cl---");
+   var splitGroup = getSplitGroup( db, csName, clName );
+   try
+   {
+      cl.split( splitGroup.srcGroup, splitGroup.tarGroup, 50 );
+   }
+   catch( e )
+   {
+      if( e !== -169)
+      {
+         throw e;
+      }
+   }
+   checkSnapshot( db, SDB_SNAP_CATALOG, csName, clName, "ShardingKey", undefined);
+   commDropCL( db, csName, clName, true, false, "clean cl" );
+   println("---end the test---");
+}
--- testcase_new/story/js/alter/commlib.js
+++ testcase_new/story/js/alter/commlib.js
@@ -28,3 +28,102 @@ function checkAlterResult(clName, fieldName, expFieldValue)
    }
    
 }
+
+
+/************************************
+*@Description: check snapshot 
+*@author:      luweikang
+*@createDate:  2018.4.25
+**************************************/
+function checkSnapshot( db, snapType, csName, clName, field, expFieldValue)
+{
+   var clFullName = csName + "." + clName;
+   var cursor = db.snapshot(snapType, {'Name': clFullName});
+   var Obj = cursor.current().toObj();
+   var actualFieldValue = Obj[field];
+   println("expFieldValue   : " + expFieldValue);
+   println("actualFieldValue: "+ actualFieldValue);
+   if ( typeof(expFieldValue) === "object" )
+   {      
+      if (JSON.stringify(expFieldValue) !== JSON.stringify(actualFieldValue))
+      {
+         throw buildException("test fieldvalue1", "check field", "value is wrong", JSON.stringify(expFieldValue), JSON.stringify(actualFieldValue));
+      }
+      
+   }
+   else
+   {
+      if (expFieldValue  !== actualFieldValue)
+      {
+         throw buildException("test fieldvalue2", "check field", "value is wrong", expFieldValue, actualFieldValue);
+      }
+   }
+   
+}
+
+/************************************
+*@Description: get all groupName
+*@author:      luweikang
+*@createDate:  2018.4.25
+**************************************/
+function getGroupName(db)
+{
+   var groupArr = db.listReplicaGroups();
+   var dataRGNames = new Array();
+   while( groupArr.next() )
+   {
+      var groupObj = groupArr.current().toObj();
+      var groupID = groupObj.GroupID;
+      if(groupID >= 1000 )
+      {
+         dataRGNames.push(groupObj.GroupName);
+      }
+   }
+   return dataRGNames;
+}
+
+/************************************
+*@Description: get Split Group
+*@author:      luweikang
+*@createDate:  2018.4.25
+**************************************/
+function getSplitGroup( db, csName, clName )
+{
+   var clFullName = csName + "." + clName;
+   var arr = getGroupName( db );
+   var srcGroup = commGetCLGroups( db, clFullName )[0];
+   var tarGroup = null;
+   for(i in arr)
+   {
+      if( arr[i] !== srcGroup )
+      {
+         tarGroup = arr[i];
+         break;
+      }
+   }
+   var splitGroup = {};
+   splitGroup.srcGroup = srcGroup;
+   splitGroup.tarGroup = tarGroup;
+   return splitGroup;
+}
+
+/************************************
+*@Description: alter cl 
+*@author:      luweikang
+*@createDate:  2018.4.25
+**************************************/
+function clSetAttributes( cl, options )
+{
+   try
+   {
+      cl.setAttributes( options );
+      throw "ALTER_SHOULD_ERR";
+   }
+   catch( e )
+   {
+      if( e !== -32 )
+      {
+         throw e;
+      }
+   }
+}
