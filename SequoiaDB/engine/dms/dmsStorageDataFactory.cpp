d4506a3cd1b67e8790cbb4b2cb93e09f958026d1
yangshangde <yangshangde@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-05-03 11:17:27 +0000

    add license
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35497 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/dms/dmsStorageDataFactory.cpp
+++ SequoiaDB/engine/dms/dmsStorageDataFactory.cpp
@@ -1,3 +1,38 @@
+/*******************************************************************************
+
+   Copyright (C) 2011-2018 SequoiaDB Ltd.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the term of the GNU Affero General Public License, version 3,
+   as published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warrenty of
+   MARCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program. If not, see <http://www.gnu.org/license/>.
+
+   Source File Name = dmsStorageDataFactory.cpp
+
+   Descriptive Name = dms Storage Data Object Factory.
+
+   When/how to use: this program may be used on binary and text-formatted
+   versions of Runtime component. This file contains structure for SU cache
+   management ( including plan cache, statistics cache, etc. )
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who Description
+   ====== =========== === ==============================================
+          04/14/2017  YSD  Initial Draft
+   Last Changed =
+
+*******************************************************************************/
 #include "dmsStorageDataFactory.hpp"
 #include "dmsStorageData.hpp"
 #include "dmsStorageDataCapped.hpp"
