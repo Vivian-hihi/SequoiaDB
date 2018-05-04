d928d017e4816f6fd278771e71bdd01318d7198d
liweichao <liweichao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-05-04 00:47:16 +0000

    change open source license to Apache License 2.0
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35500 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/sptParseMandoc.hpp
+++ SequoiaDB/engine/spt/sptParseMandoc.hpp
@@ -1,19 +1,18 @@
-/*   $Id: main.h,v 1.15 2011/10/06 22:29:12 kristaps Exp $ */
-/*
- * Copyright (c) 2009, 2010, 2011 Kristaps Dzonsons <kristaps@bsd.lv>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+/*******************************************************************************
+   Copyright (C) 2011-2018 SequoiaDB Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*******************************************************************************/
 #ifndef   SPT_PARSE_MANDOC_HPP__
 #define   SPT_PARSE_MANDOC_HPP__
 #include <core.hpp>
