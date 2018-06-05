11fa4724320e13f25497f1b4948458cbfa887086
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-06-05 07:49:27 +0000

    增加java驱动端db.msg打印日志到日志中
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35819 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/com/sequoiadb/testcommon/TimePrinterListener.java
+++ testcase_new/sdv/java/com/sequoiadb/testcommon/TimePrinterListener.java
@@ -3,6 +3,8 @@ package com.sequoiadb.testcommon;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
+import com.sequoiadb.base.Sequoiadb;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,29 +16,37 @@ public class TimePrinterListener extends TestListenerAdapter {
     @Override
     public void onConfigurationSuccess(ITestResult itr) {
         super.onConfigurationSuccess(itr);
-        if (itr.getMethod().isAfterClassConfiguration())
+        if (itr.getMethod().isAfterClassConfiguration()) {
             printEndTime(itr);
+            dbMsgEndTime(itr);
+        }
     }
 
     @Override
     public void onConfigurationFailure(ITestResult itr) {
         super.onConfigurationFailure(itr);
-        if (itr.getMethod().isAfterClassConfiguration())
+        if (itr.getMethod().isAfterClassConfiguration()) {
             printEndTime(itr);
+            dbMsgEndTime(itr);
+        }
     }
 
     @Override
     public void onConfigurationSkip(ITestResult itr) {
         super.onConfigurationSkip(itr);
-        if (itr.getMethod().isAfterClassConfiguration())
+        if (itr.getMethod().isAfterClassConfiguration()) {
             printEndTime(itr);
+            dbMsgEndTime(itr);
+        }
     }
 
     @Override
     public void beforeConfiguration(ITestResult tr) {
         super.beforeConfiguration(tr);
-        if (tr.getMethod().isBeforeClassConfiguration())
+        if (tr.getMethod().isBeforeClassConfiguration()) {
             printBeginTime(tr);
+            dbMsgBeginTime(tr);
+        }
     }
 
     private void printBeginTime(ITestResult tr) {
@@ -53,5 +63,15 @@ public class TimePrinterListener extends TestListenerAdapter {
 
     private String getCurTimeStr() {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(new Date());
+    }
+    
+    private void dbMsgBeginTime(ITestResult tr) {
+        Sequoiadb sdb = new Sequoiadb("localhost:11810", "", "");
+        sdb.msg(getCurTimeStr() + "\tbegin: " + getTestMethodName(tr));
+    }
+    
+    private void dbMsgEndTime(ITestResult tr) {
+        Sequoiadb sdb = new Sequoiadb("localhost:11810", "", "");
+        sdb.msg(getCurTimeStr() + "\tend  : " + getTestMethodName(tr));
     }
 }
