59852f0039ae89d02f922d8367363a28726280cd
xujianhui <xujianhui@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-06-21 07:21:12 +0000

    rollback fix bug: SEQUOIADBMAINSTREAM-3639 new File 的路径没有把执行文件所在路径作为根路径
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@35943 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/spt/usrdef/sptUsrFile.cpp
+++ SequoiaDB/engine/spt/usrdef/sptUsrFile.cpp
@@ -39,7 +39,7 @@
 #include "omagentDef.hpp"
 #include "sptUsrFileContent.hpp"
 #include "sptUsrRemote.hpp"
-#include "sptScope.hpp"
+//#include "sptScope.hpp"
 #include <boost/algorithm/string.hpp>
 #include "../bson/lib/md5.hpp"
 
@@ -145,7 +145,7 @@ JS_MAPPING_END()
       INT32 iMode = 0 ;
       BSONObjBuilder opBuilder ;
       string err ;
-      sptScope *pScope = NULL ;
+      //sptScope *pScope = NULL ;
 
       // get filename
       rc = arg.getString( 0, filename ) ;
@@ -182,7 +182,7 @@ JS_MAPPING_END()
          opBuilder.append( SPT_FILE_COMMON_FIELD_MODE, iMode ) ;
       }
 
-      // get scope by access private data
+/*      // get scope by access private data
       {
          sptPrivateData *pPivateData = arg.getPrivateData() ;
          if( NULL == pPivateData )
@@ -199,7 +199,8 @@ JS_MAPPING_END()
       }
 
       rc = _fileCommon.open( pScope->calcImportPath( filename),
-                             opBuilder.obj(), err ) ;
+                             opBuilder.obj(), err ) ;*/
+      rc = _fileCommon.open( filename, opBuilder.obj(), err ) ;
       if ( SDB_OK != rc )
       {
          detail = BSON( SPT_ERR << err ) ;
