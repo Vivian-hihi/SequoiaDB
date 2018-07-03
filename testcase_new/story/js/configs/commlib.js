22280d8bebad0293cce037719873224d97215733
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-07-03 04:24:42 +0000

    修改"禁止修改"配置项有效参数值
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@36043 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/configs/commlib.js
+++ testcase_new/story/js/configs/commlib.js
@@ -98,25 +98,25 @@ var Configs = ( function() {
       this.rebootConfigs.push( new ConfDesp( "instanceid", "int", 0, 1, "aassada" ) ) ;
 
       // register forbid configs
-      this.forbidConfigs.push( new ConfDesp( "dbpath", "path", "./", "", null ) ) ; 
-      this.forbidConfigs.push( new ConfDesp( "indexpath", "path", "", "", null ) ) ;  
-      this.forbidConfigs.push( new ConfDesp( "confpath", "path", "./", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "logpath", "path", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "wwwpath", "path", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "svcname", "string", "11810", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "replname", "string", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "shardname", "string", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "catalogname", "string", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "httpname", "string", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "omname", "string", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "role", "string", "standalone", "data", "person" ) ) ; 
-      this.forbidConfigs.push( new ConfDesp( "catalogaddr", "string", "", "", null ) ) ; 
+      this.forbidConfigs.push( new ConfDesp( "dbpath", "path", "./", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "indexpath", "path", "", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "confpath", "path", "./", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "logpath", "path", "", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "wwwpath", "path", "", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "svcname", "string", "11810", "50000", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "replname", "string", "", "51000", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "shardname", "string", "", "52000", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "catalogname", "string", "", "53000", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "httpname", "string", "", "54000", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "omname", "string", "", "55000", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "role", "string", "standalone", "data", "person" ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "catalogaddr", "string", "", "57000", null ) ) ;
       this.forbidConfigs.push( new ConfDesp( "logfilesz", "int", 64, 128, "10" ) ) ;
       this.forbidConfigs.push( new ConfDesp( "logfilenum", "int", 20, 10, "128" ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "lobpath", "path", "", "", null ) ) ;  
-      this.forbidConfigs.push( new ConfDesp( "lobmetapath", "path", "", "", null ) ) ;
-      this.forbidConfigs.push( new ConfDesp( "omaddr", "string", "", "", null ) ) ; 
-      this.forbidConfigs.push( new ConfDesp( "archivepath", "path", "", "", null ) ) ; 
+      this.forbidConfigs.push( new ConfDesp( "lobpath", "path", "", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "lobmetapath", "path", "", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "omaddr", "string", "", "helloworld", null ) ) ;
+      this.forbidConfigs.push( new ConfDesp( "archivepath", "path", "", "helloworld", null ) ) ;
 
       // register unknown configs
       this.unknowConfigs.push( new ConfDesp( "cataloglist", "string", "", "", null ) ) ;
