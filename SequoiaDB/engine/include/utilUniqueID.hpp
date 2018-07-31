0a16f655439906aa22212a280d5c07ce0931cc7d
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-07-31 11:06:47 +0000

    SEQUOIADBMAINSTREAM-3709 story: rename cs/cl (uniqueid部分)
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@36298 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ SequoiaDB/engine/include/utilUniqueID.hpp
@@ -0,0 +1,62 @@
+/*******************************************************************************
+
+   Copyright (C) 2011-2018 SequoiaDB Ltd.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   Source File Name = utilUniqueID.hpp
+
+   Descriptive Name =
+
+   When/how to use: Process CS/CL Unique ID
+
+   Dependencies: N/A
+
+   Restrictions: N/A
+
+   Change Activity:
+   defect Date        Who      Description
+   ====== =========== ======== ==============================================
+          05/24/2018  Ting YU  Initial Draft
+
+   Last Changed =
+
+*******************************************************************************/
+#ifndef UTIL_UNIQUEID_HPP_
+#define UTIL_UNIQUEID_HPP_
+
+#include "ossTypes.hpp"
+#include "ossUtil.hpp"
+
+namespace engine
+{
+   #define UTIL_INVALID_UNIQUEID      0
+
+   typedef UINT32 utilCSUniqueID ;
+   typedef UINT64 utilCLUniqueID ;
+
+   typedef std::pair<std::string, utilCLUniqueID> PAIR_CLNAME_ID ;
+
+   OSS_INLINE utilCSUniqueID utilGetCSUniqueID( utilCLUniqueID clUniqueID )
+   {
+      return clUniqueID >> 32 ;
+   }
+
+   OSS_INLINE utilCLUniqueID utilBuildCLUniqueID( utilCSUniqueID csUniqueID )
+   {
+      return (utilCLUniqueID)csUniqueID << 32 ;
+   }
+}
+
+#endif //UTIL_UNIQUEID_HPP_
+
