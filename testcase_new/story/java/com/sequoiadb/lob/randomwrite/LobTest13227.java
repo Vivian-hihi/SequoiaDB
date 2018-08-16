4304c1d6b05bf2c82cceb763f7f42a82d0ab2a58
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-08-16 00:18:28 +0000

    将原有代码中冗余代码去除
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@36506 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/com/sequoiadb/lob/randomwrite/LobTest13227.java
+++ testcase_new/story/java/com/sequoiadb/lob/randomwrite/LobTest13227.java
@@ -7,18 +7,13 @@ import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.SDBError;
 import com.sequoiadb.testcommon.CommLib;
 import com.sequoiadb.testcommon.SdbTestBase;
-import org.bson.BSONObject;
 import org.bson.types.ObjectId;
-import org.bson.util.JSON;
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
@@ -27,8 +22,22 @@ import static com.sequoiadb.lob.randomwrite.RandomWriteLobUtil.getRandomBytes;
 import static com.sequoiadb.lob.randomwrite.RandomWriteLobUtil.readLob;
 
 /**
- * Created by laojingtang on 17-11-22.
- */
+* FileName: LobTest13227.java
+* test content:
+* 测试用例 seqDB-13227 :: 版本: 1 :: 写lob同时并发读lob
+* 1、打开lob（创建模式是指定lob oid）
+* 2、写入lob数据，写入过程中读取该lob数据（读取lob时获取指定lob oid）
+* 3、检查操作结果
+* 1、写入lob成功，读取lob操作失败，返回对应错误信息
+* 2、再次读取lob数据和插入lob数据一致（比较MD5值相同）
+* testlink case:seqDB-13227
+* @author laojingtang
+* @UpdateAuthor wangkexin
+* @Date    2017.11.2
+* @UpdateDate 2018.08.10
+* @version 1.10
+*/
+
 public class LobTest13227 extends SdbTestBase {
     Logger log = Logger.getLogger(LobTest13237.class.getName());
     Sequoiadb db = null;
@@ -36,7 +45,6 @@ public class LobTest13227 extends SdbTestBase {
     CollectionSpace cs = null;
     String csName;
     String clName;
-    private static Map<Integer, byte[]> randomBytesMap = new HashMap<>();
 
     @BeforeClass
     public void setupClass() {
@@ -46,13 +54,6 @@ public class LobTest13227 extends SdbTestBase {
         cs = db.getCollectionSpace(csName);
         if(CommLib.isStandAlone(db))
             throw new SkipException("");
-
-        List<String> groupNames = RandomWriteLobUtil.getDataGroups(db);
-        if (groupNames.size() < 2)
-            throw new SkipException("");
-        dbcl = cs.createCollection(clName,
-                (BSONObject) JSON.parse("{ShardingKey:{\"_id\":1},ShardingType:\"hash\",Group:'" + groupNames.get(0) + "'}"));
-        dbcl.split(groupNames.get(0), groupNames.get(1), 50);
     }
 
     @AfterClass
@@ -61,14 +62,7 @@ public class LobTest13227 extends SdbTestBase {
         db.close();
     }
 
-    /**
-     * 测试用例 seqDB-13227 :: 版本: 1 :: 写lob同时并发读lob
-     * 1、打开lob（创建模式是指定lob oid）
-     * 2、写入lob数据，写入过程中读取该lob数据（读取lob时获取指定lob oid）
-     * 3、检查操作结果
-     * 1、写入lob成功，读取lob操作失败，返回对应错误信息
-     * 2、再次读取lob数据和插入lob数据一致（比较MD5值相同）
-     */
+
     @Test(dataProvider = "lobSizeDataProvider", dataProviderClass = RandomWriteLobUtil.LobSizedataProvider.class)
     public void test13227(final int lobsize) throws InterruptedException {
         final ObjectId oid = ObjectId.get();
@@ -102,6 +96,7 @@ public class LobTest13227 extends SdbTestBase {
                         Thread.sleep(500);
                     }
                 }
+                
             }
         }.ignoreExceptionCode(SDBError.SDB_FNE.getErrorCode())
                 .ignoreExceptionCode(SDBError.SDB_LOB_IS_IN_USE.getErrorCode());
