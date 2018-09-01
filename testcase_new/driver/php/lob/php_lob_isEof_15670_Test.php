d10cf25ccf256fa99de5c36320d4b9cb36b94cb0
linsuqiang <linsuqiang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-09-01 05:35:11 +0000

    add testcase for php lob.isEof()
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@36744 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/php/lob/php_lob_isEof_15670_Test.php
@@ -0,0 +1,83 @@
+/****************************************************
+@description:      random write lob
+@testlink cases:   seqDB-14796
+@modify list:
+        2018-3-13  linsuqiang init
+****************************************************/
+<?php
+define('Cur_Path', dirname(__FILE__));
+include_once Cur_Path.'/../commlib/LobUtils.php';
+include_once Cur_Path.'/../global.php';
+class LobTest14796 extends PHPUnit_Framework_TestCase
+{
+   private static $db;
+   private static $csName = "cs15670";
+   private static $clName = "cl15670";
+   private static $cl;
+   private static $LobUtils;
+   private static $oid;
+   private static $writeStr;
+   private static $writeLen = 1025;
+
+   public static function setUpBeforeClass()
+   {
+      self::$db = new Sequoiadb();
+      self::$db -> connect(globalParameter::getHostName().':'. 
+                           globalParameter::getCoordPort()) ;
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+
+      $cs = self::$db -> selectCS( self::$csName );
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+
+      self::$cl = $cs -> selectCL( self::$clName );
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+
+      self::$LobUtils = new LobUtils( self::$db, self::$cl );
+      self::$oid = self::$LobUtils -> getOid();
+      self::$writeStr = self::$LobUtils -> getRandomStr( self::$writeLen );
+      self::$LobUtils -> writeLob( self::$oid, self::$writeStr );
+   }
+
+   public function test()
+   {
+      $lobObj = self::$cl -> openLob( self::$oid, SDB_LOB_READ );
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+      
+      $rData = "";
+      $buffer = "";
+      $bufferLen = 128;
+      while( !$lobObj -> isEof() )
+      {
+         $buffer = $lobObj -> read( $bufferLen );
+         self::checkErrno( 0, self::$db -> getError()['errno'] );
+         $rData .= $buffer;
+      }
+      $lobObj -> close();
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+      
+      $actMd5 = md5( $rData );
+      $expMd5 = md5( self::$writeStr );
+      $this -> assertEquals( $actMd5, $expMd5 );
+   }
+   
+   public static function tearDownAfterClass()
+   {
+      $err = self::$db -> dropCS( self::$csName );
+      if ( $err['errno'] != 0 )
+      {
+         throw new Exception("failed to drop cs, errno=".$err['errno']);
+      }
+      
+      self::$db->close();
+   }
+
+   private static function checkErrno( $expErrno, $actErrno, $msg = "" )
+   {
+      if( $expErrno != $actErrno ) 
+      {
+         throw new Exception( "expect [".$expErrno."] but found [".$actErrno."]. ".$msg );
+      }
+   }
+   
+}
+?>
