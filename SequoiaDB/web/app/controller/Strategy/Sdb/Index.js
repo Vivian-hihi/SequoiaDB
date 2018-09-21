54b6e7812cd4882cab089b44539afc740988f198
huangweigang <huangweigang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-09-21 08:49:08 +0000

    SEQUOIADBMAINSTREAM-3807 3908 sac 1.修改策略页面tab分页名称为中文。 2.修改策略页面中时间的格式，修复表格时间排序错误的问题。
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@36903 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/web/app/controller/Strategy/Sdb/Index.js
+++ SequoiaDB/web/app/controller/Strategy/Sdb/Index.js
@@ -50,10 +50,10 @@
          var date = new Date( timestamp * 1000 ) ;
          Y = date.getFullYear() + '-' ;
          M = ( date.getMonth() + 1 < 10 ? '0' + ( date.getMonth() + 1 ) : date.getMonth() + 1 ) + '-' ;
-         D = date.getDate() + ' ' ;
-         h = date.getHours() + ':' ;
-         m = date.getMinutes() + ':' ;
-         s = date.getSeconds() ;
+         D = ( date.getDate() < 10 ? '0' + date.getDate() : date.getDate() ) + ' ' ;
+         h = ( date.getHours() < 10 ? '0' + date.getHours() : date.getHours() ) + ':' ;
+         m = ( date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes() ) + ':' ;
+         s = ( date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds() ) ;
          return Y+M+D+h+m+s;
       }
 
@@ -236,7 +236,7 @@
 
             }
          } ) ;
-         $scope.CreateTaskWindow['callback']['SetTitle']( $scope.autoLanguage( '添加Task' ) ) ;
+         $scope.CreateTaskWindow['callback']['SetTitle']( $scope.autoLanguage( '添加任务' ) ) ;
          $scope.CreateTaskWindow['callback']['SetIcon']( '' ) ;
          $scope.CreateTaskWindow['callback']['Open']() ;
       }
