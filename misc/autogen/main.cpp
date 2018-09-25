2ee7f3357df61a56011cb3355caadd7ef9cb9ea9
jwhe <jwhe@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-09-25 09:12:30 +0000

    SEQUOIADBMAINSTREAM-3739 重构autogen
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@36932 6121f079-4a18-41d3-8c30-8870a322f2c2

--- misc/autogen/main.cpp
+++ misc/autogen/main.cpp
@@ -1,108 +1,37 @@
-#include "core.hpp"
-#include "rcgen.h"
-#include "filenamegen.h"
-#include "optgen.h"
-#include "tracegen.h"
-#include "buildgen.h"
-#include "dbConfForWeb.h"
-#include "vergen.h"
-#include <iostream>
-using std::cout;
-using std::endl;
+#include "options.hpp"
+#include "generateInterface.hpp"
 
-void displayUsage (const char* argv)
+int main( int argc, char** argv )
 {
-    cout<<"Usage: "<<argv<<" <language>"<<endl
-        <<"    <language>   desciption language as below ( default is english ):"<<endl
-        <<"           en    english descriptions"<<endl
-        <<"           cn    chinese desciptions"<<endl;
-}
+   int rc = 0 ;
+   cmdOptions *opt = getCmdOptions() ;
+   generateRunner *runner = getGenerateRunner() ;
 
-static void genRC ( const char *lang )
-{
-   RCGen xml ( lang ) ;
-   xml.run () ;
-}
-
-static void genOpt ( const char *lang )
-{
-   OptGen xml ( lang ) ;
-   xml.run () ;
-}
-
-static void genTrace ()
-{
-   TraceGen::genList () ;
-}
-
-static void genVer()
-{
-   VerGen verGen ;
-   verGen.run(false) ;
-}
-
-enum supportedLangs
-{
-   LANG_CN = 0,
-   LANG_EN,
-   LANG_MAX
-} ;
-
-const CHAR *pLang[] = {
-   "cn",
-   "en"
-} ;
-
-static void genDoc ( const char *lang )
-{
-   RCGen rcGen ( lang ) ;
-   rcGen.genDoc() ;
-
-   OptGenForWeb optGen ( lang ) ;
-   optGen.run () ;
-
-   VerGen verGen ;
-   verGen.run(true) ;
-
-   for ( int i = 0; i < LANG_MAX; ++i )
+   rc = opt->parse( argc, argv ) ;
+   if ( -1 == rc )
    {
-      RCGen xml ( pLang[i] ) ;
-      // generate web console for english and chinese
-      xml.genWeb() ;  
+      rc = 0 ;
+      goto done ;
    }
-}
-
-static void genBuild ()
-{
-   BuildGen gen ;
-   gen.run () ;
-}
-
-static void genFileName ()
-{
-   FileNameGen::genList() ;
-}
-
-int main (int argc, char** argv)
-{
-   if ( ( 2 == argc && argv[1][0] == '?' ) ||
-        ( 2 < argc ))
+   else if ( rc )
    {
-      if ( argv[1][0] == '?' )
-         displayUsage ( argv[0] ) ;
-      return 0 ;
+      goto error ;
    }
-   const char * lang = "en" ;
-   if ( 2 == argc )
+
+   rc = runner->init() ;
+   if ( rc )
    {
-      lang = argv[1] ;
+      goto error ;
    }
-   genRC ( lang ) ;
-   genFileName () ;
-   genOpt ( lang ) ;
-   genTrace () ;
-   genDoc ( "cn" ) ;
-   genBuild () ;
-   genVer() ;
-   return 0;
+
+   rc = runner->run() ;
+   if ( rc )
+   {
+      goto error ;
+   }
+
+done:
+   return rc ;
+error:
+   goto done ;
 }
