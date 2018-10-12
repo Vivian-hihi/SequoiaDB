3315069065b73ed88044ab6a5d61200222f30cd9
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-12 08:58:49 +0000

    SEQUOIADBMAINSTREAM-3709【story】rename cs/cl
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37085 6121f079-4a18-41d3-8c30-8870a322f2c2

--- SequoiaDB/engine/include/msg.h
+++ SequoiaDB/engine/include/msg.h
@@ -233,11 +233,11 @@ enum MSG_TYPE
    MSG_CAT_TRUNCATE_RSP               = MAKE_REPLY_TYPE( MSG_CAT_TRUNCATE_REQ ),
 
    // rename collection space
-   MSG_CAT_RENAME_CS_REQ              = 3143
+   MSG_CAT_RENAME_CS_REQ              = 3143,
    MSG_CAT_RENAME_CS_RSP              = MAKE_REPLY_TYPE( MSG_CAT_RENAME_CS_REQ ),
 
    // rename collection
-   MSG_CAT_RENAME_CL_REQ              = 3144
+   MSG_CAT_RENAME_CL_REQ              = 3144,
    MSG_CAT_RENAME_CL_RSP              = MAKE_REPLY_TYPE( MSG_CAT_RENAME_CL_REQ ),
 
    // all messages for CATALOGUE must be smaller than MSG_CAT_CATALOGUE_END
