96de6be5431d5b0b840d0baa06af1df58fb1749d
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-12 09:18:53 +0000

    上传fulltext js自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37088 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/fullText/updateArrays_15777.js
@@ -0,0 +1,56 @@
+/************************************
+*@Description: 将全文索引字段为数组类型更新为非string类型 
+*@author:      liuxiaoxuan
+*@createdate:  2018.10.10
+*@testlinkCase: seqDB-15777
+**************************************/
+function main()
+{
+   if(commIsStandalone(db))  {   return ;   }
+
+   var csName = COMMCSNAME + "_ES_15777";
+   commDropCS( db, csName, true, "drop CS in the beginning" );
+                                                             	
+   commCreateCS( db, csName, false, "" );
+                                                              	
+   // create CL
+   var clName = COMMCLNAME + "_ES_15777";
+   var dbcl = commCreateCL( db, csName, clName );
+
+   // insert before create text index
+   var objs = new Array({a: "string1"},
+                        {a: ["arr1"]},
+                        {a: 0},
+                        {a: true},
+                        {a: {b : "ab"}},
+                        {a: {$date: "2018-10-10"}},
+                        {a: ["arr1", "arr2"]},
+                        {a: ["arr1", 1]});
+   dbcl.insert(objs);
+
+   var textIndexName = "textIndex";
+   dbcl.createIndex(textIndexName, {"a" : "text"});
+
+   // check sync to es
+   checkFullSyncToES(csName, clName, textIndexName, 2);
+   
+   // check result
+   var dbOpr = new DBOperator();
+   var findCond = {"":{"$Text":{"query":{"match_all":{}}}}};
+   var expResult = [{a: ["arr1"]}, {a: "string1"}];
+   var actResult = dbOpr.findFromCL(dbcl, findCond, {"a": {"$include": 1}});
+   actResult.sort(compare("a"));
+   checkResult(expResult, actResult);
+
+   // update to non string
+   dbcl.update({"$set" : {a : -1}}, {a : {"$isnull" : 0}});
+   checkFullSyncToES(csName, clName, textIndexName, 0);
+   
+   // check result
+   var expResult = [];
+   var actResult = dbOpr.findFromCL(dbcl, findCond);
+   checkResult(expResult, actResult);
+
+   commDropCS( db, csName, true, "drop CS in the end" );
+}
+main();
