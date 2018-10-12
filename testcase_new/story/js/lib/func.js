293f365eb82296910cfb1ac4c4b67fd346ac8eb2
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-12 06:32:13 +0000

    修改es  hostname为localhost
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37060 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/lib/func.js
+++ testcase_new/story/js/lib/func.js
@@ -19,7 +19,7 @@ if ( typeof(RSRVPORTBEGIN) == "undefined" ) { RSRVPORTBEGIN = '26000'; }
 if ( typeof(RSRVPORTEND)   == "undefined" ) { RSRVPORTEND   = '27000'; }
 if ( typeof(RSRVNODEDIR)   == "undefined" ) { RSRVNODEDIR   = "/opt/sequoiadb/database/"; }
 if ( typeof(WORKDIR)   == "undefined" ) { WORKDIR   = "/tmp/jstest"; }
-if ( typeof(ESHOSTNAME) == "undefined" ) { ESHOSTNAME = '192.168.28.143'; }
+if ( typeof(ESHOSTNAME) == "undefined" ) { ESHOSTNAME = 'localhost'; }
 if ( typeof(ESSVCNAME) == "undefined" ) { ESSVCNAME = '9200'; }
 
 var COMMCSNAME = CHANGEDPREFIX + "_cs" ;
