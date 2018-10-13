93251573b4a52821a7c5c4a007f9215ed1c6fa88
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-13 04:31:53 +0000

    上传全文索引自动化代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37113 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/fullText/update_15528.js
+++ testcase_new/story/js/fullText/update_15528.js
@@ -87,18 +87,22 @@ function insertData( dbcl )
 
 function update( dbcl )
 {
+   //update es support to es support
    dbcl.update( { $set : { _id : 111 } }, { _id : 1 } );
    dbcl.update( { $set : { _id : 22, a : "text22" } }, { _id : 2 } );
    dbcl.update( { $set : { _id : 33, a : "text33" } }, { _id : 3 } );
    dbcl.update( { $set : { _id : 44 } }, { _id : 4 } );
+   //update es support to es not support
    dbcl.update( { $set : { _id : { "$binary" : "aAAsbG8gd29ybGQ=", "$type" : "1" } } }, { _id : 5 } );
    dbcl.update( { $set : { _id : { "$binary" : "aLLsbG8gd29ybGQ=", "$type" : "1" }, a : "text66" } }, { _id : 6 } );
    dbcl.update( { $set : { _id : { "$binary" : "aHHsbG8gd29ybGQ=", "$type" : "1" }, a : "text77" } }, { _id : 7 } );
    dbcl.update( { $set : { _id : { "$binary" : "aIIsbG8gd29ybGQ=", "$type" : "1" } } }, { _id : 8 } );
+   //update es not support to es support
    dbcl.update( { $set : { _id : 9 } }, { a : "text9" } );
    dbcl.update( { $set : { _id : 10, a : "text1010" } }, { a : "text10" } );
    dbcl.update( { $set : { _id : 11, a : "text1111" } }, { b : 1 } );
    dbcl.update( { $set : { _id : 12 } }, { b : 2 } );
+   //update es not support to es not support
    dbcl.update( { $set : { _id : { "$binary" : "aBBsbG8gd29ybGQ=", "$type" : "1" } } }, { a : "text13" } );
    dbcl.update( { $set : { _id : {"$minKey" : 1 }, a : "text1414" } }, { a : "text14" } );
    dbcl.update( { $set : { _id : null, a : "text1515" } }, { b : 3 } );
