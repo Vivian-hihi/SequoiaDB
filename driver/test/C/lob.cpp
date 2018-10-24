54097c0f1d97297e7ff6db096d9b3eb363454378
tanzhaobo <tanzhaobo@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-10-24 10:22:31 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37285 6121f079-4a18-41d3-8c30-8870a322f2c2

--- driver/test/C/lob.cpp
+++ driver/test/C/lob.cpp
@@ -6,6 +6,8 @@
 TEST(lob, lob_global_test)
 {
    INT32 rc = SDB_OK ;
+   BOOLEAN eof = FALSE ;
+   INT32 counter = 0 ;
    // initialize the word environment
    rc = initEnv( HOST, SERVER, USER, PASSWD ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
@@ -43,17 +45,23 @@ TEST(lob, lob_global_test)
    bson_oid_gen( &oid ) ; 
    rc = sdbOpenLob( cl, &oid, SDB_LOB_CREATEONLY, &lob ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( TRUE, eof ) ;
    // get lob size 
    rc = sdbGetLobSize( lob, &lobSize ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
    ASSERT_EQ( 0, lobSize ) ;
    // get lob create time
-   rc = sdbGetLobCreateTime( lob, &createTime ) ;
-   ASSERT_EQ( 0, createTime ) ;
-   ASSERT_EQ( 0, createTime ) ;
+//   rc = sdbGetLobCreateTime( lob, &createTime ) ;
+//   ASSERT_EQ( 0, createTime ) ;
+//   ASSERT_EQ( 0, createTime ) ;
    // write lob 
    rc = sdbWriteLob( lob, buf, BUFSIZE1 ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( TRUE, eof ) ;
    // get lob size 
    rc = sdbGetLobSize( lob, &lobSize ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
@@ -61,24 +69,29 @@ TEST(lob, lob_global_test)
    // write lob
    rc = sdbWriteLob( lob, buf, BUFSIZE1 ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( TRUE, eof ) ;
    // get lob size
    rc = sdbGetLobSize( lob, &lobSize ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
    ASSERT_EQ( 2 * BUFSIZE1, lobSize ) ;
-   // get lob create time
-   rc = sdbGetLobCreateTime( lob, &createTime ) ;
-   ASSERT_EQ( SDB_OK, rc ) ;
-   ASSERT_EQ( 0, createTime ) ;
    // close lob
    rc = sdbCloseLob ( &lob ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
    // open lob with the mode SDB_LOB_READ
    rc = sdbOpenLob( cl, &oid, SDB_LOB_READ, &lob ) ; 
    ASSERT_EQ( SDB_OK, rc ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( FALSE, eof ) ;
    // read lob
    rc = sdbReadLob( lob, 1000, readBuf, &readCount ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
    ASSERT_TRUE( readCount > 0 ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( FALSE, eof ) ;
    for ( INT32 i = 0; i < readCount; i++ )
    {
       ASSERT_EQ( c, readBuf[i] ) ;
@@ -87,12 +100,36 @@ TEST(lob, lob_global_test)
    rc = sdbReadLob( lob, BUFSIZE2, readBuf, &readCount ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
    ASSERT_TRUE( readCount > 0 ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( FALSE, eof ) ;
    for ( INT32 i = 0; i < readCount; i++ )
    {
       ASSERT_EQ( c, readBuf[i] ) << "readCount is: " << readCount 
          << ", c is: " << c << ", i is: " 
          << i << ", readBuf[i] is: " << readBuf[i] ;
    }
+   // close lob
+   rc = sdbCloseLob ( &lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   // reopen it, and read all the content
+   rc = sdbOpenLob( cl, &oid, SDB_LOB_READ, &lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   rc = SDB_OK ;
+   counter = 0 ;
+   while( SDB_EOF != ( rc = sdbReadLob( lob, BUFSIZE2, readBuf, &readCount ) ) )
+   {
+       eof = sdbLobIsEof( lob ) ;
+       ASSERT_EQ( SDB_OK, rc ) ;
+       if ( TRUE == eof )
+       {
+          counter = 1 ;
+       }
+   }
+   ASSERT_EQ( 1, counter ) ;
+   eof = sdbLobIsEof( lob ) ;
+   ASSERT_EQ( SDB_OK, rc ) ;
+   ASSERT_EQ( TRUE, eof ) ;
    // close lob
    rc = sdbCloseLob ( &lob ) ;
    ASSERT_EQ( SDB_OK, rc ) ;
