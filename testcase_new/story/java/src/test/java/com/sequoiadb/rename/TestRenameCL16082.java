c27a6626ff43983a6130e02dca76e8484faed2e4
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-01 07:40:34 +0000

    新增检视意见
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37464 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/rename/TestRenameCL16082.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/rename/TestRenameCL16082.java
@@ -16,7 +16,7 @@ import com.sequoiadb.testcommon.SdbThreadBase;
 
 /**
  *  @FileName:TestRenameCL16082
- *  并发修改cl，其中一个cl修改为另一个cl旧名 
+ *  并发修改cl，其中一个cl修改为另一个cl旧名 //TODO:1、建议加一个字段吧，如@content 这样看起来格式统一
  *  @author chensiqin
  *  @Date 2018-10-23
  *  @version 1.00
@@ -38,7 +38,7 @@ public class TestRenameCL16082 extends SdbTestBase{
         cs = sdb.getCollectionSpace(SdbTestBase.csName);
         DBCollection cl1 = cs.createCollection(clName1);
         DBCollection cl2 = cs.createCollection(clName2);
-        RenameUtil.insertData(cl1, 5);
+        RenameUtil.insertData(cl1, 5);//TODO:2、建议不要使用常量数字，可以定义变量名或者加一些描述
         RenameUtil.insertData(cl2, 20);
     }
 
@@ -49,13 +49,13 @@ public class TestRenameCL16082 extends SdbTestBase{
         rename1.start();
         rename2.start();
         if(rename1.isSuccess() && rename2.isSuccess()) {
-            sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            checkCLRecord(newclName, 5);
+            sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");//TODO:3、setUp中已new sdb，可以直接使用，建议不用再另外new sdb
+            checkCLRecord(newclName, 5);//TODO:4、同2，建议定义有意义的变量名或者描述
             checkCLRecord(clName1, 20);
             RenameUtil.checkRenameCLResult(sdb, SdbTestBase.csName, clName1, newclName);
-            RenameUtil.checkRenameCLResult(sdb, SdbTestBase.csName, clName2, clName1);
+            RenameUtil.checkRenameCLResult(sdb, SdbTestBase.csName, clName2, clName1);//TODO:5、这个不会失败吗？实现里面看clName1不应该存在
         } else if (rename1.isSuccess() && !rename2.isSuccess()) {
-            sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+            sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");//TODO:6、同3
             RenameUtil.checkRenameCLResult(sdb, SdbTestBase.csName, clName1, newclName);
             checkCLRecord(newclName, 5);
             checkCLRecord(clName2, 20);
@@ -68,6 +68,7 @@ public class TestRenameCL16082 extends SdbTestBase{
         }
     }
     
+    //TODO:7、建议把方法都放在@afterClass后面，代码前面时beforClass、test、afterClass结构
     public void checkCLRecord(String name, int expected) {
         DBCollection cl = cs.getCollection(name);
         Assert.assertEquals(cl.getCount(), expected);
@@ -84,18 +85,18 @@ public class TestRenameCL16082 extends SdbTestBase{
         if(cs.isCollectionExist(newclName)){
             cs.dropCollection(newclName);
         }
-        if(this.sdb != null){
+        if(this.sdb != null){//TODO:8、sdb关闭要放在finally，如果上面步骤失败，无法执行close（）
             this.sdb.close();
         }
     }
     
-    
+    //TODO:9、RenameCLThread1和RenameCLThread2代码一致，建议传参，用一份代码
     private class RenameCLThread1 extends SdbThreadBase{
 
         @Override
         public void exec() throws BaseException {
             Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            try {
+            try {//TODO:10、建议使用自动释放资源方法，new db放在try条件中，不需要finally
                 CollectionSpace localcs = db.getCollectionSpace(csName);
                 localcs.renameCollection(clName1, newclName);
                 
--- testcase_new/story/java/src/test/java/com/sequoiadb/rename/TestRenameCL16087.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/rename/TestRenameCL16087.java
@@ -19,13 +19,13 @@ import com.sequoiadb.testcommon.SdbThreadBase;
 
 /**
  *  @FileName:TestRenameCL16087
- *  并发修改主表名和子表名  
+ *  并发修改主表名和子表名  //TODO:1、注释格式建议统一
  *  @author chensiqin
  *  @Date 2018-10-24
  *  @version 1.00
  */
 public class TestRenameCL16087 extends SdbTestBase {
-    /*
+    /*//TODO:2、参考java用例规范，注释放在类注释中
      * 1、并发修改子表名和attach子表，多线程执行如下操作：
      *  a、修改子表名，如修改subcl1为subcl2 
      *  b、主表上attach子表subcl1 
@@ -52,17 +52,18 @@ public class TestRenameCL16087 extends SdbTestBase {
     @Test
     public void test16087(){
         AttachCLThread attachCLThread = new AttachCLThread();
-        RenameSubCLThread subCLThread = new RenameSubCLThread();
+        RenameSubCLThread subCLThread = new RenameSubCLThread();//TODO:8、变量名没有表达实际意义，如可改成renameSubCL
         attachCLThread.start();
         subCLThread.start();
         if (attachCLThread.isSuccess() && subCLThread.isSuccess()) {
             RenameUtil.checkRenameCLResult(sdb, SdbTestBase.csName, subCLName, newSubCLName);
-            DBCollection maincl = cs.getCollection(mainCLName);
+            DBCollection maincl = cs.getCollection(mainCLName);//TODO:3、多余代码
             Assert.assertEquals(cs.isCollectionExist(subCLName), false);
+            //TODO:4、没有检测attach成功的结果，另外注释的代码如果没有问题单建议直接删掉
             //maincl.detachCollection(SdbTestBase.csName+"."+newSubCLName);
         } else if ( !attachCLThread.isSuccess() && subCLThread.isSuccess() ) {
             RenameUtil.checkRenameCLResult(sdb, SdbTestBase.csName, subCLName, newSubCLName);
-            Assert.assertEquals(cs.isCollectionExist(subCLName), false);
+            Assert.assertEquals(cs.isCollectionExist(subCLName), false);//TODO:5、公共方法已判断，重复检测点
             try {
                 DBCollection maincl = cs.getCollection(mainCLName);
                 maincl.detachCollection(SdbTestBase.csName+"."+newSubCLName);
@@ -75,6 +76,7 @@ public class TestRenameCL16087 extends SdbTestBase {
                 Assert.fail("errcode not expected : " + e.getMessage());
             }
         } else if ( attachCLThread.isSuccess() && !subCLThread.isSuccess() ) {
+        	//TODO:5、这里判断有的问题，如果atachCL已成功，还能获取到错误码吗？
             BaseException e = (BaseException)attachCLThread.getExceptions().get(0);
             if ( e.getErrorCode() != -147 ) {
                 Assert.fail("errcode not expected : " + e.getMessage());
@@ -97,7 +99,7 @@ public class TestRenameCL16087 extends SdbTestBase {
         }
         if(this.sdb != null){
             this.sdb.close();
-        }
+        }//TODO:6、close建议放到finally里面
     }
     
     
@@ -110,13 +112,13 @@ public class TestRenameCL16087 extends SdbTestBase {
         options.put("ShardingType", "range");
         cs.createCollection(mainCLName, options);
     }
-
+    
     private class RenameSubCLThread extends SdbThreadBase{
 
         @Override
         public void exec() throws BaseException {
             Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            try {
+            try {//TODO:7、建议用自动释放资源方式，new db放在try条件中
                 CollectionSpace localcs = db.getCollectionSpace(SdbTestBase.csName);
                 localcs.renameCollection(subCLName, newSubCLName);
             }finally {
@@ -130,7 +132,7 @@ public class TestRenameCL16087 extends SdbTestBase {
         @Override
         public void exec() throws Exception {
             Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            try {
+            try {//TODO:8、建议用自动释放资源方式，new db放在try条件中
                 DBCollection maincl = cs.getCollection(mainCLName);
                 BSONObject options = new BasicBSONObject();
                 options.put("LowBound", (BSONObject) JSON.parse("{\"a\":1}"));
