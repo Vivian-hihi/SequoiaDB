2a3370d392fcd1184e41b7d8864e8062653d4291
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-01 09:12:04 +0000

    优化用例中的变量名、注释等，使用例更加规范整洁
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37471 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/java/src/test/java/com/sequoiadb/transaction/TestTransaction13632.java
+++ testcase_new/driver/java/src/test/java/com/sequoiadb/transaction/TestTransaction13632.java
@@ -1,30 +1,38 @@
 package com.sequoiadb.transaction;
 
+import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.CommLib;
 import com.sequoiadb.testcommon.SdbConfTestBase;
 import com.sequoiadb.testcommon.SdbTestBase;
+
+import org.bson.BSONObject;
 import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
- * @author ljt
+ * @FileName: seqDB-13632:: 事务中snapshot9 10
+ * @author laojingtang
+ * @Date 2017-11-27
  * @version 1.00
  */
+
 public class TestTransaction13632 extends SdbConfTestBase {
     private Sequoiadb sdb;
+    private CollectionSpace cs;
     private DBCollection cl;
+    private String clName = "cl13632";
+    private String commCSName ;
+    
 
     @Override
     protected void setNodeConf() {
@@ -34,47 +42,65 @@ public class TestTransaction13632 extends SdbConfTestBase {
 
     @BeforeClass
     public void setUp() {
-        System.out.println("the TestCase Name:" + this.getClass().getName() +
-                ". the TestCase begin at:" + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
+    	commCSName = SdbTestBase.csName;
         sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-        cl=sdb.getCollectionSpace(SdbTestBase.csName)
-                .createCollection(TestTransaction13632.class.getSimpleName());
+        
+        CommLib commlib = new CommLib();
+		if (commlib.isStandAlone(sdb)) {
+			throw new SkipException("skip StandAlone");
+		}
+		
+        if (!sdb.isCollectionSpaceExist(commCSName)) {
+            try{
+                cs = sdb.createCollectionSpace(commCSName); 
+            } catch (BaseException e) {
+                Assert.assertEquals(-33, e.getErrorCode(), e.getMessage());
+            }
+        } else {
+            cs = sdb.getCollectionSpace(commCSName);
+        }
+        if (cs.isCollectionExist(clName)) {
+            cs.dropCollection(clName);
+        }
+        cl=sdb.getCollectionSpace(SdbTestBase.csName).createCollection(clName);
     }
 
     @Test
     public void testTransactionBeginCommit() {
-        if (!Util.isCluster(sdb)) {
-            throw new SkipException("It is not cluster");
-        }
-        List r;
-        r = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS);
-        Assert.assertEquals(r.size(), 0);
-        r = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS_CURRENT);
-        Assert.assertEquals(r.size(), 0);
+        List<BSONObject> transactionList;
+        transactionList = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS);
+        Assert.assertEquals(transactionList.size(), 0);
+        transactionList = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS_CURRENT);
+        Assert.assertEquals(transactionList.size(), 0);
 
         sdb.beginTransaction();
         cl.insert("{a:1}");
-        r = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS);
-        Assert.assertTrue(r.size() > 0,"actual: "+r);
-        r = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS_CURRENT);
-        Assert.assertTrue(r.size() > 0,"actual: "+r);
+        transactionList = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS);
+        Assert.assertTrue(transactionList.size() > 0,"actual: "+transactionList);
+        transactionList = getSnap(Sequoiadb.SDB_SNAP_TRANSACTIONS_CURRENT);
+        Assert.assertTrue(transactionList.size() > 0,"actual: "+transactionList);
         sdb.commit();
     }
 
-    private List getSnap(int code) {
+    private List<BSONObject> getSnap(int code) {
         DBCursor cursor = sdb.getSnapshot(code, "", "", "");
-        List r = new ArrayList(5);
+        List<BSONObject> transactionList = new ArrayList<BSONObject>(5);
         while (cursor.hasNext())
-            r.add(cursor.getNext());
-        return r;
+        	transactionList.add(cursor.getNext());
+        return transactionList;
     }
 
     @AfterClass
-    public void tearDown() {
-        System.out.println("the TestCase Name:" + this.getClass().getName() +
-                ". the TestCase end at:" + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
-        sdb.getCollectionSpace(SdbTestBase.csName)
-                .dropCollection(cl.getName());
-        sdb.disconnect();
+    public void tearDown(){
+        try{
+	        CollectionSpace cs = sdb.getCollectionSpace(csName);    
+	        if(cs.isCollectionExist(cl.getName())){
+	            cs.dropCollection(cl.getName());
+	        }
+        }catch(BaseException e){            
+            Assert.fail(e.getMessage());
+        }finally{
+            sdb.close();
+        }
     }
 }
