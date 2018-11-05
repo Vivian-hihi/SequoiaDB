1f6850bd5bfe277b245696d2bb9033f6832e7730
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-05 04:27:14 +0000

    回归问题单3858,提交用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37510 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/renameTransaction/renameCS_16105.js
@@ -0,0 +1,61 @@
+/************************************
+*@Description: 事务操作过程中修改cs名
+*@author:      luweikang
+*@createdate:  2018.10.12
+*@testlinkCase:seqDB-16105
+**************************************/
+
+main();
+
+function main()
+{
+   println("---begin rename cs test---");
+   var oldcsName = COMMCSNAME+"_16105_old";
+   var newcsName = COMMCSNAME+"_16105_new";
+   var clName = CHANGEDPREFIX + "_16105_cl";
+   
+   var cs = commCreateCS( db, oldcsName, false, "create cs in begine", "");
+   var cl = commCreateCLByOption( db, oldcsName, clName, {}, false, false, "create CL in the begin");
+   
+   db.transBegin();
+   cl.insert({"no":10086, customerName:"testTrans", "phone":13700010086, "openDate":1402990912105});
+   cl.insert({"no":10000, customerName:"testTrans", "phone":13700010000, "openDate":1402990912106});
+   
+   for(var i=0; i<10; i++)
+   {
+      try
+      {
+         db.renameCS(oldcsName, newcsName);
+      }
+      catch(e)
+      {
+         if(e!==-3)
+         {
+            throw buildException("rename cs during the trans", e, "trans", -3, e);
+         }
+      }
+   }
+   
+   cl.insert({"no":10001, customerName:"testTrans", "phone":13700010001, "openDate":1402990912107});
+   cl.insert({"no":10100, customerName:"testTrans", "phone":13700010248, "openDate":1402990912108});
+   
+   db.transCommit();
+   
+   db.renameCS(oldcsName, newcsName);
+   
+   cl = db.getCS(newcsName).getCL(clName);
+   checkCLRecord(cl, {customerName: 'testTrans'}, 4);
+   
+   checkRenameCSResult(oldcsName, newcsName, 1);
+   
+   commDropCS( db, newcsName, true, false, "clean cs---" );
+   println("---end the test---");
+}
+
+function checkCLRecord( cl, cond, expNum )
+{
+   var actNum = cl.count(cond);
+   if(actNum != expNum){
+      throw buildException("checkCLRecord", "", "check the cl recond num error", expNum, actNum);
+   }
+}
