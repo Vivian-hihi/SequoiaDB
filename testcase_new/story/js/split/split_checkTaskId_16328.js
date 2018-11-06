f6642bda8f8f780d9d3ff0b6ef0c1be72c5977a1
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-06 02:41:43 +0000

    SEQUOIADBMAINSTREAM-3860【BUG】【old】【story】【数据切分】异步切分任务建立后返回任务ID为undefined
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37546 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/split/split_checkTaskId_16328.js
@@ -0,0 +1,135 @@
+/* *****************************************************************************
+@discretion: cl asynchronous split , check the task id
+@author：2018-11-06 wangkexin
+***************************************************************************** */
+
+main(db);
+function main(db)
+{	  
+	try
+	{	
+	   if (commGetGroupsNum(db) < 2)
+      {
+         println("--least two groups");
+         return ;
+      }   
+
+	  var clName = CHANGEDPREFIX + "_checktaskidcl16328";     
+      commDropCL( db, COMMCSNAME, clName, true, true, "clear collection in the beginning" ) ; 
+      var options = {ShardingKey:{No:1}, ShardingType:"range",ReplSize:0,Compressed:true};
+      
+      var dbcl = commCreateCLByOption( db, COMMCSNAME, clName, options, true, true );
+      var recordNums = 10000;      
+      insertData( db, COMMCSNAME, clName, recordNums ); 
+         
+      var targetGroupNums = 2;
+      var groupsInfo = getSplitGroups( COMMCSNAME, clName, targetGroupNums );
+      var taskId = splitCL( COMMCSNAME, clName );      
+      
+      println("---Begin to check result ");
+	  checkTaskId( taskId );
+      checkSplitResult( COMMCSNAME, clName, recordNums, groupsInfo);     
+      
+      commDropCL( db, COMMCSNAME, clName, true, true, "clear collection in the ending" ) ;      
+   }
+   catch( e )
+   {
+      throw e ;
+   }   
+}
+
+function splitCL( csName, clName )
+{
+   try
+   {
+      println("---Begin to splitAsync"); 
+      var dbcl = db.getCS( csName ).getCL( clName );             
+      var percent = 50;
+      var targetGroupNums = 2;
+      var groupsInfo = getSplitGroups( COMMCSNAME, clName, targetGroupNums );
+      var srcGroupName = groupsInfo[0].GroupName;
+      var dstGroupName = groupsInfo[1].GroupName;      
+      var taskId = dbcl.splitAsync(srcGroupName, dstGroupName, percent); 
+      return taskId;
+   } 
+   catch( e )
+   {
+      throw buildException("splitAsync datas", e)
+   } 
+}
+
+function checkTaskId( taskId )
+{
+	try
+   {
+	   println("---Begin to check the task id");
+	   var resultArr = db.listTasks({"TaskID":taskId}).toArray();
+	   if(resultArr.length !== 1)
+	   {
+		   throw buildException("check listTasks()", null, "check the task id",
+									taskId, "not exist");
+		}
+   }
+   catch(e)
+   {
+      throw buildException("check task id", e)
+   } 
+}
+
+function checkSplitResult( csName, clName, expRecordNums, groupsInfo )
+{   
+   try
+   {  println("---Begin to check the split result");
+	  //waiting for split 
+      var sleepInteval=10;
+      var sleepDuration=0;
+      var maxSleepDuration=10000;
+	  
+      while( (db.listTasks({ "Name": csName + "."+ clName }).next() !== undefined ) && sleepDuration < maxSleepDuration )
+      {        
+         sleep( sleepInteval );
+         sleepDuration += sleepInteval;                       
+      }
+	  
+	  //check the record nums      
+      var dbcl = db.getCS( csName ).getCL( clName );
+      var count = dbcl.count();      
+      if( count != expRecordNums  )
+      {
+         throw buildException("check datas", null, "check the new cl record nums",
+									expRecordNums, count);
+      }   
+       
+      //test record nums of split groups
+      for( var i = 0; i < 2; i++ )
+      {         
+         try
+         {
+            var sdb = new Sdb(groupsInfo[i].HostName,groupsInfo[i].svcname);            
+            var cl = sdb.getCS( csName ).getCL( clName );
+            var num = cl.count();  
+            println(num)       
+      	
+            if( Number(num) !== expRecordNums/2 )			
+	         {  
+               throw buildException("checkClSplitRecordNums", "count wrong", "count()",expRecordNums/2, num)
+	         }	      
+         }
+         catch(e)
+         {
+            throw buildException("checkClSplitResult()", e);
+         }
+         finally
+         {
+            if (sdb !== undefined)
+      	   {
+               sdb.close();      	      
+      	   } 
+         }	           
+      }    
+   }
+   catch(e)
+   {
+      throw buildException("check split", e)
+   }  
+}
