cd54147824b00d845840e7150551fb3c648e99db
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-06 07:10:25 +0000

    优化用例，去除冗余代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37560 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/java/src/test/java/com/sequoiadb/auth/TestSdbUser16280.java
+++ testcase_new/driver/java/src/test/java/com/sequoiadb/auth/TestSdbUser16280.java
@@ -1,21 +1,9 @@
 package com.sequoiadb.auth;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.bson.BSONObject;
-import org.bson.BasicBSONObject;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import com.sequoiadb.base.CollectionSpace;
-import com.sequoiadb.base.DBCollection;
-import com.sequoiadb.base.DBCursor;
-import com.sequoiadb.base.Node;
-import com.sequoiadb.base.ReplicaGroup;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
@@ -33,31 +21,13 @@ public class TestSdbUser16280 extends SdbTestBase{
     
     @BeforeClass
     public void setUp() {
-        try {
-            this.coordAddr = SdbTestBase.coordUrl;
-            System.out.println("the TestCase Name:" + this.getClass().getName() + 
-                    ". the TestCase begin at:" + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
-            sdb = new Sequoiadb(this.coordAddr, "", "");
-            
-        }catch (BaseException e) {
-            Assert.fail("Sequoiadb driver TestSdbUser16820 setUp error, error description:" + e.getMessage());
-        }
+        this.coordAddr = SdbTestBase.coordUrl;
+        sdb = new Sequoiadb(this.coordAddr, "", "");
     }
+    
     @Test
     public void test() {
         testSdbUser();
-    }
-    
-    public void testSdbUser() {
-        if (!Util.isCluster(this.sdb)) {
-            return ;
-        }
-        try {
-            sdb.createUser("admin", "admin");
-            Sequoiadb sdb = new Sequoiadb(coordAddr, "admin", "");
-        }catch (BaseException e) {
-            Assert.assertEquals(e.getErrorCode(), -179);
-        }
     }
     
     @AfterClass
@@ -70,13 +40,19 @@ public class TestSdbUser16280 extends SdbTestBase{
                     Assert.assertTrue(false, "drop user, errMsg: " + e.getMessage());
                 }
             }
-            
             sdb.close();
-            System.out.println("the TestCase Name:" + this.getClass().getName() + 
-                    ". the TestCase end at:" + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
         } catch (BaseException e) {
             Assert.fail(e.getMessage());
         }
     } 
+    
+    public void testSdbUser() {
+        try {
+            sdb.createUser("admin", "admin");
+            Sequoiadb sdb = new Sequoiadb(coordAddr, "admin", "");
+        }catch (BaseException e) {
+            Assert.assertEquals(e.getErrorCode(), -179);
+        }
+    }
 }
  
