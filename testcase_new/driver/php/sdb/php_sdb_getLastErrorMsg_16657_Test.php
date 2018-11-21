244bc28091c965d3ac45a492692fd7e8ad35f3c1
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-21 06:55:10 +0000

    回归问题单:2060规范错误传递新增自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37884 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/php/sdb/php_sdb_getLastErrorMsg_16657_Test.php
@@ -0,0 +1,73 @@
+/****************************************************
+@description:      check error msg
+@testlink cases:   seqDB-16657
+@modify list:
+        2018-11-19 Luweikang init
+****************************************************/
+<?php
+define('Cur_Path', dirname(__FILE__));
+include_once Cur_Path.'/../global.php';
+
+class GetLastErrMsg16657 extends PHPUnit_Framework_TestCase
+{
+   private static $csName = "cs16657";
+   private static $clName = "cl16657";
+   private static $cl;
+   private static $db;
+   
+   public static function setUpBeforeClass()
+   {
+      self::$db = new Sequoiadb();
+      self::$db -> connect(globalParameter::getHostName().':'. 
+                           globalParameter::getCoordPort()) ;
+      self::checkErrno( 0, self::$db -> getError()['errno'] );                     
+                           
+      $cs = self::$db -> selectCS( self::$csName );
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+      self::$cl = $cs -> selectCL( self::$clName );	
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+      self::$cl -> createIndex( array( 'a' => 1), "myIndex", true );
+   }
+   
+   function test()
+   {
+      echo "\n---Begin to check getError.\n";
+      self::$cl -> insert( array( 'a' => 1) );
+      self::checkErrno( 0, self::$db -> getError()['errno'] );
+      self::$cl -> insert( array( 'a' => 1) );
+      $errMsg = sdbGetLastErrorMsg();
+      self::checkErrorMsg( $errMsg, -38, "insert the same record");
+      self::$db -> getCL("text.text");
+      $errMsg = sdbGetLastErrorMsg();
+      self::checkErrorMsg( $errMsg, -23, "get not exist cl");
+   }
+   
+   public static function tearDownAfterClass()
+   {
+      $err = self::$db -> dropCS( self::$csName );
+      if ( $err['errno'] != 0 )
+      {
+         throw new Exception("failed to drop cs, errno=".$err['errno']);
+      }
+      self::$db->close();
+   }
+   
+   private static function checkErrno( $expErrno, $actErrno, $msg = "" )
+   {
+      if( $expErrno != $actErrno ) 
+      {
+         throw new Exception( "expect [".$expErrno."] but found [".$actErrno."]. ".$msg );
+      }
+   }
+
+   private static function checkErrorMsg( $errorMsg, $expErrno, $msg = "" )
+   {
+      $actErrno = $errorMsg['errno'];
+      if( $actErrno != $expErrno )
+      {
+         throw new Exception( "expect [".$expErrno."] but found [".$actErrno."]. ".$msg );
+      }
+   }
+   
+}
+?>
