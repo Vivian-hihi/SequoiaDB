6eae9f4a8603e1a152409898b5235612cafe4b8d
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-21 01:50:06 +0000

    根据检视意见修改代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37866 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/CSharp/Crud/Insert/Insert16622.cs
+++ testcase_new/driver/CSharp/Crud/Insert/Insert16622.cs
@@ -24,6 +24,8 @@ namespace CSharp.Crud.Insert
     {
         private Sequoiadb sdb = null;
         private CollectionSpace cs = null;
+        private DBCollection cl1 = null;
+        private DBCollection cl2 = null;
 
         private string clName1 = "cl16622_1";
         private string clName2 = "cl16622_2";
@@ -58,82 +60,82 @@ namespace CSharp.Crud.Insert
         //BsonDocument Insert(BsonDocument record, int flags)
         public void TestInsert16622_1()
         {
-            DBCollection cl = cs.CreateCollection(clName1);
+            cl1 = cs.CreateCollection(clName1);
             BsonDocument record = new BsonDocument();
             record.Add("_id", 1);
             record.Add("num", 1);
-            cl.Insert(record);
+            cl1.Insert(record);//插入第一条记录
             try
-            {//TODO:1这里插入的记录没有重复，不会报-38，另外如果预期报错，需要判断如果成功则要抛异常
-                cl.Insert(record, 0);
+            {
+                cl1.Insert(record, 0);//重复插入record
+                Assert.Fail("expected insert throw exception ");
             }
             catch (BaseException e)
             {
                 Assert.AreEqual(-38, e.ErrorCode);
             }
 
-            //TODO:2、没有校验设置为FLG_INSERT_CONTONDUP的结果，建议和其他flag分开校验
-            cl.Insert(record, SDBConst.FLG_INSERT_CONTONDUP);
+            cl1.Insert(record, SDBConst.FLG_INSERT_CONTONDUP);
+            Assert.AreEqual(1, cl1.GetCount(null));
+
             record = new BsonDocument();
             record.Add("_id", 2);
             record.Add("num", 2);
-            BsonDocument doc = cl.Insert(record, SDBConst.FLG_INSERT_RETURN_OID);
-            Assert.AreEqual(2, cl.GetCount(null));
+            BsonDocument doc = cl1.Insert(record, SDBConst.FLG_INSERT_RETURN_OID);
+            Assert.AreEqual(2, cl1.GetCount(null));
             Assert.AreEqual("{ \"_id\" : 2 }", doc.ToString());
             cs.DropCollection(clName1);
         }
         // BsonDocument Insert(List<BsonDocument> recordList, int flags)
         public void TestInsert16622_2()
         {
+            cl2 = cs.CreateCollection(clName2);
             InsertDuplicateKey0();
             InsertDuplicateKey1();
             InsertReturnOID();
+            cs.DropCollection(clName2);
         }
 
         //set the flag is 0
         public void InsertDuplicateKey0()
         {
-            DBCollection cl = cs.CreateCollection(clName2);
             try
             {
-                cl.Delete(null);
+                cl2.Delete(null);
                 List<BsonDocument> insertor = GenerateDuplicateData();
-                cl.Insert(insertor, 0);
+                cl2.Insert(insertor, 0);
                 Assert.Fail("bulkInsert will interrupt when Duplicate key exist");
             }
             catch (BaseException e)
             {
                 Assert.AreEqual(e.ErrorCode, -38, e.ErrorCode + e.Message);
             }
-            Assert.AreEqual(1, cl.GetCount(null));
-            //TODO:3、这里的校验请描述下为啥要验证{ "test16622", "test16622" }
-            Assert.AreEqual(0, cl.GetCount(new BsonDocument { { "test16622", "test16622" } }));
-            cs.DropCollection(clName2);
+            Assert.AreEqual(1, cl2.GetCount(null));
+            //flag为0时失败，报-38之后记录不会再插入
+            Assert.AreEqual(0, cl2.GetCount(new BsonDocument { { "test16622", "test16622" } }));
         }
 
         //set the flag is SDBConst.FLG_INSERT_CONTONDUP
         public void InsertDuplicateKey1()
         {
-         //TODO:4、这里可以直接truncate数据，没有必要再重建cl
-            DBCollection cl = cs.CreateCollection(clName2);
+            cl2.Truncate();
             try
             {
                 List<BsonDocument> insertor = GenerateDuplicateData();
-                cl.Insert(insertor, SDBConst.FLG_INSERT_CONTONDUP);
+                cl2.Insert(insertor, SDBConst.FLG_INSERT_CONTONDUP);
             }
             catch (BaseException e)
             {
                 Assert.Fail("Failed to bulkinsert:", e.ErrorCode + e.Message);
             }
-            Assert.AreEqual(2, cl.GetCount(null));
-            Assert.AreEqual(1, cl.GetCount(new BsonDocument { { "test16622", "test16622" } }));
-            cs.DropCollection(clName2);
+            Assert.AreEqual(2, cl2.GetCount(null));
+            Assert.AreEqual(1, cl2.GetCount(new BsonDocument { { "test16622", "test16622" } }));
         }
 
         //set the flag is SDBConst.FLG_INSERT_RETURN_OID
         public void InsertReturnOID()
         {
-            DBCollection cl = cs.CreateCollection(clName2);
+            cl2.Truncate();
             List<BsonDocument> insertor = new List<BsonDocument>();
             for (int i = 0; i < 2; i++)
             {
@@ -143,9 +145,8 @@ namespace CSharp.Crud.Insert
                             Add("name", "zhangsan" + i);
                 insertor.Add(obj);
             }
-            BsonDocument doc = cl.Insert(insertor, SDBConst.FLG_INSERT_RETURN_OID);
+            BsonDocument doc = cl2.Insert(insertor, SDBConst.FLG_INSERT_RETURN_OID);
             Assert.AreEqual("{ \"_id\" : [0, 1] }", doc.ToString());
-            cs.DropCollection(clName2);
         }
 
         public List<BsonDocument> GenerateDuplicateData()
@@ -156,7 +157,7 @@ namespace CSharp.Crud.Insert
                 BsonDocument obj = new BsonDocument();
                 obj.Add("_id", 1).
                             Add("operation", "Insert").
-                            Add("name", "zhangsan"+i);
+                            Add("name", "zhangsan" + i);
                 insertor.Add(obj);
             }
 
