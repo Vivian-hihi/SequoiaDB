ade0ba40ba5437c6e34c7b6c35059a97f2ed7925
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-11-22 07:03:24 +0000

    skip指定为主节点-1个
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@37904 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/java/src/test/java/com/sequoiadb/sdb/TestgetSnapshot15758.java
+++ testcase_new/driver/java/src/test/java/com/sequoiadb/sdb/TestgetSnapshot15758.java
@@ -17,12 +17,12 @@ import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
 
 /**  
-    * FileName:        TestgetSnapshot15758   seqDB-15758
-    * test interface:  getSnapshot(int snapType, BSONObject matcher, BSONObject selector, 
-                                   BSONObject orderBy, BSONObject hint, long skipRows, long returnRows)
-    * @author          chensiqin
-    * @Date           2018.9.7
-    * @version        1.00
+     * FileName:        TestgetSnapshot15758   seqDB-15758
+     * test interface:  getSnapshot(int snapType, BSONObject matcher, BSONObject selector, 
+                                BSONObject orderBy, BSONObject hint, long skipRows, long returnRows)
+     * @author          chensiqin
+     * @Date           2018.9.7
+     * @version        1.00
 */
 public class TestgetSnapshot15758 extends SdbTestBase{
     private Sequoiadb sdb;
@@ -50,13 +50,22 @@ public class TestgetSnapshot15758 extends SdbTestBase{
         BSONObject orderBy = new BasicBSONObject();
         orderBy.put("NodeName", 1);
         BSONObject hint = new BasicBSONObject();
-        DBCursor cursor = sdb.getSnapshot(Sequoiadb.SDB_SNAP_HEALTH, matcher, selector, orderBy, hint, 0, 1);
+        DBCursor cursor = sdb.getSnapshot(Sequoiadb.SDB_SNAP_HEALTH, "{ \"IsPrimary\" : true , \"ServiceStatus\" : true }", "", "");
+        int  length = 0;
+        while(cursor.hasNext()){
+            cursor.getNext();
+            length++;
+        }
+        cursor.close();
+        
+        cursor = sdb.getSnapshot(Sequoiadb.SDB_SNAP_HEALTH, matcher, selector, orderBy, hint, length-1, 1);
         int  num = 0;
         while(cursor.hasNext()){
             num++;
             Assert.assertEquals(cursor.getNext().toString(), "{ \"IsPrimary\" : true , \"ServiceStatus\" : true }");
         }
         Assert.assertEquals(num, 1);
+        cursor.close();
     }
     
     @AfterClass
