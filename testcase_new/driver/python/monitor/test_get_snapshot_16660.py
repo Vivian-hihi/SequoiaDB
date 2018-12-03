a616f1e16b3cddfecf179ccc379db647ee786f55
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-03 06:44:15 +0000

    更新Python驱动get_snapshot自动化测试用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38100 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/python/monitor/test_get_snapshot_16660.py
+++ testcase_new/driver/python/monitor/test_get_snapshot_16660.py
@@ -1,4 +1,4 @@
-# @decription appoint hint/num_to_skip/num_to_return to get SDB_SNAP_ACCESSPLANS information
+# @decription appoint hint/num_to_skip/num_to_return to get SDB_SNAP_CATALOG information
 # @testlink   seqDB-16660
 # @interface  get_snapshot	( self, snap_type, kwargs )		
 # @author     yinzhen 2018-11-29
@@ -8,11 +8,15 @@ from lib import sdbconfig
 from pysequoiadb.error import (SDBBaseError, SDBEndOfCursor)
 import time
 
-SDB_SNAP_ACCESSPLANS = 11
+SDB_SNAP_CATALOG = 8
 SDB_SNAP_CONFIGS = 13
 
 class TestGetSnapshot16660(testlib.SdbTestBase):
    def setUp(self):
+      #judge not standlone mode
+      if testlib.is_standalone():
+         self.skipTest("skip! This testcase do not support standlone")
+   
       # create cs cl   
       testlib.drop_cs(self.db, self.cs_name, ignore_not_exist=True)      
       self.cs = self.db.create_collection_space(self.cs_name)
@@ -22,30 +26,34 @@ class TestGetSnapshot16660(testlib.SdbTestBase):
       #insert data
       self.insert_records()
       
-      # get_snapshot snap_type is SDB_SNAP_ACCESSPLANS
-      actResult = self.get_snapshot_result(SDB_SNAP_ACCESSPLANS, num_to_skip = 10, num_to_return = 10)
+      #create collections
+      for i in range(0, 30):
+         self.cs.create_collection("testcl_" + str(i))
+      
+      # get_snapshot snap_type is SDB_SNAP_CATALOG
+      actResult = self.get_snapshot_result(SDB_SNAP_CATALOG, num_to_skip = 10, num_to_return = 10)
       actResultCount = len(actResult)
       expResultCount = 10
       self.assertEqual(actResultCount, expResultCount, "actResult length is not equal to expResult length ====> NOT appoint")
       
-      actResult = self.get_snapshot_result(SDB_SNAP_ACCESSPLANS, condition = {"NodeName":{"$exists":1}}, num_to_skip = 8, num_to_return = 8)
+      actResult = self.get_snapshot_result(SDB_SNAP_CATALOG, condition = {"UniqueID":{"$exists":1}}, num_to_skip = 8, num_to_return = 8)
       actResultCount = len(actResult)
       expResultCount = 8
       self.assertEqual(actResultCount, expResultCount, "actResult length is not equal to expResult length ====> appoint condition")
       
-      actResult = self.get_snapshot_result(SDB_SNAP_ACCESSPLANS, selector = {"NodeName":""}, num_to_skip = 9, num_to_return = 9)
+      actResult = self.get_snapshot_result(SDB_SNAP_CATALOG, selector = {"Name":""}, num_to_skip = 9, num_to_return = 9)
       actResultCount = len(actResult)
       expResultCount = 9
       self.assertEqual(actResultCount, expResultCount, "actResult length is not equal to expResult length ====> appoint selector")
       
-      actResult = self.get_snapshot_result(SDB_SNAP_ACCESSPLANS, order_by = {"NodeName":-1}, num_to_skip = 12, num_to_return = 12)
+      actResult = self.get_snapshot_result(SDB_SNAP_CATALOG, order_by = {"Name":-1}, num_to_skip = 12, num_to_return = 12)
       actResultCount = len(actResult)
       expResultCount = 12
       self.assertEqual(actResultCount, expResultCount, "actResult length is not equal to expResult length ====> appoint order_by")
       
-      actResult = self.get_snapshot_result(SDB_SNAP_CONFIGS, hint = {"$Option":{"expand":"false"}}, num_to_skip = 2, num_to_return = 2)
+      actResult = self.get_snapshot_result(SDB_SNAP_CONFIGS, hint = {"$Option":{"expand":"false"}}, num_to_skip = 0, num_to_return = 1)
       actResultCount = len(actResult)
-      expResultCount = 2
+      expResultCount = 1
       self.assertEqual(actResultCount, expResultCount, "actResult length is not equal to expResult length ====> appoint hint")
 	
    def tearDown(self):
@@ -73,3 +81,4 @@ class TestGetSnapshot16660(testlib.SdbTestBase):
          self.cl.bulk_insert(flags, doc)
       except SDBBaseError as e:
          self.fail('insert fail: ' + e.detail)
+         
