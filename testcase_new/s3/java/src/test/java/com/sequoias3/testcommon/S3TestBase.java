5ebedd84d60d3a2fe61eb4bfeefb8e2499061fba
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-07 00:59:02 +0000

    提交s3环境准备相关代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38187 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/s3/java/src/test/java/com/sequoias3/testcommon/S3TestBase.java
+++ testcase_new/s3/java/src/test/java/com/sequoias3/testcommon/S3TestBase.java
@@ -1,8 +1,19 @@
 package com.sequoias3.testcommon;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
+import org.bson.BSONObject;
+import org.bson.types.BasicBSONList;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
@@ -10,6 +21,8 @@ import org.testng.annotations.Parameters;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.sequoiadb.base.ReplicaGroup;
+import com.sequoiadb.base.Sequoiadb;
 
 public class S3TestBase {
     protected static String coordUrl;
@@ -27,14 +40,16 @@ public class S3TestBase {
     protected static String workDir;
     protected static String s3UserName;
     protected static String s3AccessKeyId;
+    protected static String confTool;
+    private static SdbConfTestBase sdbConfTestBase = new SdbConfTestBase();
 
     @Parameters({"HOSTNAME", "SVCNAME", "CHANGEDPREFIX", "RSRVPORTBEGIN", "RSRVPORTEND",
-    	"RSRVNODEDIR", "WORKDIR","S3HOSTNAME","S3PORT","S3USERNAME","S3ACCESSKEYID"})
+    	"RSRVNODEDIR", "WORKDIR","S3HOSTNAME","S3PORT","S3USERNAME","S3ACCESSKEYID","CONFTOOL"})
     @BeforeSuite
     public static void initSuite(String HOSTNAME, String SVCNAME, String COMMCSNAME,
                                  int RSRVPORTBEGIN, int RSRVPORTEND, String RSRVNODEDIR,
                                  String WORKDIR,String S3HOSTNAME, String S3PORT,String S3USERNAME,
-                                 String S3ACCESSKEYID) {
+                                 String S3ACCESSKEYID, String CONFTOOL) {
         hostName = HOSTNAME;
         serviceName = SVCNAME;
         csName = COMMCSNAME;
@@ -50,7 +65,10 @@ public class S3TestBase {
         s3ClientUrl = "http://"+ S3HOSTNAME + ":" + S3PORT;
         bucketName = "commbucket";
         enableVerBucketName = "commbucketwithversion";
+        confTool = CONFTOOL;
 
+        sdbConfTestBase.openTransaction(confTool, hostName, serviceName);
+        createCSCLAndStartS3();
         //clean file
         File workDirFile = new File(workDir);
         if (!workDirFile.exists()) {
@@ -68,7 +86,7 @@ public class S3TestBase {
     			if( bucketVerStatus == "null"){
     				CommLib.deleteAllObjects(s3Client, bucketName);
     			}else{
-    				CommLib.deleteAllObjectVersions( s3Client, bucketName );;	
+    				CommLib.deleteAllObjectVersions( s3Client, bucketName );
     			}
     			s3Client.deleteBucket(bucketName);
     		}          
@@ -83,12 +101,13 @@ public class S3TestBase {
             if (s3Client != null) {
                 s3Client.shutdown();
             }
-        }        
+        }    
+
     }
 
     @AfterSuite
     public static void finiSuite() {
-       
+        sdbConfTestBase.closeTransaction(hostName, serviceName);
     }
 
     public static String getDefaultCoordUrl() {
@@ -101,5 +120,87 @@ public class S3TestBase {
     
     public static String getDefaultS3ClientUrl() {
         return s3ClientUrl;
+    }
+    
+    public static void createCSCLAndStartS3() {
+        try{
+            Properties prop = new Properties();
+            InputStream in = new FileInputStream( new File("/etc/default/sequoiadb") );
+            prop.load( in );
+            String installPath = prop.getProperty( "INSTALL_DIR" );
+            String sdbFullName = installPath + "/bin/sdb";
+            System.out.println("begin exec createCSCLexample.js");
+            String[] strCmd = new String[5];
+            strCmd[0] = sdbFullName;
+            strCmd[1] = "-f";
+            strCmd[2] = installPath+"/tools/sequoias3/createCSCLexample.js";
+            strCmd[3] = "-e";
+            strCmd[4] = "var COORDSVCNAME='" + serviceName + "'";
+            System.out.println( "exec cmd: " + Arrays.toString( strCmd ) );
+            Process process = Runtime.getRuntime().exec( strCmd );
+         
+            BufferedReader input = new BufferedReader( new InputStreamReader( process.getInputStream() ) );
+            String line = "";
+            while( (line = input.readLine()) != null ){
+                System.out.println(line);
+            }
+         
+            int exitValue = process.waitFor();
+            if( 0 != exitValue ){
+                Assert.fail( "fail to exec createCSCLexample.js, return code=" + exitValue );
+            }
+            System.out.println("finish exec createCSCLexample.js");
+            //更新properties
+            System.out.println("begin update application.properties");
+            Sequoiadb localdb = null;
+            try{
+                localdb = new Sequoiadb(coordUrl, "", "");
+                ReplicaGroup rg = localdb.getReplicaGroup("SYSCoord");
+                BSONObject rgDetail = rg.getDetail();
+                BasicBSONList groupInfo = (BasicBSONList) rgDetail.get("Group");
+                String coordUrls = "";
+                for(int i = 0;i<groupInfo.toArray().length;i++){
+                    BSONObject groupObj =  (BSONObject) groupInfo.toArray()[i];
+                    String groupName = (String) groupObj.get("HostName");
+                    if ( i != 0 ) {
+                        coordUrls = coordUrls+",";
+                    }
+                    coordUrls = coordUrls + groupName+":"+serviceName;
+                }
+
+                
+                FileWriter f = new FileWriter( installPath+"/tools/sequoias3/config/application.properties" );
+                f.write( "sdbs3.sequoiadb.url=sequoiadb://"     + coordUrls       + "\n" );
+                f.flush();
+                f.close();
+                
+                System.out.println("write properties: " + installPath + "/tools/sequoias3/config/application.properties" );
+            }catch( Exception e ){
+                e.printStackTrace();
+                Assert.fail( "update application.properties file failed" );
+            }finally {
+                if(localdb != null) {
+                    localdb.close();
+                }
+            }
+            System.out.println("finish update application.properties");
+            
+            //启动
+            String[] cmd = new String[2];
+            cmd[0] = installPath+"/tools/sequoias3/sequoias3.sh";
+            cmd[1] = "start";
+            System.out.println( "exec cmd: " + Arrays.toString( cmd ) );
+            process = Runtime.getRuntime().exec( cmd );
+            input = new BufferedReader( new InputStreamReader( process.getInputStream() ) );
+
+            exitValue = process.waitFor();
+            if( 0 != exitValue ){
+                Assert.fail( "fail to start s3, return code=" + exitValue );
+            }  
+        }
+        catch( InterruptedException | IOException e ){
+            e.printStackTrace();
+            Assert.fail( "fail to createCSCLAndStartS3" );
+        }
     }
 }
