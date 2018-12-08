91bc043e1c9111541878f9efa1fe236f91b5dffa
fanyu <fanyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-08 01:11:28 +0000

    s3的用户名不支持+号
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38218 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/s3/java/src/test/java/com/sequoias3/user/Param_CreateUser16278.java
+++ testcase_new/s3/java/src/test/java/com/sequoias3/user/Param_CreateUser16278.java
@@ -3,6 +3,8 @@ package com.sequoias3.user;
 import com.sequoias3.testcommon.S3TestBase;
 import com.sequoias3.testcommon.s3utils.UserUtils;
 import org.json.JSONObject;
+import org.json.XML;
+import org.springframework.web.client.HttpClientErrorException;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -22,28 +24,30 @@ public class Param_CreateUser16278 extends S3TestBase {
     private void setUp() {
     }
 
-//    @Test
-//    private void testNameIsEmpty() {
-//        String name = "";
-//        JSONObject userJSON = null;
-//        try {
-//            userJSON = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
-//            Assert.fail("exp failed but act success");
-//        } catch (HttpServerErrorException e) {
-//            JSONObject json = new JSONObject(e.getResponseBodyAsString());
-//            if (!json.getString(UserCommDefind.errorCode).contains("InvalidUserName")) {
-//                Assert.fail(e.getResponseBodyAsString());
-//            }
-//        } finally {
-//            if (userJSON != null) {
-//                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
-//            }
-//        }
-//    }
+    @Test
+    private void testNameIsEmpty() {
+        String name = "";
+        JSONObject userJSON = null;
+        try {
+            userJSON = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
+            Assert.fail("exp failed but act success");
+        } catch (HttpClientErrorException e) {
+            String errorMsg = e.getResponseBodyAsString();
+            JSONObject json = XML.toJSONObject(errorMsg).getJSONObject(UserCommDefind.error);
+            if (!json.getString(UserCommDefind.errorCode).contains("InvalidUserName")) {
+                Assert.fail(e.getResponseBodyAsString());
+            }
+        } finally {
+            if (userJSON != null) {
+                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
+            }
+        }
+    }
 
     @Test
     private void testNameIsSepcial() {
-        String name = ".+=,ABCDEMN@_-adcdefghijklmnopqrstuvwxyzOPQRSTUVWXYZ012345789";
+        //+ means backspace
+        String name = ".B=,ABCDEMN@_-adcdefghijklmnopqrstuvwxyzOPQRSTUVWXYZ012345789";
         JSONObject actUser = null;
         try {
             // create
@@ -60,111 +64,111 @@ public class Param_CreateUser16278 extends S3TestBase {
         }
     }
 
-//    @Test
-//    private void testNameLenEq64() {
-//        String name = getRandomString(64);
-//        JSONObject actUser = null;
-//        try {
-//            // create
-//            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
-//            // update
-//            JSONObject updateUser = UserUtils.updateUser(name, UserUtils.accessKeyId);
-//            // get
-//            JSONObject expUser = UserUtils.getUser(name, UserUtils.accessKeyId);
-//            checkResult(updateUser, expUser);
-//        } finally {
-//            if (actUser != null) {
-//                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
-//            }
-//        }
-//    }
-//
-//    @Test
-//    private void testNameLenGt64() {
-//        String name = getRandomString(65);
-//        JSONObject actUser = null;
-//        try {
-//            // create
-//            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
-//            Assert.fail("exp fail but act success");
-//        } catch (HttpClientErrorException e) {
-//            String errorMsg = e.getResponseBodyAsString();
-//            JSONObject json = XML.toJSONObject(errorMsg);
-//            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode)
-//                    .contains("InvalidUserName")) {
-//                Assert.fail(e.getMessage());
-//            }
-//        } finally {
-//            if (actUser != null) {
-//                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
-//            }
-//        }
-//    }
-//
-//    @Test
-//    private void testNameSpecial() {
-//        String name = ".+=,名字#￥%；@_-";
-//        JSONObject actUser = null;
-//        try {
-//            // create
-//            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
-//            Assert.fail("exp fail but act success");
-//        } catch (HttpClientErrorException e) {
-//            String errorMsg = e.getResponseBodyAsString();
-//            JSONObject json = XML.toJSONObject(errorMsg);
-//            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode)
-//                    .contains("InvalidUserName")) {
-//                Assert.fail(e.getMessage());
-//            }
-//        } finally {
-//            if (actUser != null) {
-//                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
-//            }
-//        }
-//    }
-//
-//    @Test
-//    private void testRoleInvalid() {
-//        String name = "16278";
-//        JSONObject actUser = null;
-//        try {
-//            // create
-//            actUser = UserUtils.createUser(name, "admin|normal", UserUtils.accessKeyId);
-//            Assert.fail("exp fail but act success");
-//        } catch (HttpClientErrorException e) {
-//            String errorMsg = e.getResponseBodyAsString();
-//            JSONObject json = XML.toJSONObject(errorMsg);
-//            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode).contains("InvalidRole")) {
-//                Assert.fail(e.getMessage());
-//            }
-//        } finally {
-//            if (actUser != null) {
-//                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
-//            }
-//        }
-//    }
-//
-//    @Test
-//    private void testAccessKeyIDInvalid() {
-//        String name = "16278_1";
-//        JSONObject actUser = null;
-//        try {
-//            // create
-//            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId + "123");
-//            Assert.fail("exp fail but act success");
-//        } catch (HttpClientErrorException e) {
-//            String errorMsg = e.getResponseBodyAsString();
-//            JSONObject json = XML.toJSONObject(errorMsg);
-//            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode)
-//                    .contains("InvalidAccessKeyId")) {
-//                Assert.fail(e.getMessage());
-//            }
-//        } finally {
-//            if (actUser != null) {
-//                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
-//            }
-//        }
-//    }
+    @Test
+    private void testNameLenEq64() {
+        String name = getRandomString(64);
+        JSONObject actUser = null;
+        try {
+            // create
+            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
+            // update
+            JSONObject updateUser = UserUtils.updateUser(name, UserUtils.accessKeyId);
+            // get
+            JSONObject expUser = UserUtils.getUser(name, UserUtils.accessKeyId);
+            checkResult(updateUser, expUser);
+        } finally {
+            if (actUser != null) {
+                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
+            }
+        }
+    }
+
+    @Test
+    private void testNameLenGt64() {
+        String name = getRandomString(65);
+        JSONObject actUser = null;
+        try {
+            // create
+            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
+            Assert.fail("exp fail but act success");
+        } catch (HttpClientErrorException e) {
+            String errorMsg = e.getResponseBodyAsString();
+            JSONObject json = XML.toJSONObject(errorMsg);
+            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode)
+                    .contains("InvalidUserName")) {
+                Assert.fail(e.getMessage());
+            }
+        } finally {
+            if (actUser != null) {
+                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
+            }
+        }
+    }
+
+    @Test
+    private void testNameSpecial() {
+        String name = ".=,名字#￥%；@_-";
+        JSONObject actUser = null;
+        try {
+            // create
+            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId);
+            Assert.fail("exp fail but act success");
+        } catch (HttpClientErrorException e) {
+            String errorMsg = e.getResponseBodyAsString();
+            JSONObject json = XML.toJSONObject(errorMsg);
+            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode)
+                    .contains("InvalidUserName")) {
+                Assert.fail(e.getMessage());
+            }
+        } finally {
+            if (actUser != null) {
+                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
+            }
+        }
+    }
+
+    @Test
+    private void testRoleInvalid() {
+        String name = "16278";
+        JSONObject actUser = null;
+        try {
+            // create
+            actUser = UserUtils.createUser(name, "admin|normal", UserUtils.accessKeyId);
+            Assert.fail("exp fail but act success");
+        } catch (HttpClientErrorException e) {
+            String errorMsg = e.getResponseBodyAsString();
+            JSONObject json = XML.toJSONObject(errorMsg);
+            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode).contains("InvalidRole")) {
+                Assert.fail(e.getMessage());
+            }
+        } finally {
+            if (actUser != null) {
+                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
+            }
+        }
+    }
+
+    @Test
+    private void testAccessKeyIDInvalid() {
+        String name = "16278_1";
+        JSONObject actUser = null;
+        try {
+            // create
+            actUser = UserUtils.createUser(name, UserCommDefind.admin, UserUtils.accessKeyId + "123");
+            Assert.fail("exp fail but act success");
+        } catch (HttpClientErrorException e) {
+            String errorMsg = e.getResponseBodyAsString();
+            JSONObject json = XML.toJSONObject(errorMsg);
+            if (!json.getJSONObject(UserCommDefind.error).getString(UserCommDefind.errorCode)
+                    .contains("InvalidAccessKeyId")) {
+                Assert.fail(e.getMessage());
+            }
+        } finally {
+            if (actUser != null) {
+                UserUtils.deleteUser(name, UserUtils.accessKeyId, true);
+            }
+        }
+    }
 
     @AfterClass
     private void tearDown() {
