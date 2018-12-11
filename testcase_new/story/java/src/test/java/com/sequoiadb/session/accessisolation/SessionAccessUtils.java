92e20bfeef5f916582551628254185583f2db191
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-11 02:18:21 +0000

    优化创建节点时，如果存在端口或者目录冲突，重新制定新的目录和端口
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38278 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/session/accessisolation/SessionAccessUtils.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/session/accessisolation/SessionAccessUtils.java
@@ -1,6 +1,5 @@
 package com.sequoiadb.session.accessisolation;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,11 +8,13 @@ import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
 import org.bson.util.JSON;
+import org.testng.Assert;
 
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.ReplicaGroup;
 import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
 
 public class SessionAccessUtils extends SdbTestBase {
@@ -35,18 +36,26 @@ public class SessionAccessUtils extends SdbTestBase {
         String tmphostName = sdb.getReplicaGroup("SYSCatalogGroup").getMaster().getHostName();
         for( int i = 0; i < nodeNum; i++ ){        	
         	int dataPort = reservedPortBegin + 100*i;
-        	String dataPath = workDir + "/" + dataPort + "/";        	
-        	int times = 0;
-        	int maxRetryTimes = 10;
-        	while(new File(dataPath).exists() && times < maxRetryTimes ){ 
-        		dataPort += 10;
-        		dataPath = workDir + "/" + dataPort + "/"; 
-        		System.out.println("--path="+dataPath);
-        		times++;
-        	}        	
-            
-            BSONObject dataConfigue = (BSONObject) JSON.parse("{instanceid :"+instanceidarr[i]+"}");
-            dataRG.createNode(tmphostName, dataPort, dataPath, dataConfigue);
+        	String dataPath = workDir + "/" + dataPort + "/";    
+        	BSONObject dataConfigue = (BSONObject) JSON.parse("{instanceid :"+instanceidarr[i]+"}"); 
+        	boolean checkSucc = false;
+            int times = 0;
+            int maxRetryTimes = 10;
+            do {				
+				try {
+					dataRG.createNode(tmphostName, dataPort, dataPath, dataConfigue);
+					checkSucc = true;						
+				} catch (BaseException e) {
+					 // -145:Node already exists
+					if (e.getErrorCode() == -145 || e.getErrorCode() == -290  ){
+						dataPort = dataPort + 10;
+						dataPath = workDir + "/" + dataPort + "/";
+					} else {
+						Assert.fail("create node fail! port="+dataPort);
+					}
+				}
+				times++;							
+			} while (!checkSucc && times < maxRetryTimes);            
             
             String svcName = tmphostName + ":" + dataPort;
             instanceidTosvcName.put(instanceidarr[i], svcName);
