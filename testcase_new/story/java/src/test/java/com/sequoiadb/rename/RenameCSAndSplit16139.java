02ec19b0234029bd0763bd1c76038ea671c6d9d5
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-21 05:54:14 +0000

    SEQUOIADBMAINSTREAM-4005  补充测试点，替换tab键为空格
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38508 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/rename/RenameCSAndSplit16139.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/rename/RenameCSAndSplit16139.java
@@ -11,6 +11,7 @@ import org.testng.Assert;
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.sequoiadb.base.CollectionSpace;
@@ -30,170 +31,191 @@ import com.sequoiadb.testcommon.SdbThreadBase;
 * @version   1.00
 */
 public class RenameCSAndSplit16139 extends SdbTestBase{
-	
-	private String csName = "renameCS16139";
-	private String newCSName = "renameCSNew16139";
-	private String clName = "rename16139";
-	private String srcGroupName = null;
-	private String targetGroupName = null;	
-	private DBCollection cl = null;
-	private Sequoiadb sdb = null;	
-	
-	@BeforeClass
-	public void setUp(){
-		sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-		if(CommLib.isStandAlone(sdb)){
-			throw new SkipException("standAlone skip testcase16128");
-		}
-		
-		if (CommLib.OneGroupMode(sdb)){
-			throw new SkipException("less than two groups skip testcase16139");
-		}
-		RenameUtil.removeCS(sdb, newCSName);
-		
-		List<String> rgNames = CommLib.getDataGroupNames(sdb);		
-		srcGroupName = rgNames.get(0);
-		targetGroupName = rgNames.get(1);
-		CollectionSpace cs = RenameUtil.createCS(sdb, csName);
-		String clOptions = "{ShardingKey:{no:1},ShardingType:'range',"
-				+ "ReplSize:0,Compressed:true}, Group:" + srcGroupName + "}";
-		cl = RenameUtil.createCL(cs, clName, clOptions);
-		
-		insertDatas( cl );
-	}
-	
-	@Test
-	public void test(){ 
-		SplitCLThread splitCLThread = new SplitCLThread();		
-		RenameCSThread renameCSThread = new RenameCSThread();
-		splitCLThread.start();		
-		renameCSThread.start();				
-		
-		if(renameCSThread.isSuccess()){	
-			Assert.assertTrue(!splitCLThread.isSuccess(),splitCLThread.getErrorMsg() );
-			BaseException e=(BaseException)(splitCLThread.getExceptions().get(0));			
-			if ( e.getErrorCode() != -147 && e.getErrorCode() != -172 
-					&& e.getErrorCode() != -23  ){
-				Assert.fail("split fail:"+splitCLThread.getErrorMsg()+ "  e:"+e.getErrorCode());
-			}
-			
-			int clNums = 1;
-			RenameUtil.checkRenameCSResult(sdb, csName, newCSName, clNums);
-			checkSplitFailResult( sdb );
-		}else {
-			Assert.assertTrue(splitCLThread.isSuccess(),splitCLThread.getErrorMsg() );			
-			BaseException e=(BaseException)(renameCSThread.getExceptions().get(0));	
-			if ( e.getErrorCode() != -33 && e.getErrorCode() != -334 ){
-				Assert.fail("renameCS fail:"+renameCSThread.getErrorMsg() + "  e:"+e.getErrorCode());
-			}
-			
-			checkSplitResult(sdb);
-			checkRenameFailResult( csName, newCSName );
-		}	
-	}
-	
-	@AfterClass
-	public void tearDown(){
-		try{					
-			RenameUtil.removeCS(sdb, newCSName);	
-			RenameUtil.removeCS(sdb, csName);
-		}catch(BaseException e){			
-			Assert.assertTrue(false,"clean up failed:"+e.getMessage());
-		}finally {
-			if( sdb != null )
-			sdb.close();
-		}
-	}
-	
-	public class RenameCSThread extends SdbThreadBase{
-		@Override
-		public void exec() throws BaseException {	
-			try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")){
-				//random wait time to create cs
-				Random random = new Random();
-				int result = random.nextInt(2000);
-				try {
-					Thread.sleep(result);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-						
-				db.renameCollectionSpace(csName, newCSName);				
-			}
-		}
-	}
-	
-	public class SplitCLThread extends SdbThreadBase{
-		@Override
-		public void exec() throws Exception {
-			
-			try(Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
-				DBCollection dbcl = db.getCollectionSpace(csName).getCollection(clName);				
-				int present = 80;
-				dbcl.split(srcGroupName, targetGroupName, present);				
-			}
-		}
-	}
-	
-	private void insertDatas(DBCollection dbcl){
-		ArrayList<BSONObject> insertRecods = new ArrayList<BSONObject>();
-		for ( int i = 0; i < 10000; i++){
-			 BSONObject obj = new BasicBSONObject();				
-			 obj.put("testa", "test"+i);					 
-			 String str = "32345.067891234567890123456789" + i;
-			 BSONDecimal decimal = new BSONDecimal(str);			
-			 obj.put("decimala",decimal);
-			 obj.put("no", i);	
-			 obj.put("flag", i);	
-			 obj.put("str", "test_" + String.valueOf(i));
-			 insertRecods.add(obj);			 
-		 }
-		dbcl.insert(insertRecods);
-	}
-	
-	private void checkSplitFailResult( Sequoiadb sdb ){	
-		try (Sequoiadb dataNode = sdb.getReplicaGroup(targetGroupName).getMaster().connect()){			
-			dataNode.getCollectionSpace(csName).getCollection(clName);			
-			Assert.fail(" the cl should not be in the targetGroup!");			
-		}catch(BaseException e){
-			if(e.getErrorCode() != -34){
-				Assert.fail(" get cl fail by targetGroupName:" + e.getErrorCode());
-			}
-		}
-	}
-	
-	private void checkRenameFailResult( String oldCSName, String newCSName ){
-		if( sdb.isCollectionSpaceExist(newCSName)){
-			Assert.fail(" the new cs should not exist!");
-		}
-		if( !sdb.isCollectionSpaceExist(csName)){
-			Assert.fail(" the old cs should be exist!");
-		}
-	}
-	
-	private void checkSplitResult( Sequoiadb sdb ){		
-		String srcGroupMather = "{no:{$gte:0,$lt:2000}}";
-		long srcGroupCount = 2000;
-		checkGroupData(sdb, srcGroupCount, srcGroupMather, srcGroupName);
-		
-		String targetGroupMather = "{no:{$gte:2000,$lt:10000}}";
-		long targetGroupCount = 8000;
-		checkGroupData(sdb, targetGroupCount, targetGroupMather, targetGroupName);
-		
-		long expectTotalCount = 10000;
-		long actCount = cl.getCount();
-		Assert.assertEquals(actCount, expectTotalCount, " count after split is incorrect! ");
-		
-	}
-	
-	private void checkGroupData(Sequoiadb sdb, long expectedCount, String macher, String groupName)	{	
-		try (Sequoiadb dataNode = sdb.getReplicaGroup(groupName).getMaster().connect()){			
-			DBCollection dbcl = dataNode.getCollectionSpace(csName).getCollection(clName);			
-			long count = dbcl.getCount(macher);
-			if (count != expectedCount) {
-				Assert.fail("check data are incorrect!"+ groupName + " count is  " + count);
-			}			
-		} 
-	}	
+    
+    private String clName = "rename16139";
+    private String srcGroupName = null;
+    private String targetGroupName = null;    
+    private DBCollection cl = null;
+    private Sequoiadb sdb = null;    
+    
+    @BeforeClass
+    public void setUp(){
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        if(CommLib.isStandAlone(sdb)){
+            throw new SkipException("standAlone skip testcase16128");
+        }
+        
+        if (CommLib.OneGroupMode(sdb)){
+            throw new SkipException("less than two groups skip testcase16139");
+        }
+    }
+    
+    @DataProvider(name = "operData")
+    public Object[][] generateDatas(){
+        return new Object[][]{
+            new Object[]{"renameCS16139", "renameCSNew16139"},
+            new Object[]{"cs^cs16139", "newcs^newcs16139"},
+        };
+    }
+    
+    @Test(dataProvider = "operData")
+    public void test(String testCSName, String testNewCSName){ 
+        RenameUtil.removeCS(sdb, testNewCSName);
+        
+        List<String> rgNames = CommLib.getDataGroupNames(sdb);      
+        srcGroupName = rgNames.get(0);
+        targetGroupName = rgNames.get(1);
+        CollectionSpace cs = RenameUtil.createCS(sdb, testCSName);
+        String clOptions = "{ShardingKey:{no:1},ShardingType:'range',"
+                + "ReplSize:0,Compressed:true}, Group:" + srcGroupName + "}";
+        cl = RenameUtil.createCL(cs, clName, clOptions);
+        
+        insertDatas( cl );
+        
+        SplitCLThread splitCLThread = new SplitCLThread( testCSName );        
+        RenameCSThread renameCSThread = new RenameCSThread(testCSName, testNewCSName);
+        splitCLThread.start();        
+        renameCSThread.start();                
+        
+        if(renameCSThread.isSuccess()){    
+            Assert.assertTrue(!splitCLThread.isSuccess(),splitCLThread.getErrorMsg() );
+            BaseException e=(BaseException)(splitCLThread.getExceptions().get(0));            
+            if ( e.getErrorCode() != -147 && e.getErrorCode() != -172 
+                    && e.getErrorCode() != -23  ){
+                Assert.fail("split fail:"+splitCLThread.getErrorMsg()+ "  e:"+e.getErrorCode());
+            }
+            
+            int clNums = 1;
+            RenameUtil.checkRenameCSResult(sdb, testCSName, testNewCSName, clNums);
+            checkSplitFailResult( testCSName, sdb );
+        }else {
+            Assert.assertTrue(splitCLThread.isSuccess(),splitCLThread.getErrorMsg() );            
+            BaseException e=(BaseException)(renameCSThread.getExceptions().get(0));    
+            if ( e.getErrorCode() != -33 && e.getErrorCode() != -334 ){
+                Assert.fail("renameCS fail:"+renameCSThread.getErrorMsg() + "  e:"+e.getErrorCode());
+            }
+            
+            checkSplitResult(testCSName, sdb);
+            checkRenameFailResult( testCSName, testNewCSName );
+        }    
+    }
+    
+    @AfterClass
+    public void tearDown(){
+        try{                
+            RenameUtil.removeCS(sdb, "renameCS16139");    
+            RenameUtil.removeCS(sdb, "renameCSNew16139");
+            RenameUtil.removeCS(sdb, "cs^cs16139");
+            RenameUtil.removeCS(sdb, "newcs^newcs16139");
+        }catch(BaseException e){            
+            Assert.assertTrue(false,"clean up failed:"+e.getMessage());
+        }finally {
+            if( sdb != null )
+            sdb.close();
+        }
+    }
+    
+    public class RenameCSThread extends SdbThreadBase{
+        private String oldLocalCSName;
+        private String newLocalCSName;
+        
+        public RenameCSThread( String oldLocalCSName, String newLocalCSName){
+            this.oldLocalCSName = oldLocalCSName;
+            this.newLocalCSName = newLocalCSName;
+        }
+        @Override
+        public void exec() throws BaseException {    
+            try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")){
+                //random wait time to create cs
+                Random random = new Random();
+                int result = random.nextInt(2000);
+                try {
+                    Thread.sleep(result);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                        
+                db.renameCollectionSpace(oldLocalCSName, newLocalCSName);                
+            }
+        }
+    }
+    
+    public class SplitCLThread extends SdbThreadBase{
+        private String localCSName;
+        public SplitCLThread( String localCSName){
+            this.localCSName = localCSName;
+        }
+        @Override
+        public void exec() throws Exception {
+            
+            try(Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
+                DBCollection dbcl = db.getCollectionSpace(localCSName).getCollection(clName);                
+                int present = 80;
+                dbcl.split(srcGroupName, targetGroupName, present);                
+            }
+        }
+    }
+    
+    private void insertDatas(DBCollection dbcl){
+        ArrayList<BSONObject> insertRecods = new ArrayList<BSONObject>();
+        for ( int i = 0; i < 10000; i++){
+             BSONObject obj = new BasicBSONObject();                
+             obj.put("testa", "test"+i);                     
+             String str = "32345.067891234567890123456789" + i;
+             BSONDecimal decimal = new BSONDecimal(str);            
+             obj.put("decimala",decimal);
+             obj.put("no", i);    
+             obj.put("flag", i);    
+             obj.put("str", "test_" + String.valueOf(i));
+             insertRecods.add(obj);             
+         }
+        dbcl.insert(insertRecods);
+    }
+    
+    private void checkSplitFailResult(String testCSName, Sequoiadb sdb ){    
+        try (Sequoiadb dataNode = sdb.getReplicaGroup(targetGroupName).getMaster().connect()){            
+            dataNode.getCollectionSpace(testCSName).getCollection(clName);            
+            Assert.fail(" the cl should not be in the targetGroup!");            
+        }catch(BaseException e){
+            if(e.getErrorCode() != -34){
+                Assert.fail(" get cl fail by targetGroupName:" + e.getErrorCode());
+            }
+        }
+    }
+    
+    private void checkRenameFailResult( String oldCSName, String newCSName ){
+        if( sdb.isCollectionSpaceExist(newCSName)){
+            Assert.fail(" the new cs should not exist!");
+        }
+        if( !sdb.isCollectionSpaceExist(oldCSName)){
+            Assert.fail(" the old cs should be exist!");
+        }
+    }
+    
+    private void checkSplitResult(String testCSName, Sequoiadb sdb ){        
+        String srcGroupMather = "{no:{$gte:0,$lt:2000}}";
+        long srcGroupCount = 2000;
+        checkGroupData(testCSName, sdb, srcGroupCount, srcGroupMather, srcGroupName);
+        
+        String targetGroupMather = "{no:{$gte:2000,$lt:10000}}";
+        long targetGroupCount = 8000;
+        checkGroupData(testCSName, sdb, targetGroupCount, targetGroupMather, targetGroupName);
+        
+        long expectTotalCount = 10000;
+        long actCount = cl.getCount();
+        Assert.assertEquals(actCount, expectTotalCount, " count after split is incorrect! ");
+        
+    }
+    
+    private void checkGroupData(String testCSName, Sequoiadb sdb, long expectedCount, String macher, String groupName)    {    
+        try (Sequoiadb dataNode = sdb.getReplicaGroup(groupName).getMaster().connect()){            
+            DBCollection dbcl = dataNode.getCollectionSpace(testCSName).getCollection(clName);            
+            long count = dbcl.getCount(macher);
+            if (count != expectedCount) {
+                Assert.fail("check data are incorrect!"+ groupName + " count is  " + count);
+            }            
+        } 
+    }    
 }
+
