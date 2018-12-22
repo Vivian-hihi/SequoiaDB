aa8bd8aa319faa8e22140e6893ef6b8e0e95f751
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2018-12-22 02:01:16 +0000

    新增alter并发用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38522 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/meta/TestAlterCL14992.java
@@ -0,0 +1,164 @@
+package com.sequoiadb.meta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.CommLib;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbThreadBase;
+
+/**
+* FileName: TestAlterCL14992.java
+* test content:Concurrent alter cl ,alter the same field
+* 			   test a: concurrent modification to the same value
+* 			   test b: concurrent modification to the different value
+* testlink case:seqDB-14992
+* @author wuyan
+* @Date    2018.4.28
+* @version 1.00
+*/
+public class TestAlterCL14992 extends SdbTestBase {
+	private String clName = "altercl_14992";	
+	private static Sequoiadb sdb = null;
+	private CollectionSpace cs = null;	
+    	
+	@BeforeClass
+	public void setUp(){
+		try{
+			sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		}catch(BaseException e){			
+			Assert.assertTrue(false,"connect %s failed,"+SdbTestBase.coordUrl+e.getMessage());
+		}
+		if (CommLib.isStandAlone(sdb))
+        {
+            throw new SkipException("is standalone");
+        }
+		createCL();
+		insertData();
+	}		
+		
+	@Test
+	public void testAlterCl(){	
+		AlterUseEnable alterTask1 = new AlterUseEnable();
+		AlterUseSet alterTask2 = new AlterUseSet();
+		AlterSameValue alterTask3 = new AlterSameValue();
+		alterTask1.start();
+		alterTask2.start();
+		alterTask3.start(100);
+		
+		Assert.assertTrue( alterTask1.isSuccess(), alterTask1.getErrorMsg());
+		Assert.assertTrue( alterTask2.isSuccess(), alterTask2.getErrorMsg());
+		Assert.assertTrue( alterTask3.isSuccess(), alterTask3.getErrorMsg());
+		checkAlterResult( );
+	}
+
+	@AfterClass
+	public void tearDown(){		
+		try{
+			if(cs.isCollectionExist(clName)){
+				cs.dropCollection(clName);
+			}
+		}catch(BaseException e){			
+			Assert.assertTrue(false,"clean up failed:"+e.getMessage());
+		}finally {
+			if( sdb != null){
+				sdb.close();
+			}
+		}
+	}	
+		
+	
+	private class AlterUseEnable extends SdbThreadBase{
+		public void exec() throws BaseException {					
+		    try(Sequoiadb sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "")){		    	
+		    	DBCollection dbcl = sdb.getCollectionSpace(SdbTestBase.csName).getCollection(clName);
+		    	BSONObject options = (BSONObject)JSON.parse("{ShardingKey:{a:1}}");
+		    	dbcl.enableSharding(options);
+		    }
+		}
+	}
+	
+	private class AlterUseSet extends SdbThreadBase{
+		public void exec() throws BaseException {				
+		    try(Sequoiadb sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "")){		    	
+		    	DBCollection dbcl = sdb.getCollectionSpace(SdbTestBase.csName).getCollection(clName);
+		    	BSONObject options = (BSONObject)JSON.parse("{ShardingKey:{b:1}}");
+		    	dbcl.setAttributes(options);
+		    }
+		}
+	}	
+	
+	private class AlterSameValue extends SdbThreadBase{
+		public void exec() throws BaseException {					
+		    try(Sequoiadb sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "")){		    	
+		    	DBCollection dbcl = sdb.getCollectionSpace(SdbTestBase.csName).getCollection(clName);
+		    	BSONObject options = (BSONObject)JSON.parse("{CompressionType:'lzw'}");
+		    	dbcl.setAttributes(options);
+		    }
+		}
+	}
+	
+	private void insertData() {
+		int count = 0;
+		DBCollection dbcl = sdb.getCollectionSpace(SdbTestBase.csName).getCollection(clName);
+		for ( int i = 0; i < 2; i++){
+			List<BSONObject>list = new ArrayList<BSONObject>();	
+			for (int j = 0; j < 10000; j++) {	
+				int value = count++;
+				BSONObject obj = (BSONObject) JSON.parse("{sk:" + value +", test:"+"'testasetatatatatat'" + "}");				
+				list.add(obj);				
+			}
+			dbcl.insert(list);
+		}		
+	}
+	
+	private void createCL(){			
+	    try
+	    {
+	    	 cs = sdb.getCollectionSpace(SdbTestBase.csName);			
+	    	 cs.createCollection(clName);			
+	    }catch(BaseException e){
+		    Assert.assertTrue(false,"create cl fail "+e.getErrorType()+":"+e.getMessage());
+	    }	    
+	 }
+	
+	private void checkAlterResult( ){
+		String cond = String.format("{Name:\"%s.%s\"}", SdbTestBase.csName, clName);	
+		DBCursor collections = sdb.getSnapshot(8, cond, null, null);	
+		String  actCompressionType = "";
+		String  expCompressionType = "lzw";
+		Object  actShardingKey = null;
+		while(collections.hasNext()){
+			BasicBSONObject doc = (BasicBSONObject)collections.getNext();		
+			actCompressionType = (String) doc.get("CompressionTypeDesc");
+			actShardingKey = doc.get("ShardingKey");			
+		}
+		collections.close();
+		Assert.assertEquals(actCompressionType, expCompressionType);
+		
+		List<BSONObject> objects = new ArrayList<BSONObject>();
+		BSONObject subObj1 = new BasicBSONObject();
+		BSONObject subObj2 = new BasicBSONObject();
+		subObj1.put("a", 1);
+		subObj2.put("b", 1);
+		objects.add(subObj1);
+		objects.add(subObj2);
+		Assert.assertTrue(objects.contains(actShardingKey),"shardingKey for one of the modified values!");
+	}
+}
+
+
