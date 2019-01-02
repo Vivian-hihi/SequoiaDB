60d4c1a3398192b69bc46925d9aa306a9fe0fbe3
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-02 02:10:56 +0000

    SEQUOIADBMAINSTREAM-4049【BUG】【new】【story】【S3对象管理】HEAD接口If-Modified-Since和If-Unmodified-Since设置为与对象LastModified时间相等时返回结果不正确
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38692 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/head/TestGetObjectMetadata17035.java
@@ -0,0 +1,90 @@
+package com.sequoias3.head;
+
+import java.util.Date;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectMetadataRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.RestClient;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.s3utils.HeadUtils;
+import com.sequoias3.testcommon.s3utils.UserUtils;
+
+/**
+ * test content: 带versionId查询对象，指定ifUnModifiedSince与对象LastModified时间相等
+ * testlink-case: seqDB-17035 
+ * @author wangkexin
+ * @Date 2019.01.02
+ * @version 1.00
+ */
+
+public class TestGetObjectMetadata17035  extends S3TestBase{
+	private boolean runSuccess = false;
+	private String bucketName = "bucket17035";
+	private String userName = "user17035";
+	private String roleName = "normal";
+	private String keyName = "key17035";
+	private String content = "content17035";
+	private static CloseableHttpClient client;
+	private String[] accessKeys = null;
+	private AmazonS3 s3Client = null;
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		CommLib.clearUser(userName);
+		accessKeys = UserUtils.createUser(userName, roleName);
+		s3Client = CommLib.buildS3Client(accessKeys[0], accessKeys[1]);
+	}
+
+	@Test
+	private void testGetObjectMetadata() throws Exception {
+		s3Client.createBucket(bucketName);
+		CommLib.setBucketVersioning(s3Client, bucketName, "Enabled");
+		PutObjectResult result =  s3Client.putObject(bucketName, keyName, content+"v1");
+		String versionId = result.getVersionId();
+		String expEtag = result.getETag();
+		
+		ObjectMetadata metadata = s3Client.getObjectMetadata(new GetObjectMetadataRequest(bucketName, keyName, versionId));
+		Date unModifiedSince = metadata.getLastModified();
+		
+		s3Client.putObject(bucketName, keyName, content+"v2");
+		s3Client.putObject(bucketName, keyName, content+"v3");
+		
+		HttpHead request = new HttpHead(S3TestBase.s3ClientUrl + "/s3/"+bucketName+"/"+keyName+"?versionId="+versionId);
+	    request.setHeader("Authorization", "Credential="+accessKeys[0]);
+	    request.setHeader("If-Unmodified-Since", HeadUtils.getGMTDate(unModifiedSince));
+	    
+	    client = RestClient.createHttpClient();
+		CloseableHttpResponse resp = RestClient.sendRequest(client, request);
+		Assert.assertEquals(resp.getFirstHeader("ETag").getValue(), expEtag);
+		Assert.assertEquals(resp.getFirstHeader("x-amz-version-id").getValue(), versionId);
+		
+		runSuccess = true;
+	}
+
+	@AfterClass
+	private void tearDown() throws Exception {
+		try {
+			if (runSuccess) {
+				UserUtils.deleteUser(userName);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if (s3Client != null) {
+				s3Client.shutdown();
+			}
+		}
+	}
+}
