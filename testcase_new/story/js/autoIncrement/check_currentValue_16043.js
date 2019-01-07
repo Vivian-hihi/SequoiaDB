73de179dbcb62d7b68df70a12101f9167435bd99
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-07 02:40:03 +0000

    提交自增字段自动化补充测试点
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38807 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/autoIncrement/check_currentValue_16043.js
+++ testcase_new/story/js/autoIncrement/check_currentValue_16043.js
@@ -18,18 +18,24 @@ function main()
    dbcl.createAutoIncrement([{Field : "a" }, { Field : "a1" }, { Field : "a2", Increment : -1}, { Field : "a3"},
                              { Field : "a4"}, { Field : "a5", Increment : -1 }, { Field : "a6" }]);
    
+   dbcl.setAttributes({ AutoIncrement : { Field : "a3", CurrentValue : 10 } });
+   
+   dbcl.insert( { "q" : 1 } );
+   
+   var rc = dbcl.find();
+   var expRecs = [{"q" : 1, "a" : 1, "a1" : 1, "a2" : -1, "a3" : 11, "a4" : 1, "a5" : -1, "a6" : 1}];
+   checkRec( rc, expRecs );
    
    dbcl.setAttributes({ AutoIncrement : { Field : "a" } });
    dbcl.setAttributes({ AutoIncrement : { Field : "a1", CurrentValue : { "$numberLong" : "9223372036854775807" } } });
    //修改CurrentValue的同时修改MinValue才能通过，已提单，单号为SEQUOIADBMAINSTREAM-4060
    dbcl.setAttributes({ AutoIncrement : { Field : "a2", CurrentValue : { "$numberLong" : "-9223372036854775808" }, MinValue : { "$numberLong" : "-9223372036854775808" } } });
-   dbcl.setAttributes({ AutoIncrement : { Field : "a3", CurrentValue : 10 } });
    
    dbcl.setAttributes({ AutoIncrement : { Field : "a4", CurrentValue : { "$numberLong" : "9223372036854775809" } } });  
     //修改CurrentValue的同时修改MinValue才能通过，已提单，单号为SEQUOIADBMAINSTREAM-4060
    dbcl.setAttributes({ AutoIncrement : { Field : "a5", CurrentValue : { "$numberLong" : "-9223372036854775809" }, MinValue : { "$numberLong" : "-9223372036854775808" } } });
 
-      try
+   try
    {
       dbcl.setAttributes({ AutoIncrement : { Field : "a6", CurrentValue : "a" } });
       throw "setAttributes error!";
@@ -50,13 +56,13 @@ function main()
                         "SYS_" + clID + "_a4_SEQ",
                         "SYS_" + clID + "_a5_SEQ",
                         "SYS_" + clID + "_a6_SEQ"]; 
-   var expSequences =  [{},
+   var expSequences =  [{ CurrentValue : 1001 },
                         { CurrentValue : { "$numberLong" : "9223372036854775807" } },
                         { CurrentValue : { "$numberLong" : "-9223372036854775808" }, Increment : -1, MinValue : { "$numberLong" : "-9223372036854775808" }, MaxValue : -1, "StartValue":-1 },
-                        { CurrentValue : 10 },
+                        { CurrentValue : 1010 },
                         { CurrentValue : { "$numberLong" : "9223372036854775807" } },
                         { CurrentValue : { "$numberLong" : "-9223372036854775808" },Increment : -1, MinValue : { "$numberLong" : "-9223372036854775808" }, MaxValue : -1,"StartValue":-1 },
-                        {}];
+                        { CurrentValue : 1001 }];
    for(var i in sequenceNames)
    {
        checkSequence(sequenceNames[i], expSequences[i]);
@@ -64,7 +70,7 @@ function main()
    
    try
    {
-       dbcl.insert( { "q" : 1 } );
+       dbcl.insert( { "q" : 2 } );
        throw "insert ERROR";
    }catch(e)
    {
@@ -79,10 +85,11 @@ function main()
    //检查_exceeded变量是否复位
    dbcl.setAttributes({ AutoIncrement : { Field : "a1", CurrentValue : 4 } });
    
-   dbcl.insert({record : 5});
+   dbcl.insert({"q" : 3});
    
    var rc = dbcl.find();
-   var expRecs = [{"record" : 5, "a" : 2, "a1" : 5, "a3" : 11, "a6" : 1}];
+   var expRecs = [{"q" : 1, "a" : 1, "a1" : 1, "a2" : -1, "a3" : 11, "a4" : 1, "a5" : -1, "a6" : 1},
+                  {"q" : 3, "a" : 1002, "a1" : 5, "a3" : 12, "a6" : 2}];
    checkRec( rc, expRecs );
    
    commDropCL( db, COMMCSNAME, clName );
