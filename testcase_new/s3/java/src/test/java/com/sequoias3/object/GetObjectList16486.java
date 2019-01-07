31e1d055d5c640126257b35fb433f9f8bc8bde5f
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-07 01:01:40 +0000

    新增对象管理并发用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38803 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/GetObjectList16486.java
@@ -0,0 +1,179 @@
+package com.sequoias3.object;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.S3ThreadBase;
+import com.sequoias3.testcommon.s3utils.ObjectUtils;
+import com.sequoias3.testcommon.s3utils.UserUtils;
+
+/**
+ * test content: 并发不同条件查询对象列表 
+ * testlink-case: seqDB-16486
+ * @author wangkexin
+ * @Date 2019.01.03
+ * @version 1.00
+ */
+public class GetObjectList16486 extends S3TestBase {
+	private String bucketName = "bucket16486";
+	private String userName = "user16486";
+	private String roleName = "normal";
+	private String keyName = "/dir/dir";
+	private String prefix = "/dir";
+	private String delimiter = "/";
+	private List<String> expresultList1 = new ArrayList<String>();
+	private List<String> expresultList2 = new ArrayList<String>();
+	private List<String> expresultList3 = new ArrayList<String>();
+	private int objectTotalNum = 100;
+	private AmazonS3 s3Client = null;
+	private String[] acessKeys = null;
+	private boolean runSuccess = false;
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		CommLib.clearUser(userName);
+		acessKeys = UserUtils.createUser(userName, roleName);
+		s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);
+		s3Client.createBucket(new CreateBucketRequest(bucketName));
+		
+		//put multiple objects
+		for(int i = 0 ; i < objectTotalNum ; i++){
+			String currentKeyName = keyName+i+"/16486";
+			s3Client.putObject(bucketName, currentKeyName, "object_file16486");
+			expresultList1.add(currentKeyName);
+			expresultList2.add(currentKeyName);
+		}
+		
+		//put another objects that do not match prefix
+		s3Client.putObject(bucketName, "/testa", "object_file16486");
+		s3Client.putObject(bucketName, "/testb", "object_file16486");
+		expresultList1.add("/testa");
+		expresultList1.add("/testb");
+		
+		Collections.sort(expresultList1);
+		Collections.sort(expresultList2);
+		expresultList3.add("/dir/");
+	}
+
+	@Test
+	public void testGetObjectList() throws Exception {
+		ListObjectThread listObject = new ListObjectThread();
+		ListObjectWithPerfixThread listObjectWithPerfix = new ListObjectWithPerfixThread();
+		ListObjectWithPerfixAndDelimiterThread listObjectWithPerfixAndDelimiter = new ListObjectWithPerfixAndDelimiterThread();
+		listObject.start();
+		listObjectWithPerfix.start();
+		listObjectWithPerfixAndDelimiter.start();
+		
+		Assert.assertTrue( listObject.isSuccess(), listObject.getErrorMsg());
+		Assert.assertTrue( listObjectWithPerfix.isSuccess(), listObjectWithPerfix.getErrorMsg());
+		Assert.assertTrue( listObjectWithPerfixAndDelimiter.isSuccess(), listObjectWithPerfixAndDelimiter.getErrorMsg());
+		
+		runSuccess =true;
+	}
+
+	@AfterClass
+	private void tearDown() {
+		try {
+			if (runSuccess) {
+				UserUtils.deleteUser(userName);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if( s3Client != null ){
+				s3Client.shutdown();
+			}
+		}
+	}
+	
+	private class ListObjectThread extends S3ThreadBase{
+		@Override
+		public void exec() throws Exception {
+			AmazonS3 s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);	
+			try{
+				List<S3ObjectSummary> contentsResult = new ArrayList<>();
+				ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName);
+				ListObjectsV2Result result; 
+				
+				do{
+					result = s3Client.listObjectsV2(req);
+					contentsResult.addAll(result.getObjectSummaries());
+					String nextContinuationToken = result.getNextContinuationToken();
+					req.setContinuationToken(nextContinuationToken);
+				}while(result.isTruncated());
+			
+				ObjectUtils.checkListObjectsV2KeyName(contentsResult, expresultList1);
+			}finally{
+				if (s3Client != null) {
+					s3Client.shutdown();
+				}
+			}			
+		}		
+	}
+	
+	private class ListObjectWithPerfixThread extends S3ThreadBase{
+		@Override
+		public void exec() throws Exception {
+			AmazonS3 s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);	
+			try{
+				List<S3ObjectSummary> contentsResult = new ArrayList<>();
+				ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName)
+								.withPrefix(prefix);
+				ListObjectsV2Result result; 
+				
+				do{
+					result = s3Client.listObjectsV2(req);
+					contentsResult.addAll(result.getObjectSummaries());
+					String nextContinuationToken = result.getNextContinuationToken();
+					req.setContinuationToken(nextContinuationToken);
+				}while(result.isTruncated());
+			
+				ObjectUtils.checkListObjectsV2KeyName(contentsResult, expresultList2);
+			}finally{
+				if (s3Client != null) {
+					s3Client.shutdown();
+				}
+			}			
+		}		
+	}
+	
+	private class ListObjectWithPerfixAndDelimiterThread extends S3ThreadBase{
+		@Override
+		public void exec() throws Exception {
+			AmazonS3 s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);	
+			try{
+				List<String> commprefixesResult = new ArrayList<>();
+				ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(bucketName)
+								.withPrefix(prefix).withDelimiter(delimiter);
+				ListObjectsV2Result result; 
+				
+				do{
+					result = s3Client.listObjectsV2(req);
+					commprefixesResult.addAll(result.getCommonPrefixes());
+					String nextContinuationToken = result.getNextContinuationToken();
+					req.setContinuationToken(nextContinuationToken);
+				}while(result.isTruncated());
+			
+				ObjectUtils.checkListObjectsV2Commprefixes(commprefixesResult, expresultList3);
+			}finally{
+				if (s3Client != null) {
+					s3Client.shutdown();
+				}
+			}			
+		}		
+	}
+}
---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/UpdateAndDelectSameObject16504.java
@@ -0,0 +1,113 @@
+package com.sequoias3.object;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.S3ThreadBase;
+import com.sequoias3.testcommon.TestTools;
+import com.sequoias3.testcommon.s3utils.UserUtils;
+
+/**
+ * test content: 开启版本控制，并发更新和删除相同对象 
+ * testlink-case: seqDB-16504
+ * @author wangkexin
+ * @Date 2019.01.04
+ * @version 1.00
+ */
+public class UpdateAndDelectSameObject16504 extends S3TestBase {
+	private boolean runSuccess = false;	
+	private String userName = "user16504";
+	private String bucketName = "bucket16504";
+	private String keyName = "key16504";
+	private String roleName = "normal";
+	private String oldContent = "test16504";
+	private String newContent = "newtest16504";
+	private String[] acessKeys = null;
+	private AmazonS3 s3Client = null;
+	
+	@BeforeClass
+	private void setUp() throws Exception {	
+		CommLib.clearUser(userName);
+		acessKeys = UserUtils.createUser(userName, roleName);
+		s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);	
+		s3Client.createBucket(bucketName);
+		CommLib.setBucketVersioning(s3Client, bucketName, "Enabled");
+		s3Client.putObject(bucketName, keyName, oldContent);
+	}
+	
+	@Test
+	public void testUpdateAndDeleteObject() throws Exception {
+		UpdateObjectThread updateObject = new UpdateObjectThread(keyName);
+		DeleteObjectThread deleteObject = new DeleteObjectThread(keyName);
+		updateObject.start();
+		deleteObject.start();
+		
+		Assert.assertTrue( updateObject.isSuccess(), updateObject.getErrorMsg());
+		Assert.assertTrue( deleteObject.isSuccess(), deleteObject.getErrorMsg());
+		
+		if (s3Client.doesObjectExist(bucketName, keyName)) {
+			ObjectMetadata object = s3Client.getObjectMetadata(bucketName, keyName);
+			Assert.assertEquals(object.getETag(), TestTools.getMD5(newContent.getBytes()));
+		}
+		runSuccess = true;			
+	}
+	
+	@AfterClass
+	private void tearDown() throws Exception {
+		try {
+			if (runSuccess) {
+				UserUtils.deleteUser(userName);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if( s3Client != null ){
+				s3Client.shutdown();
+			}
+		}
+	}
+	
+	private class UpdateObjectThread extends S3ThreadBase{
+		String keyName;	
+		public UpdateObjectThread ( String keyName ){
+			this.keyName = keyName;	
+		}
+		@Override
+		public void exec() throws Exception {
+			AmazonS3 s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);	
+			try{
+				s3Client.putObject(bucketName, keyName, newContent);
+			}finally{
+				if (s3Client != null) {
+					s3Client.shutdown();
+				}
+			}			
+		}		
+	}	
+	
+	private class DeleteObjectThread extends S3ThreadBase{
+		String keyName;	
+		public DeleteObjectThread ( String keyName ){
+			this.keyName = keyName;	
+		}
+		@Override
+		public void exec() throws Exception {
+			AmazonS3 s3Client = CommLib.buildS3Client(acessKeys[0], acessKeys[1]);	
+			try{
+				Thread.sleep(30);
+				s3Client.deleteObject(bucketName, keyName);
+			}finally{
+				if (s3Client != null) {
+					s3Client.shutdown();
+				}
+			}			
+		}		
+	}	
+}
