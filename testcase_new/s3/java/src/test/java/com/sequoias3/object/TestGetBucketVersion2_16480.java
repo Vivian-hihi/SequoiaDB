e4e5e7eee88e1345fb9af5a45d6936fd179f339c
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-09 04:32:48 +0000

    新增对象管理参数校验用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38867 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/TestGetBucketVersion2_16480.java
@@ -0,0 +1,81 @@
+package com.sequoias3.object;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+
+/**
+ * test content:  GetBucketVersion2接口参数校验
+ * testlink-case: seqDB-16480
+ * @author wangkexin
+ * @Date 2019.01.08
+ * @version 1.00
+ */
+public class TestGetBucketVersion2_16480 extends S3TestBase {
+	private String bucketName = "bucket16480";
+	private AmazonS3 s3Client = null;
+	private boolean runSuccess = false;
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		s3Client = CommLib.buildS3Client();
+		CommLib.clearBucket(s3Client, bucketName);
+		s3Client.createBucket(new CreateBucketRequest(bucketName));
+		s3Client.putObject(bucketName, "aa/bb/key16480_1/123", "content16480_1");
+		s3Client.putObject(bucketName, "aa/bb/key16480_2/456", "content16480_2");
+	}
+
+	@Test
+	public void testListObjectsV2(){
+		//test a : 合法值
+		ListObjectsV2Request request = new ListObjectsV2Request().withBucketName(bucketName).withEncodingType("url");
+		request.withMaxKeys(1);
+		request.withPrefix("aa/bb/");
+		request.withDelimiter("/");
+		request.withFetchOwner(true);
+		request.withStartAfter("aa");
+		ListObjectsV2Result result = s3Client.listObjectsV2(request);
+
+		List<String> commonPrefixes = result.getCommonPrefixes();
+		Assert.assertEquals(commonPrefixes.size(), 1);
+		Assert.assertEquals(commonPrefixes.get(0), "aa/bb/key16480_1/");
+		
+		
+		//test b : 非法值，取值为null
+		ListObjectsV2Request request2 = new ListObjectsV2Request();
+		try{
+			s3Client.listObjectsV2(request2);
+			Assert.fail("exp fail but found true");
+		}catch(IllegalArgumentException e){
+			Assert.assertEquals(e.getMessage(), "The bucket name parameter must be specified when listing objects in a bucket");
+		}
+		
+		runSuccess = true;
+	}
+
+	@AfterClass
+	private void tearDown() {
+		try {
+			if (runSuccess) {
+				CommLib.clearBucket(s3Client, bucketName);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if( s3Client != null ){
+				s3Client.shutdown();
+			}
+		}
+	}
+}
---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/TestPutObjectRequest16478.java
@@ -0,0 +1,148 @@
+package com.sequoias3.object;
+
+import java.io.File;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.TestTools;
+import com.sequoias3.testcommon.s3utils.ObjectUtils;
+
+/**
+ * test content:  PutObjectRequest接口参数校验 
+ * testlink-case: seqDB-16478
+ * @author wangkexin
+ * @Date 2019.01.07
+ * @version 1.00
+ */
+public class TestPutObjectRequest16478 extends S3TestBase {
+	@DataProvider(name = "legalKeyNameProvider")
+	public Object[][] generateKeyName() {
+		String a = new String();
+		for(int i=0 ; i < 32 ; i ++){
+				a+=(char)i;
+		}
+		for(int i=127 ; i < 256 ; i ++){
+				a+=(char)i;
+		}
+		return new Object[][] {
+				// test a : 范围内取值
+				new Object[] {"/dir1/test.txt"},
+				// test b : 长度边界值
+				new Object[] {ObjectUtils.getRandomString(1)},
+				new Object[] {ObjectUtils.getRandomString(900)},
+				// test c : 包含特殊字符
+				new Object[] {"!-_.*'()"},
+				// test d : 包含 数字字符[0-9a-zA-Z]
+				new Object[] {"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"},
+				// test e : 包含需要特殊处理的字符   SEQUOIADBMAINSTREAM-4088
+				//new Object[] {"&@:,$=+?;" + a + " "},
+				// test f : 包含不建议使用的字符
+				new Object[] {"\\^`><{}][#%“~|"},
+				// test g : 包含中文字符
+				new Object[] {"测试对象名"},
+		};
+	}
+	private String bucketName = "bucket16478";
+	private AmazonS3 s3Client = null;
+	private int fileSize = 1024 * 1024;
+	private File localPath = null;
+	private String filePath = null;
+	private boolean runSuccess = false;
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		localPath = new File(S3TestBase.workDir + File.separator + TestTools.getClassName());
+		filePath = localPath + File.separator + "localFile_" + fileSize + ".txt";
+		s3Client = CommLib.buildS3Client();
+		s3Client.createBucket(new CreateBucketRequest(bucketName));
+	}
+
+	@Test(dataProvider = "legalKeyNameProvider")
+	public void testLegalKeyName(String keyName) throws Exception {
+		TestTools.LocalFile.removeFile(localPath);
+		TestTools.LocalFile.createDir(localPath.toString());
+		TestTools.LocalFile.createFile(filePath, fileSize);
+		PutObjectResult result = s3Client.putObject(new PutObjectRequest(bucketName, keyName, new File(filePath)));
+		String actMd5 = result.getETag();
+		String expMd5 = TestTools.getMD5(filePath);
+		Assert.assertEquals(actMd5, expMd5, "md5 is wrong! the key name is : " + keyName);
+		TestTools.LocalFile.removeFile(localPath);
+	}
+	
+	@Test
+	public void testIllegalKeyName() throws Exception {
+		TestTools.LocalFile.removeFile(localPath);
+		TestTools.LocalFile.createDir(localPath.toString());
+		TestTools.LocalFile.createFile(filePath, fileSize);
+		//test a : 对象名为空串，null，901个字节
+		
+		//SEQUOIADBMAINSTREAM-4088
+		/*try{
+			s3Client.putObject(new PutObjectRequest(bucketName, "", new File(filePath)));
+			Assert.fail("when key name is '',it should fail");
+		}catch(IllegalArgumentException e){
+			Assert.assertEquals(e.getMessage(), "The key parameter must be specified when uploading an object");
+		}*/
+		
+		try{
+			s3Client.putObject(new PutObjectRequest(bucketName, null, new File(filePath)));
+			Assert.fail("when key name is null,it should fail");
+		}catch(IllegalArgumentException e){
+			Assert.assertEquals(e.getMessage(), "The key parameter must be specified when uploading an object");
+		}
+		
+		try{
+			s3Client.putObject(new PutObjectRequest(bucketName, ObjectUtils.getRandomString(901), new File(filePath)));
+			Assert.fail("when key name is 901 characters,it should fail");
+		}catch(AmazonS3Exception e){
+			Assert.assertEquals(e.getErrorMessage(), "Your key is too long.");
+		}
+		
+		//test b : 桶名为null
+		try{
+			s3Client.putObject(new PutObjectRequest(null, "/dir/test16478", new File(filePath)));
+			Assert.fail("when bucket name is null,it should fail");
+		}catch(IllegalArgumentException e){
+			Assert.assertEquals(e.getMessage(), "The bucket name parameter must be specified when uploading an object");
+		}
+		
+		//test c : file 为不存在的路径、文件名不存在
+		String nonexistentFilePath = localPath + File.separator + "nonexistentFilePath.txt";
+		try{
+			s3Client.putObject(new PutObjectRequest(bucketName, "/dir/test16478", new File(nonexistentFilePath)));
+			Assert.fail("when file path does not exist,it should fail");
+		}catch(SdkClientException e){
+			Assert.assertTrue(e.getMessage().contains("No such file or directory"));
+		}
+		runSuccess = true;
+	}
+
+	@AfterClass
+	private void tearDown() {
+		try {
+			if (runSuccess) {
+				CommLib.deleteAllObjectVersions(s3Client, bucketName);
+				s3Client.deleteBucket(bucketName);
+				TestTools.LocalFile.removeFile(localPath);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if( s3Client != null ){
+				s3Client.shutdown();
+			}
+		}
+	}
+}
