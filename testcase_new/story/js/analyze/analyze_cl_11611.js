1cc790ad27a72e8884642619f585d23ccd1e6467
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-16 00:43:18 +0000

    开发回滚3982，屏蔽相关用例测试点
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38959 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/analyze/analyze_cl_11611.js
+++ testcase_new/story/js/analyze/analyze_cl_11611.js
@@ -314,17 +314,17 @@ function main()
    checkAnalyzeInvalidResult({Collection: csName + ".non_exist_cl"}, expectErrCode);
    
    //check analyze not exist cs and cl 
-   var expectErrCode_alone = -34;
-   var expectErrCode_cluster = -23;
-   var cs_null = "non_exist_cl.non_exist_cl"
-   if(commIsStandalone(db))
-   {
-      checkAnalyzeInvalidResult({Collection: cs_null}, expectErrCode_alone);
-   }
-   else
-   {
-      checkAnalyzeInvalidResult({Collection: cs_null}, expectErrCode_cluster);
-   }
+//   var expectErrCode_alone = -34;
+//   var expectErrCode_cluster = -23;
+//   var cs_null = "non_exist_cl.non_exist_cl"
+//   if(commIsStandalone(db))
+//   {
+//      checkAnalyzeInvalidResult({Collection: cs_null}, expectErrCode_alone);
+//   }
+//   else
+//   {
+//      checkAnalyzeInvalidResult({Collection: cs_null}, expectErrCode_cluster);
+//   }
                                                           	
    println("check result after analyze success!");
          
