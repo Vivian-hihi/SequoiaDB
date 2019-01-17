50c0782fae1a45801fae5d58aad8e3b6dd2dc7ce
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-17 02:47:51 +0000

    因重命名s3涉及的cs和cl名称，所以需要做对应修改
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@38985 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/s3/java/src/test/java/com/sequoias3/testcommon/TestResidualData.java
+++ testcase_new/s3/java/src/test/java/com/sequoias3/testcommon/TestResidualData.java
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,87 +14,90 @@ import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
-import com.sequoiadb.exception.BaseException;
-import com.sequoiadb.exception.SDBError;
 
 public class TestResidualData extends S3TestBase{
+    Sequoiadb db = null;
+	int errorCount = 0;
+    String printInfo = "";
+
 	@BeforeClass
-	private void setUp() throws Exception {}
+	private void setUp() {}
 
 	@Test
 	private void printResidualData() throws Exception{
-        int errorCount = 0;
-        Sequoiadb db = null;
-        DBCursor cursor = null;
-        String printInfo = "";
-        try{
-            db =  new Sequoiadb(S3TestBase.coordUrl, "", "");
-            CollectionSpace metaCs = db.getCollectionSpace("MetaCollectionSpace");
-            List<DBCollection> clList = new ArrayList<DBCollection>();
-            List<String> clNameList = metaCs.getCollectionNames();
+    	List<String> csNames = new ArrayList<String>();
+    	List<String> s3CSNames = new ArrayList<String>();
+        db =  new Sequoiadb(S3TestBase.coordUrl, "", "");
+        csNames = db.getCollectionSpaceNames();
+        
+        for(String csName : csNames){
+        	if(csName.startsWith("S3_")){
+        		s3CSNames.add(csName);
+        	}
+        }
+        
+        for(String csName : s3CSNames){
+        	CollectionSpace cs = db.getCollectionSpace(csName);
+        	List<DBCollection> clList = new ArrayList<DBCollection>();
+            List<String> clNameList = cs.getCollectionNames();
             for(String csclName : clNameList){
-                String clname = csclName.substring(metaCs.getName().length()+1);
-                clList.add(metaCs.getCollection(clname));
+                String clname = csclName.substring(cs.getName().length()+1);
+                clList.add(cs.getCollection(clname));
             }
-            
-            for(DBCollection cl : clList) {
-                cursor = cl.query();
-                if(cursor.hasNext()) {
-                	printInfo+="\n===============begin print " + metaCs.getName() +"."+ cl.getName() + " data============\n";
-                    while(cursor.hasNext()){
-                        if(cursor.getNext().containsField("Name")){
-                            if(!cursor.getCurrent().get("Name").equals(S3TestBase.s3UserName)&&!cursor.getCurrent().get("Name").equals(S3TestBase.bucketName)&&!cursor.getCurrent().get("Name").equals(S3TestBase.enableVerBucketName)){
-                            	printInfo+=cursor.getCurrent().toString()+"\n";
-                                errorCount++;
-                            }
-                        }else{
-                        	printInfo+=cursor.getCurrent().toString()+"\n";
-                            errorCount++;
-                        }
-                    }
-                    printInfo+="\n===============end print " + metaCs.getName() +"."+ cl.getName() + " data==============\n";
-                }
-            }
-            cursor.close();
-            
-            CollectionSpace dataCs = db.getCollectionSpace("DataCollectionSpace");
-            try{
-                DBCollection objectDataList = dataCs.getCollection("ObjectDataList");
-                cursor = objectDataList.listLobs();
-                if(cursor.hasNext()){
-                	printInfo+="\n===============begin print " + dataCs.getName() +"."+ objectDataList.getName() + " data============\n";
-                    while(cursor.hasNext()){
-                    	printInfo+=cursor.getNext().toString()+"\n";
-                        errorCount++;
-                    }
-                    printInfo+="\n===============end print " + dataCs.getName() +"."+ objectDataList.getName() + " data============\n";
-                }
-            } catch(BaseException e){
-                Assert.assertEquals(e.getErrorCode(), SDBError.SDB_DMS_NOTEXIST.getErrorCode(), "getCollection ObjectDataList failed");
-            }
-            
-            File file = new File(S3TestBase.installPath+"/tools/sequoias3/log/residualdata.log");
-            if(!file.exists()) {
-                file.createNewFile();
-            }
-            
-            FileWriter fw = new FileWriter(file.getAbsoluteFile(),false);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(printInfo);
-            bw.close();
-            
-            if(errorCount != 0){
-                throw new Exception("There is data residue problem");
-            }
-        } finally{
-            if(cursor != null){
+            printCLResidualData(cs, clList);
+        }
+        
+        writeToFile();
+    }
+	
+	private void printCLResidualData(CollectionSpace cs, List<DBCollection> clList){
+        DBCursor cursor = null;
+        try{
+		    for(DBCollection cl : clList) {
+		        cursor = cl.query();
+		        if(cursor.hasNext()) {
+		        	printInfo+="\n===============begin print " + cs.getName() +"."+ cl.getName() + " data============\n";
+		            while(cursor.hasNext()){
+		                if(cursor.getNext().containsField("Name")){
+		                    if(!cursor.getCurrent().get("Name").equals(S3TestBase.s3UserName)&&!cursor.getCurrent().get("Name").equals(S3TestBase.bucketName)&&!cursor.getCurrent().get("Name").equals(S3TestBase.enableVerBucketName)){
+		                    	printInfo+=cursor.getCurrent().toString()+"\n";
+		                        errorCount++;
+		                    }
+		                }else{
+		                	printInfo+=cursor.getCurrent().toString()+"\n";
+		                    errorCount++;
+		                }
+		            }
+		            printInfo+="===============end print " + cs.getName() +"."+ cl.getName() + " data==============\n";
+		        }
+		    }
+		    cursor.close();
+        }finally{
+        	if(cursor != null){
                 cursor.close();
             }
-            if(db != null){
-                db.close();
-            }
         }
-    }
+	}
+	
+	private void writeToFile() throws Exception{
+	    File file = new File(S3TestBase.installPath+"/tools/sequoias3/log/residualdata.log");
+	    if(!file.exists()) {
+	        file.createNewFile();
+	    }
+	    
+	    FileWriter fw = new FileWriter(file.getAbsoluteFile(),false);
+	    BufferedWriter bw = new BufferedWriter(fw);
+	    bw.write(printInfo);
+	    bw.close();
+	    
+	    if(errorCount != 0){
+	        throw new Exception("There is data residue problem");
+	    }
+	}
 	@AfterClass
-	private void tearDown() throws Exception {}
+	private void tearDown() throws Exception {
+    	if(db != null){
+            db.close();
+        }
+	}
 }
