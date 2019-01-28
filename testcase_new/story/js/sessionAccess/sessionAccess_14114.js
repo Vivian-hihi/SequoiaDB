5951eb9cf1ed52be28dc390f4890ded563eba1bb
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-28 07:07:04 +0000

    根据问题单4092修改timeout最小值为1s，补充查询不超时的结果
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39206 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/sessionAccess/sessionAccess_14114.js
+++ testcase_new/story/js/sessionAccess/sessionAccess_14114.js
@@ -1,10 +1,7 @@
 /* *****************************************************************************
-@discretion: setSessionAttr(),set instatceid and timeout, query timeout
-             metaopr timeout:test createcl timeout
-             crud timeout :test insert/query timeout
+@discretion: setSessionAttr(),set instatceid and timeout, query timeout;test getSessionAttr(); 
 @author£º2018-1-29 wuyan  Init
 ***************************************************************************** */
-
 main();
 function main()
 {	  
@@ -15,35 +12,20 @@ function main()
       {
          println( "run mode is standalone" );
          return;
-      } 
-      
-      //create group and node  
-      var groupName = "group14114";   
-      var instanceidList = [ 14, 29, 15];      
-      createRGAndNode(sdb, groupName, instanceidList);      
-      
+      }     
+     
       //create cl and insert data
       var csName = CHANGEDPREFIX + "_cs14114";
       var clName = CHANGEDPREFIX + "_sessionAcess14114";  
       commCreateCS( sdb, csName, false, "Failed to create CS.");
-      var options = {ReplSize:0,Group:groupName};
+      var options = {ReplSize:0};
       var dbcl = commCreateCLByOption( sdb, csName, clName, options, true, true );       
-      insertData( dbcl);
+      buckInsertData( dbcl, 80000);
               
       println("---begin to test query timeout ");   
-      testQueryTimeout(csName, clName);
-      
-      println("---begin to test createcl timeout ");  
-      setCreateCLTimeout(csName); 
-      
-      println("---begin to test insertcl timeout ");
-      setInsertTimeout(csName, clName);      
-      
-      println("---begin dropcs ");      
-      commDropCS( sdb, csName, false, "Failed to drop CS.");       
-      
-      println("---begin to remove RG ");        
-      sdb.removeRG(groupName);
+      testQueryTimeout(csName, clName);     
+        
+      commDropCS( sdb, csName, false, "Failed to drop CS.");      
    }
    catch( e )
    {
@@ -62,16 +44,15 @@ function testQueryTimeout(csName, clName)
 {   
    try
    {
-      var sdb = new Sdb(COORDHOSTNAME, COORDSVCNAME );
-      var instanceid = 15;
-      sdb.setSessionAttr( { PreferedInstance: instanceid, Timeout : 1} );      
+      var timeOutValue = 1000;
+      var sdb = new Sdb(COORDHOSTNAME, COORDSVCNAME );      
+      sdb.setSessionAttr( { PreferedInstance: "M", Timeout : timeOutValue} );      
       var dbcl = sdb.getCS(csName).getCL(clName);
       var rc = dbcl.find().sort({a:1});
       while( rc.next() )
       {    
          var atcObj = rc.current().toObj();      
-      } 
-      throw "need throw error";
+      }       
    }
    catch( e )
    {
@@ -80,6 +61,7 @@ function testQueryTimeout(csName, clName)
    }
    finally
    {
+      checkTimeoutValue(sdb, timeOutValue);
       sdb.setSessionAttr( { Timeout : -1} );
       if ( rc != null)
       {         
@@ -93,70 +75,60 @@ function testQueryTimeout(csName, clName)
    }
 }
 
-function setCreateCLTimeout(csName)
+function checkTimeoutValue(sdb, timeOutValue)
 {   
-   try
+   var sessionResult = sdb.getSessionAttr().toObj();
+   var timeout = sessionResult.Timeout;
+   if ( JSON.stringify(timeout) !== JSON.stringify(timeOutValue))
    {
-      var db1 = new Sdb(COORDHOSTNAME, COORDSVCNAME );
-      
-      db1.setSessionAttr( { Timeout : 1} );
-      var clName = "test14114";
-      var options = {ReplSize:0,ShardingKey:{a:1}};
-      db1.getCS(csName).createCL(clName, options);
-      throw "need throw error";
+      throw buildException("checkTimeoutValue()", e,"getSeesionAttr()",timeOutValue,JSON.stringify(timeout)  ); 
    }
-   catch( e )
+   
+}
+
+function buckInsertData( dbcl, insertNums, beginNums)
+{	
+   if( undefined == beginNums ){ beginNums = 0 ; }
+	try
    {
-      if( e !== -13 )
-            throw buildException( "check createcl timeout", e );     
-   }
-   finally
-   {
-      db1.setSessionAttr( { Timeout : -1} );
-      if ( db1 != null)
-      {         
-         db1.close();
-      }
+      println( "---begin to buckInsert data." ) ;
+      var batchNums = 10000;     
+      var recs = [];
+      var times = insertNums/batchNums;
       
+      for(var k = 0; k < times; k++)
+      {        
+         var doc = [];  
+         for( var i = 0; i < batchNums; ++i )
+         {    
+            var count = beginNums++  
+            var no = count ;
+            var str = getRandomString(100) +  "teststr_" + count ;                          
+            var inta = count ;         
+            var fc = count + 0.7898;
+            var objs = { "no":no, "str":str, "inta":inta, "fc":fc};         
+            doc.push(objs);             
+            recs.push(objs);              
+         }	
+         dbcl.insert( doc );  
+      }    
+      println("---end bulkInsert data.")
+      return recs;
+   }
+   catch ( e )
+   {
+      throw buildException("bulkInsertDate()", e);      
    }
 }
 
-function setInsertTimeout(csName, clName)
-{   
-   try
+function getRandomString(len) 
+{     
+   var str = "";
+   var chars = "ABCDEFGHIJKLMNOPQRATUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+   var maxPos = chars.length;
+   for( var i = 0; i < len; i++ )
    {
-      var db = new Sdb(COORDHOSTNAME, COORDSVCNAME );
-      var dbcl = db.getCS(csName).getCL(clName);
-      db.setSessionAttr( { Timeout : 1} );
-      var docs = [];
-      for( var i = 0; i < 10000; ++i )
-      {      
-         var no = i;
-         var a = i;
-         var user = "test"+i;
-         var phone = 13700000000+i;
-         var time = new Date().getTime(); 
-         var doc = {no:no, a:a,customerName:user, phone:phone, openDate:time};      
-         //data example: {"no":5, customerName:"test5", "phone":13700000005, "openDate":1402990912105
-         
-         docs.push( doc );
-      }	
-      dbcl.insert( docs );       
-      throw "need throw error";
+      str += chars.charAt(Math.floor(Math.random() * maxPos));     
    }
-   catch( e )
-   {
-      if( e !== -13 )
-            throw buildException( "check timeout", e );     
-   }
-   finally
-   {
-      db.setSessionAttr( { Timeout : -1} );
-      if ( db != null)
-      {         
-         db.close();
-      }
-      
-   }
+   return str;
 }
-
