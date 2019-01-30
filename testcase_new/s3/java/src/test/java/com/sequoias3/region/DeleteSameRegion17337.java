2d5f6a59587cbaca92ac07a4e51817fdb0c7f5df
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-30 07:39:43 +0000

    新增区域管理并发用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39230 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/region/DeleteSameRegion17337.java
@@ -0,0 +1,50 @@
+package com.sequoias3.region;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.S3ThreadBase;
+import com.sequoias3.testcommon.s3utils.RegionUtils;
+
+/**
+ * test content: 并发删除区域 
+ * testlink-case: seqDB-17337
+ * @author wangkexin
+ * @Date 2019.01.29
+ * @version 1.00
+ */
+
+public class DeleteSameRegion17337 extends S3TestBase{
+	private String regionName = "Beijing17337";
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		if(RegionUtils.headRegion(regionName)){
+			RegionUtils.deleteRegion(regionName);
+		}
+		Region region = new Region();
+        region.withName(regionName);
+        RegionUtils.putRegion(region);
+	}
+	
+	@Test
+	public void testDeleteRegion() throws Exception {
+		DeleteRegionThread deleteRegionThread = new DeleteRegionThread();
+		deleteRegionThread.start(100);
+		Assert.assertTrue(deleteRegionThread.isSuccess());
+		Assert.assertFalse(RegionUtils.headRegion(regionName));
+	}
+	
+	@AfterClass
+	private void tearDown() throws Exception {}
+	
+	private class DeleteRegionThread extends S3ThreadBase{
+		@Override
+		public void exec() throws Exception {
+			RegionUtils.deleteRegion(regionName);
+		}
+	}
+}
