2a6fef97a1ad1a36cf523aba717c921df1f33dcc
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-01-31 01:32:07 +0000

    更新lob自动切分到数据组中的误差值
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39243 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/lob/basicoperation/TestLobAutoSplit7845.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/lob/basicoperation/TestLobAutoSplit7845.java
@@ -82,7 +82,7 @@ public class TestLobAutoSplit7845 extends SdbTestBase {
 	
 	@Test( dependsOnMethods = "testAutoSplitLob")
 	public void checkSplitResult(){	
-		double expErrorValue = 0.8;	
+		double expErrorValue = 0.95;	
 		LobOprUtils.checkSplitResult(sdb, csName, clName, groupList, expErrorValue);		
 	}
 	
