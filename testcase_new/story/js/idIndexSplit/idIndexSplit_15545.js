464d351703ce096c1af7afaf50c9e921003c3e23
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-01 08:00:34 +0000

    优化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39268 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/idIndexSplit/idIndexSplit_15545.js
+++ testcase_new/story/js/idIndexSplit/idIndexSplit_15545.js
@@ -4,35 +4,24 @@
 ******************************************************************************/
 function main()
 {
-   if( true == commIsStandalone( db ) )
-	{
-	   println( "Standalone environment!" );
-      return;	
-	}
-	
-	//get groups from sdb
    var groupNames = getGroupNames();
-   if( ( 2 > groupNames.length ) )
+   if(commIsStandalone(db) || groupNames.length < 2)
    {
       println( "Only one group or standalone environment!" );
       return;
    }
    
-   //define the name of CL and domain
    var csName = COMMCSNAME + "_15545";
-	var clName = COMMCLNAME + "_15545";	
-	var domName = "mydomain" + "_15545"
-	var srcGroup = groupNames[0];
+   var clName = COMMCLNAME + "_15545";	
+   var domName = "mydomain" + "_15545"
+   var srcGroup = groupNames[0];
    var desGroup = groupNames[1];
 	
-	//drop domain before
-	dropDomain( domName, csName );
+   dropDomain( domName, csName );
 	
-	//create domain
-	db.createDomain( domName, [srcGroup, desGroup] );
+   db.createDomain( domName, [srcGroup, desGroup] );
 	
-	//create CL
-	var varCL = db.createCS( csName,{Domain:domName} ).createCL( clName,{ShardingKey:{a:1}, ShardingType:"hash",AutoIndexId:false,AutoSplit:true} )
+   var dbcl = db.createCS( csName,{Domain:domName} ).createCL( clName,{ShardingKey:{a:1}, ShardingType:"hash",AutoIndexId:false,AutoSplit:true} )
 
    //check id index not existed;
    checkIdIndex( clName, "NoIDIndex", csName);
@@ -40,16 +29,14 @@ function main()
    //check catalog information
    checkCataInfo( clName, srcGroup, 2, csName );
    
-   //insert data
    for( var i=1; i<=50; i++ )
    {
-      varCL.insert( {a:i} );
+      dbcl.insert( {a:i} );
    }
    
-   //check data
    checkSplitResult( srcGroup, desGroup, clName, csName )
    
-   commDropCL( db, COMMCSNAME, clName );
+   commDropCS( db, csName )
    dropDomain( domName, csName );
 }
 
