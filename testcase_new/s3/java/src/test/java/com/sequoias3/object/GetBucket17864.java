1bdf77b4ec96de674f94ff7167fca3f9bc98ec79
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-15 09:20:33 +0000

    SEQUOIADBMAINSTREAM-4088 【BUG】【new】【story】【S3对象管理】使用PutObjectRequest接口指定对象名创建对象，部分创建失败
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39376 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/GetBucket17864.java
@@ -0,0 +1,73 @@
+package com.sequoias3.object;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.RestClient;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.s3utils.UserUtils;
+
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * test content: GET /s3/bucketname验证 
+ * testlink-case: seqDB-17864  
+ * @author wangkexin
+ * @Date 2019.02.15
+ * @version 1.00
+ */
+public class GetBucket17864 extends S3TestBase {
+	private boolean runSuccess = false;
+	private String userName = "user17864";
+	private String bucketName = "bucket17864";
+	private String roleName = "normal";
+	private AmazonS3 s3Client = null;
+	private String[] accessKeys = null;
+	private static CloseableHttpClient client;
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		CommLib.clearUser(userName);
+		accessKeys = UserUtils.createUser(userName, roleName);
+		s3Client = CommLib.buildS3Client(accessKeys[0], accessKeys[1]);
+	}
+
+	@Test
+	private void testCreateBucket() throws Exception {
+		s3Client.createBucket(bucketName);
+		try{
+			getBucket(bucketName);
+		}catch(Exception e){
+			Assert.assertTrue(e.getMessage().contains("Please use list objects V2 request instead of list objects V1.") , e.getMessage());
+		}
+		runSuccess = true;
+	}
+
+	@AfterClass
+	private void tearDown() throws Exception {
+		try {
+			if (runSuccess) {
+				UserUtils.deleteUser(userName);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if (s3Client != null) {
+				s3Client.shutdown();
+			}
+		}
+	}
+
+	private void getBucket(String bucketName) throws Exception {
+		HttpGet request = new HttpGet(S3TestBase.s3ClientUrl + "/s3/" + bucketName);
+		request.setHeader("Authorization", "Credential=" + accessKeys[0]);
+		client = RestClient.createHttpClient();
+		RestClient.sendRequest(client, request);
+	}
+
+}
---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/TestObjectRequest17861.java
@@ -0,0 +1,112 @@
+package com.sequoias3.object;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectMetadataRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+import com.sequoiadb.exception.BaseException;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.TestTools;
+
+/**
+ * test content: Put/Get/Delete/HeadObject请求携带特殊字符校验
+ * testlink-case: seqDB-17861
+ * @author wangkexin
+ * @Date 2019.02.15
+ * @version 1.00
+ */
+public class TestObjectRequest17861 extends S3TestBase {
+	private String bucketName = "bucket17861";
+	private AmazonS3 s3Client = null;
+	private int fileSize = 1024 * 1024;
+	private File localPath = null;
+	private String filePath = null;
+	private boolean runSuccess = false;
+
+	@BeforeClass
+	private void setUp() throws Exception {
+		localPath = new File(S3TestBase.workDir + File.separator + TestTools.getClassName());
+		filePath = localPath + File.separator + "localFile_" + fileSize + ".txt";
+		TestTools.LocalFile.removeFile(localPath);
+		TestTools.LocalFile.createDir(localPath.toString());
+		TestTools.LocalFile.createFile(filePath, fileSize);
+		
+		s3Client = CommLib.buildS3Client();
+		CommLib.clearBucket(s3Client, bucketName);
+		s3Client.createBucket(new CreateBucketRequest(bucketName));
+	}
+
+	@Test
+	public void testasciiKeyName( ) throws Exception {
+		String asciiKeyName = new String();
+		for(int i=1 ; i < 32 ; i ++){
+			asciiKeyName+=(char)i;
+		}
+		for(int i=127 ; i < 256 ; i ++){
+			asciiKeyName+=(char)i;
+		}
+		
+		//test Put Object Request
+		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, asciiKeyName, new File(filePath));
+		ObjectMetadata metadata = new ObjectMetadata();
+		Map<String ,String> xMeta = new HashMap <String	,String>();
+		xMeta.put("meta17861", "17861");
+		metadata.setUserMetadata(xMeta);
+		putObjectRequest.withMetadata(metadata);
+		PutObjectResult result = s3Client.putObject(putObjectRequest);
+		String actMd5 = result.getETag();
+		String expMd5 = TestTools.getMD5(filePath);
+		Assert.assertEquals(actMd5, expMd5, "putobjectrequest : md5 is wrong! the key name is : " + asciiKeyName);
+		
+		//test Get Object Request
+		GetObjectRequest getObjectRequest = new GetObjectRequest(bucketName, asciiKeyName);
+		S3Object object = s3Client.getObject(getObjectRequest);
+		actMd5 = object.getObjectMetadata().getETag();
+		Assert.assertEquals(actMd5, expMd5, "getobjectrequest : md5 is wrong! the key name is : " + asciiKeyName);
+		
+		//test Head Object Request
+		Assert.assertTrue(s3Client.doesObjectExist(bucketName, asciiKeyName));
+		GetObjectMetadataRequest getObjectMetadataRequest = new GetObjectMetadataRequest(bucketName, asciiKeyName);
+		ObjectMetadata metadata2 = s3Client.getObjectMetadata(getObjectMetadataRequest);
+		actMd5 = metadata2.getETag();
+		Assert.assertEquals(actMd5, expMd5, "getobjectrequest : md5 is wrong! the key name is : " + asciiKeyName);
+		
+		//test Delete Object Request
+		DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucketName, asciiKeyName);
+		s3Client.deleteObject(deleteObjectRequest);
+		Assert.assertFalse(s3Client.doesObjectExist(bucketName, asciiKeyName));
+		
+		runSuccess = true;
+	}
+	
+	@AfterClass
+	private void tearDown() {
+		try {
+			if (runSuccess) {
+				CommLib.clearBucket(s3Client, bucketName);
+				TestTools.LocalFile.removeFile(localPath);
+			}
+		} catch (BaseException e) {
+			Assert.fail("clean up failed:" + e.getMessage());
+		} finally {
+			if( s3Client != null ){
+				s3Client.shutdown();
+			}
+		}
+	}
+}
