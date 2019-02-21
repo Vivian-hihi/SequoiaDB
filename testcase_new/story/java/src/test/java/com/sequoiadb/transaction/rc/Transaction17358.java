853ef6db5bcd4ed778c4a22d579d7625d422421b
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-21 05:53:13 +0000

    提交事务自动化用例检视意见
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39473 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rc/Transaction17358.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rc/Transaction17358.java
@@ -32,6 +32,7 @@ public class Transaction17358 extends SdbTestBase {
     private Sequoiadb sdb2 = null;
     private Sequoiadb sdb3 = null;
     private DBCollection cl = null;
+    //TODO:集合名变量名定义不合规范，其他用例类似，均需修改
     private DBCollection CLTrans1 = null;
     private DBCollection CLTrans2 = null;
     private DBCollection CLTrans3 = null;
@@ -51,6 +52,9 @@ public class Transaction17358 extends SdbTestBase {
         sdb3 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
         cl = sdb.getCollectionSpace(csName).createCollection(clName);
         cl.createIndex("a", "{a:1}", false, false);
+        //TODO:为什么要在这里new？new完也不用，建议在定义的地方new，data、data2、data3等也类似，其他测试用例也需修改
+        //这里我有个疑问，为什么要在这里赋值，又不在这里使用；为了测试用例的清晰刻度，我觉得：这个变量是个全局变量，应该在定义的时候new，在使用的时候赋值
+        //另外：这里定义数据的时候，是不是使用字符串强转为BSONObject，可读性更强
         expDataList = new ArrayList<BSONObject>();
         data = new BasicBSONObject();
         data.put("_id", "insertID17358_1");
@@ -58,6 +62,7 @@ public class Transaction17358 extends SdbTestBase {
         data.put("b", 1);
         data.put("c", 13700000000L);
         data.put("d", "customer transaction type data application.");
+        //TODO：插入记录，放到test中更合理，这个是个操作步骤，不算是一个预置条件
         cl.insert(data);
         
         data2 = new BasicBSONObject();
@@ -81,6 +86,7 @@ public class Transaction17358 extends SdbTestBase {
         data4.put("c", 13700000000L);
         data4.put("d", "customer transaction type data application.");
         
+        //TODO:在这里开启事务，对应的应该在teardown里面关闭事务，建议这个步骤也放到test里面
         sdb1.beginTransaction();
         sdb2.beginTransaction();
         sdb3.beginTransaction();
@@ -102,7 +108,7 @@ public class Transaction17358 extends SdbTestBase {
         
         expDataList.add(data);
         expDataList.add(data2);
-        //step4: trans1 read
+        //step4: trans1 read  TODO:注释和代码行之间，统一一下，都空格吧
         recordCur = CLTrans1.query("{'a': {'$isnull': 0}}", null, "{a:1}", "{'': null}");
         actDataList = TransUtils.getReadActList(recordCur);
         Assert.assertEquals(actDataList, expDataList);
@@ -113,6 +119,7 @@ public class Transaction17358 extends SdbTestBase {
         Assert.assertEquals(actDataList, expDataList);
         actDataList.clear();
         
+        //TODO:这个变量的赋值，作为事务读的预期结果，应该放到注释之下，其他地方类似，变量在哪里使用，再哪里赋值
         expDataList.clear();
         expDataList.add(data);
         //step5: trans3 read
@@ -141,6 +148,7 @@ public class Transaction17358 extends SdbTestBase {
         actDataList.clear();
         
         //step7: read after trans1 commit 
+        //TODO:事务2的更新操作，返回成功了，就是不阻塞了，不需要重复判断
         sdb1.commit();
         Assert.assertTrue(updateThread.isSuccess(), updateThread.getErrorMsg());
         Assert.assertFalse(updateThread.matchBlockingMethod(CLTrans2.getClass().getName(), "update"));
@@ -215,6 +223,8 @@ public class Transaction17358 extends SdbTestBase {
     
     @AfterClass
     public void tearDown(){
+        
+        //TODO:这里没有必要使用try
         try {
             sdb.getCollectionSpace(csName).dropCollection(clName);
         } finally {
@@ -240,6 +250,7 @@ public class Transaction17358 extends SdbTestBase {
         
         @Override
         public void exec() throws BaseException {
+            //TODO:建议风格统一，跟query接口一样，都使用字符串，增加测试代码可读性
             BSONObject modifier = new BasicBSONObject();
             modifier.put("a", 2);
             modifier.put("b", 2);
