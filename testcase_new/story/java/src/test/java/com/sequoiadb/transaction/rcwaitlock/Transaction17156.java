766dfebc881414d5a061ea47bbb28441e9413160
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-26 09:04:35 +0000

    根据检视意见修改用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39568 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rcwaitlock/Transaction17156.java
@@ -0,0 +1,145 @@
+package com.sequoiadb.transaction.rcwaitlock;
+
+/**
+ * @Description seqDB-17156:  删除记录与读记录并发，事务提交 
+ * @author xiaoni Zhao
+ * @date 2019-1-22
+ */
+import java.util.List;
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbThreadBase;
+import com.sequoiadb.transaction.TransUtils;
+
+@Test(groups = "rcwaitlock")
+public class Transaction17156 extends SdbTestBase {
+	private String clName = "cl_17156";
+	private Sequoiadb sdb = null;
+	private Sequoiadb db1 = null;
+	private DBCollection cl = null;
+	private DBCollection cl1 = null;
+	private DBCursor cursor = null;
+
+	@BeforeClass
+	public void setUp() {
+		sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		db1 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		cl = sdb.getCollectionSpace(csName).createCollection(clName);
+		cl1 = db1.getCollectionSpace(csName).getCollection(clName);
+		cl.createIndex("a", "{a:1}", false, false);
+		BSONObject insertR1 = (BSONObject) JSON.parse("{_id:1, a:1, b:1}");
+		cl.insert(insertR1);
+	}
+
+	@Test
+	public void test() {
+		// 开启事务1
+		db1.beginTransaction();
+
+		// 事务1删除记录
+		cl1.delete(null, "{'':'a'}");
+
+		// 事务2表扫描记录
+		Read read1 = new Read("{'':null}");
+		read1.start();
+		Assert.assertTrue(read1.matchBlockingMethod(DBCursor.class.getName(), "hasNext"));
+
+		// 事务2索引扫描记录
+		Read read2 = new Read("{'':'a'}");
+		read2.start();
+		Assert.assertTrue(read2.matchBlockingMethod(DBCursor.class.getName(), "hasNext"));
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, null, "{'':null}");
+		Assert.assertTrue(TransUtils.getReadActList(cursor).isEmpty());
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, null, "{'':'a'}");
+		Assert.assertTrue(TransUtils.getReadActList(cursor).isEmpty());
+
+		db1.commit();
+
+		// 校验被阻塞线程返回的记录
+		if (!read1.isSuccess() || !read2.isSuccess()) {
+			Assert.fail(read1.getErrorMsg() + read2.getErrorMsg());
+		}
+		try {
+			Assert.assertTrue(((List<BSONObject>) read1.getExecResult()).isEmpty());
+			Assert.assertTrue(((List<BSONObject>) read2.getExecResult()).isEmpty());
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+		
+		cursor.close();
+	}
+
+	private class Read extends SdbThreadBase {
+		private Sequoiadb db = null;
+		private Sequoiadb db2 = null;
+		private DBCollection cl = null;
+		private DBCollection cl2 = null;
+		private String hint = null;
+
+		public Read(String hint) {
+			// TODO Auto-generated constructor stub
+			this.hint = hint;
+		}
+
+		@Override
+		public void exec() throws Exception {
+			// TODO Auto-generated method stub
+			db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+			db2 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+			cl = db.getCollectionSpace(csName).getCollection(clName);
+			cl2 = db2.getCollectionSpace(csName).getCollection(clName);
+			
+			// 开启并发事务2
+			db2.beginTransaction();
+			
+			try {
+				DBCursor cursor = cl2.query(null, null, "{_id:1}", hint);
+				List<BSONObject> records = TransUtils.getReadActList(cursor);
+				setExecResult(records);
+
+				// 事务2扫描记录
+				cursor = cl2.query(null, null, "{_id:1}", hint);
+				Assert.assertTrue(TransUtils.getReadActList(cursor).isEmpty());
+
+				// 非事务扫描记录
+				cursor = cl.query(null, null, "{_id:1}", hint);
+				Assert.assertTrue(TransUtils.getReadActList(cursor).isEmpty());
+
+				db2.commit();
+			} catch (BaseException e) {
+				e.printStackTrace();
+				throw e;
+			} finally {
+				db2.close();
+			}
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (!db1.isClosed()) {
+			db1.close();
+		}
+		CollectionSpace cs = sdb.getCollectionSpace(csName);
+		if (cs.isCollectionExist(clName)) {
+			cs.dropCollection(clName);
+		}
+		if (!sdb.isClosed()) {
+			sdb.close();
+		}
+	}
+}
---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rcwaitlock/Transaction17159B.java
@@ -0,0 +1,159 @@
+package com.sequoiadb.transaction.rcwaitlock;
+
+/**
+ * @Description seqDB-17159:  多个原子操作与读并发，事务回滚 
+ * @author xiaoni Zhao
+ * @date 2019-1-23
+ */
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbThreadBase;
+import com.sequoiadb.transaction.TransUtils;
+
+@Test(groups = "rcwaitlock")
+public class Transaction17159B extends SdbTestBase {
+	private String clName = "cl_17159B";
+	private Sequoiadb sdb = null;
+	private Sequoiadb db1 = null;
+	private DBCollection cl = null;
+	private DBCollection cl1 = null;
+	private DBCursor cursor = null;
+	private List<BSONObject> expList = new ArrayList<BSONObject>();
+
+	@BeforeClass
+	public void setUp() {
+		sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		db1 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		cl = sdb.getCollectionSpace(csName).createCollection(clName);
+		cl1 = db1.getCollectionSpace(csName).getCollection(clName);
+		cl.createIndex("a", "{a:1}", false, false);
+	}
+
+	@Test
+	public void test() {
+		BSONObject insertR1 = (BSONObject) JSON.parse("{_id:1, a:1, b:1}");
+		cl.insert(insertR1);
+		BSONObject insertR2 = (BSONObject) JSON.parse("{_id:2, b:2}");
+		cl.insert(insertR2);
+
+		// 开启事务1
+		db1.beginTransaction();
+
+		// 事务1对不同记录执行多个原子操作
+		BSONObject insertR3 = (BSONObject) JSON.parse("{_id:3, a:3, b:3}");
+		cl1.insert(insertR3);
+		cl1.update("{_id:1}", "{$set:{a:11}}", "{'':'a'}");
+		BSONObject updateR1 = (BSONObject) JSON.parse("{_id:1, a:11, b:1}");
+		cl1.delete("{_id:2}", "{'':'a'}");
+		expList.add(updateR1);
+		expList.add(insertR3);
+
+		// 事务2表扫描记录
+		Read read1 = new Read("{'':null}");
+		read1.start();
+		Assert.assertTrue(read1.matchBlockingMethod(DBCursor.class.getName(), "hasNext"));
+
+		// 事务2索引扫描记录
+		Read read2 = new Read("{'':'a'}");
+		read2.start();
+		Assert.assertTrue(read2.matchBlockingMethod(DBCursor.class.getName(), "hasNext"));
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':null}");
+		Assert.assertEquals(TransUtils.getReadActList(cursor), expList);
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':'a'}");
+		Assert.assertEquals(TransUtils.getReadActList(cursor), expList);
+
+		expList.clear();
+		expList.add(insertR1);
+		expList.add(insertR2);
+		db1.rollback();
+
+		// 校验阻塞线程返回的记录
+		if (!read1.isSuccess() || !read2.isSuccess()) {
+			Assert.fail(read1.getErrorMsg() + read2.getErrorMsg());
+		}
+		try {
+			Assert.assertEquals(read1.getExecResult(), expList);
+			Assert.assertEquals(read2.getExecResult(), expList);
+		} catch (Exception e) {
+			Assert.fail(e.getMessage());
+		}
+
+		cursor.close();
+	}
+
+	private class Read extends SdbThreadBase {
+		private Sequoiadb db = null;
+		private Sequoiadb db2 = null;
+		private DBCollection cl = null;
+		private DBCollection cl2 = null;
+		private String hint = null;
+
+		public Read(String hint) {
+			// TODO Auto-generated constructor stub
+			this.hint = hint;
+		}
+
+		@Override
+		public void exec() throws Exception {
+			// TODO Auto-generated method stub
+			db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+			db2 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+			cl = db.getCollectionSpace(csName).getCollection(clName);
+			cl2 = db2.getCollectionSpace(csName).getCollection(clName);
+
+			// 开启并发事务2
+			db2.beginTransaction();
+			
+			try {
+				DBCursor cursor = cl2.query(null, null, "{_id:1}", hint);
+				List<BSONObject> records = TransUtils.getReadActList(cursor);
+				setExecResult(records);
+
+				// 事务2扫描记录
+				cursor = cl2.query(null, null, "{_id:1}", hint);
+				Assert.assertEquals(TransUtils.getReadActList(cursor), expList);
+
+				// 非事务扫描记录
+				cursor = cl.query(null, null, "{_id:1}", hint);
+				Assert.assertEquals(TransUtils.getReadActList(cursor), expList);
+
+				db2.commit();
+			} catch (BaseException e) {
+				e.printStackTrace();
+				throw e;
+			} finally {
+				db2.close();
+			}
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (!db1.isClosed()) {
+			db1.close();
+		}
+		CollectionSpace cs = sdb.getCollectionSpace(csName);
+		if (cs.isCollectionExist(clName)) {
+			cs.dropCollection(clName);
+		}
+		if (!sdb.isClosed()) {
+			sdb.close();
+		}
+	}
+}
