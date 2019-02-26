ba91af40a9cebec0516c046868d1435a72217aa9
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-26 09:04:07 +0000

    根据检视意见修改用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39567 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/ru/Transaction17207.java
@@ -0,0 +1,131 @@
+package com.sequoiadb.transaction.ru;
+
+/**
+ * @Description seqDB-17207:事务中批量更新与读并发 
+ * @author xiaoni Zhao
+ * @date 2019-1-17
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.transaction.TransUtils;
+
+@Test(groups = "ru")
+public class Transaction17207 extends SdbTestBase {
+	private String clName = "cl_17207";
+	private Sequoiadb sdb = null;
+	private Sequoiadb db1 = null;
+	private Sequoiadb db2 = null;
+	private DBCollection cl = null;
+	private DBCollection cl1 = null;
+	private DBCollection cl2 = null;
+	private DBCursor cursor = null;
+	private List<BSONObject> expList = new ArrayList<BSONObject>();
+	private List<BSONObject> actList = new ArrayList<BSONObject>();
+
+	@BeforeClass
+	public void setUp() {
+		sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		db1 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		db2 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		cl = sdb.getCollectionSpace(csName).createCollection(clName);
+		cl1 = db1.getCollectionSpace(csName).getCollection(clName);
+		cl2 = db2.getCollectionSpace(csName).getCollection(clName);
+		cl.createIndex("a", "{a:1}", false, false);
+        TransUtils.insertDatas(cl, 0, 50000, 1);
+	}
+
+	@Test
+	public void test() {
+		// 开启两个并发事务
+		db1.beginTransaction();
+		db2.beginTransaction();
+
+		// 事务1执行批量更新
+		cl1.update("{a:1}", "{$set:{a:2}}", null);
+		for (int i = 0; i < 50000; i++) {
+			BSONObject updateR = (BSONObject) JSON.parse("{_id:" + i + ", a:2, b:"+ i +"}");
+			expList.add(updateR);
+		}
+
+		// 事务2表扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 事务2索引扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		db1.commit();
+
+		// 事务2表扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 事务2索引扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+
+		db2.commit();
+		cursor.close();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (!db1.isClosed()) {
+			db1.close();
+		}
+		if (!db2.isClosed()) {
+			db2.close();
+		}
+		CollectionSpace cs = sdb.getCollectionSpace(csName);
+		if (cs.isCollectionExist(clName)) {
+			cs.dropCollection(clName);
+		}
+		if (!sdb.isClosed()) {
+			sdb.close();
+		}
+	}
+}
