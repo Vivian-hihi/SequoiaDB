62098a624e1d87d86d69f89fc128547c5e6d9e4e
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-26 09:04:54 +0000

    根据检视意见修改用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39569 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rc/Transaction17117.java
@@ -0,0 +1,161 @@
+package com.sequoiadb.transaction.rc;
+
+/**
+ * @Description seqDB-17117:   更新多个索引，同时与读并发   
+ * @author Zhao Xiaoni
+ * @date 2019-1-21
+ */
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.transaction.TransUtils;
+
+@Test(groups = "rc")
+public class Transaction17117 extends SdbTestBase {
+	private String clName = "cl_17117";
+	private Sequoiadb sdb = null;
+	private Sequoiadb db1 = null;
+	private Sequoiadb db2 = null;
+	private DBCollection cl = null;
+	private DBCollection cl1 = null;
+	private DBCollection cl2 = null;
+	private DBCursor cursor = null;
+	private List<BSONObject> expList = new ArrayList<BSONObject>();
+	private List<BSONObject> actList = new ArrayList<BSONObject>();
+
+	@BeforeClass
+	public void setUp() {
+		sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		db1 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		db2 = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		cl = sdb.getCollectionSpace(csName).createCollection(clName);
+		cl1 = db1.getCollectionSpace(csName).getCollection(clName);
+		cl2 = db2.getCollectionSpace(csName).getCollection(clName);
+		cl.createIndex("a", "{a:1}", false, false);
+		cl.createIndex("b", "{b:1}", false, false);
+		cl.createIndex("c", "{c:1}", false, false);
+	}
+
+	@Test
+	public void test() {
+		BSONObject insertR1 = (BSONObject) JSON.parse("{_id:1, a:1, b:1, c:1}");
+		cl.insert(insertR1);
+
+		db1.beginTransaction();
+		db2.beginTransaction();
+
+		// 事务1更新全部索引字段
+		cl1.update(null, "{$set:{a:2, b:2, c:2}}", "{'':'a'}");
+		BSONObject updateR1 = (BSONObject) JSON.parse("{_id:1, a:2, b:2, c:2}");
+		expList.add(updateR1);
+
+		// 事务1表扫描记录
+		cursor = cl1.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 事务1索引扫描记录
+		cursor = cl1.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 事务2表扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		expList.clear();
+		expList.add(insertR1);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 事务2索引扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		expList.clear();
+		expList.add(updateR1);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+
+		db1.commit();
+
+		// 事务2表扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		expList.clear();
+		expList.add(updateR1);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 事务2索引扫描记录
+		cursor = cl2.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		db2.commit();
+
+		// 非事务表扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':null}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+		actList.clear();
+
+		// 非事务索引扫描记录
+		cursor = cl.query(null, null, "{_id:1}", "{'':'a'}");
+		actList = TransUtils.getReadActList(cursor);
+		Assert.assertEquals(actList, expList);
+
+		cursor.close();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (!db1.isClosed()) {
+			db1.close();
+		}
+		if (!db2.isClosed()) {
+			db2.close();
+		}
+		CollectionSpace cs = sdb.getCollectionSpace(csName);
+		if (cs.isCollectionExist(clName)) {
+			cs.dropCollection(clName);
+		}
+		if (!sdb.isClosed()) {
+			sdb.close();
+		}
+	}
+}
