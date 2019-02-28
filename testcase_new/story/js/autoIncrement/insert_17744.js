6862207b5e1585365854901b456365e39be2e9b9
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-02-28 01:53:24 +0000

    SEQUOIADBMAINSTREAM-4045,修改用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39606 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/autoIncrement/insert_17744.js
+++ testcase_new/story/js/autoIncrement/insert_17744.js
@@ -13,7 +13,7 @@ function main()
     
    var clName = COMMCLNAME + "_17744";
    var increment = -1;
-   var acquireSize = 100;
+   var acquireSize = 5;
    var maxValue = { "$numberLong" : "9223372036854775807" } 
    
    commDropCL( db, COMMCSNAME, clName );
@@ -22,6 +22,11 @@ function main()
                                    AcquireSize : acquireSize, MaxValue : maxValue }});
    commCreateIndex( dbcl, "a", {id : 1}, true );
    
+   dbcl.setAttributes({AutoIncrement : {Field : "id", CurrentValue : maxValue}});
+   
+   //连接所有coord插入部分记录,coord缓存分别为[{ "$numberLong" : "9223372036854775802" },{ "$numberLong" : "9223372036854775806" }],
+   //[{ "$numberLong" : "9223372036854775797" },{ "$numberLong" : "9223372036854775801" }],
+   //[{ "$numberLong" : "9223372036854775792" },{ "$numberLong" : "9223372036854775796" }]
    var expRecs = [];
    var cl = new Array();
    var coord  = new Array();
@@ -30,26 +35,79 @@ function main()
       coord[i] = new Sdb(coordNodes[i]);
       cl[i] = coord[i].getCS(COMMCSNAME).getCL(clName);
       cl[i].insert({a : i});
-      expRecs.push({a : i, id : -1 - i*acquireSize});
    }
    
-   dbcl.setAttributes({AutoIncrement : {Field : "id", CurrentValue : maxValue}});
+   expRecs.push({a : 0, id : { "$numberLong" : "9223372036854775806" }});
+   expRecs.push({a : 1, id : { "$numberLong" : "9223372036854775801" }});
+   expRecs.push({a : 2, id : { "$numberLong" : "9223372036854775796" }});
    
-   var insertR1 = {a : 3, id : { "$numberLong" : "-9223372036854775808" } }
-   cl[0].insert(insertR1);
+   var insertR1 = {a : 4, id : { "$numberLong" : "-9223372036854775808" } }
+   cl[1].insert(insertR1);
    expRecs.push(insertR1);
    
+   //coordA插入记录，消耗完本coord的缓存[{ "$numberLong" : "9223372036854775802" },{ "$numberLong" : "9223372036854775806" }]
+   for(var i = 0; i < 4; i++)
+   {
+      cl[0].insert({a:i});
+   }
+   expRecs.push({a:0, id:{$numberLong:"9223372036854775805"}});
+   expRecs.push({a:1, id:{$numberLong:"9223372036854775804"}});
+   expRecs.push({a:2, id:{$numberLong:"9223372036854775803"}});
+   expRecs.push({a:3, id:{$numberLong:"9223372036854775802"}});
+   println("---coordA insert success");
+   
+   //coordA插入记录，插入失败，超出序列值返回
    try
    {
-      cl[0].insert({a : 4});
-      throw "insert error!";
+      cl[0].insert({a:1});
+      throw "NEED_ERROR";
    }catch(e)
    {
-      if(e != -325)
+      if(e !== -325)
       {
          throw e;
       }
    }
+   println("---coordA get cache success");
+      
+   //coordB插入记录，插入失败，超出序列值范围
+   try
+   {
+      cl[1].insert({a:1});
+      throw "NEED_ERROR";
+   }catch(e)
+   {
+      if(e !== -325)
+      {
+         throw e;
+      }
+   }
+   println("---coordB get cache success");
+   
+   //coordC插入记录，消耗完本coord的缓存[{ "$numberLong" : "922337203685477601" },{ "$numberLong" : "9223372036854775705" }]
+   for(var i = 0; i < 4; i++)
+   {
+      cl[2].insert({a : i});
+   }
+   expRecs.push({a:0, id:{$numberLong:"9223372036854775795"}});
+   expRecs.push({a:1, id:{$numberLong:"9223372036854775794"}});
+   expRecs.push({a:2, id:{$numberLong:"9223372036854775793"}});
+   expRecs.push({a:3, id:{$numberLong:"9223372036854775792"}});
+   println("---coordC insert success");
+   
+   //coordC插入记录，插入失败，超出序列值范围
+   try
+   {
+      cl[2].insert({a:1});
+      throw "NEED_ERROR";
+   }catch(e)
+   {
+      if(e !== -325)
+      {
+         throw e;
+      }
+   }
+   println("---coordC get cache success");
    
    var rc = dbcl.find().sort({_id:1});
    checkRec(rc, expRecs.sort(compare("_id")));
