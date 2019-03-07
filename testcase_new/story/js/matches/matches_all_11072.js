2b8c4b3040f3023c5ab8042a78a7206209234d4e
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-07 07:14:12 +0000

    修改用例格式
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39760 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/matches/matches_all_11072.js
+++ testcase_new/story/js/matches/matches_all_11072.js
@@ -9,40 +9,41 @@ main();
 
 function main()
 {
-	var csName = CHANGEDPREFIX + "_11072_CS";
-	var clName = CHANGEDPREFIX + "_11072_CL";
-	
-	commDropCS(db, csName, true, "drop cs in the begin");
-	var cl = commCreateCL( db, csName, clName, null, null, true, false, "create cl in the begin" );
-	
-	println("---begin test---");
-	cl.insert({_id:1,a:[1,{"$regex":"^W","$options":""}]});
-	cl.insert({_id:2,a:1});
-	cl.insert({_id:3,a:{"$regex":"^W","$options":""}});
-	
-	var cursor = cl.find({a:{$all:[1,{"$regex":"^W","$options":""}]}});
-	var expRecs = '[{"_id":1,"a":[1,{"$regex":"^W","$options":""}]}]';
-	checkCLData( cursor, expRecs, 1);
+    var csName = CHANGEDPREFIX + "_11072_CS";
+    var clName = CHANGEDPREFIX + "_11072_CL";
 
-	commDropCS( db, csName, true, "drop CS in the end" );
+    commDropCS(db, csName, true, "drop cs in the begin");
+    var cl = commCreateCL( db, csName, clName, null, null, true, false, "create cl in the begin" );
+
+    println("---begin test---");
+    cl.insert({_id:1,a:[1,{"$regex":"^W","$options":""}]});
+    cl.insert({_id:2,a:1});
+    cl.insert({_id:3,a:{"$regex":"^W","$options":""}});
+
+    var cursor = cl.find({a:{$all:[1,{"$regex":"^W","$options":""}]}});
+    var expRecs = '[{"_id":1,"a":[1,{"$regex":"^W","$options":""}]}]';
+    checkCLData( cursor, expRecs, 1);
+
+    commDropCS( db, csName, true, "drop CS in the end" );
 }	
 
 function checkCLData( rc, expRecs, expCnt )
 {
-	println("\n---Begin to check cl data.");
-	var recsArray = [];
-	while( tmpRecs = rc.next() )
-	{
-		recsArray.push( tmpRecs.toObj() );
-	}
-	
-	var actCnt  = recsArray.length;
-	var actRecs = JSON.stringify( recsArray );
-	if( actCnt !== expCnt || actRecs !== expRecs )
-	{
-		throw buildException( "checkCLdata", null, "[find]",
-		  "[cnt:"+ expCnt +", recs:"+ expRecs +"]",
-		  "[cnt:"+ actCnt +", recs:"+ actRecs +"]" );
-	}
-	println( "cl records: "+ actRecs );
+    println("\n---Begin to check cl data.");
+    var recsArray = [];
+    while( tmpRecs = rc.next() )
+    {
+        recsArray.push( tmpRecs.toObj() );
+    }
+    rc.close();
+
+    var actCnt  = recsArray.length;
+    var actRecs = JSON.stringify( recsArray );
+    if( actCnt !== expCnt || actRecs !== expRecs )
+    {
+        throw buildException( "checkCLdata", null, "[find]",
+          "[cnt:"+ expCnt +", recs:"+ expRecs +"]",
+          "[cnt:"+ actCnt +", recs:"+ actRecs +"]" );
+    }
+    println( "cl records: "+ actRecs );
 }
--- testcase_new/story/js/matches/matches_all_11076.js
+++ testcase_new/story/js/matches/matches_all_11076.js
@@ -9,20 +9,21 @@ main();
 
 function main()
 {
-	var csName = CHANGEDPREFIX + "_11706_CS";
-	var clName = CHANGEDPREFIX + "_11706_CL";
-	
-	commDropCS(db, csName, true, "drop cs in the begin");
-	var cl = commCreateCL( db, csName, clName, null, null, true, false, "create cl in the begin" );
-	
-	println("---begin test---");
-	cl.insert({a:[Regex("^W","i"),3]});
-	
-	var cursor = cl.find({a:{$all:[Regex("^W","i"),Regex("^s","i")]}});
-	if(cursor.next()!=null)
-	{
-		throw buildException("find()",null,"check record", "no data","have data");
-	}
-	
-	commDropCS( db, csName, true, "drop CS in the end" );
+    var csName = CHANGEDPREFIX + "_11706_CS";
+    var clName = CHANGEDPREFIX + "_11706_CL";
+
+    commDropCS(db, csName, true, "drop cs in the begin");
+    var cl = commCreateCL( db, csName, clName, null, null, true, false, "create cl in the begin" );
+
+    println("---begin test---");
+    cl.insert({a:[Regex("^W","i"),3]});
+
+    var cursor = cl.find({a:{$all:[Regex("^W","i"),Regex("^s","i")]}});
+    if(cursor.next()!=null)
+    {
+        throw buildException("find()",null,"check record", "no data","have data");
+    }
+    cursor.close();
+
+    commDropCS( db, csName, true, "drop CS in the end" );
 }
