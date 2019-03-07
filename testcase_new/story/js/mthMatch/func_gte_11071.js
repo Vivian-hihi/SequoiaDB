06d08ba1af35228e841e99ace29bc2c54906373f
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-07 07:12:45 +0000

    修改用例格式
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39759 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/mthMatch/func_gte_11071.js
+++ testcase_new/story/js/mthMatch/func_gte_11071.js
@@ -6,17 +6,17 @@
 **************************************/
 function main()
 {
-	var csName = CHANGEDPREFIX + "_11072_CS";
-	var clName = CHANGEDPREFIX + "_11072_CL";
-	
-	commDropCS(db, csName, true, "drop cs in the begin");
-	var cl = commCreateCL( db, csName, clName, null, null, true, false, "create cl in the begin" );
-	
-	//insert data 
-	cl.insert({a:[1,2,3]});
-	
-	var findCondition = {a:{$gte:[1,2,3]}};
-	var expRecs = [{"a":[1,2,3]}];
-	checkResult( cl, findCondition, null, expRecs, {_id:1} );
+    var csName = CHANGEDPREFIX + "_11072_CS";
+    var clName = CHANGEDPREFIX + "_11072_CL";
+
+    commDropCS(db, csName, true, "drop cs in the begin");
+    var cl = commCreateCL( db, csName, clName, null, null, true, false, "create cl in the begin" );
+
+    //insert data 
+    cl.insert({a:[1,2,3]});
+
+    var findCondition = {a:{$gte:[1,2,3]}};
+    var expRecs = [{"a":[1,2,3]}];
+    checkResult( cl, findCondition, null, expRecs, {_id:1} );
 }
 main()
