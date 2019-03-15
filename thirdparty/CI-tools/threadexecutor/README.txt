e06ab0f9cda73c01b32453991b291af0b97e9450
linyoubin <linyoubin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-15 02:13:24 +0000

    上传ThreadExecutor线程调度代码
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39830 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ thirdparty/CI-tools/threadexecutor/README.txt
@@ -0,0 +1,2 @@
+code：源码目录及测试样例
+example：testng测试代码，只是代码。要运行请直接加到测试现有sequoiadb-testcases-story-java工程中，并修改相应的testng.xml
---
+++ thirdparty/CI-tools/threadexecutor/code/src/main/java/com/sequoiadb/threadexecutor/WorkingContext.java
@@ -0,0 +1,70 @@
+package com.sequoiadb.threadexecutor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.sequoiadb.threadexecutor.annotation.parser.MethodInfo;
+import com.sequoiadb.threadexecutor.exception.SchException;
+
+class WorkingContext {
+    private Lock lock = new ReentrantLock();
+    private Set<MethodInfo> expectFinishMethods = new HashSet<>();
+    private Set<MethodInfo> expectBlockingMethods = new HashSet<>();
+    private Set<Worker> notifyWorkers = new HashSet<>();
+    private int step;
+    private ThreadExecutor es;
+
+    WorkingContext(ThreadExecutor es, int step) {
+        this.es = es;
+        this.step = step;
+    }
+
+    public void addExpectFinishMethod(MethodInfo m) {
+        expectFinishMethods.add(m);
+    }
+
+    public Set<MethodInfo> getExpectFinishMethod() {
+        return expectFinishMethods;
+    }
+
+    public void addNeedNotifyWorker(Worker w) {
+        notifyWorkers.add(w);
+    }
+
+    public Set<Worker> getNeedNotifyWorkers() {
+        return notifyWorkers;
+    }
+
+    public void addExpectBlockingMethd(MethodInfo m) {
+        expectBlockingMethods.add(m);
+    }
+
+    public Set<MethodInfo> getExpectBlockingMethd() {
+        return expectBlockingMethods;
+    }
+
+    public void finishStep(int step, MethodInfo m) throws SchException {
+        lock.lock();
+        try {
+            if (this.step != step) {
+                throw new SchException("method should be stop in step:" + step
+                        + ",while stop in step:" + this.step + ",method=" + m);
+            }
+
+            if (!expectFinishMethods.contains(m)) {
+                throw new SchException("running step:" + this.step + " method is mismatch=" + m);
+            }
+
+            expectFinishMethods.remove(m);
+
+            if (expectFinishMethods.size() == 0) {
+                es.notifySchedule();
+            }
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+}
