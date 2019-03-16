af92e02e9fd2d0460f4fbbb32a1d01df458715c6
linyoubin <linyoubin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-16 08:06:25 +0000

    解决word由于表格过多无法生成的问题。
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39851 6121f079-4a18-41d3-8c30-8870a322f2c2

--- doc/tools/wordConvertor/src/com/sequoiadb/wordConvertor/WordConvertor.java
+++ doc/tools/wordConvertor/src/com/sequoiadb/wordConvertor/WordConvertor.java
@@ -11,154 +11,213 @@ public class WordConvertor {
     private static final int max_word_picture_width = 420;
     private static Logger logger = Logger.getLogger(WordConvertor.class);
 
-    public WordConvertor(){
+    public WordConvertor() {
     }
-    
-    public void htmlToWord(String htmlFile, String outWordFile){
+
+    public void htmlToWord(String htmlFile, String outWordFile) throws Exception {
         ActiveXComponent app = new ActiveXComponent("Word.Application");
-        try{
+        try {
             app.setProperty("Visible", new Variant(false));
-            
+
             Dispatch documents = app.getProperty("Documents").toDispatch();
-            Dispatch doc = Dispatch.invoke(documents, "Open", Dispatch.Method, 
-                    new Object[]{htmlFile, new Variant(false), new Variant(true)}, 
-                    new int[1]).toDispatch();
-            
-            Dispatch.invoke(doc, "SaveAs", Dispatch.Method, new Object[]{
-                    outWordFile, new Variant(1) }, new int[1]);
-            
+            Dispatch doc = Dispatch.invoke(documents, "Open", Dispatch.Method,
+                    new Object[] { htmlFile, new Variant(false), new Variant(true) }, new int[1])
+                    .toDispatch();
+
+            Dispatch.invoke(doc, "SaveAs", Dispatch.Method,
+                    new Object[] { outWordFile, new Variant(1) }, new int[1]);
+
             Dispatch.call(doc, "Close", new Variant(0));
             //Dispatch.call(documents, "Close", new Variant(0));
         }
-        catch(Exception e){
+        catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
-        finally{
-            try{
-                app.invoke("Quit", new Variant[]{});
+        finally {
+            try {
+                app.invoke("Quit", new Variant[] {});
             }
-            catch(Exception e){
+            catch (Exception e) {
                 e.printStackTrace();
                 throw e;
             }
-            finally{
+            finally {
                 ComThread.Release();
             }
         }
     }
-    
-    private void setHeaderFooter(ActiveXComponent app, Dispatch doc, String headerText){
+
+    private void setHeaderFooter(ActiveXComponent app, Dispatch doc, String headerText) {
         //move selection to the begin of the document
         Dispatch selection = app.getProperty("Selection").toDispatch();
         Dispatch.call(selection, "HomeKey", new Variant(6));
-    
+
         Dispatch activeWindow = app.getProperty("ActiveWindow").toDispatch();
         Dispatch activePane = Dispatch.get(activeWindow, "ActivePane").toDispatch();
         Dispatch view = Dispatch.get(activePane, "View").toDispatch();
-        
+
         Dispatch.put(view, "SeekView", new Variant("9")); //header
         Dispatch.put(selection, "Text", new Variant(headerText));
-        
+
         Dispatch.put(view, "SeekView", new Variant("10")); //footer
-        
+
         final Dispatch sections = Dispatch.get(doc, "Sections").toDispatch();
         final Dispatch item = Dispatch.call(sections, "Item", new Variant(1)).toDispatch();
         final Dispatch footer = Dispatch.get(item, "Footers").toDispatch();
         final Dispatch f1 = Dispatch.call(footer, "Item", new Variant(1)).toDispatch();
         final Dispatch range = Dispatch.get(f1, "Range").toDispatch();
         final Dispatch fields = Dispatch.get(range, "Fields").toDispatch();
-        
+
         Dispatch paragraphFormat = Dispatch.get(selection, "ParagraphFormat").getDispatch();
         Dispatch.put(paragraphFormat, "Alignment", 1);
         //the page number
-        Dispatch.call(fields, "Add", Dispatch.get(selection, "Range").toDispatch(), 
-                new Variant(-1), "Page", true).toDispatch();
+        Dispatch.call(fields, "Add", Dispatch.get(selection, "Range").toDispatch(), new Variant(-1),
+                "Page", true).toDispatch();
         Dispatch.call(selection, "TypeText", "/");
         //total page numbers
-        Dispatch.call(fields, "Add", Dispatch.get(selection, "Range").toDispatch(), 
-                new Variant(-1), "NumPages", true).toDispatch();
+        Dispatch.call(fields, "Add", Dispatch.get(selection, "Range").toDispatch(), new Variant(-1),
+                "NumPages", true).toDispatch();
     }
-    
-    private int getScale(int width, int height){
+
+    private int getScale(int width, int height) {
         int scale = 100;
-        if(width > max_word_picture_width){
-            scale = (max_word_picture_width * 100 )/width;
+        if (width > max_word_picture_width) {
+            scale = (max_word_picture_width * 100) / width;
         }
-        
+
         return scale;
     }
-    
+
     private void adjustPicture(Dispatch doc) {
         Dispatch shapes = Dispatch.get(doc, "InLineShapes").toDispatch();
         int count = Dispatch.get(shapes, "Count").toInt();
         logger.debug("pictures count=" + count);
-        
-        for(int i = 1; i <= count; i++){
+
+        for (int i = 1; i <= count; i++) {
             Dispatch shape = Dispatch.call(shapes, "Item", new Variant(i)).toDispatch();
             Dispatch.call(shape, "Select");
             int width = Dispatch.get(shape, "Width").toInt();
             int height = Dispatch.get(shape, "Height").toInt();
             int type = Dispatch.get(shape, "Type").toInt();
-            
-            
-            if( type == 4 || type == 3){
+
+            if (type == 4 || type == 3) {
                 // 4 Linked picture. 
                 // 3 Picture. 
                 int scale = getScale(width, height);
-                logger.debug("picture[" + i + "]:width=" + width + ",height=" + height
-                    + ",scale=" + scale + ",type=" + type);
-                if (100!=scale){
+                logger.debug("picture[" + i + "]:width=" + width + ",height=" + height + ",scale="
+                        + scale + ",type=" + type);
+                if (100 != scale) {
                     Dispatch.put(shape, "ScaleWidth", new Variant(scale));
                     Dispatch.put(shape, "ScaleHeight", new Variant(scale));
                 }
             }
         }
     }
-    
-    private void adjustTables(Dispatch doc){
+
+    private void adjustTables(Dispatch doc) {
         Dispatch tables = Dispatch.get(doc, "Tables").toDispatch();
         int count = Dispatch.get(tables, "Count").toInt();
         logger.debug("tables count=" + count);
-        
-        for(int i = 1; i <= count; i++){
-            Dispatch table = Dispatch.call(tables, "Item", new Variant(i)).toDispatch();
-            Dispatch.call(table, "Select");
-            Dispatch.put(table, "ApplyStyleFirstColumn", new Variant(true));
-            Dispatch borders = Dispatch.get(table, "Borders").toDispatch();
-            Dispatch.put(borders, "InsideLineStyle", new Variant(1));
-            logger.debug("adjust table[" + i + "]");
+
+        for (int i = 1; i <= count; i++) {
+            try {
+                Dispatch table = Dispatch.call(tables, "Item", new Variant(i)).toDispatch();
+                Dispatch.call(table, "Select");
+                Dispatch.put(table, "ApplyStyleFirstColumn", new Variant(true));
+                Dispatch borders = Dispatch.get(table, "Borders").toDispatch();
+                Dispatch.put(borders, "InsideLineStyle", new Variant(1));
+                logger.debug("adjust table[" + i + "]");
+            }
+            catch (Exception e) {
+                logger.error("adjust table failed:i=" + i, e);
+            }
         }
     }
-    
-    private void addTablesOfContent(ActiveXComponent app){
+
+    private void addTablesOfContent(ActiveXComponent app) {
         //选定插入点
         Dispatch selection = app.getProperty("Selection").toDispatch();
         Dispatch.call(selection, "HomeKey", new Variant(6));
         Dispatch range = Dispatch.get(selection, "Range").toDispatch();
-        
+
         Dispatch activeDocument = app.getProperty("ActiveDocument").toDispatch();
-        
-        Dispatch tableOfContent = Dispatch.get(activeDocument, 
-                "TablesOfContents").toDispatch();
-        
+
+        Dispatch tableOfContent = Dispatch.get(activeDocument, "TablesOfContents").toDispatch();
+
         Dispatch.put(tableOfContent, "Format", new Variant(0));
-        Dispatch.call(tableOfContent, "Add", range, new Variant(true),
-                new Variant(1), new Variant(3));
+        Dispatch.call(tableOfContent, "Add", range, new Variant(true), new Variant(1),
+                new Variant(3));
     }
-    
-    public static void main(String args[]) throws Exception{
+
+    //    public void printTabContent(String wordFile) throws Exception {
+    //        ActiveXComponent app = new ActiveXComponent("Word.Application");
+    //        Dispatch doc = null;
+    //        try {
+    //            //app.setProperty("Visible", new Variant(false));
+    //            Dispatch documents = app.getProperty("Documents").toDispatch();
+    //
+    //            //open the document
+    //            doc = Dispatch.call(documents, "Open", wordFile).toDispatch();
+    //            Dispatch tables = Dispatch.get(doc, "Tables").toDispatch();
+    //            int count = Dispatch.get(tables, "Count").toInt();
+    //            logger.debug("tables count=" + count);
+    //            for (int k = 458; k < 460; k++) {
+    //                Dispatch table = Dispatch.call(tables, "Item", new Variant(k)).toDispatch();
+    //                int rows = Dispatch.get(Dispatch.call(table, "Rows").toDispatch(), "Count").toInt();
+    //                int columns = Dispatch.get(Dispatch.call(table, "Columns").toDispatch(), "Count")
+    //                        .toInt();
+    //                logger.info("table:" + k);
+    //                for (int j = 1; j < columns; j++) {
+    //                    for (int i = 1; i < rows; i++) {
+    //
+    //                        Dispatch cell = Dispatch.call(table, "Cell", new Variant(i), new Variant(j))
+    //                                .toDispatch();
+    //                        Dispatch range = Dispatch.get(cell, "Range").toDispatch();
+    //                        String t = Dispatch.get(range, "Text").toString();
+    //                        System.out.print(new String(t.getBytes("utf-8")) + "\t");
+    //                    }
+    //                    System.out.println();
+    //                }
+    //            }
+    //        }
+    //        catch (Exception e) {
+    //            e.printStackTrace();
+    //            throw e;
+    //        }
+    //        finally {
+    //            try {
+    //                if (null != doc) {
+    //                    Dispatch.call(doc, "Save");
+    //                    Dispatch.call(doc, "Close", new Variant(0));
+    //                }
+    //
+    //                app.invoke("Quit", new Variant[] {});
+    //            }
+    //            catch (Exception e) {
+    //                e.printStackTrace();
+    //                throw e;
+    //            }
+    //            finally {
+    //                ComThread.Release();
+    //            }
+    //        }
+    //    }
+
+    public static void main(String args[]) throws Exception {
         ConvertHelper helper = new ConvertHelper();
         int result = helper.parseArgs(args);
-        if(0 != result){
+        if (0 != result) {
             throw new Exception("param error");
         }
-        
+
         String input = helper.getInput();
         String output = helper.getOutput();
         Boolean hasTableOfContent = helper.getHasTableOfContent();
-        
+
         WordConvertor wc = new WordConvertor();
+        //wc.printTabContent(output);
+        //        
         logger.info("start to convert to doc");
         wc.htmlToWord(input, output);
         logger.info("start to adjust");
@@ -166,63 +225,58 @@ public class WordConvertor {
         logger.info("finish");
     }
 
-    private Dispatch saveAndReopen(Dispatch documents, Dispatch doc, 
-            String wordFile){
+    private Dispatch saveAndReopen(Dispatch documents, Dispatch doc, String wordFile) {
         Dispatch.call(doc, "Save");
         Dispatch.call(doc, "Close", new Variant(0));
 
         doc = Dispatch.call(documents, "Open", wordFile).toDispatch();
         return doc;
     }
-    
-    public void adjust(String wordFile, Boolean hasTableOfContent) {
+
+    public void adjust(String wordFile, Boolean hasTableOfContent) throws Exception {
         ActiveXComponent app = new ActiveXComponent("Word.Application");
         Dispatch doc = null;
-        try{
+        try {
             app.setProperty("Visible", new Variant(false));
             Dispatch documents = app.getProperty("Documents").toDispatch();
-            
+
             //open the document
             doc = Dispatch.call(documents, "Open", wordFile).toDispatch();
-            
-            if(hasTableOfContent){
+
+            if (hasTableOfContent) {
                 logger.info("start to add table of content");
                 addTablesOfContent(app);
             }
-            
+
             logger.info("start to add header and footer");
             setHeaderFooter(app, doc, "SequoiaDB用户使用手册");
             //save and reopen the doc. otherwise, setHeaderFooter will take no effect.
             doc = saveAndReopen(documents, doc, wordFile);
-            
+
             //in default pictures are too big to display in the word.
             //adjust picture to a appropriate size
-            
             logger.info("start to adjust pictures");
             adjustPicture(doc);
             doc = saveAndReopen(documents, doc, wordFile);
-            
-            logger.info("start to adjust tables");
-            adjustTables(doc);
         }
-        catch(Exception e){
+        catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
-        finally{
-            try{
-                if(null != doc){
+        finally {
+            try {
+                if (null != doc) {
                     Dispatch.call(doc, "Save");
                     Dispatch.call(doc, "Close", new Variant(0));
                 }
-                
-                app.invoke("Quit", new Variant[]{});
+
+                app.invoke("Quit", new Variant[] {});
             }
-            catch(Exception e){
+            catch (Exception e) {
                 e.printStackTrace();
                 throw e;
             }
-            finally{
+            finally {
                 ComThread.Release();
             }
         }
