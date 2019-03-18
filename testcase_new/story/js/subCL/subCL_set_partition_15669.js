961402bff1a956dc073cf31a367ffff0fd75a8af
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-18 08:15:08 +0000

    代码检视意见，请修改
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39881 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/subCL/subCL_set_partition_15669.js
+++ testcase_new/story/js/subCL/subCL_set_partition_15669.js
@@ -6,9 +6,9 @@
 **************************************/
 function main()
 {
-    var csName = CHANGEDPREFIX + "_cs15669" ;
-    var mainCL_Name = CHANGEDPREFIX + "_maincl15669" ;
-    var subCL_Name = CHANGEDPREFIX + "_subcl15669";
+    var csName = CHANGEDPREFIX + "_cs15669" ;//TODO: "cs15669"，不需要加CHANGEDPREFIX，这个是之前不好的代码习惯，不用参考
+    var mainCL_Name = CHANGEDPREFIX + "_maincl15669" ;//TODO: 同上
+    var subCL_Name = CHANGEDPREFIX + "_subcl15669";//TODO: 同上
     var domainName = "domain15669";
     
     if(true == commIsStandalone( db ))
@@ -20,14 +20,14 @@ function main()
     //创建多个组
     var groupsArray = commGetGroups(db, false, "", false, true, true );
     var hostName = groupsArray[1][1].HostName;
-    var dataGroupNames = createDataGroups(db, hostName, 10);
+    var dataGroupNames = createDataGroups(db, hostName, 10);//TODO: 只在3组3节点测，新增6个组，每组一个节点
     
-    println("start create domain.");
-    //创建Domain
+    println("start create domain.");//TODO: 打印信息很奇怪，测试点没有打印信息创建domain有，建议此处删除，在创建主子表打印
+    //创建Domain   //TODO: 方法名很明显的可以不用写批注
     db.createDomain(domainName,dataGroupNames,{AutoSplit:true});
-    //创建cs
+    //创建cs      //TODO：同上，批注去掉
     commCreateCS(db, csName, true, "", {Domain:domainName});
-    //创建主表
+    //创建主表    //TODO: 创建主子表放到一个另一个方法里面，main里面代码尽量简洁
     var mainCLOption = { ShardingKey:{"a":1}, ShardingType:"range", IsMainCL:true};
     var dbcl = commCreateCLByOption( db, csName, mainCL_Name, mainCLOption, true, true);
     
@@ -39,9 +39,12 @@ function main()
         throw buildException("createCL()",null,"create collection should fail", "createCL failed", "createCL success");
     }catch( e )
     {
-        if( e!=-6 )
+        if( e!=-6 )//TODO: 比较值都用3个等号，"==="或"!=="
         {
             throw buildException("createCL()",e ,"create collection should fail", '-6', e );
+            /* TODO: 括号/逗号前后空格统一，逗号后需要有空格，另外变量前后空格
+               TODO: buildException( 函数名（此处为main，在其他方法的话则使用实际对应的方法名）,...,
+                     操作（如create subCL）, ... )，上面信息跟方法对不上 */
         }
     }
     
@@ -74,4 +77,4 @@ function removeDataRG( db, dataGroupNames )
         db.removeRG(dataGroupNames[i]);
     }
 }
-main();
+main();   //TODO: 写到最前面
