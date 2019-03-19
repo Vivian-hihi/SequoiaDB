3b2b03ad2d328d3b695794f13d88f2d0e45c900a
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-19 07:08:44 +0000

    新增，用例补充自动化
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39910 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/jsObject/fileScp_13658.js
@@ -0,0 +1,27 @@
+﻿/******************************************************************************
+*@Description : seqDB-13658:scp命令结束后，File.scp内部显示调用close关闭File对象 
+*@Author      : 2019-3-19  XiaoNi Huang
+******************************************************************************/
+main();
+
+function main()
+{  
+	println("\n---Begin to run test");
+	var installDir = toolGetSequoiadbDir( COORDHOSTNAME, CMSVCNAME );
+	var srcFile = installDir[0] +'/bin/sdblist'; 
+	var dstFile = WORKDIR + '/sdblist'; 
+	var cmd = new Cmd();
+	
+	println("\n---Begin to exec File.scp");
+	// expect success, if it fails, it will throw "~bash: ./sdblist: Text file busy"
+	File.scp( srcFile, dstFile );
+	// check results
+	var rc = cmd.run( dstFile +" -t all" );
+	var rcObj = rc.split('\n');
+	var actRcCode1 = rcObj[0].indexOf("sequoiadb(");
+	var actRcCode2 = rcObj[0].indexOf("sdb");
+	if( actRcCode1 !== 0 && actRcCode2 !== 0 )
+	{
+	   throw buildException( "main", null, "", "0", "  " + actRcCode1 && actRcCode2 );
+	}
+}
