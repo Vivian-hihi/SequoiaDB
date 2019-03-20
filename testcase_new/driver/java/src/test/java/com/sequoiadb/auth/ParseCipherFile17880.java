4aa3afdd0daa3036013ee882c2ccc552bfdb12a8
fanyu <fanyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-20 09:04:02 +0000

    根据检视意见修改用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@39945 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/java/src/test/java/com/sequoiadb/auth/ParseCipherFile17880.java
+++ testcase_new/driver/java/src/test/java/com/sequoiadb/auth/ParseCipherFile17880.java
@@ -2,6 +2,7 @@ package com.sequoiadb.auth;
 
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.CommLib;
 import com.sequoiadb.testcommon.SdbTestBase;
 import com.sequoiadb.util.SdbDecrypt;
 import com.sequoiadb.util.SdbDecryptUserInfo;
@@ -27,46 +28,25 @@ public class ParseCipherFile17880 extends SdbTestBase {
     private String[] passwords = {/*"user@17880","user:17880",*/"  user 17880 ", "测试17880", "token17880A", "token17880B", "token17880C"};
     private String passwdFileName = "/password17880";
     private String passwordFilePath;
+    private String[] tokens = {getRandomString(257)," ","测试"};
 
     @BeforeClass(alwaysRun = true)
     private void setUp() throws Exception {
         sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-        //TODO:1、请使用CommLib中的公共方法
-        if (!Util.isCluster(sdb)) {
+        if (CommLib.isStandAlone(sdb)) {
             throw new SkipException("skip StandAlone");
         }
+        cleanEnvAndPrepare();
     }
 
     @DataProvider(name = "range-provider")
     public Object[][] generateRangData() throws Exception {
-    	//TODO:2、removeUser的方法不要放在数据生成器中，下面方法中不是数据生成器的参数都需要提取出来。
-        for (int i = 0; i < usernames.length; i++) {
-            try {
-                sdb.removeUser(usernames[i], passwords[i]);
-            } catch (BaseException e) {
-                if (e.getErrorCode() != -300) {
-                    throw e;
-                }
-            }
-            sdb.createUser(usernames[i], passwords[i]);
-        }
-        Util.createPasswdFile(usernames[0], passwords[0], passwdFileName);
-        Util.createPasswdFile(usernames[1], passwords[1], passwdFileName);
-        String token1 = getRandomString(257);
-        String token2 = " ";
-        String token3 = "测试";
-        Util.createPasswdFile(usernames[2], passwords[2], passwdFileName, token1);
-        Util.createPasswdFile(usernames[3], passwords[3], passwdFileName, token2);
-        Util.createPasswdFile(usernames[4], passwords[4], passwdFileName, token3);
-        String toolsPath = Util.getSdbInstallDir() + "/bin/";
-        Util.downLoadFileToLocal(SdbTestBase.workDir, toolsPath + passwdFileName);
-        passwordFilePath = SdbTestBase.workDir + passwdFileName;
         return new Object[][]{
                 {usernames[0], passwords[0], null},
                 {usernames[1], passwords[1], null},
-                {usernames[2], passwords[2], token1},
-                {usernames[3], passwords[3], token2},
-                {usernames[4], passwords[4], token3}
+                {usernames[2], passwords[2], tokens[0]},
+                {usernames[3], passwords[3], tokens[1]},
+                {usernames[4], passwords[4], tokens[2]}
         };
     }
 
@@ -100,8 +80,8 @@ public class ParseCipherFile17880 extends SdbTestBase {
         //check
         Assert.assertEquals(info.getUserName(), username, info.toString());
         Assert.assertEquals(info.getPasswd(), password);
-        //TODO:1、验证用户密码可用，建议连接db后执行一个基本操作
         Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, info.getUserName(), info.getPasswd());
+        Assert.assertFalse(db.isCollectionSpaceExist("ParseCipherFile17880"));
         db.disconnect();
     }
 
@@ -124,6 +104,27 @@ public class ParseCipherFile17880 extends SdbTestBase {
                 sdb.disconnect();
             }
         }
+    }
+
+    private void cleanEnvAndPrepare() throws Exception {
+        for (int i = 0; i < usernames.length; i++) {
+            try {
+                sdb.removeUser(usernames[i], passwords[i]);
+            } catch (BaseException e) {
+                if (e.getErrorCode() != -300) {
+                    throw e;
+                }
+            }
+            sdb.createUser(usernames[i], passwords[i]);
+        }
+        Util.createPasswdFile(usernames[0], passwords[0], passwdFileName);
+        Util.createPasswdFile(usernames[1], passwords[1], passwdFileName);
+        Util.createPasswdFile(usernames[2], passwords[2], passwdFileName, tokens[0]);
+        Util.createPasswdFile(usernames[3], passwords[3], passwdFileName, tokens[1]);
+        Util.createPasswdFile(usernames[4], passwords[4], passwdFileName, tokens[2]);
+        String toolsPath = Util.getSdbInstallDir() + "/bin/";
+        Util.downLoadFileToLocal(SdbTestBase.workDir, toolsPath + passwdFileName);
+        passwordFilePath = SdbTestBase.workDir + passwdFileName;
     }
 
     private String getRandomString(int length) {
