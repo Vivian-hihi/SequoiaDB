ae0b0830e4ecf14903e9c2d6e5aad3d4b8696474
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-29 05:43:33 +0000

    新增索引键长度限制测试用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40116 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/index/IndexUtils.java
@@ -0,0 +1,76 @@
+package com.sequoiadb.index;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.testng.Assert;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+
+/**
+* FileName: IndexUtils.java
+* public call function for test index
+* @author wuyan
+    * @Date    2019.3.27
+* @version 1.00
+*/
+public class IndexUtils {
+	
+	public static DBCollection createCSAndCL(Sequoiadb sdb,String csName,String clName,int pagesize){
+		if (sdb.isCollectionSpaceExist(csName)){
+			sdb.dropCollectionSpace(csName);
+		}
+		
+		BSONObject options = new BasicBSONObject();
+		options.put("PageSize", pagesize);	
+		CollectionSpace cs = sdb.createCollectionSpace(csName, options);	
+		DBCollection dbcl = cs.createCollection(clName);
+		return dbcl;
+	}	
+	
+	public static ArrayList< BSONObject > insertData(DBCollection dbcl,int recordNum ,int length){		
+		ArrayList< BSONObject > insertRecord = new ArrayList< BSONObject >() ;		
+        for ( int i = 0; i < recordNum; i++ ) {  
+        	String keyValue = getRandomString(length);
+            BSONObject obj = new BasicBSONObject() ;
+            obj.put( "testa",  keyValue) ;            
+            obj.put( "no", i ) ;           
+            insertRecord.add( obj ) ;            
+        }
+        dbcl.insert( insertRecord ) ;
+		return insertRecord;        
+	}
+	
+	
+		
+	public static void checkRecords( DBCollection dbcl, List< BSONObject > expRecord, String matcher,String hint) {
+		DBCursor cursor = dbcl.query( matcher, "", "{'no':1}", hint );	        
+	    int count = 0 ;
+	    while ( cursor.hasNext() ) {
+	        BSONObject record = cursor.getNext();	        
+	        Assert.assertEquals( record, expRecord.get( count++ ) ) ;
+	    }
+	    cursor.close() ;
+	    Assert.assertEquals( count, expRecord.size() ) ;
+	}
+	
+	public static String getRandomString( int length){
+		String str = "ABCDEFGHIJKLMNOPQRATUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+		Random random = new Random();
+		StringBuffer sbBuffer = new StringBuffer();
+		for( int i = 0; i < length; i++ ){
+			int number = random.nextInt(62);
+			sbBuffer.append(str.charAt(number));
+		}
+		return sbBuffer.toString();		
+	}
+	
+	
+	
+}
