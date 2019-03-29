42d282477fe3b05be759d725e4699c509c13ee66
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-29 05:51:00 +0000

    新增索引键长度最大值用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40118 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/index/index_keyLenLimit_18049.js
@@ -0,0 +1,32 @@
+﻿/************************************************************************
+@Description : create index, Insert index key length is maximum,eg: pagesize is 4096,the maximum length of the index key is 1024
+@Modify list :
+               2019-3-27  wuyan
+************************************************************************/
+main(db);
+function main(db)
+{     
+   try
+   {   
+      var csName = CHANGEDPREFIX + "_indexcs18049";  
+      var clName = CHANGEDPREFIX + "_indexcl18049";  
+      commDropCS( db, csName, true, "clear cs in the beginning"  );
+      
+      var options = { PageSize:4096};
+      db.createCS( csName, options );
+      var dbcl = commCreateCL( db, csName, clName, 0, true, false );      
+      dbcl.createIndex("idxa",{'stra':-1},true);
+      
+      var maxLen = 1011;
+      var strValue = getRandomString(maxLen);
+      dbcl.insert({'stra' : strValue, 'no' : 1});
+      
+      checkResult(dbcl,{'stra' : strValue, 'no' : 1});     
+      
+      commDropCS( db, csName, true, "clear cs in the ending"  );;      
+   }
+   catch( e )
+   {
+      throw e ;
+   }   
+}
