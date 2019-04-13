9c3d8b52fa80c79017d37bb68593be6f997ec6e0
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-13 06:07:22 +0000

    SEQUOIADBMAINSTREAM-4358 提交setup脚本
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40392 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ tools/install/setup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+TYPE="ask"
+INSTALL_PG=0
+INSTALL_DB=0
+INSTALL_MYSQL=0
+
+function err_para()
+{
+   local para_name=$1
+   local para_value=$2
+   echo "setup: unrecognized $para_name \"$para_value\"" >&2
+   echo 'Try "setup --help" for more information.'
+   exit 64
+}
+
+function install()
+{
+   local name=$1
+   echo "--------------------------begin to install $name-------------------------"
+   for file_run in ./$name-*-installer.run;
+   do
+      echo "$file_run --mode text"
+      chmod u+x $file_run
+      $file_run --mode text
+      test $? -ne 0 && { echo "ERROR: Fail to $file_run --mode text" >&2 && exit 1; }
+   done
+   echo "----------------------------end install $name----------------------------"
+   echo
+}
+
+function ask_user()
+{
+   read -p "Install sequoiadb Y/N: " choice
+   [ -z $choice ] && choice="Y"
+   [[ "$choice" == "Y" || "$choice" == "y" ]] && install "sequoiadb"
+
+   while :
+   do
+      read -p "Install sequoiasql-postgresql or sequoiasql-mysql, represented by 1 and 2, default:1, 1/2: " select
+      [ -z $select ] && select=1
+      [[ "$select" == 1 || "$select" == 2 ]] && break
+   done
+   [[ "$select" == 1 ]] && install "sequoiasql-postgresql" || install "sequoiasql-mysql"
+}
+
+function build_help()
+{
+   echo ""
+   echo "Usage:"
+   echo "  --db         install sequoiadb"
+   echo "  --pg         install sequoiasql-postgresql"
+   echo "  --mysql      install sequoiasql-mysql"
+}
+
+#Parse command line parameters
+#test $# -eq 0 && { build_help && exit 64; }
+
+ARGS=`getopt -o h --long help,db,pg,mysql -n 'test' -- "$@"`
+ret=$?
+test $ret -ne 0 && exit $ret
+
+eval set -- "${ARGS}"
+
+while true
+do
+   case "$1" in
+      --db )           INSTALL_BD=1
+                       TYPE="sequoiadb"
+                       install $TYPE
+                       shift
+                       ;;
+      --pg )           INSTALL_PG=1
+                       TYPE="sequoiasql-postgresql"
+                       install $TYPE
+                       shift
+                       ;;
+      --mysql )        INSTALL_MYSQL=1
+                       TYPE="sequoiasql-mysql"
+                       install $TYPE
+                       shift
+                       ;;
+      -h | --help )    build_help
+                       exit 0
+                       ;;
+      --)              shift
+                       break
+                       ;;
+      *)               echo "Internal error!"
+                       exit 64
+                       ;;
+   esac
+done
+
+case "$TYPE" in
+   ask)           ask_user; shift 1;;
+   *)             shift 1;;
+esac
