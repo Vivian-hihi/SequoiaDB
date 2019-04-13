8d0252aa2fcf78f4690d768e4f27f0404a34e8d3
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-13 08:05:04 +0000

    SEQUOIADBMAINSTREAM-4355 提供quickDeploy.sh脚本，以方便用户快速部署sequoiadb/mysql/postgresql
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40399 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ tools/deploy/quickDeploy.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+function build_help()
+{
+   echo ""
+   echo "Usage:"
+   echo "  --db         deploy SequoiaDB"
+   echo "  --mysql      deploy SequoiaSQL-MySQL"
+   echo "  --pg         deploy SequoiaSQL-PostgreSQL"
+}
+
+DEPLOY_DB=0
+DEPLOY_MYSQL=0
+DEPLOY_PG=0
+HAS_PARAMETER=0
+
+#Parse command line parameters
+ARGS=`getopt -o h --long help,db,pg,mysql -n 'quickDeploy.sh' -- "$@"`
+ret=$?
+test $ret -ne 0 && exit $ret
+
+eval set -- "${ARGS}"
+
+while true
+do
+   case "$1" in
+      --db )           DEPLOY_DB=1
+                       HAS_PARAMETER=1
+                       shift
+                       ;;
+      --pg )           DEPLOY_PG=1
+                       HAS_PARAMETER=1
+                       shift
+                       ;;
+      --mysql )        DEPLOY_MYSQL=1
+                       HAS_PARAMETER=1
+                       shift
+                       ;;
+      -h | --help )    build_help
+                       exit 0
+                       ;;
+      --)              shift
+                       break
+                       ;;
+      *)               echo "Internal error!"
+                       exit 64
+                       ;;
+   esac
+done
+
+if [ $HAS_PARAMETER -eq 0 ]; then
+   DEPLOY_DB=1
+   DEPLOY_MYSQL=1
+   DEPLOY_PG=1
+fi
+
+##################################
+#    main entry
+##################################
+
+#get this script path
+myPath=`dirname $0`
+if [[ ${dir_name:0:1} != "/" ]]; then
+   myPath=$(pwd)/$myPath  #relative path
+else
+   myPath=$myPath         #absolute path
+fi
+
+#get sdb shell path
+sdbShellPath=$myPath/../../bin/sdb
+
+#get quickDeploy.js path
+jsFile=$myPath/quickDeploy.js
+
+#generate parameters
+jsParameter="'"
+
+if [ $DEPLOY_DB -eq 1 ]; then
+   jsParameter=${jsParameter}"var sdb=true;"
+fi
+
+if [ $DEPLOY_MYSQL -eq 1 ]; then
+   jsParameter=${jsParameter}"var mysql=true;"
+fi
+
+if [ $DEPLOY_PG -eq 1 ]; then
+   jsParameter=${jsParameter}"var pg=true;"
+fi
+
+jsParameter=${jsParameter}"'"
+
+#execute command
+command=$sdbShellPath" -f "$jsFile" -e "$jsParameter
+echo "Execute command: "$command
+eval $command
+
