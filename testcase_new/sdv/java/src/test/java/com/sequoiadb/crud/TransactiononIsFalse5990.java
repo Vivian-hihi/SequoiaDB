a9bd6b70fe36ec6e97a2156639c4702219ca35a9
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-16 00:58:52 +0000

    屏蔽独立模式
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40444 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/src/test/java/com/sequoiadb/crud/TransactiononIsFalse5990.java
+++ testcase_new/sdv/java/src/test/java/com/sequoiadb/crud/TransactiononIsFalse5990.java
@@ -3,12 +3,14 @@ package com.sequoiadb.crud;
 import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.crud.compress.concurrency.CompressUtils;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
 
@@ -28,6 +30,9 @@ public class TransactiononIsFalse5990 extends SdbTestBase {
 	@BeforeClass
 	private void setup() {
 		sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+		if (CompressUtils.isStandAlone(sdb)){
+            throw new SkipException("is standalone skip testcase");
+        }
 		cl = sdb.getCollectionSpace(SdbTestBase.csName).createCollection(clName);
 	}
 
