ebba50d861f81d526e434ff1969e8a94f057f268
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-17 00:29:10 +0000

    回归问题单SEQUOIADBMAINSTREAM-4274取消屏蔽
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40486 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/alter/Alter14996.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/alter/Alter14996.java
@@ -19,66 +19,69 @@ import com.sequoiadb.testcommon.SdbThreadBase;
  * @Date 2019年3月11日
  */
 public class Alter14996 extends SdbTestBase {
-    
+
     private Sequoiadb sdb = null;
     private String csName = "cs_14996";
     private String clName = "cl_14996";
-    
-    @BeforeClass(enabled = false)
-    public void setUp(){
+
+    @BeforeClass
+    public void setUp() {
         sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
         sdb.createCollectionSpace(csName, new BasicBSONObject("PageSize", 4096));
     }
-    
-    //TODO SEQUOIADBMAINSTREAM-4274
-    @Test(enabled = false)
-    public void test(){
+
+    @Test
+    public void test() {
         AlterCS alterCS = new AlterCS();
         CreateCL createCL = new CreateCL();
 
         alterCS.start();
         createCL.start();
-        
+
         boolean alterCSResult = alterCS.isSuccess();
         boolean createCLResult = createCL.isSuccess();
-        
-        if(alterCSResult&&createCLResult){
+
+        if (alterCSResult && createCLResult) {
             Assert.assertTrue(sdb.getCollectionSpace(csName).isCollectionExist(clName), "check cl exist");
-            DBCursor snapCur = sdb.getSnapshot(Sequoiadb.SDB_SNAP_COLLECTIONSPACES, new BasicBSONObject("Name", csName), null, null);
+            DBCursor snapCur = sdb.getSnapshot(Sequoiadb.SDB_SNAP_COLLECTIONSPACES, new BasicBSONObject("Name", csName),
+                    null, null);
             BSONObject csInfo = snapCur.getNext();
             Assert.assertEquals(csInfo.get("PageSize"), 16384, "check cs PageSize");
             snapCur.close();
-        }else if(alterCSResult&&!createCLResult){
+        } else if (alterCSResult && !createCLResult) {
             Assert.assertFalse(sdb.getCollectionSpace(csName).isCollectionExist(clName), "check cl exist");
             sdb.getCollectionSpace(csName).createCollection(clName);
-            DBCursor snapCur = sdb.getSnapshot(Sequoiadb.SDB_SNAP_COLLECTIONSPACES, new BasicBSONObject("Name", csName), null, null);
+            DBCursor snapCur = sdb.getSnapshot(Sequoiadb.SDB_SNAP_COLLECTIONSPACES, new BasicBSONObject("Name", csName),
+                    null, null);
             BSONObject csInfo = snapCur.getNext();
             Assert.assertEquals(csInfo.get("PageSize"), 16384, "check cs PageSize");
             snapCur.close();
-        }else if(!alterCSResult&&createCLResult){
+        } else if (!alterCSResult && createCLResult) {
             Assert.assertTrue(sdb.getCollectionSpace(csName).isCollectionExist(clName), "check cl exist");
-            DBCursor snapCur = sdb.getSnapshot(Sequoiadb.SDB_SNAP_COLLECTIONSPACES, new BasicBSONObject("Name", csName), null, null);
+            DBCursor snapCur = sdb.getSnapshot(Sequoiadb.SDB_SNAP_COLLECTIONSPACES, new BasicBSONObject("Name", csName),
+                    null, null);
             BSONObject csInfo = snapCur.getNext();
             Assert.assertEquals(csInfo.get("PageSize"), 4096, "check cs PageSize");
             snapCur.close();
-        }else{
-            Assert.fail("setAttr and createCL all failed, setAttr: "+ alterCS.getErrorMsg() +", createCL: " + createCL.getErrorMsg());
+        } else {
+            Assert.fail("setAttr and createCL all failed, setAttr: " + alterCS.getErrorMsg() + ", createCL: "
+                    + createCL.getErrorMsg());
         }
     }
-    
-    @AfterClass(enabled = false)
-    public void tearDown(){
+
+    @AfterClass
+    public void tearDown() {
         sdb.dropCollectionSpace(csName);
-        if(sdb != null){
+        if (sdb != null) {
             sdb.close();
         }
     }
-    
-    public class AlterCS extends SdbThreadBase{
+
+    public class AlterCS extends SdbThreadBase {
 
         @Override
         public void exec() throws Exception {
-            try(Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")){
+            try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
                 CollectionSpace cs = db.getCollectionSpace(csName);
                 BSONObject options = new BasicBSONObject();
                 options.put("PageSize", 16384);
@@ -86,16 +89,15 @@ public class Alter14996 extends SdbTestBase {
             }
         }
     }
-    
-    public class CreateCL extends SdbThreadBase{
-        
+
+    public class CreateCL extends SdbThreadBase {
+
         @Override
         public void exec() throws Exception {
-            try(Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")){
+            try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
                 CollectionSpace cs = db.getCollectionSpace(csName);
                 cs.createCollection(clName);
             }
         }
     }
 }
-
