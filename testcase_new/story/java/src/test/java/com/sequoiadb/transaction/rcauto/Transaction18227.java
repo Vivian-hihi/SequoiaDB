dc156f838a07f9b9c07a352a079efd9f51dd287a
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-17 09:29:44 +0000

    增加 rcauto 用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40520 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rcauto/Transaction18227.java
@@ -0,0 +1,102 @@
+package com.sequoiadb.transaction.rcauto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.CommLib;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.transaction.TransUtils;
+
+/**
+ * @testcase seqDB-18227: 批量插入操作失败自动回滚
+ * @date 2019-4-17
+ * @author yinzhen
+ *
+ */
+@Test(groups = "rcauto")
+public class Transaction18227 extends SdbTestBase {
+    private Sequoiadb sdb = null;
+    private String clName = "cl18227";
+    private DBCollection cl = null;
+    private List<String> groupNames;
+    private List<BSONObject> expList = new ArrayList<>();
+
+    @BeforeClass
+    public void setUp() {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        if (CommLib.isStandAlone(sdb)) {
+            throw new SkipException("STANDALONE MODE");
+        }
+        if (CommLib.OneGroupMode(sdb)) {
+            throw new SkipException("less than two groups");
+        }
+
+        groupNames = CommLib.getDataGroupNames(sdb);
+        cl = sdb.getCollectionSpace(csName).createCollection(clName, (BSONObject) JSON
+                .parse("{ShardingType:'range', ShardingKey:{b:1}, Group:'" + groupNames.get(0) + "'}"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        CollectionSpace cs = sdb.getCollectionSpace(csName);
+        if (cs.isCollectionExist(clName)) {
+            cs.dropCollection(clName);
+        }
+        if (!sdb.isClosed()) {
+            sdb.close();
+        }
+    }
+
+    @Test
+    public void test() {
+        // 集合使用分区表，插入的记录分布在多个组上
+        cl.split(groupNames.get(0), groupNames.get(1), (BSONObject) JSON.parse("{b:100}"),
+                (BSONObject) JSON.parse("{b:200}"));
+
+        // 在集合中创建正序的唯一索引，比如：a为唯一索引，插入一条包含索引字段的记录R1
+        cl.createIndex("idx18227", "{a:1, b:1}", true, false);
+        BSONObject record = (BSONObject) JSON.parse("{_id:1, a:1, b:1}");
+        cl.insert(record);
+        expList.add(record);
+
+        DBCursor cursor = cl.query("", "", "{a:1, b:1}", "");
+        List<BSONObject> actList = TransUtils.getReadActList(cursor);
+        Assert.assertEquals(actList, expList);
+
+        // 批量插入记录，过程中某条记录由于唯一索引键与R1冲突导致插入失败
+        try {
+            insertData();
+            Assert.fail("Auto Rollback Error");
+        } catch (BaseException e) {
+            Assert.assertEquals(e.getErrorCode(), -38);
+        }
+
+        cursor = cl.query("", "", "{a:1, b:1}", "");
+        actList = TransUtils.getReadActList(cursor);
+        Assert.assertEquals(actList, expList);
+    }
+
+    private void insertData() {
+        List<BSONObject> records = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            BSONObject record = (BSONObject) JSON.parse("{_id:" + i + ", a:" + i + ", b:" + i + "}");
+            records.add(record);
+        }
+        Collections.shuffle(records);
+        cl.insert(records);
+    }
+}
