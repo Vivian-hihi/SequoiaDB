a9b3925e8a2838856946901e165450c70c7e65ed
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-18 09:42:32 +0000

    SEQUOIADBMAINSTREAM-3863 将 rc 目录下的用例移至此目录，并修改
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40559 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/metadata/Transaction18211B.java
@@ -0,0 +1,134 @@
+package com.sequoiadb.transaction.metadata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.testcommon.CommLib;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbThreadBase;
+
+/**
+ * @testcase seqDB-18211:删除集合与事务操作并发
+ * @date 2019-4-11
+ * @author yinzhen
+ *
+ */
+@Test(groups = { "rc", "ru" })
+public class Transaction18211B extends SdbTestBase {
+    private Sequoiadb sdb = null;
+    private String clName = "cl18211B";
+    private DBCollection cl = null;
+    private List<String> groupNames;
+
+    @BeforeClass
+    public void setUp() {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        if (CommLib.isStandAlone(sdb)) {
+            throw new SkipException("STANDALONE MODE");
+        }
+        groupNames = CommLib.getDataGroupNames(sdb);
+        if (2 > groupNames.size()) {
+            throw new SkipException("groups less than 2");
+        }
+
+        cl = sdb.getCollectionSpace(csName).createCollection(clName, (BSONObject) JSON
+                .parse("{Group:'" + groupNames.get(0) + "', ShardingKey:{b:1}, ShardingType:'range'}"));
+        cl.createIndex("idx18211", "{a:1}", false, false);
+        insertDatas(cl, 0, 10000);
+        cl.split(groupNames.get(0), groupNames.get(1), 50);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        CollectionSpace cs = sdb.getCollectionSpace(csName);
+        if (cs.isCollectionExist(clName)) {
+            cs.dropCollection(clName);
+        }
+        if (!sdb.isClosed()) {
+            sdb.close();
+        }
+    }
+
+    @Test
+    public void test() {
+        // 开启并发事务
+        OperatorTh operatorTh = new OperatorTh();
+        operatorTh.start();
+
+        DropCLTh dropCLTh = new DropCLTh();
+        dropCLTh.start();
+
+        Assert.assertTrue(operatorTh.isSuccess(), operatorTh.getErrorMsg());
+        Assert.assertTrue(dropCLTh.isSuccess(), dropCLTh.getErrorMsg());
+    }
+
+    private class OperatorTh extends SdbThreadBase {
+        private Sequoiadb db;
+        private DBCollection cl;
+
+        private OperatorTh() {
+            db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+            cl = db.getCollectionSpace(csName).getCollection(clName);
+        }
+
+        @Override
+        public void exec() throws Exception {
+            try {
+                db.beginTransaction();
+                insertDatas(cl, 10000, 20000);
+                cl.delete("{$and:[{a:{$gte:0}},{a:{$lt:5000}}]}", "{'':'idx18211'}");
+                cl.update("{$and:[{a:{$gte:5000}},{a:{$lt:15000}}]}", "{$inc:{a:10}}", "{}'':'idx18211'");
+            } finally {
+                db.commit();
+                db.close();
+            }
+        }
+    }
+
+    // 在非事务内删除集合
+    private class DropCLTh extends SdbThreadBase {
+        private Sequoiadb db;
+
+        private DropCLTh() {
+            db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        }
+
+        @Override
+        public void exec() throws Exception {
+            try {
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        db.getCollectionSpace(csName).dropCollection(clName);
+                        break;
+                    } catch (BaseException e) {
+                        Assert.assertEquals(e.getErrorCode(), -190);
+                    }
+                }
+            } finally {
+                db.close();
+            }
+        }
+    }
+
+    private void insertDatas(DBCollection cl, int startId, int endId) {
+        List<BSONObject> records = new ArrayList<>();
+        for (int i = startId; i < endId; i++) {
+            records.add((BSONObject) JSON.parse("{_id:" + i + ", a:" + i + ", b:" + i + "}"));
+        }
+        cl.insert(records);
+    }
+}
+
