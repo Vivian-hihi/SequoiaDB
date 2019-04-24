25a23bd6f4c023a54faddb512192ac199f907339
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-04-24 00:09:36 +0000

    补充锁冲突时校验
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40670 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/sdv/java/src/test/java/com/sequoiadb/crud/Insert11425.java
+++ testcase_new/sdv/java/src/test/java/com/sequoiadb/crud/Insert11425.java
@@ -6,12 +6,15 @@ import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.testcommon.SdbTestBase;
 import com.sequoiadb.testcommon.SdbThreadBase;
+
+import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -20,54 +23,76 @@ import java.util.Random;
 public class Insert11425 extends SdbTestBase {
     private Sequoiadb db = null;
     private static final String CLNAME = Insert11425.class.getSimpleName();
-    private DBCollection dbcl;
-
+    private CollectionSpace cs = null;
+    
     @BeforeClass
     public void setup() {
         db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-        CollectionSpace cs = db.getCollectionSpace(SdbTestBase.csName);
-        dbcl = cs.createCollection(CLNAME);
-    }
-
-    @AfterClass
-    public void teardown() {
-        if (db != null) {
-            db.disconnect();
-        }
-    }
+        cs = db.getCollectionSpace(SdbTestBase.csName);
+        cs.createCollection(CLNAME);
+    }   
 
     /**
      * 1.多线程插入，同时删除cl
      */
     @Test
     public void test() throws InterruptedException {
-        SdbThreadBase insert = new SdbThreadBase() {
-            @Override
-            public void exec() throws Exception {
-                Sequoiadb db = null;
-                try {
-                    db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-                    DBCollection cl = db.getCollectionSpace(SdbTestBase.csName)
-                            .getCollection(CLNAME);
-                    for (int i = 0; i < 10000; i++) {
-                        cl.insert(new BasicBSONObject("b", i));
-                    }
-                } catch (BaseException e) {
-                    if (e.getErrorCode() != -23) throw e;
-                } finally {
-                    if (db != null)
-                        db.disconnect();
-                }
-            }
-        };
-
+    	InsertTask insert = new InsertTask();
         insert.start(20);
 
-        Thread.sleep(300 + new Random().nextInt(200));
-        CollectionSpace cs = db.getCollectionSpace(SdbTestBase.csName);
-        cs.dropCollection(CLNAME);
+        Thread.sleep(new Random().nextInt(1000));         
+        //drop cl fail is -147/-190, repeat drop cL again
+        int eachSleepTime = 1000;
+        int maxSleetTime = 60000;
+   	 	int alreadySleepTime = 0;
+   	 	int errorNo = 0;
+   	 	do{
+   	 		errorNo = dropCL( cs );   	 		
+   	 		Thread.sleep(eachSleepTime);
+   	 		alreadySleepTime += eachSleepTime;
+   	 		if( alreadySleepTime > maxSleetTime )
+   	 			Assert.fail("drop cl fail exceeds maximum waiting time:"+alreadySleepTime);
+   		 }while( errorNo == -147 || errorNo == -190);
 
         Assert.assertTrue(insert.isSuccess(), insert.getErrorMsg());
-        Assert.assertFalse(cs.isCollectionExist(CLNAME));
+        Assert.assertFalse(cs.isCollectionExist(CLNAME),"drop CL fail!e=" + errorNo);
     }
+    
+    @AfterClass
+    public void teardown() {
+        if (db != null) {
+            db.close();
+        }
+    }
+    
+    private int dropCL(CollectionSpace cs){
+   	 int errorNo = 0;
+   	 try{   	
+   		cs.dropCollection(CLNAME); 
+     }catch (BaseException e) {  
+       	 errorNo = e.getErrorCode();         	
+     }    	 
+   	 return errorNo;    	 
+   }
+    
+    private class InsertTask extends SdbThreadBase {   	
+		@Override
+        public void exec() throws Exception {            
+            try(Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {               
+                DBCollection cl = db.getCollectionSpace(SdbTestBase.csName).getCollection(CLNAME);                 
+                for ( int i = 0; i < 10000; i++){  	
+        			BSONObject obj = new BasicBSONObject();
+    				obj.put("a", i);         				
+    				obj.put("str", "test_" + String.valueOf(i));    		
+    				Date now = new Date();
+    				obj.put("date",now);        			
+        			cl.insert(obj);        			
+        		}		              
+            } catch (BaseException e) {             	
+                if( e.getErrorCode() != -23&& e.getErrorCode() != -248 && e.getErrorCode() != -190&& e.getErrorCode() != -147){
+                	Assert.fail("insert fail!! e=" + e.getErrorCode() );
+                }
+            }
+        }    
+    } 
 }
