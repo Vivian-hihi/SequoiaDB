75c011a6695f2c31e6adf1dc148497453778cde8
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-05-04 12:51:19 +0000

    回归问题单:SEQUOIADBMAINSTREAM-4407补充自动化
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@40823 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/configs/updateTransConf_18337A.js
@@ -0,0 +1,85 @@
+/******************************************************************************
+ * @Description : the connection takes effect immediately after the transaction configuration is updated
+                  seqDB-18337:事务中更改事务配置后连接立即生效可用
+ * @author      : luweikang
+ * @date        ：2019.05.04
+ ******************************************************************************/
+ 
+main();
+ 
+function main()
+{
+   if( commIsStandalone( db ) )
+   {
+      println(" run mode is standalone");
+      return ;
+   }
+   
+   var snaphostCur = db.snapshot(13, {"svcname": COORDSVCNAME.toString()});
+   //println(snaphostCur);
+   var obj = snaphostCur.next().toObj();
+   snaphostCur.close()
+   if(obj.transisolation != 0)
+   {
+      db.updateConf({"transisolation": 0}, {Global: false});
+   }
+   
+   var csName = "cs_18321";
+   var clName = "cl_18321";
+   var r1 = {"_id": 1, "a": 1};
+   var r2 = {"_id": 2, "a": 2}
+   
+   var db1 = new Sdb(COORDHOSTNAME, COORDSVCNAME);
+   var db2 = new Sdb(COORDHOSTNAME, COORDSVCNAME);
+   
+   var cl1 = db1.createCS(csName).createCL(clName);
+   var cl2 = db2.getCS(csName).getCL(clName);
+   
+   db1.transBegin();
+   db2.transBegin();
+   
+   cl1.insert(r1);
+   var cursor = cl2.find({"a": 1});
+   var record = cursor.next();
+   cursor.close();
+   
+   if(JSON.stringify(r1) != JSON.stringify(record.toObj()))
+   {
+      throw buildException( "", "", "check ru read record", JSON.stringify(r1), JSON.stringify(record.toObj()) ) ;
+   }
+   
+   db.updateConf({"transisolation": 1}, {Global: false});
+   
+   db1.transCommit();
+   db2.transCommit();
+   
+   db1.transBegin();
+   db2.transBegin();
+   
+   cl1.insert(r2);
+   var cursor = cl2.find({"a": 2});
+   if(cursor.next())
+   {
+      throw buildException( "", "", "check rc read record", "no record", cursor.current().toString() ) ;
+   }
+   cursor.close();
+   
+   db1.transCommit();
+   
+   var cursor = cl2.find({"a": 2});
+   var record = cursor.next();
+   cursor.close();
+   db2.transCommit();
+   if(JSON.stringify(r2) != JSON.stringify(record.toObj()))
+   {
+      throw buildException( "", "", "check rc read record", JSON.stringify(r2), JSON.stringify(record.toObj()) ) ;
+   }
+   
+   db.deleteConf({"transisolation": 1}, {Global: false});
+   
+   db1.close();
+   db2.close();
+   
+   db.dropCS(csName);
+   
+}
