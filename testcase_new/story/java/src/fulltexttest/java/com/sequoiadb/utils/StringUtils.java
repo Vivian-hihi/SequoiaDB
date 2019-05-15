4455425ad9932fe2c331569a529b6f88ebb6ec4f
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-05-15 09:11:46 +0000

    修改全文索引公共的string工具类，扩大公共字符串
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41014 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/fulltexttest/java/com/sequoiadb/utils/StringUtils.java
+++ testcase_new/story/java/src/fulltexttest/java/com/sequoiadb/utils/StringUtils.java
@@ -6,22 +6,23 @@ import java.util.Random;
  * 字符串的工具类
  */
 public class StringUtils {
-    
+
     private static String base = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()!";
     private static StringBuffer sb;
-    
+
     /**
-     * 加載類時構造一個公共的大字符串
+     * 加载类时构造一个公共的大字符串
      */
     static {
         sb = new StringBuffer();
-        for ( int i = 0; i < 100; i++ ) {
+        for ( int i = 0; i < 10000; i++ ) {
             sb.append( base );
-        } 
+        }
     }
 
     /**
      * 获取随机字符串
+     * 
      * @param length
      * @return String
      * @Author liuxiaoxuan
@@ -32,6 +33,7 @@ public class StringUtils {
         int end = start + length;
         StringBuffer newSB = new StringBuffer();
         newSB.append( sb );
-        return newSB.toString().substring( start, end );      
+        if(end >= newSB.length()) {  return  newSB.toString().substring( 0, length );  }
+        return newSB.toString().substring( start, end );
     }
 }
