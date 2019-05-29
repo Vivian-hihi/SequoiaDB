ffb49a3e229e314a9eb37ac741442043feec4076
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-05-29 06:46:23 +0000

    版本冲突，回退版本，重新优化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41305 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/java/src/test/java/com/sequoiadb/autoincrement/killnode/Insert15976.java
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/autoincrement/killnode/Insert15976.java
@@ -1,5 +1,4 @@
 package com.sequoiadb.autoincrement.killnode;
-<<<<<<< .mine
 /**
  * @FileName:seqDB-15976： CacheSize及AcquireSize均设置为1，不指定自增字段插入时coord主节点异常重启 
  * 预置条件：集合中已存在自增字段且CacheSize及AcquireSize均设置为1
@@ -9,9 +8,6 @@ package com.sequoiadb.autoincrement.killnode;
  * @Date 2018-11-19
  * @Version 1.00
  */
-=======
-
->>>>>>> .r41294
 import java.util.ArrayList;
 import java.util.List;
 import org.bson.BSONObject;
@@ -21,13 +17,13 @@ import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
 import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.commlib.GroupMgr;
 import com.sequoiadb.commlib.SdbTestBase;
+import com.sequoiadb.datasync.Utils;
 import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.exception.ReliabilityException;
 import com.sequoiadb.fault.KillNode;
@@ -35,54 +31,33 @@ import com.sequoiadb.task.FaultMakeTask;
 import com.sequoiadb.task.OperateTask;
 import com.sequoiadb.task.TaskMgr;
 
-/**
- * @FileName:seqDB-15976： CacheSize及AcquireSize均设置为1，不指定自增字段插入时coord主节点异常重启
- *                        预置条件：集合中已存在自增字段且CacheSize及AcquireSize均设置为1
- *                        测试步骤：1.不指定自增字段插入记录，同时coord异常 2.待节点正常后，不指定自增字段继续插入记录
- *                        预期结果：1.coord异常后，插入失败，错误信息正确
- *                        2.记录插入成功，自增字段值从起始到结束值唯一且递增，值正确，主备节点数据一致
- * @Author zhaoxiaoni
- * @Date 2018-11-19
- * @Version 1.00
- */
-
 public class Insert15976 extends SdbTestBase {
     private Sequoiadb sdb = null;
-<<<<<<< .mine
 	private GroupMgr groupMgr = null;
 	private String clName = "cl_15976";
 	private int cacheSize = 1;
-=======
-    private GroupMgr groupMgr = null;
-    private DBCollection cl = null;
-    private String clName = "cl_15976";
-    private int cacheSize = 1;
->>>>>>> .r41294
     private int acquireSize = 1;
     private List<Long> records = new ArrayList<Long>();
-<<<<<<< .mine
 	
 	@BeforeClass
 	public void setUP(){
-        	try {
-                sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-                sdb.getCollectionSpace(csName).createCollection(clName,(BSONObject) JSON.parse
-                        ("{AutoIncrement:{Field:'id',CacheSize:" + cacheSize + ",AcquireSize:"+acquireSize+"}}"));
-                groupMgr = GroupMgr.getInstance();
-                if (!groupMgr.checkBusiness()) {
-                    throw new SkipException("checkBusiness failed");
-                }
-            } catch (ReliabilityException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+	    sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        try{
+        	groupMgr = GroupMgr.getInstance();
+			if (!groupMgr.checkBusiness()) {
+	            throw new SkipException("checkBusiness failed");
+	        }
+			sdb.getCollectionSpace(csName).createCollection(clName,(BSONObject) JSON.parse("{AutoIncrement:{Field:'id',CacheSize:" + cacheSize + ",AcquireSize:"+acquireSize+"}}"));
+        }catch(ReliabilityException e){
+        	Assert.fail(this.getClass().getName() + " setUp error, error description:" + e.getMessage() + "\r\n"
+                    + Utils.getKeyStack(e, this));
+        }	
 	}
 	
 	@SuppressWarnings("resource")
     @Test
 	public void test(){
 		Sequoiadb db = null; 
-		DBCollection cl = null;
 		try {
 			db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
 			FaultMakeTask faultTask = KillNode.getFaultMakeTask(db.getHost(), String.valueOf(db.getPort()), 1);
@@ -96,9 +71,9 @@ public class Insert15976 extends SdbTestBase {
 	        if (!groupMgr.checkBusinessWithLSN(600)) {
 	        	Assert.fail("checkBusinessWithLSN() occurs timeout"); 
 	        }
-	        
+	     
 	        db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            cl = db.getCollectionSpace(csName).getCollection(clName);
+	        DBCollection cl = db.getCollectionSpace(csName).getCollection(clName);
 	        insertData(cl, 100);
             
 			//获取实际记录并按照id排序（由于驱动生成_id,coord生成自增字段值，因此无法通过_id排序来校验自增字段），则自增字段会从当前值严格递增
@@ -121,91 +96,23 @@ public class Insert15976 extends SdbTestBase {
 			if (db != null){
 				db.close();
 			}
-=======
-
-    @BeforeClass
-    public void setUP() {
-        try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
-            groupMgr = GroupMgr.getInstance();
-            if (!groupMgr.checkBusiness()) {
-                throw new SkipException("checkBusiness failed");
-            }
-            cl = db.getCollectionSpace(csName).createCollection(clName, (BSONObject) JSON
-                    .parse("{AutoIncrement:{Field:'id',CacheSize:" + cacheSize + ",AcquireSize:" + acquireSize + "}}"));
-        } catch (ReliabilityException e) {
-            Assert.fail(this.getClass().getName() + " setUp error, error description:" + e.getMessage() + "\r\n"
-                    + Utils.getKeyStack(e, this));
->>>>>>> .r41294
         }
-    }
-
-    @Test
-    public void test() {
-        Sequoiadb db = null;
-        try {
-            db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            FaultMakeTask faultTask = KillNode.getFaultMakeTask(db.getHost(), String.valueOf(db.getPort()), 1);
-            TaskMgr mgr = new TaskMgr();
-            InsertTask insertTask = new InsertTask();
-            mgr.addTask(insertTask);
-            mgr.addTask(faultTask);
-            mgr.execute();
-
-            // faultTask.start();
-            Assert.assertEquals(mgr.isAllSuccess(), true, mgr.getErrorMsg());
-            if (!groupMgr.checkBusinessWithLSN(600)) {
-                Assert.fail("checkBusinessWithLSN() occurs timeout");
-            }
-
-            db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            DBCollection cl = db.getCollectionSpace(csName).getCollection(clName);
-            insertData(cl, 100);
-
-            // 获取实际记录并按照id排序（由于驱动生成_id,coord生成自增字段值，因此无法通过_id排序来校验自增字段），则自增字段会从当前值严格递增
-            DBCursor cursor = cl.query("", "", "{id:1}", "");
-            while (cursor.hasNext()) {
-                BSONObject record = cursor.getNext();
-                records.add((long) record.get("id"));
-            }
-            for (int i = 0; i < records.size() - 1; i++) {
-                if (records.get(i) >= records.get(i + 1)) {
-                    Assert.fail("records error!");
-                }
-            }
-        } catch (ReliabilityException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            Assert.fail(e.getMessage());
-        } finally {
-            if (db != null) {
-                db.close();
-            }
-        }
-    }
-
-    private class InsertTask extends OperateTask {
+	}
+	
+	private class InsertTask extends OperateTask {
         @Override
         public void exec() throws Exception {
             Sequoiadb db = null;
-            try{
+            try {
                 db = new Sequoiadb(coordUrl, "", "");
                 CollectionSpace cs = db.getCollectionSpace(csName);
                 DBCollection cl = cs.getCollection(clName);
-<<<<<<< .mine
                 for(int i=0; i< 10000; i++){
         			BSONObject obj = (BSONObject) JSON.parse("{a:" + i + "}");
         			cl.insert(obj);
         		}
-            }catch (BaseException e){
-                e.printStackTrace();
-=======
-                for (int i = 0; i < 10000; i++) {
-                    BSONObject obj = (BSONObject) JSON.parse("{a:" + i + "}");
-                    cl.insert(obj);
-                }
-            } catch (BaseException e) {
-                e.printStackTrace();
->>>>>>> .r41294
+                }catch (BaseException e){
+                	e.printStackTrace();
             } finally {
                 if (db != null) {
                     db.close();
@@ -213,22 +120,20 @@ public class Insert15976 extends SdbTestBase {
             }
         }
     }
-
-    public void insertData(DBCollection cl, int insertNum) {
-        ArrayList<BSONObject> arrList = new ArrayList<BSONObject>();
-        for (int i = 0; i < insertNum; i++) {
-            BSONObject obj = (BSONObject) JSON.parse("{mustCheckAutoIncrement:" + i + "}");
-            arrList.add(obj);
-        }
-        cl.insert(arrList);
-    }
-
-    @AfterClass
+	
+	public void insertData(DBCollection cl, int insertNum){
+		ArrayList<BSONObject> arrList = new ArrayList<BSONObject>();
+		for(int i=0; i< insertNum; i++){
+			BSONObject obj = (BSONObject) JSON.parse("{mustCheckAutoIncrement:" + i + "}");
+			arrList.add(obj);
+		}
+		cl.insert(arrList);
+	}
+	
+	@AfterClass
     public void tearDown() {
-<<<<<<< .mine
-	    @SuppressWarnings("resource")
-        Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-	    CollectionSpace cs = db.getCollectionSpace(csName);
+	    Sequoiadb sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+	    CollectionSpace cs = sdb.getCollectionSpace(csName);;
         if(cs.isCollectionExist(clName)){
             cs.dropCollection(clName);
         }
@@ -236,10 +141,4 @@ public class Insert15976 extends SdbTestBase {
             sdb.close();
         }
 	}
-=======
-        Sequoiadb sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-        sdb.getCollectionSpace(csName).dropCollection(clName);
-        sdb.close();
-    }
->>>>>>> .r41294
 }
