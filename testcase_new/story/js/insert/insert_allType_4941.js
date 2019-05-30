7e85c989528b3107a93f84070afc9b972d10f183
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-05-30 09:28:05 +0000

    新增参数校验用例，删除insert中多余用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41333 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/insert/insert_allType_4941.js
@@ -0,0 +1,55 @@
+/******************************************************************************
+*@Description : seqDB-4941:所有数据类型边界值校验                    
+*@Author      : 2019-5-29  wuyan modify
+******************************************************************************/
+main();
+function main()
+{
+   var clName = "insert4941";
+   var cl = readyCL( clName );
+     
+   var expRecords = insertRecords( cl);     
+   var actRecords = cl.find(); 
+   checkRec( actRecords, expRecords );    
+   deleteRecordsAndCheckResult( cl );
+    
+   cleanCL( clName );   	
+}
+     
+
+function insertRecords( cl)
+{
+   println("---begin to insert records.");
+   
+   var values = [ -2147483648,2147483647, {"$numberLong":"-9223372036854775808"}, {"$numberLong":"9223372036854775807"},
+                  -1.7E+308, 1.7e+308, "","test_id",{a:1}, true, {"$date":"0000-01-01"},{"$date":"9999-12-31"},
+                  { "$timestamp" : "1902-01-01-00.00.00.000000" },{ "$timestamp" : "2037-12-31-23.59.59.999999" },
+                  { "$binary" : "aGVsbG8gd29ybGQ=", "$type" : "255" } ,{ "$regex" : "^张", "$options" : "i" } ,
+                  [ -2147483648, 0, "def" ],[], null, {"$minKey": 1 },{"$maxKey": 1 }];
+   
+   var docs = [];
+   for(var i = 0 ; i < values.length ; ++i )
+   {       
+      var fieldValue = values[i];
+      var objs = { "no": i, "fieldName":fieldValue};       
+      docs.push(objs);         
+   }    
+   cl.insert( docs );    
+   return docs;
+}
+
+function deleteRecordsAndCheckResult( cl )
+{
+   cl.remove();
+   
+   var actCount = cl.count();
+   var expCount = 0;
+   if ( Number(expCount) !== Number(actCount) )
+   {
+      throw buildException( "deleteRecords", null, "cl.count()", expCount, actCount );
+   }
+}
+
+
+
+
---
+++ testcase_new/story/js/insert/insert_illegalParameter_6710.js
@@ -0,0 +1,63 @@
+/******************************************************************************
+*@Description : seqDB-6710:插入binary类型$type非法                  
+*@Author      : 2019-5-29  wuyan modify
+******************************************************************************/
+main();
+function main()
+{
+     var clName = "insert6710";
+     var cl = readyCL( clName );
+     
+     //test $type is 256
+     insertWithTypeErrorA( cl ); 
+     //test $type is -1  
+     insertWithTypeErrorB( cl );
+     
+     cleanCL( clName );   	
+}
+
+function insertWithTypeErrorA( cl )
+{
+   println("---begin to insert binary with $type:256.");   
+   try
+   {
+      var binary = { "$binary" : "aGVsbG8gd29ybGQ=", "$type" : "256" };
+      cl.insert({ binary: binary });
+      throw "insert should be fail!";
+   }  
+   catch( e )   
+   {
+      if ( -6 !== e )
+      {
+         throw buildException( "insertRecords", e );
+      }
+   }
+   
+   var expCount = 0;
+   var count = cl.count();
+   if ( Number(expCount) !== Number(count) )
+   {
+      throw buildException( "insertRecords", "count vaule error", "count()", expCount, count );
+   }   
+}
+
+function insertWithTypeErrorB( cl)
+{
+   println("---begin to insert binary with $type:-1.");  
+   var binary = { "$binary" : "aGVsbG8gd29ybGQ=", "$type" : "-1" };   
+   cl.insert({ binary: binary });
+   
+   //小于最小值时，自动修正为最大值255
+   var expBinaryValue = { "$binary" : "aGVsbG8gd29ybGQ=", "$type" : "255" }; 
+   var expRecords = [];
+   expRecords.push({binary: expBinaryValue});
+  
+   var actRecords = cl.find( {}, { "_id": { "$include": 0 } } );    
+   checkRec( actRecords, expRecords );  
+}
+
+
+
+
+
+
