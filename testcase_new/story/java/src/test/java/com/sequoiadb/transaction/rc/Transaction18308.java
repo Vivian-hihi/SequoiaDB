3c2bba4d60136da6ecbe18e4e1b691e06f9d73cb
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-03 07:57:53 +0000

    JIRA 4390 问题单，新增用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41381 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rc/Transaction18308.java
@@ -0,0 +1,112 @@
+package com.sequoiadb.transaction.rc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.testcommon.SdbThreadBase;
+
+/**
+ * @testcase seqDB-18308:集合空间上存在事务操作，并发创建/删除集合空间
+ * @date 2019-4-25
+ * @author yinzhen
+ *
+ */
+
+@Test(groups = "rc")
+public class Transaction18308 extends SdbTestBase {
+    private String clName = "cl18308";
+    private CountDownLatch latch;
+
+    @BeforeClass
+    public void setUp() {
+        Sequoiadb sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        for (int i = 0; i < 10; i++) {
+            String csName = "cs18308_" + i;
+            if (sdb.isCollectionSpaceExist(csName)) {
+                sdb.dropCollectionSpace(csName);
+            }
+        }
+        if (sdb != null) {
+            sdb.close();
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+    }
+
+    @Test
+    public void test() {
+        latch = new CountDownLatch(10);
+
+        List<CreateAndDropCSTh> thList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            CreateAndDropCSTh th = new CreateAndDropCSTh("cs18308_" + i);
+            thList.add(th);
+            th.start();
+        }
+
+        for (CreateAndDropCSTh createAndDropCSTh : thList) {
+            Assert.assertTrue(createAndDropCSTh.isSuccess(), createAndDropCSTh.getErrorMsg());
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class CreateAndDropCSTh extends SdbThreadBase {
+        private String csName;
+
+        private CreateAndDropCSTh(String csName) {
+            this.csName = csName;
+        }
+
+        @Override
+        public void exec() throws Exception {
+            Sequoiadb db = null;
+            try {
+                db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+                int doTimes = 0;
+                while (true) {
+                    DBCollection cl = db.createCollectionSpace(csName).createCollection(clName);
+                    cl.createIndex("idx", "{a:1}", false, false);
+                    cl.insert((BSONObject) JSON.parse("{_id:1, a:1, b:1}"));
+
+                    // 开启事务，对该记录进行更新后删除，提交事务
+                    db.beginTransaction();
+                    cl.update("{a:1}", "{$set:{a:10}}", "{'':'idx'}");
+                    cl.delete("{a:10}", "{'':'idx'}");
+                    db.commit();
+
+                    // 删除集合空间
+                    db.dropCollectionSpace(csName);
+                    if (++doTimes == 200) {
+                        System.out.println("CSNAME: " + csName + " doTimes: " + doTimes);
+                        break;
+                    }
+                }
+            } finally {
+                db.commit();
+                if (db.isCollectionSpaceExist(csName)) {
+                    db.dropCollectionSpace(csName);
+                }
+                db.close();
+                latch.countDown();
+            }
+        }
+    }
+}
