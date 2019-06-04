4574582b193e2e4753b41d9605c2c81639fb3e52
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-04 10:06:57 +0000

    新增查询参数校验用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41422 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/query/query_parameterValid_5288.js
@@ -0,0 +1,46 @@
+﻿/****************************************************
+@description: seqDB-5288: skip，参数超过边界值
+@author:
+              2019-6-3 wuyan init
+****************************************************/
+main();
+
+function main()
+{
+   var clName  = COMMCLNAME+"_query5288";      
+   var cl = readyCL( clName );
+      
+   var recordNum = 100;
+   var expRecords = insertRecs( cl, recordNum );
+   
+   println( "---Begin to test skip < 0(skip = -1). " );
+   var skipNum = -1;
+   queryRecsAndCheckResult( cl, expRecords, skipNum );
+   
+   println( "---Begin to test skip > maxInt(skip = 2147483648). " );
+   var skipNum = 2147483648;
+   queryRecsAndCheckResult( cl, expRecords, skipNum );
+   
+   cleanCL( clName );
+     
+}
+
+function insertRecs( cl, recordNum )
+{
+   println("\n---Begin to insert records.");
+   
+   var docs = [];
+   for( i = 0 ; i < recordNum ; i++ )
+   {
+      var objs = { "no": i, "str": "test" + i };  
+      docs.push(objs); 
+   }
+   cl.insert( docs );
+   return docs;
+}
+
+function queryRecsAndCheckResult( cl, expRecs, skipNum )
+{   
+   var query = cl.find().skip(skipNum);   
+   checkRec( query, expRecs );   
+}
