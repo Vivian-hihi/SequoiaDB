2cee4f9eeca484a4eda00c3556ad2abee4fd6506
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-05 00:50:25 +0000

    新增参数校验用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41434 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/split_sunc/split_srcRGMaxLen_4978.js
@@ -0,0 +1,76 @@
+/************************************
+*@Description: 源分区组名取最大长度值进行数据切分_ST.split.01.001
+*@author:      wangkexin
+*@createDate:  2019.5.30
+*@testlinkCase: seqDB-4978
+**************************************/
+main();
+function main()
+{
+   if(true == commIsStandalone( db ))
+   {
+      println( "run mode is standalone");
+      return;
+   }
+   
+   var groupsArray = commGetGroups( db, false, "", true, true, true );
+   var dataRGNum = groupsArray.length;
+   if(dataRGNum < 1)
+   {
+      println( "at least one data group.");
+      return;
+   }
+    
+   var csName = COMMCSNAME;
+   var clName = CHANGEDPREFIX + "_cl_4978";
+   var srcRGName = "";
+   var tarRGName = groupsArray[0][0].GroupName;
+   var hostName = groupsArray[0][1].HostName;
+   var dataNum = 10000;
+   for(var i = 0; i < 123; i++)
+   {
+       srcRGName += 's';
+   }
+   srcRGName += "4978";
+   
+   //clean environment before test
+   commDropCL( db, csName, clName, true, true, "drop CL in the beginning." );
+   removeDataRG( srcRGName );
+   
+   
+   try
+   {
+      println("begin to create data group");
+      var logBackupPath = createDataGroup( srcRGName, hostName );
+      
+      var optionObj = {ShardingKey:{a:1}, ShardingType:"range", ReplSize:0, Group:srcRGName};
+      var cl = commCreateCLByOption( db, csName, clName, optionObj, true, false );
+
+      //insert data and split
+      insertData( cl, dataNum );
+      var condition = 0;
+      var endcondition = 3000;
+
+      cl.split(srcRGName, tarRGName, {a : condition}, {a : endcondition});
+      println("begin to check");
+      var srcDataArr = getExpDataArr( 3000, dataNum );
+      var tarDataArr = getExpDataArr( 0, 3000 );
+      checkData(csName, clName, srcRGName, srcDataArr);
+      checkData(csName, clName, tarRGName, tarDataArr);
+   }
+   catch ( e )
+   {
+      println("catch e : " + e);
+      //将新建组日志备份到/tmp/ci/rsrvnodelog目录下
+      var backupDir = "/tmp/ci/rsrvnodelog/4978";
+      File.mkdir(backupDir);
+      File.scp( logBackupPath, backupDir + "/sdbdiag.log" );
+      throw e;
+   }
+   finally
+   { 
+      //清理环境
+      commDropCL( db, csName, clName, true, true, "drop CL in the end." );
+      removeDataRG( srcRGName );
+   }
+}
