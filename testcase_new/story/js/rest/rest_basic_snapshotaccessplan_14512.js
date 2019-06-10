060aee78856abd1422662d8319c2479935c1f615
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-10 06:09:50 +0000

    CI失败用例，在文本用例中修改查询计划的缓存级别为3
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41502 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/rest/rest_basic_snapshotaccessplan_14512.js
+++ testcase_new/story/js/rest/rest_basic_snapshotaccessplan_14512.js
@@ -7,82 +7,64 @@ main();
 
 function main()
 {  
+   var curlPara = [ 'cmd=update config&configs={plancachelevel:3}' ];
+   runCurl( curlPara);
+   
    var csName = COMMCSNAME + "_14512";
    var clName = COMMCLNAME + "_14512";
     
-	try
-	{
-	   ready( csName );
-	   createCS( csName );
-	   createCL( csName, clName );
-	   //insert 
-	   var recs = [];
-	   var totalCnt = 1000;
-	   for( var i = 0; i < totalCnt; i++ ) 
-	   {
-	      var rec = { NO:i, Flag:true, Index:"str_"+i.toString() };
-	      recs.push( rec );
-	   }	   
-	   insertRecs( csName, clName, recs );
-	  
-	  var cond1 = "{NO:{$lt:100}}";
-	  var cond2 = "{NO:{$gte:500}}";
-	  var cond3 = "{$and:[{NO:{$lt:500}},{NO:{$gte:200}}]}"
-	  queryRecs( csName, clName, recs, cond1); 
-      queryRecs( csName, clName, recs, cond2);
-      queryRecs( csName, clName, recs, cond3);	 
-	  var actInfo = snapshot( csName,clName,"accessplans");
-      var expInfo = ["{\"MinTimeSpentQuery\":{\"ReturnNum\":100}}", "{\"MinTimeSpentQuery\":{\"ReturnNum\":300}}","{\"MinTimeSpentQuery\":{\"ReturnNum\":500}}"];
-	   check(actInfo,expInfo);
-	   dropCL( csName, clName );
-	   dropCS( csName );
-	}
-	catch( e )
-	{
-	   throw e ;
-	}
-	finally
-	{
-	}
+   commDropCS( db, csName, true, "drop cs in begin" );
+   
+   curlPara = [ 'cmd=create collectionspace', 'name='+csName ]
+   runCurl( curlPara);
+   curlPara = [ 'cmd=create collection&name='+csName+'.'+clName,'options={ReplSize:-1}' ];
+   runCurl( curlPara);
+   //insert 
+   var recs = [];
+   var totalCnt = 1000;
+   for( var i = 0; i < totalCnt; i++ ) 
+   {
+      var rec = { NO:i, Flag:true, Index:"str_"+i.toString() };
+      recs.push( rec );
+   }	   
+   insertRecs( csName, clName, recs );
+  
+   var cond1 = "{NO:{$lt:100}}";
+   var cond2 = "{NO:{$gte:500}}";
+   var cond3 = "{$and:[{NO:{$lt:500}},{NO:{$gte:200}}]}"
+   queryRecs( csName, clName, recs, cond1 ); 
+   queryRecs( csName, clName, recs, cond2 );
+   queryRecs( csName, clName, recs, cond3 );	 
+   
+   var actInfo = snapshot( csName,clName,"accessplans");
+   var expInfo = ["{\"MinTimeSpentQuery\":{\"ReturnNum\":100}}", "{\"MinTimeSpentQuery\":{\"ReturnNum\":300}}","{\"MinTimeSpentQuery\":{\"ReturnNum\":500}}"];
+   check(actInfo,expInfo);
+   curlPara = [ "cmd=drop collection&name="+csName+'.'+clName ];
+   runCurl( curlPara );
+   curlPara = [ "cmd=drop collectionspace&name="+csName ]
+   runCurl( curlPara );
 }
 
 function insertRecs( csName, clName, recs )
 {
-	println( "\n---Begin to excute " + getFuncName() );
-	
 	for( var i in recs )
 	{
-   	var recStr = JSON.stringify( recs[i] );
-   		
-   	var curlPara = [ 'cmd=insert', 
-   	                 'name=' + csName + '.' + clName,
-   	                 'insertor=' + recStr ];
-   	var expErrno = 0;
-   	runCurl( curlPara);
+   	   var recStr = JSON.stringify( recs[i] );
+   	   var curlPara = [ 'cmd=insert&name=' + csName + '.' + clName+'&insertor=' + recStr ];
+   	   runCurl( curlPara);
    }
 }
 
 function queryRecs( csName, clName, recs,cond)
 {
-   println( "\n---Begin to excute " + getFuncName() );
-
-	var curlPara = [ 'cmd=query', 
-	                 'name=' + csName + '.' + clName,
-	                 'filter=' + cond];
-	var expErrno = 0;
-	runCurl( curlPara );
+   var curlPara = [ 'cmd=query&name=' + csName + '.' + clName+'&filter=' + cond];
+   runCurl( curlPara );
 }
 
 function snapshot( csName,clName,type)
 {
-	println( "\n---Begin to excute " + getFuncName() );
-
-	var curlPara = [ 'cmd=snapshot ' + type,
-	                 'filter={Collection:"'+csName+'.'+clName+'"}',
-					 'selector='+"{'MinTimeSpentQuery.ReturnNum':{$include:1}}",
-					 'sort=' + "{'MinTimeSpentQuery.ReturnNum':1}" 
-	               ];
-	var expErrno = 0;
+	var curlPara = [ 'cmd=snapshot ' + type+'&filter={Collection:"'+csName+'.'+clName+'"}',
+					 'selector={MinTimeSpentQuery.ReturnNum:{$include:1}}&sort={MinTimeSpentQuery.ReturnNum:1}' ];
 	runCurl( curlPara);
     var resp = infoSplit;
 	resp.shift();
@@ -91,7 +73,6 @@ function snapshot( csName,clName,type)
 
 function check(actInfo,expInfo)
 {
-  println( "\n---Begin to excute " + getFuncName() );
   if(actInfo.length != expInfo.length)
   {
    throw "actInfo.length  by snapshot is not equal axpInfo.length by self";
@@ -100,57 +81,7 @@ function check(actInfo,expInfo)
   {
     if(actInfo[i].replace(/\//g,"").replace(/\s+/g,"") != expInfo[i])
     {
-      throw "actInfo:" + JSON.stringify(actInfo[i])+"\nexpInfo:" + expInfo[i];
+       throw "actInfo:" + JSON.stringify(actInfo[i])+"\nexpInfo:" + expInfo[i];
     }
   } 
 }
-
-function dropCL( csName, clName )
-{
-	println( "\n---Begin to excute " + getFuncName() );
-	
-	var curlPara = [ "cmd=drop collection", "name="+csName+'.'+clName ];
-   runCurl( curlPara);
-}
-
-function dropCS( csName )
-{
-	println( "\n---Begin to excute " + getFuncName() );
-	
-	var curlPara = [ "cmd=drop collectionspace", "name="+csName ]
-	runCurl( curlPara);
-}
-
-function createCS( csName )
-{
-	println( "\n---Begin to excute " + getFuncName() );
-	
-	var curlPara = [ 'cmd=create collectionspace', 'name='+csName ]
-	runCurl( curlPara);
-}
-
-function createCL( csName, clName, group )
-{
-	println( "\n---Begin to excute " + getFuncName() );
-	
-	if ( group == undefined )
-	{
-   	var curlPara = [ 'cmd=create collection', 
-   	                 'name='+csName+'.'+clName,
-   	                 'options={ReplSize:-1}' ];
-   }
-   else
-   {
-      var curlPara = [ 'cmd=create collection', 
-   	                 'name='+csName+'.'+clName,
-   	                 'options={ReplSize:-1, Group:"'+group+'"}' ];
-   }
-	runCurl( curlPara);
-}
-
-function ready( csName, clName )
-{
-   println( "\n---Begin to excute " + getFuncName() );
-   commDropCS( db, csName, true, "drop cs in begin" );
-}
-
