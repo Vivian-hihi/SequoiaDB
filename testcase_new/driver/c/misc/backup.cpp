12eb0023cae6b76642049bb9cb7349d6e28811fb
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-18 01:21:30 +0000

    回归4396，补充c驱动sdbBackup接口测试
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41660 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/c/misc/backup.cpp
+++ testcase_new/driver/c/misc/backup.cpp
@@ -1,6 +1,7 @@
 /**************************************************************
  * @Description: test case of Jira questionaire
  *					  SEQUOIADBMAINSTREAM-2593
+ *       SEQUOIADBMAINSTREAM-4396
  *				     list backup and remove backup with group ID
  * @Modify     : Liang xuewang Init
  *           	  2017-08-02
@@ -27,7 +28,7 @@ protected:
    }
 } ;
 
-TEST_F( backupTest, backupWithOption )
+TEST_F( backupTest, backupOfflineWithOption )
 {
    INT32 rc = SDB_OK ;
    if( isStandalone( db ) ) 
@@ -68,6 +69,60 @@ TEST_F( backupTest, backupWithOption )
    bson_finish( &option ) ;
    rc = sdbBackupOffline( db, &option ) ;
    ASSERT_EQ( SDB_OK, rc ) << "fail to backup offline" ;
+
+   // list backup 
+   rc = sdbListBackup( db, &option, NULL, NULL, NULL, &cursor ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to list backup" ;
+   rc = sdbCloseCursor( cursor ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to close cursor" ;
+
+   // remove backup
+   rc = sdbRemoveBackup( db, &option ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to remove backup" ;
+
+   sdbReleaseCursor( cursor ) ;
+}
+
+TEST_F( backupTest, backupWithOption )
+{
+   INT32 rc = SDB_OK ;
+   if( isStandalone( db ) ) 
+   {
+      printf( "Run mode is standalone.\n" ) ;
+      return ;
+   }
+
+   // get a data group id
+   sdbCursorHandle cursor = SDB_INVALID_HANDLE ;
+   string groupname ; 
+   bson obj ;
+   bson_init( &obj ) ;
+   rc = sdbListReplicaGroups( db, &cursor ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to list replica groups" ;
+   
+   while( !sdbNext( cursor, &obj ) )
+   {
+      bson_iterator it ;
+      bson_find( &it, &obj, "GroupName" ) ;
+      groupname = bson_iterator_string( &it ) ;
+      if( groupname != "SYSCoord" && groupname != "SYSCatalogGroup" )
+      {
+         break ;
+      }
+      bson_destroy( &obj ) ;
+      bson_init( &obj ) ;
+   }
+   bson_destroy( &obj ) ;
+   rc = sdbCloseCursor( cursor ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to close cursor" ;
+
+   // backup with group name
+   bson option ;
+   bson_init( &option ) ;
+   bson_append_string( &option, "GroupName", groupname.c_str() ) ;
+   bson_finish( &option ) ;
+   rc = sdbBackup( db, &option ) ;
+   ASSERT_EQ( SDB_OK, rc ) << "fail to backup" ;
 
    // list backup 
    rc = sdbListBackup( db, &option, NULL, NULL, NULL, &cursor ) ;
