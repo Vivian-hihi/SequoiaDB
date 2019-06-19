bf4bae920a03edf8837032477c270ad746a311f9
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-19 08:32:37 +0000

    优化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41715 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/sdbexprt/filelimitDatasize_13524.js
+++ testcase_new/story/js/sdbexprt/filelimitDatasize_13524.js
@@ -2,143 +2,134 @@
 * @Description : test export with --filelimit
 *                seqDB-13524:指定文件大小=数据量大小
 *                seqDB-13525:指定文件大小<数据量大小，导出数据到json
-*                seqDB-13537:指定文件大小<数据量大小，导出数据到csv          
-* @author      : Liang XueWang 
+*                seqDB-13537:指定文件大小<数据量大小，导出数据到csv 
+* @author      : chensiqin 
 *
 *******************************************************************/
 var csname = COMMCSNAME ;
 var clname = COMMCLNAME + "_sdbexprt13524" ;
-var clname1 = COMMCLNAME + "_sdbimprt13524" ;
-var kbs = [ 10, 12 ] ;
-var filelimit = "10K" ;
-var expSize = 10*1024 ;
-var kb ;
 
 main() ;
 
 function main()
 {
-   for( var i = 0;i < kbs.length;i++ )
-   {
-      kb = kbs[i] ;
-      println( "test filelimit: " + filelimit + " data: " + kb ) ;
-      testFileLimit() ;
-   }
-}
-
-function testFileLimit()
-{
-   var cl = commCreateCL( db, csname, clname, 0 ) ;
-   var cl1 = commCreateCL( db, csname, clname1, 0 ) ;
-   
-   insertDocs( cl, kb ) ; 
-  
-   testExprtImprtCsv() ;
-   testExprtImprtJson() ;
-  
-   if( parseInt( cl1.count() ) !== 2*(kb*1024-1) )
-   {
-      throw buildException( "testFileLimit", null, "check import cl count", 
-            2*(kb*1024-1), cl1.count() ) ;   
-   }
-   var cursor = cl1.find() ;
-   var obj ;
-   while( obj = cursor.next() )
-   {
-      var actVal = obj.toObj()["a"] ;
-      if( actVal !== 1 )
-      {
-         throw buildException( "testFileLimit", null, "check import cl rec",
-               1, actVal ) ;
-      }
-   }
-   
    commDropCL( db, csname, clname ) ;
-   commDropCL( db, csname, clname1 ) ;
+   var cl = commCreateCL( db, csname, clname, 0 ) ;   
+   var exportDir = workDir + "13524/" ;
+   var kb = 4 ;
+   
+   //指定文件大小=数据量大小 
+   testFilelimit13524(cl, kb, "44K", "json", exportDir);
+   cl.truncate();
+   //指定文件大小<数据量大小json
+   testFilelimit13525(cl, kb, "2K", "json", exportDir);
+   cl.truncate();
+   
+   //指定文件大小<数据量大小csv
+   testFilelimit13537(cl, kb, "2K", "csv", exportDir);
+
+   commDropCL( db, csname, clname ) ;
 }
 
-// insert { a: 1 } repeatly until kb
+//指定文件大小=数据量大小
+function testFilelimit13524(cl, kb, filelimit, fileType, exportDir)
+{
+   var expSize = 44*1024 ;
+   insertDocs( cl, 4 ) ; 
+   testExprtWithLimit(filelimit, fileType, exportDir)
+   //校验导出后文件是否正确
+   checkExprtfileSize(exportDir, fileType, expSize);
+   testImportWithLimit(filelimit, fileType, exportDir);
+   checkCl( cl, kb );
+}
+
+//指定文件大小<数据量大小 json
+function testFilelimit13525(cl, kb, filelimit, fileType, exportDir)
+{
+   var expSize = 2*1024 ;
+   insertDocs( cl, 4 ) ; 
+   testExprtWithLimit(filelimit, fileType, exportDir)
+   //校验导出后文件是否正确
+   checkExprtfileSize(exportDir, fileType, expSize);
+   testImportWithLimit(filelimit, fileType, exportDir);
+   checkCl( cl, kb );
+}
+
+//指定文件大小<数据量大小 csv
+function testFilelimit13537(cl, kb, filelimit, fileType, exportDir)
+{
+   var expSize = 2*1024 ;
+   insertDocs( cl, 4 ) ; 
+   testExprtWithLimit(filelimit, fileType, exportDir)
+   //校验导出后文件是否正确
+   checkExprtfileSize(exportDir, fileType, expSize);
+   testImportWithLimit(filelimit, fileType, exportDir);
+   checkCl( cl, kb );
+}
+
 function insertDocs( cl, kb )
 {
-   var bytes = kb*1024 ;
-   for( var i = 0;i < bytes-1;i++ )
+   var bytes = kb*1024 ;//4*1024=4K  4096条记录  一条记录等于11个字节
+   var doc = [];
+   for( var i = 1;i < bytes; i++ )
    {
-      cl.insert( { a: 1 } ) ;
+      doc.push({ a: 1 })
    }
+   cl.insert( doc );
 }
 
-function testExprtImprtCsv()
+function testExprtWithLimit(filelimit, fileType, exportDir)
 {
-   var csvDir = workDir + "13524/" ;
-   commMakeDir( "localhost", csvDir ) ;
-   var csvfile = csvDir + "sdbexprt13524.csv" ;
-   cmd.run( "rm -rf " + csvfile ) ;
+   commMakeDir( "localhost", exportDir ) ;
+   var exportfile = exportDir + "sdbexprt13524."+fileType ;
+   cmd.run( "rm -rf " + exportfile ) ;
    
    var command = installPath + "bin/sdbexprt" +
                  " -s " + COORDHOSTNAME +
                  " -p " + COORDSVCNAME + 
                  " -c " + csname + 
                  " -l " + clname +
-                 " --file " + csvfile + 
+                 " --file " + exportfile + 
                  " --filelimit " + filelimit +
-                 " --type csv" +
-                 " --included false" +
+                 " --type " + fileType +
                  " --fields a" ;
+                 
+   //println(command);
    testRunCommand( command ) ;
    
-   var actSize = parseInt( File.stat( csvfile ).toObj()["size"] ) ;
-   if( actSize !== expSize )
-   {
-      throw buildException( "testExprtImprtCsv", null, "check file size",
-            expSize, actSize ) ;
-   }
-   
+}
+
+function testImportWithLimit(filelimit, fileType, exportDir)
+{
    command = installPath + "bin/sdbimprt" +
              " -s " + COORDHOSTNAME +
              " -p " + COORDSVCNAME +
              " -c " + csname +
-             " -l " + clname1 +
-             " --file " + csvDir +
-             " --type csv " +
-             " --fields='a int'" ;  
+             " -l " + clname +
+             " --file " + exportDir +
+             " --type " + fileType +
+             " --fields='a int'";  
    testRunCommand( command ) ;
    
-   cmd.run( "rm -rf " + csvDir ) ;
+   cmd.run( "rm -rf " + exportDir ) ;
 }
 
-function testExprtImprtJson()
+function checkCl( cl, kb )
 {
-   var jsonDir = workDir + "13527/" ;
-   commMakeDir( "localhost", jsonDir ) ;
-   var jsonfile = jsonDir + "sdbexprt13527.json" ;
-   cmd.run( "rm -rf " + jsonfile ) ;
-   
-   var command = installPath + "bin/sdbexprt" +
-                 " -s " + COORDHOSTNAME +
-                 " -p " + COORDSVCNAME + 
-                 " -c " + csname + 
-                 " -l " + clname +
-                 " --file " + jsonfile + 
-                 " --filelimit " + filelimit +
-                 " --type json" +
-                 " --fields a" ;
-   testRunCommand( command ) ;
-   
-   var actSize = parseInt( File.stat( jsonfile ).toObj()["size"] ) ;
+   if( parseInt( cl.find({a:1}).count() ) !== 2*(kb*1024-1) )
+   {
+      throw buildException( "testFileLimit", null, "check import cl count", 
+            2*(kb*1024-1), cl.find({a:1}).count() ) ;   
+   }
+}
+
+function checkExprtfileSize(exportDir, fileType, expSize)
+{
+   var file = exportDir + "sdbexprt13524." + fileType ;
+   var actSize = parseInt( File.stat( file ).toObj()["size"] ) ;
    if( actSize > expSize )
    {
       throw buildException( "testExprtImprtJson", null, "check file size",
             expSize, actSize ) ;
    }
-   
-   command = installPath + "bin/sdbimprt" +
-             " -s " + COORDHOSTNAME +
-             " -p " + COORDSVCNAME +
-             " -c " + csname +
-             " -l " + clname1 +
-             " --file " + jsonDir +
-             " --type json" ;  
-   testRunCommand( command ) ;
-   
-   cmd.run( "rm -rf " + jsonDir ) ;
 }
