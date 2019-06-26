ce87744500972df9c691464afe1ccfccd24dbc9d
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-06-26 01:35:30 +0000

    回归4334，补充全文索引异常用例查询分别走原主、新主的测试点
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@41807 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/java/src/test/java/com/sequoiadb/fulltext/killnode/KillMasterNode14406.java
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/fulltext/killnode/KillMasterNode14406.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
 import org.bson.util.JSON;
 import org.testng.Assert;
 import org.testng.SkipException;
@@ -15,7 +16,9 @@ import org.testng.annotations.Test;
 
 import com.sequoiadb.base.CollectionSpace;
 import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
 import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.base.Node;
 import com.sequoiadb.commlib.CommLib;
 import com.sequoiadb.commlib.GroupMgr;
 import com.sequoiadb.commlib.GroupWrapper;
@@ -47,39 +50,44 @@ public class KillMasterNode14406 extends SdbTestBase {
     @BeforeClass()
     public void setUp() {
         try {
-            sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-            if (CommLib.isStandAlone(sdb)) {
-                throw new SkipException("StandAlone environment!");
+            sdb = new Sequoiadb( SdbTestBase.coordUrl, "", "" );
+            if ( CommLib.isStandAlone( sdb ) ) {
+                throw new SkipException( "StandAlone environment!" );
             }
 
-            System.out.println("the TestCase Name:" + this.getClass().getName() + ". the TestCase begin at:"
-                    + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
+            System.out.println( "the TestCase Name:" + this.getClass().getName()
+                    + ". the TestCase begin at:"
+                    + new SimpleDateFormat( "YYYY-MM-dd HH:mm:ss.SSS" )
+                            .format( new Date() ) );
             groupMgr = GroupMgr.getInstance();
 
             // CheckBusiness(true),检测当前集群环境，若存在异常返回false，
-            if (!groupMgr.checkBusiness(20)) {
-                throw new SkipException("checkBusiness return false");
+            if ( !groupMgr.checkBusiness( 20 ) ) {
+                throw new SkipException( "checkBusiness return false" );
             }
-            groupName = groupMgr.getAllDataGroupName().get(0);
-            cs = sdb.getCollectionSpace(csName);
-            cl = cs.createCollection(clName, (BSONObject) JSON.parse("{Group:'" + this.groupName + "'}"));
+            groupName = groupMgr.getAllDataGroupName().get( 0 );
+            cs = sdb.getCollectionSpace( csName );
+            cl = cs.createCollection( clName, ( BSONObject ) JSON
+                    .parse( "{Group:'" + this.groupName + "'}" ) );
 
-        } catch (ReliabilityException e) {
-            if (sdb != null) {
+        } catch ( ReliabilityException e ) {
+            if ( sdb != null ) {
                 sdb.close();
             }
-            Assert.fail(this.getClass().getName() + " setUp error, error description:" + e.getMessage());
+            Assert.fail( this.getClass().getName()
+                    + " setUp error, error description:" + e.getMessage() );
         }
     }
 
     public void insertData() {
-        List<BSONObject> records = new ArrayList<BSONObject>();
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 5000; j++) {
-                BSONObject record = (BSONObject) JSON.parse("{a:'a" + i + "" + j + "',g:'g" + i + "" + j + "'}");
-                records.add(record);
+        List< BSONObject > records = new ArrayList< BSONObject >();
+        for ( int i = 0; i < 100; i++ ) {
+            for ( int j = 0; j < 5000; j++ ) {
+                BSONObject record = ( BSONObject ) JSON.parse(
+                        "{a:'a" + i + "" + j + "',g:'g" + i + "" + j + "'}" );
+                records.add( record );
             }
-            this.cl.insert(records);
+            this.cl.insert( records );
             records.clear();
         }
     }
@@ -87,43 +95,88 @@ public class KillMasterNode14406 extends SdbTestBase {
     @Test
     public void test() throws Exception {
         try {
-            this.cl.createIndex(fullIndexName, "{\"a\":\"text\"}", false, false);
+            this.cl.createIndex( fullIndexName, "{\"a\":\"text\"}", false,
+                    false );
             this.insertData();
-            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, 500000));
-            GroupWrapper subCLGroup = groupMgr.getGroupByName(groupName);
+            Assert.assertTrue(
+                    FullTextUtils.isIndexCreated( cl, fullIndexName, 500000 ) );
+            GroupWrapper subCLGroup = groupMgr.getGroupByName( groupName );
             NodeWrapper subCLGroupMaster = subCLGroup.getMaster();
-            System.out.println("Kill node:" + subCLGroupMaster.hostName() + ":" + subCLGroupMaster.svcName());
+
+            // get masterNode
+            Node preMasterNode = this.sdb
+                    .getReplicaGroup( groupName ).getMaster();
+            String preMasterHostName = subCLGroupMaster.hostName();
+            String preMasterSvcName = subCLGroupMaster.svcName();
+            System.out.println(
+                    "Kill node:" + preMasterHostName + ":" + preMasterSvcName );
 
             // 建立并行任务
-            FaultMakeTask faultTask = KillNode.getFaultMakeTask(subCLGroupMaster.hostName(), subCLGroupMaster.svcName(),
-                    0);
-            TaskMgr mgr = new TaskMgr(faultTask);
+            FaultMakeTask faultTask = KillNode
+                    .getFaultMakeTask( preMasterHostName, preMasterSvcName, 0 );
+            TaskMgr mgr = new TaskMgr( faultTask );
             mgr.execute();
-            Assert.assertTrue(mgr.isAllSuccess(), mgr.getErrorMsg());
-            Assert.assertTrue(groupMgr.checkBusinessWithLSN(120));
+            Assert.assertTrue( mgr.isAllSuccess(), mgr.getErrorMsg() );
+            Assert.assertTrue( groupMgr.checkBusinessWithLSN( 120 ) );
 
-            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, 500000));
+            Assert.assertTrue(
+                    FullTextUtils.isIndexCreated( cl, fullIndexName, 500000 ) );
 
             // insert
             this.insertData();
-            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, 1000000));
+            Assert.assertTrue( FullTextUtils.isIndexCreated( cl, fullIndexName,
+                    1000000 ) );
 
             // update
-            this.cl.update("{a:'a01'}", "{'$set':{a:'helloworld'}}", "{'':null}");
-            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, 1000000));
+            this.cl.update( "{a:'a01'}", "{'$set':{a:'helloworld'}}",
+                    "{'':null}" );
+            Assert.assertTrue( FullTextUtils.isIndexCreated( cl, fullIndexName,
+                    1000000 ) );
 
             // delete
-            this.cl.delete("{a:'a11'}");
-            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, 999998));
+            this.cl.delete( "{a:'a11'}" );
+            Assert.assertTrue(
+                    FullTextUtils.isIndexCreated( cl, fullIndexName, 999998 ) );
 
-            // query
-            this.cl.query();
-            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, 999998));
+            // get old and new masterNode
+            Sequoiadb preMasterNodeDB = new Sequoiadb( preMasterHostName,
+                    Integer.valueOf( preMasterSvcName ), "", "" );
+            Sequoiadb currentMasterNodeDB = this.sdb
+                    .getReplicaGroup( groupName ).getMaster()
+                    .connect();
+
+            // query previous masterNode
+            BSONObject matcher = new BasicBSONObject();
+            matcher.put( "a", "a22" );
+            DBCursor cursor = preMasterNodeDB
+                    .getCollectionSpace( SdbTestBase.csName )
+                    .getCollection( this.clName )
+                    .query( matcher, null, null, null );
+            int count = 0;
+            while ( cursor.hasNext() ) {
+                cursor.getNext();
+                count++;
+            }
+            cursor.close();
+            Assert.assertEquals( 2, count );
+
+            // query current masterNode
+            cursor = currentMasterNodeDB
+                    .getCollectionSpace( SdbTestBase.csName )
+                    .getCollection( this.clName )
+                    .query( matcher, null, null, null );
+            count = 0;
+            while ( cursor.hasNext() ) {
+                cursor.getNext();
+                count++;
+            }
+            cursor.close();
+            Assert.assertEquals( 2, count );
 
             clearFlag = true;
-        } catch (ReliabilityException e) {
+        } catch ( ReliabilityException e ) {
             e.printStackTrace();
-            Assert.fail(e.getMessage());
+            Assert.fail( e.getMessage() );
         } finally {
             sdb.closeAllCursors();
         }
@@ -133,18 +186,20 @@ public class KillMasterNode14406 extends SdbTestBase {
     @AfterClass
     public void tearDown() {
         try {
-            if (clearFlag) {
-                CollectionSpace commCS = sdb.getCollectionSpace(csName);
-                commCS.dropCollection(clName);
+            if ( clearFlag ) {
+                CollectionSpace commCS = sdb.getCollectionSpace( csName );
+                commCS.dropCollection( clName );
             }
-        } catch (BaseException e) {
-            Assert.fail(e.getMessage());
+        } catch ( BaseException e ) {
+            Assert.fail( e.getMessage() );
         } finally {
-            if (sdb != null) {
+            if ( sdb != null ) {
                 sdb.close();
             }
-            System.out.println("the TestCase Name:" + this.getClass().getName() + ". the TestCase end at:"
-                    + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
+            System.out.println( "the TestCase Name:" + this.getClass().getName()
+                    + ". the TestCase end at:"
+                    + new SimpleDateFormat( "YYYY-MM-dd HH:mm:ss.SSS" )
+                            .format( new Date() ) );
         }
     }
 }
