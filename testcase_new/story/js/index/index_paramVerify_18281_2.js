31cad68125ee2d1474637898088ba72b03918233
zhaoxiaoni <zhaoxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-12 08:25:40 +0000

    SEQUOIADBMAINSTREAM-4415优化自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42115 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/index/index_paramVerify_18281_2.js
+++ testcase_new/story/js/index/index_paramVerify_18281_2.js
@@ -2,10 +2,15 @@
 *@Description : seqDB-18281:options参数校验
 *@Author      : 2019-5-6  XiaoNi Zhao
 ******************************************************************************/
-
 main();
 function main()
 {
+   if ( commIsStandalone( db ) )
+   {
+      println("\nThe mode is standalone.");
+      return;
+   } 
+   
    var mainClName = "mainCl_18281_2";
    var subClName = "subCl_18281_2";
    var indexName = "idx";
@@ -16,99 +21,19 @@ function main()
    var mainCl = commCreateCLByOption( db, COMMCSNAME, mainClName, { IsMainCL: true, ShardingKey: { a: 1 } } );
    var subCl = commCreateCLByOption( db, COMMCSNAME, subClName, { ShardingKey: { a: 1 } } );
    var fullName = COMMCSNAME +"."+ subClName;
-   println("COMMCSNAME===="+COMMCSNAME);
-   println("fullName====="+fullName);
    mainCl.attachCL( fullName, { LowBound: { a: 0 }, UpBound: { a: 1000 } } );
                            
-   /**************************** test1, field name lowercase ***************************/
-   println("\n---Test1, create index, field name lowercase.");
-   mainCl.createIndex( indexName, {a:1}, {unique:true, enforced:true} );          
-   
-   println("---Check results."); 
-   checkIndex( mainCl, indexName, true, true, false ); 
-      
-   mainCl.dropIndex( indexName );
-    
-                           
-   /**************************** test2, field name invalid ***************************/
-   println("\n---Test2, create index, field name invalid.");
-   var keyArr = [{isUnique:true}, {enforced:true}, {sortBufferSize:true}, {notNull:true}, {aa:true}];
-   for ( i = 0; i < keyArr.length; i++) 
-   {
-      try
-      {
-         mainCl.createIndex( indexName, {a:1}, keyArr[i] );
-      }
-      catch ( e ) 
-      {
-         if( e !== -6 )
-         {
-            throw buildException( "checkResult", null, "", -6, "  " + e );
-         }  
-      }
-   } 
-   
-   try 
-   {
-      mainCl.getIndex( indexName );
-   } 
-   catch ( e ) 
-   {
-      if( e !== -47 )
-      {
-         throw buildException( "checkResult", null, "", -47, "  " + e );
-      } 
-   }  
-   
-                           
-   /**************************** test3, default value ***************************/
-   println("\n---Test3, create index, default value.");
-   mainCl.createIndex( indexName, {a:1},{unique:true} );          
-   
-   println("---Check results."); 
-   checkIndex( mainCl, indexName, true, false, false ); 
-      
-   // mainClean index
-   mainCl.dropIndex( indexName ); 
-   
-                           
-   /**************************** test4, 2 diff name for same field ***************************/
-   println("\n---Test3, create index, 2 diff name for same field.");
-   var keyArr = [{enforced:true, Enforced:false}, {unique:false, Unique:false}, {NotNull:true, aa:false}];
-   try
-   {
-      mainCl.createIndex( indexName, {a:1}, keyArr[i] );
-   }
-   catch ( e ) 
-   {
-      if( e !== -6 )
-      {
-         throw buildException( "checkResult", null, "", -6, "  " + e );
-      }  
-   }
-   
-                           
-   /**************************** test5, boolean:0 ***************************/
-   println("\n---Test5, create index, boolean:0.");
+   /**************************** test1, unique:1, enforced:0, NotNull:0 ***************************/
+   println("\n---Test5, create index, unique:1, enforced:1,NotNull:1.");
    mainCl.createIndex( indexName, {a:1}, {unique:1, enforced:0,NotNull:0} );
    println("---Check results."); 
    checkIndex( mainCl, indexName, true, false, false );
-    
-   // mainClean index
-   mainCl.dropIndex( indexName ); 
-   mainCl.remove();
-   
-                           
-   /**************************** test6, unique:1, enforced:1,NotNull:1 ***************************/
-   println("\n---Test6, create index, unique:1, enforced:1,NotNull:1.");
-   mainCl.createIndex( indexName, {a:1}, {unique:1, enforced:1,NotNull:1} );
-   println("---Check results."); 
-   checkIndex( mainCl, indexName, true, true, true );
-  
+   var insertR1 = [{a:1, b:1}];
+   mainCl.insert(insertR1);
    try
    {
-      mainCl.insert({a:1,b:4});
-   } 
+      mainCl.insert(insertR1);
+   }
    catch ( e ) 
    {
       if( e !== -38 )
@@ -116,41 +41,31 @@ function main()
          throw e;
       }  
    }
+   checkRecords( mainCl, insertR1 );
    mainCl.dropIndex( indexName ); 
    mainCl.remove();
    
-   /**************************** test7, unique:1, enforced:1 ***************************/
-   println("\n---Test7, create index, unique:1, enforced:1.");
-   mainCl.createIndex( indexName, { a: 1 }, { unique: 1, enforced: 1 } );
+                           
+   /**************************** test2, unique:1, enforced:1,NotNull:1 ***************************/
+   println("\n---Test2, create index, unique:1, enforced:1,NotNull:1.");
+   mainCl.createIndex( indexName, {a:1}, {unique:1, enforced:1,NotNull:1} );
    println("---Check results."); 
-   checkIndex( mainCl, indexName, true, true );   
-   mainCl.dropIndex( indexName ); 
-   mainCl.remove();
-             
-   /**************************** test8, unique:0, enforced:0,NotNull:0 ***************************/
-   println("\n---Test8, create index, unique:0, enforced:0,NotNull:0.");
-   mainCl.createIndex( indexName, {a:1}, {unique:1, enforced:0, NotNull:0} );
-   println("---Check results."); 
-   checkIndex( mainCl, indexName, true, false, false );
-   mainCl.dropIndex( indexName ); 
-   mainCl.remove();
-   
-   /**************************** test9, NotNull:string/otherNum ***************************/
-   println("\n---Test9, create index, NotNull:string/otherNum.");
-   var keyArr = [{NotNull:"true"}, {NotNull:"false"}, {NotNull:2}];
+   checkIndex( mainCl, indexName, true, true, true );
+   var insertR1 = [{a:1, b:1}];
    try
    {
-      mainCl.createIndex( indexName, {a:1}, keyArr[i] );
+      mainCl.insert(insertR1);
    }
    catch ( e ) 
    {
-      if( e !== -6 )
+      if( e !== -38 )
       {
-         throw buildException( "checkResult", null, "", -6, "  " + e );
+         throw e;
       }  
    }
-                          
-   // mainClean env
+   checkRecords( mainCl, insertR1 );
+   mainCl.dropIndex( indexName ); 
+   mainCl.remove();
    commDropCL( db, COMMCSNAME, mainClName, false, false, "Failed to drop mainCl in the end-condition" );
 }
 
@@ -171,16 +86,16 @@ function checkIndex( mainCl, indexName, expUni, expEnf, expNot )
       throw buildException( "checkResult", null, "", expResults, "  " + actResults );
    }    
 }
-
-function checkRecords( mainCl, expRecs ) 
+function checkRecords( cl, expRecs ) 
 {
-   println("---Check records.");
-   var rc = mainCl.find( {}, {_id:{$include:0}} ).sort({b:1} );
-   var actRecs = [];
-   while( rc.next() )
+   println("   Check records.");
+   var rc = cl.find( {}, {_id:{$include:0}} ).sort({b:1} );
+   var actRecs = new Array();
+   while( tmpRecs = rc.next() )
    {
-      actRecs.push( rc.current().toObj() );
-   }
+      actRecs.push( tmpRecs.toObj() );
+   }   
+      
    if( JSON.stringify( expRecs ) !== JSON.stringify( actRecs ) )
    {
       throw buildException( "checkResult", null, "", JSON.stringify( expRecs ), "  " + JSON.stringify( actRecs ) );
