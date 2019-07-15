a104ee436509a4156d4ad3199bc2ee95eff236ba
wangkexin <wangkexin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-15 12:11:43 +0000

    由于用例中涉及到了时间戳边界值1902-01-01的测试点，而在1928年1月1日前，由UTC时间戳转成中国本地时区的时间，会加上5分52秒，所以需要修改用例规避上述问题在不同机器上产生的误差。
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42170 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/sql/sql_timestamp_13705.js
+++ testcase_new/story/js/sql/sql_timestamp_13705.js
@@ -17,14 +17,14 @@ function main()
     //正常timestamp类型数据、边界值、非法值
     //Timestamp类型能表示的时间范围为1902-01-01 00:00:00.000000至2037-12-31 23:59:59.999999
     insertSQL(db, cl, csName, clName, 'Timestamp("2019-03-02-10:48:50.000000")', {$timestamp:"2019-03-02-10.48.50.000000"}, true);
-    insertSQL(db, cl, csName, clName, 'Timestamp("1902-01-01-00:00:00.000000")', {$timestamp:"1902-01-01-00.00.00.000000"}, true);
+    var timestamp = insertSQL(db, cl, csName, clName, 'Timestamp("1902-01-01-00:00:00.000000")', {$timestamp:"1902-01-01-00.00.00.000000"}, true);
     insertSQL(db, cl, csName, clName, 'Timestamp("2037-12-31-23:59:59.999999")', {$timestamp:"2037-12-31-23.59.59.999999"}, true);
     insertSQL(db, cl, csName, clName, 'Timestamp("1800-01-01-00:00:00.000000")', false);
     insertSQL(db, cl, csName, clName, 'Timestamp("2038-12-20-00:00:00.000000")', false);
     insertSQL(db, cl, csName, clName, 'Timestamp("978192000000")', false);
     
     selectSQL(db, csName, clName, 'Timestamp("2019-03-02-10:48:50.000000")', {$timestamp:"2019-03-02-10.48.50.000000"}, true);
-    selectSQL(db, csName, clName, 'Timestamp("1902-01-01-00:00:00.000000")', {$timestamp:"1902-01-01-00.00.00.000000"}, true);
+    selectSQL(db, csName, clName, 'Timestamp("1902-01-01-00:00:00.000000")', {$timestamp:timestamp}, true);
     selectSQL(db, csName, clName, 'Timestamp("2037-12-31-23:59:59.999999")', {$timestamp:"2037-12-31-23.59.59.999999"}, true);
     selectSQL(db, csName, clName, 'Timestamp("1800-01-01-00:00:00.000000")', false);
     selectSQL(db, csName, clName, 'Timestamp("2038-12-20-00:00:00.000000")', false);
@@ -56,16 +56,34 @@ function insertSQL(db, cl, csName, clName, insertValue, checkValue, result)
     var sql = 'insert into '+csName+'.'+clName+'(num, textFields ) values (3, ' + insertValue + ')';
     if(result)
     {
+        var cursor = null;
         try
         {
             db.execUpdate( sql );
-            var cursor = cl.find({textFields:checkValue},{"_id":{"$include":0}});
-            var expstring = '{"num":3,"textFields":' + JSON.stringify(checkValue) + '}';
-            var actString = JSON.stringify(cursor.next().toObj());
-            if(expstring !== actString)
+            if(JSON.stringify(checkValue) == '{"$timestamp":"1902-01-01-00.00.00.000000"}')
             {
-                throw buildException("insertSQL()",null,"check record " + insertValue, expstring, actString);
+                var checkValue1 = {$timestamp:"1902-12-31-23.54.08.000000"};
+                var checkValue2 = {$timestamp:"1902-01-01-00.05.52.000000"};
+                var count = cl.find({textFields:{$in:[checkValue1, checkValue, checkValue2]}}).count();
+                if(Number(count) !== 1)
+                {
+                    throw buildException("insertSQL()",null,"check record " + insertValue, 1, Number(count));
+                }
+                var cursor = cl.find({textFields:{$in:[checkValue1, checkValue, checkValue2]}});
+                var timestamp = cursor.next().toObj()["textFields"]["$timestamp"];
+                return timestamp;
             }
+            else
+            {
+                cursor = cl.find({textFields:checkValue},{"_id":{"$include":0}});
+                var expstring = '{"num":3,"textFields":' + JSON.stringify(checkValue) + '}';
+                var actString = JSON.stringify(cursor.next().toObj());
+                if(expstring !== actString)
+                {
+                    throw buildException("insertSQL()",null,"check record " + insertValue, expstring, actString);
+                }
+            }
+            
         }
         catch( e )
         {
@@ -73,7 +91,10 @@ function insertSQL(db, cl, csName, clName, insertValue, checkValue, result)
         }
         finally
         {
-            cursor.close()
+            if(cursor != null)
+            {
+                cursor.close();
+            }
         }
     }
     else
@@ -98,15 +119,29 @@ function updateSQL(db, cl, csName, clName, oldValue, newValue, checkValue, resul
     var sql = 'update '+csName+'.'+clName+' set textFields=' + newValue + ' where textFields=' + oldValue;
     if(result)
     {
+        var cursor = null;
         try
         {
             db.execUpdate(sql);
-            var cursor = cl.find({textFields:checkValue},{"_id":{"$include":0}});
-            var expstring = '{"num":3,"textFields":' + JSON.stringify(checkValue) + '}';
-            var actString = JSON.stringify(cursor.next().toObj());
-            if(expstring !== actString)
+            if(JSON.stringify(checkValue) == '{"$timestamp":"1902-12-01-00.00.00.000000"}')
             {
-                throw buildException("updateSQL()", null, "check record " + insertValue, expstring, actString);
+                var checkValue1 = {$timestamp:"1901-12-31-23.54.08.000000"};
+                var checkValue2 = {$timestamp:"1902-12-01-00.05.52.000000"};
+                var count = cl.find({textFields:{$in:[checkValue1, checkValue, checkValue2]}}).count();
+                if(Number(count) !== 1)
+                {
+                    throw buildException("updateSQL()",null,"check record " + oldValue, 1, Number(count));
+                }
+            }
+            else
+            {
+                cursor = cl.find({textFields:checkValue},{"_id":{"$include":0}});
+                var expstring = '{"num":3,"textFields":' + JSON.stringify(checkValue) + '}';
+                var actString = JSON.stringify(cursor.next().toObj());
+                if(expstring !== actString)
+                {
+                    throw buildException("updateSQL()", null, "check record " + oldValue, expstring, actString);
+                }
             }
         }
         catch( e )
@@ -115,7 +150,10 @@ function updateSQL(db, cl, csName, clName, oldValue, newValue, checkValue, resul
         }
         finally
         {
-            cursor.close()
+            if(cursor != null)
+            {
+                cursor.close();
+            }
         }
     }
     else
@@ -147,7 +185,7 @@ function selectSQL(db, csName, clName, value, checkValue, result)
             var actString = JSON.stringify(cursor.next().toObj());
             if(expstring !== actString)
             {
-                throw buildException("selectSQL()", null, "check record " + insertValue, expstring, actString);
+                throw buildException("selectSQL()", null, "check record " + value, expstring, actString);
             }
         }
         catch( e )
