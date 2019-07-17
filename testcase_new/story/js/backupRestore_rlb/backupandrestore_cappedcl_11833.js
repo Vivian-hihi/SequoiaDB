b37c0e0443c3355da757b8c478626e5733697a6a
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-17 10:34:59 +0000

    补充固定集合备份恢复用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42252 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/js/backupRestore_rlb/backupandrestore_cappedcl_11833.js
@@ -0,0 +1,57 @@
+/************************************
+*@Description: 备份恢复固定集合
+*@author:      liuxiaoxuan
+*@createdate:  2019.7.17
+*@testlinkCase:seqDB-11833
+**************************************/
+main();
+
+function main()
+{
+   if ( commIsStandalone( db ) )
+   {
+      return;
+   }
+   
+   var csName = COMMCAPPEDCSNAME + "_11833";
+   commDropCS( db, csName, true, "drop CS in the beginning" );
+   commCreateCS( db, csName, true, "", {Capped : true} );
+   var options = {Capped:true, Size:1, Max:100, AutoIndexId:false};
+   var dbcl =  commCreateCLByOption( db, csName, clName, options, false, false, "create cappedCL" );
+   
+   var groupNames = commGetCLGroups( db, csName + "." + clName );
+   var backupName = CHANGEDPREFIX + "_backup11833" ;    
+   bakInsertData( dbcl ) ;
+   bakRemoveBackups( db, backupName, true ) ;
+   // back up
+   bakBackup( this.db , { "Name": backupName, GroupName: groupNames} );
+   
+   var nodeinfo;
+   var cmd = new Cmd();
+   try
+   {
+      checkBackupInfo( db, "check default backup failed", backupName ) ;
+      var rg = db.getRG( groupNames[0] ).getDetail().next().toObj();
+      for ( var i = 0; i < rg.Group.length; ++i )
+      {
+         if ( rg.PrimaryNode === rg.Group[i].NodeID )
+         {
+            var hostName = rg.Group[i].HostName ;
+            var svcName = rg.Group[i].Service[0].Name ;
+            var dbPath = rg.Group[i].dbpath ;
+            println( "hostName: " + hostName + "svcName: " + svcName + "dbPath: " + dbPath );
+            nodeinfo = new nodeInfo( groupNames[0], hostName, svcName, dbPath);        
+            cmd = getCmdByHostName( cmd, hostName )  ;
+            break ;
+         }
+      }
+      var bakInfo = new backUpInfo( backupName, nodeinfo.dbPath + "bakfile" ) ;
+      // restore
+      sdbRestore( db, cmd, bakInfo, nodeinfo ) ;
+   }
+   finally
+   {
+      bakRemoveBackups( db, backupName, false ) ;
+   } 
+   commDropCS( db, csName, true, "drop CS in the end" );
+}
