405c25b17f3d9ca8bd9d9d096518c3b4a811f392
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-19 04:50:41 +0000

    SEQUOIADBMAINSTREAM-4518 补充自动化用例，增加正常重启节点后，再强杀此节点用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42345 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/transaction/killnode/Transaction18835.java
@@ -0,0 +1,114 @@
+package com.sequoiadb.transaction.killnode;
+
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.commlib.CommLib;
+import com.sequoiadb.commlib.GroupMgr;
+import com.sequoiadb.commlib.GroupWrapper;
+import com.sequoiadb.commlib.NodeWrapper;
+import com.sequoiadb.commlib.SdbTestBase;
+import com.sequoiadb.exception.ReliabilityException;
+import com.sequoiadb.fault.KillNode;
+import com.sequoiadb.fault.NodeRestart;
+import com.sequoiadb.task.FaultMakeTask;
+import com.sequoiadb.task.TaskMgr;
+import com.sequoiadb.transaction.common.TransUtil;
+import com.sequoiadb.transaction.common.TransferTh;
+
+/**
+ * @Description seqDB-18835:hash分区表/主子表，转账的过程中正常重启数据主节点后，再次异常重启该节点
+ * @author yinzhen
+ * @date 2019-7-17
+ *
+ */
+@Test(groups = "rcauto")
+public class Transaction18835 extends SdbTestBase {
+    private Sequoiadb sdb;
+    private String hashCLName = "cl18835_hash";
+    private String mainCLName = "cl18835_main";
+    private String subCLName1 = "subcl18835_1";
+    private String subCLName2 = "subcl18835_2";
+    private GroupMgr groupMgr;
+    private List<String> groupNames;
+
+    @BeforeClass
+    public void setUp() throws ReliabilityException {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        if (CommLib.isStandAlone(sdb)) {
+            throw new SkipException("STANDALONE MODE");
+        }
+        groupNames = CommLib.getDataGroupNames(sdb);
+        if (groupNames.size() < 2) {
+            throw new SkipException("ONE GROUP MODE");
+        }
+        groupMgr = GroupMgr.getInstance();
+        if (!groupMgr.checkBusiness(120)) {
+            throw new SkipException("GROUP ERROR");
+        }
+
+        // 创建hash分区表/主子表(主表下挂载多个子表，子表覆盖分区表)，replSize设置为1，且已切分到所有组上，切分键为账户字段
+        // 并插入数据 10000 个账户，每个账户 10000 元
+        TransUtil.createCLsAndInsertData(sdb, csName, hashCLName, mainCLName, subCLName1, subCLName2);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        try {
+            CollectionSpace cs = sdb.getCollectionSpace(csName);
+            cs.dropCollection(hashCLName);
+            cs.dropCollection(mainCLName);
+        } finally {
+            if (sdb != null) {
+                sdb.close();
+            }
+        }
+    }
+
+    @DataProvider(name = "getCL")
+    private Object[][] getCLName() {
+        return new Object[][] { { hashCLName }, { mainCLName } };
+    }
+
+    @Test(dataProvider = "getCL")
+    public void test(String clName) throws ReliabilityException {
+        // 正常重启一个数据主节点
+        TaskMgr taskMgr = new TaskMgr();
+        String groupName = groupNames.get(0);
+        GroupWrapper group = groupMgr.getGroupByName(groupName);
+        NodeWrapper node = group.getMaster();
+        FaultMakeTask task = NodeRestart.getFaultMakeTask(node, 180, 10);
+        taskMgr.addTask(task);
+
+        for (int i = 0; i < 200; i++) {
+            taskMgr.addTask(new TransferTh(csName, clName));
+        }
+        taskMgr.execute();
+
+        Assert.assertTrue(taskMgr.isAllSuccess(), taskMgr.getErrorMsg());
+        Assert.assertTrue(groupMgr.checkBusinessWithLSN(300), "GROUP ERROR");
+
+        // 待集群正常后，再次异常重启该节点
+        task = KillNode.getFaultMakeTask(node, 5);
+        taskMgr.clear();
+        taskMgr.addTask(task);
+        taskMgr.execute();
+        Assert.assertTrue(taskMgr.isAllSuccess(), taskMgr.getErrorMsg());
+        Assert.assertTrue(groupMgr.checkBusinessWithLSN(300), "GROUP ERROR");
+
+        // 待集群正常后，查询所有账户的金额总和
+        DBCursor cursor = sdb.exec("select sum(balance) as balance from " + csName + "." + clName);
+        double balance = (double) cursor.getNext().get("balance");
+        cursor.close();
+        Assert.assertEquals((int) balance, 100000000);
+    }
+}
