a4236fca3fc79d2f961c1d8e4d5401f5d022697b
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-07-19 02:20:47 +0000

    SEQUOIADBMAINSTREAM-4688,补充自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42341 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/rcauto/Transaction18890.java
@@ -0,0 +1,89 @@
+package com.sequoiadb.transaction.rcauto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.CommLib;
+import com.sequoiadb.testcommon.SdbTestBase;
+
+/**
+ * @testcase seqDB-18890:分区表执行多次游标查询，不取完游标
+ * @date 2019-7-19
+ * @author zhaoyu
+ *
+ */
+@Test(groups = "rcauto")
+public class Transaction18890 extends SdbTestBase {
+    private Sequoiadb sdb = null;
+    private String clName = "cl18890";
+    private DBCollection cl = null;
+
+    @BeforeClass
+    public void setUp() {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        if (CommLib.isStandAlone(sdb)) {
+            throw new SkipException("STANDALONE MODE");
+        }
+        if (CommLib.OneGroupMode(sdb)) {
+            throw new SkipException("less than two groups");
+        }
+
+        cl = sdb.getCollectionSpace(csName).createCollection(clName,
+                (BSONObject) JSON.parse("{ShardingKey:{a:1}, ShardingType:'hash', AutoSplit: true}"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        try {
+            CollectionSpace cs = sdb.getCollectionSpace(csName);
+            if (cs.isCollectionExist(clName)) {
+                cs.dropCollection(clName);
+            }
+        } finally {
+            if (!sdb.isClosed()) {
+                sdb.close();
+            }
+        }
+
+    }
+
+    @Test
+    public void test() {
+        // 插入记录
+        insertData();
+
+        // 开启游标查询，游标未走完，关闭游标后，再次执行游标查询
+        DBCursor cursor = cl.query();
+        cursor.getNext();
+        cursor.close();
+
+        DBCursor cursor1 = cl.query();
+        cursor1.getNext();
+        cursor1.close();
+    }
+
+    private void insertData() {
+        for (int j = 0; j < 10; j++) {
+            List<BSONObject> records = new ArrayList<>();
+            for (int i = 0; i < 1000; i++) {
+                BSONObject record = (BSONObject) JSON.parse("{a:" + i + ", b:" + i + "}");
+                records.add(record);
+            }
+            Collections.shuffle(records);
+            cl.insert(records);
+        }
+
+    }
+}
