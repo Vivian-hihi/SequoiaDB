5f5624db35ff132e751b5dfde1c1c1712bd0d74d
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-08-09 04:07:24 +0000

    replSize不为1时，停止节点的时候记录插入报-252，实际记录已插入，属于正常工程，用例需规避
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42605 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/java/src/test/java/com/sequoiadb/fulltext/killnode/Fulltext12079.java
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/fulltext/killnode/Fulltext12079.java
@@ -86,7 +86,15 @@ public class Fulltext12079 extends SdbTestBase {
             node.start();
         }
         Assert.assertEquals(groupMgr.checkBusinessWithLSN(600), true);
-        Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIndexName, insertNum));
+        long clCount = cl.getCount("{'':{'$Text':{query:{match_all:{}}}}}");
+
+        // 节点停止的时候，插入记录未校验则报-252，可能存在记录插入成功的情况，因此预期结果可能为2
+        if (clCount != insertNum && clCount != insertNum + 1) {
+            Assert.fail("expect cl count: 1 or 2, but actual cl count:" + clCount);
+        }
+        Assert.assertTrue(FullTextUtils.isCLConsistency(cl));
+        Assert.assertTrue(FullTextUtils.isCLDataConsistency(cl));
+        Assert.assertTrue(FullTextUtils.isIndexConsistency(cl, fullIndexName));
 
     }
 
