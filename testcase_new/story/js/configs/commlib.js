b57bc55c06cc6dd51db99e3fb47a426ed77b193a
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-08-10 06:29:09 +0000

    SEQUOIADBMAINSTREAM-4809 屏蔽run级别boolean配置项测试
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42640 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/configs/commlib.js
+++ testcase_new/story/js/configs/commlib.js
@@ -49,11 +49,11 @@ var Configs = ( function() {
       this.runConfigs.push( new ConfDesp( "startshifttime", "int", 600, 1200, "some" ) ) ;
       this.runConfigs.push( new ConfDesp( "preferedinstance", "string", "M", "S", 123 ) ) ;
       this.runConfigs.push( new ConfDesp( "preferedinstancemode", "string", "random", "ordered", 123 ) ) ;
-      this.runConfigs.push( new ConfDesp( "directioinlob", "bool", "FALSE", "TRUE", "1234" ) ) ;
-      this.runConfigs.push( new ConfDesp( "sparsefile", "bool", "FALSE", "TRUE", null ) ) ;
+      //this.runConfigs.push( new ConfDesp( "directioinlob", "bool", "FALSE", "TRUE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "sparsefile", "bool", "FALSE", "TRUE", null ) ) ;
       this.runConfigs.push( new ConfDesp( "weight", "int", 10, 20, "asdafd" ) ) ;
-      this.runConfigs.push( new ConfDesp( "usessl", "bool", "FALSE", "TRUE", "1234" ) ) ;
-      this.runConfigs.push( new ConfDesp( "auth", "bool", "TRUE", "FALSE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "usessl", "bool", "FALSE", "TRUE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "auth", "bool", "TRUE", "FALSE", "1234" ) ) ;
       this.runConfigs.push( new ConfDesp( "planbuckets", "int", 500, 200, "125" ) ) ;
       this.runConfigs.push( new ConfDesp( "optimeout", "int", 60000, 100000, "20" ) ) ;
       this.runConfigs.push( new ConfDesp( "overflowratio", "int", 12, 22, "11" ) ) ;
@@ -66,17 +66,17 @@ var Configs = ( function() {
       this.runConfigs.push( new ConfDesp( "maxsyncjob", "int", 10, 20, "gghttr" ) ) ;
       this.runConfigs.push( new ConfDesp( "syncinterval", "int", 10000, 20000, "sadafe" ) ) ;
       this.runConfigs.push( new ConfDesp( "syncrecordnum", "int", 0, 1000, "ssss" ) ) ;
-      this.runConfigs.push( new ConfDesp( "syncdeep", "bool", "FALSE", "TRUE", "1234" ) ) ;
-      this.runConfigs.push( new ConfDesp( "archivecompresson", "bool", "TRUE", "FALSE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "syncdeep", "bool", "FALSE", "TRUE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "archivecompresson", "bool", "TRUE", "FALSE", "1234" ) ) ;
       this.runConfigs.push( new ConfDesp( "archivetimeout", "int", 600, 300, "1213" ) ) ;
       this.runConfigs.push( new ConfDesp( "archiveexpired", "int", 240, 120, "no" ) ) ;
       this.runConfigs.push( new ConfDesp( "archivequota", "int", 10, 20, "why" ) ) ;
       this.runConfigs.push( new ConfDesp( "dataerrorop", "int", 1, 2, "I" ) ) ;
       this.runConfigs.push( new ConfDesp( "dmschkinterval", "int", 0, 120, "O" ) ) ;
-      this.runConfigs.push( new ConfDesp( "perfstat", "bool", "FALSE", "TRUE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "perfstat", "bool", "FALSE", "TRUE", "1234" ) ) ;
       this.runConfigs.push( new ConfDesp( "optcostthreshold", "int", 20, 10, "M" ) ) ;
       this.runConfigs.push( new ConfDesp( "maxconn", "int", 0, 3000, "12345" ) ) ;
-      this.runConfigs.push( new ConfDesp( "enablemixcmp", "bool", "FALSE", "TRUE", "1234" ) ) ;
+      //this.runConfigs.push( new ConfDesp( "enablemixcmp", "bool", "FALSE", "TRUE", "1234" ) ) ;
       this.runConfigs.push( new ConfDesp( "plancachelevel", "int", 3, 4, "88" ) ) ;
 
       // register reboot configs
