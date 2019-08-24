d733dc553629e131b41a49b979c0f3cf978acef9
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-08-24 01:09:37 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42817 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/fulltext/commlib.js
+++
@@ -1,7 +0,0 @@
-/****************************************************
-@description:	commlib of FullText		
-@Date : 2018-09-18
-@Author: liuxiaoxuan
-                     
-****************************************************/
-import ( "../lib/fulltext_commlib.js" );
