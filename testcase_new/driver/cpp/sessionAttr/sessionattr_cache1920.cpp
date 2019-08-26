9fb5be8b07fc7c4a117ccda47fe46d0e723dced0
wangwenjing <wangwenjing@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-08-26 02:30:00 +0000

    SEQUOIADBMAINSTREAM-4862 完善
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42853 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/driver/cpp/sessionAttr/sessionattr_cache1920.cpp
+++ testcase_new/driver/cpp/sessionAttr/sessionattr_cache1920.cpp
@@ -51,6 +51,10 @@ TEST_F( sessionAttrTestCache_1920, getFromCache )
    rc = db.updateConfig(BSON("transactiontimeout" << origTransTimeOut), BSON("Global" << TRUE)) ;
    ASSERT_EQ( rc, SDB_OK ) ;
  
+   rc = db.getSessionAttr( ret ) ;
+   ASSERT_EQ( rc, SDB_OK ) ;
+   ASSERT_EQ(ret.getIntField("TransTimeout"), origTransTimeOut + 60 );   
+
    rc = db.getSessionAttr( ret, FALSE ) ;
    cout << ret.toString() << endl ;
    ASSERT_EQ( rc, SDB_OK ) ;
@@ -63,12 +67,17 @@ TEST_F( sessionAttrTestCache_1920, clearCache )
    int origTransTimeOut = orig.getIntField("TransTimeout") ;
    INT32 rc = db.updateConfig(BSON("transactiontimeout" << origTransTimeOut + 60), BSON("Global" << TRUE)) ;
    ASSERT_EQ( rc, SDB_OK ) ;
-   
+  
+   BSONObj ret ; 
+   rc = db.getSessionAttr( ret ) ;
+   ASSERT_EQ( rc, SDB_OK ) ;
+   cout << ret.toString() << endl ;
+   ASSERT_EQ(ret.getIntField("TransTimeout"), origTransTimeOut  );
+
    BSONObj emptyObj ;
    rc = db.setSessionAttr( emptyObj ) ;
    ASSERT_EQ( rc, SDB_OK ) ;
    
-   BSONObj ret ;
    rc = db.getSessionAttr( ret ) ;
    ASSERT_EQ( rc, SDB_OK ) ;
    cout << ret.toString() << endl ;
