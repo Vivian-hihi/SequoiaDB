95561c79deec9db0986e52283dbd7e47ed18e868
liuxiaoxuan <liuxiaoxuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-08-29 09:13:57 +0000

    优化耗时js用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@42932 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/query/split_queryByOr_7466.js
+++ testcase_new/story/js/query/split_queryByOr_7466.js
@@ -5,179 +5,63 @@
 *@Modify list :
 *               2014-07-17 pusheng Ding  Init
 ******************************************************************************/
-CHANGEDPREFIX_IDX = CHANGEDPREFIX+"idx" ;
-
 function main()
 {
-   //get ReplicaGroups
-   try{
-      var grouplist = Array();
-      var cur = db.listReplicaGroups();
-      while(cur.next()){
-         if(cur.current().toObj()['GroupID'] >= DATA_GROUP_ID_BEGIN ){
-            grouplist.push(cur.current().toObj()['GroupName']);
-         }
-      }
-      var groups_num = grouplist.length;
-      if(groups_num == 1)
-      {
-         println("only one ReplicaGroup:" + grouplist + " Skip the testcase");
-         return;
-      }
-   }catch(e)
+   if ( commGetGroupsNum(db) < 2 )
    {
-      println("get ReplicaGroups info fail! rc="+e);
-      throw e;
-   }
-   println("ReplicaGroups: " + grouplist);
-
-   //create CS
-   try{
-      var varCS = commCreateCS( db, COMMCSNAME, true, "create CS" );
-   }catch(e)
-   {
-      println("can't create CS:" + COMMCSNAME + " rc="+e);
-      throw e;
-   }
-   println("createCS " + COMMCSNAME + " finished");
-
-   //create rangeCL
-   try{
-      var varCL = varCS.createCL(COMMCLNAME,{ShardingKey:{b:1},ShardingType:'range',ReplSize:0});
-      var sn1 = db.snapshot(8,{Name:COMMCSNAME+"."+COMMCLNAME});
-      var sourceGroup = sn1.current().toObj()['CataInfo'][0]['GroupName'];
-   }catch(e)
-   {
-      println("can't create range-CL:" + COMMCLNAME + " rc="+e);
-      throw e;
-   }
-   println("createCL " + COMMCLNAME + " at ReplicaGroup:" + sourceGroup + " finished!");
+       println("--least two groups");
+       return ;
+   }  
+   
+   //create CL
+   var groups = commGetGroups( db );
+   var srcGroupName = groups[0][0].GroupName;
+   var destGroupName = groups[1][0].GroupName;
+   var clName = COMMCLNAME + "_7466";
+   commDropCL( db, COMMCSNAME, clName, true, true, "drop CL in the beginning." );
+   var dbcl = commCreateCLByOption( db, COMMCSNAME, clName, {ShardingKey:{b:1}, ShardingType:'range', ReplSize:0, Group:srcGroupName}, true );
 
    //insert data
-   try{
-         var docs = [];
-         for(var i=0;i<10000;i++)
-         {
-            docs.push({a:i-10000,b:i,c:"abcdefghijkl"+i});
-         }
-         varCL.insert(docs);
-   }
-   catch(e)
+   var docs = [];
+   for( var i = 0; i < 10000; i++ )
    {
-      println("insert-data fail! rc="+e);
+       docs.push( { a:i-10000, b:i, c:"abcdefghijkl"+i } );
    }
-   println("insert-data succ!");
+   dbcl.insert( docs );
 
    //create index
-   try{
-      varCL.createIndex(CHANGEDPREFIX_IDX,{a:1},false,false);
-   }catch(e)
-   {
-      println("cat't create index " + CHANGEDPREFIX_IDX + " rc=" + e);
-      throw e;
-   }
-   println("create index finished!");
+   dbcl.createIndex( "idx_7466", {a:1}, false, false );
 
    //split
-   try{
-      if(groups_num>1){
-         var tarGroupIndex=-1;
-         var stepId = 3000;
-         var partId = 3;
-         var lowId = 0;
-         var highId = 0;
-         for(var i=0;i<partId;i++){
-            tarGroupIndex++;
-            if(tarGroupIndex == groups_num)
-               tarGroupIndex=0;
-            if(grouplist[tarGroupIndex]==sourceGroup)
-            {
-               i--;
-               continue;
-            }
-            lowId = i*stepId;
-            highId = (i+1)*stepId;
-            varCL.splitAsync(sourceGroup, grouplist[tarGroupIndex],{b:lowId},{b:highId});
-            println(COMMCLNAME+" split from "+sourceGroup+" to "+ grouplist[tarGroupIndex]+" {b:"+lowId+"} {b:"+highId+"}");
-         }
-         println("split success!");
-      }
-      else{
-         println("can't split to groups!groupsNum is "+groups_num);
-      }
-   }catch(e)
-   {
-      println("split fail! rc="+e);
-      throw e;
-   }
+   dbcl.split( srcGroupName, destGroupName, 50 );
 
    //select * from ... where a<=-9000 or a>=-1000 or b=2000
-   try{
-      db.setSessionAttr({PreferedInstance:"M"});
-      var sel = varCL.find({$or:[{a:{$lte:-9000}},{a:{$gte:-1000}}, {b:2000}]}).sort({a:1});
-      var size=0;
-      var flag=true;
+   try
+   {
+      var cursor = dbcl.find( { $or:[{a:{$lte:-9000}},{a:{$gte:-1000}}, {b:2000}] } ).sort( {a:1} );
+      var count = 0;
       var expected = 2002;
-      while(sel.next())
-     {
-      size++;
-      if(size>expected)
+      while( cursor.next() )
       {
-         flag = false;
-         throw 1;
-      }
-      var ret = sel.current();
-      if(!(ret.toObj()['a']<=-9000 || ret.toObj()['a']>=-1000 || ret.toObj()['b']==2000))
+         count++;
+         var ret = cursor.current();
+         if( !(ret.toObj()['a']<=-9000 || ret.toObj()['a']>=-1000 || ret.toObj()['b']==2000) )
+         {
+            println("return incorrect record! " + ret);
+            throw "check result fail";
+         }
+      }  
+      if( count != expected )
       {
-         flag = false;
-         throw 2;
-      }
-     }
-     sel.close();
-     if(flag && size!=expected)
-     {
-      flag = false;
-      throw 1;
-     }
-   }catch(e){
-      if(e!=1 && e!=2)
-      {
-         println("select data fail! rc="+e);
-         throw e;
-      }
-      else if(e==1)
-      {
-         println("return rows not expected! expected:"+expected+" return:"+ size +(size>expected?" or more":""));
-         throw e;
-      }
-      else if(e==2)
-      {
-         println("return incorrect record! " + ret);
-         throw e;
+          println("return rows not expected! expected:" + expected + " return:" + count + ( count > expected? " or more":""));
+          throw "check result fail";
       }
    }
-}
-
-try
-{
-   if( true == commIsStandalone( db ) )
+   finally
    {
-      println( "run mode is standalone" );
+      cursor.close();
    }
-   else
-   {
-      commDropCL( db, COMMCSNAME, COMMCLNAME, true, true,
-                  "drop CL in the beginning" );
-      main();
-      commDropCL( db, COMMCSNAME, COMMCLNAME, true, true,
-                  "drop CL in the end" );
-      db.close();
-   }
+   
+   commDropCL( db, COMMCSNAME, clName, true, true, "drop CL in the end" );              
 }
-catch( e )
-{
-   commDropCL( db, COMMCSNAME, COMMCLNAME, true, true,
-               "drop CL in the end, wrong" );
-   db.close();
-   throw e ;
-}
+main();
