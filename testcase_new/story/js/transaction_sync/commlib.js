64c09d91a7aa3d2ce390243256524b889bdf2289
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-20 01:16:03 +0000

    已存在对应的java用例，删除js用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43277 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/js/transaction_sync/commlib.js
+++
@@ -1,7 +0,0 @@
-/****************************************************
-@description:	commlib of transaction		
-@Date : 2019-09-12
-@Author: zhaoyu
-                     
-****************************************************/
-import ( "../lib/transaction_commlib.js" );
