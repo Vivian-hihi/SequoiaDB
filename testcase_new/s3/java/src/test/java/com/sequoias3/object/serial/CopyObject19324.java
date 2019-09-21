a43ca10ad28608bce422de6753f194470cdd789d
wuyan <wuyan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-21 04:42:46 +0000

    新增复制对象用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43311 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/serial/CopyObject19324.java
@@ -0,0 +1,84 @@
+package com.sequoias3.object.serial;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectMetadataRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.TestTools;
+import com.sequoias3.testcommon.s3utils.ObjectUtils;
+
+/**
+ * @Description seqDB-19324:复制对象，指定源对象内容较大（自动化用例验证复制1G大小文件）
+ * @author wuyan
+ * @Date 2019.09.18
+ * @version 1.00
+ */
+public class CopyObject19324 extends S3TestBase {
+
+    private boolean runSuccess = false;
+    private String srcKeyName = "/srcObject19324";
+    private String destKeyName = "/dest/object19324";
+    private String bucketName = "bucket19324";
+    private AmazonS3 s3Client = null;
+    private int fileSize = 1024 * 1024 * 1024 * 1;
+    private File localPath = null;
+    private String filePath = null;
+
+    @BeforeClass
+    private void setUp() throws IOException {
+        localPath = new File(S3TestBase.workDir + File.separator + TestTools.getClassName());
+        filePath = localPath + File.separator + "localFile_" + fileSize + ".txt";
+        TestTools.LocalFile.removeFile(localPath);
+        TestTools.LocalFile.createDir(localPath.toString());
+        TestTools.LocalFile.createFile(filePath, fileSize);
+
+        s3Client = CommLib.buildS3Client();
+        CommLib.clearBucket(s3Client, bucketName);
+        s3Client.createBucket(bucketName);
+        s3Client.putObject(bucketName, srcKeyName, new File(filePath));
+    }
+
+    @Test
+    public void testCopyObject() throws Exception {
+        s3Client.copyObject(bucketName, srcKeyName, bucketName, destKeyName);
+
+        checkObjectAttributeInfo(bucketName, destKeyName);
+        checkObjectContent(bucketName, destKeyName);
+
+        runSuccess = true;
+    }
+
+    @AfterClass
+    private void tearDown() {
+        try {
+            if (runSuccess) {
+                CommLib.clearBucket(s3Client, bucketName);
+                TestTools.LocalFile.removeFile(localPath);
+            }
+        } finally {
+            s3Client.shutdown();
+        }
+    }
+
+    private void checkObjectContent(String bucketName, String keyName) throws Exception {
+        String downfileMd5 = ObjectUtils.getMd5OfObject(s3Client, localPath, bucketName, keyName);
+        Assert.assertEquals(downfileMd5, TestTools.getMD5(filePath));
+    }
+
+    private void checkObjectAttributeInfo(String bucketName, String keyName) throws IOException {
+        GetObjectMetadataRequest request = new GetObjectMetadataRequest(bucketName, keyName);
+        ObjectMetadata result = s3Client.getObjectMetadata(request);
+
+        Assert.assertEquals(result.getETag(), TestTools.getMD5(filePath));
+        Assert.assertEquals(result.getContentLength(), fileSize);
+    }
+}
