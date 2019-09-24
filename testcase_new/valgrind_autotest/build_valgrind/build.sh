225f391ca52c49f95e3f020c0be549924ab56922
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-24 09:36:46 +0000

    valgrind自动化脚本
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43374 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/valgrind_autotest/build_valgrind/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+curpath=$(cd `dirname $0`; pwd)
+source ${curpath}/func.sh
+
+args=`getopt -o h -l help -n 'build' -- "$@"`
+test $? -ne 0 && exit $?
+eval set -- "${args}"
+
+while true
+do
+    case "$1" in
+        -h | --help )     help
+                          exit 0
+                          ;;
+        -- )              shift
+                          break
+                          ;;
+        * )               echo "Unknown argument: $1"
+                          exit 4
+                          ;;
+    esac
+done
+
+# Chechk that the IP address is spelled correctly
+check_host
+ssh_copy_id
+
+# Send tar packages and install them on each host
+if [[ "${needcover}"M = "true"M || "${needinstall}"M = "true"M ]];then
+    cp_package
+    install_sdb
+fi
+
+# Start valgrind and run testcase then stop
+startvalgrind
+runtestcase
+stop_valgrind
+
+# Get run valgrind result
+get_result
