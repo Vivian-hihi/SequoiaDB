bef311d1f43a5492d464cecbb771aed63023ba0b
fanyu <fanyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-26 06:44:24 +0000

    回归问题单：4392
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43446 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/s3/java/src/test/java/com/sequoias3/delimiter/serial/ListObjectsWithDelimiter18120.java
+++ testcase_new/s3/java/src/test/java/com/sequoias3/delimiter/serial/ListObjectsWithDelimiter18120.java
@@ -1,51 +1,39 @@
 package com.sequoias3.delimiter.serial;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.sequoias3.delimiter.DelimiterConfiguration;
+import com.sequoias3.testcommon.CommLib;
+import com.sequoias3.testcommon.S3TestBase;
+import com.sequoias3.testcommon.s3utils.DelimiterUtils;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ListObjectsV2Request;
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.sequoias3.testcommon.CommLib;
-import com.sequoias3.testcommon.S3TestBase;
-import com.sequoias3.testcommon.s3utils.DelimiterUtils;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @Description seqDB-18120: To get a list of objects within a bucket.specify
- *              matching delimiter with different formats
  * @author wuyan
- * @Date 2019.4.23
  * @version 1.00
+ * matching delimiter with different formats
+ * @Date 2019.4.23
  */
 public class ListObjectsWithDelimiter18120 extends S3TestBase {
-    @DataProvider(name = "listWithDelimiterProvider")
-    public Object[][] generatePageSize() {
-        return new Object[][] {
-                // the parameter : delimiter and matchObjectPosition
-                // test a: delimiter type is letters and numbers
-                new Object[] { "/test1/AZ/", 0 },
-                // test b:delimiter type is special chararcter
-                new Object[] { "/test*_.(d!-t'')", 1 },
-                // test c:delimiter type is &@:,$=+?;ASCII
-                new Object[] { "/test&@:,$=+? t_1", 2 }, new Object[] { "\040te\065s", 3 },
-                // http://jira:8080/browse/SEQUOIADBMAINSTREAM-4392
-                // new Object[] { "\010te\065s", 3 },
-                // new Object[] { "/\031te\41a\57", 4 },
-                // test d: delimiter type is 、^`><{}[]#%"~|
-                new Object[] { "test、^`><{}[]#%\"~|_1", 5 } };
-    }
-
     private boolean runSuccess = false;
     private String bucketName = "bucket18120";
+    private String encodingType = "utf-8";
     private AmazonS3 s3Client = null;
     private List<String> keyList = null;
 
@@ -54,15 +42,38 @@ public class ListObjectsWithDelimiter18120 extends S3TestBase {
         s3Client = CommLib.buildS3Client();
         CommLib.clearBucket(s3Client, bucketName);
         s3Client.createBucket(bucketName);
-        String[] keyNames = { "/test1/AZ/18120_test1.txt", "/test*_.(d!-t'')/18120_test2.png",
+        String[] keyNames = {"/test1/AZ/18120_test1.txt", "/test*_.(d!-t'')/18120_test2.png",
                 "/test&@:,$=+? t_18120_test3", "\040te\065st_18120_test4", "/\035te\41a\57st_18120_test5",
-                "test、^`><{}[]#%\"~|_18120_test6" };
+                "test、^`><{}[]#%\"~|_18120_test6"};
         keyList = putObjects(keyNames);
+    }
+
+    @DataProvider(name = "listWithDelimiterProvider")
+    public Object[][] generatePageSize() throws UnsupportedEncodingException {
+        return new Object[][]{
+                // the parameter : delimiter and matchObjectPosition
+                // test a: delimiter type is letters and numbers
+                new Object[]{URLEncoder.encode("/test1/AZ/", encodingType), 0},
+                // test b:delimiter type is special chararcter
+                new Object[]{URLEncoder.encode("/test*_.(d!-t'')", encodingType), 1},
+                // test c:delimiter type is &@:,$=+?;ASCII
+                new Object[]{URLEncoder.encode("/test&@:,$=+? t_1", encodingType), 2},
+                // http://jira:8080/browse/SEQUOIADBMAINSTREAM-4392
+                new Object[]{URLEncoder.encode("\040te\065s", encodingType), 3},
+                new Object[]{URLEncoder.encode("/\035te\41a\57", encodingType), 4},
+                // test d: delimiter type is 、^`><{}[]#%"~|
+                new Object[]{URLEncoder.encode("test、^`><{}[]#%\"~|_1", encodingType), 5}
+        };
     }
 
     @Test(dataProvider = "listWithDelimiterProvider")
     public void testListObjects(String delimiter, int position) throws Exception {
-        DelimiterUtils.updateDelimiterSuccessAgain(bucketName, delimiter);
+        updateDelimiterSuccessAgain(bucketName, delimiter, S3TestBase.s3AccessKeyId, "url");
+        DelimiterConfiguration currDelimiter = DelimiterUtils.getDelimiterWithEncodingType(bucketName,
+                S3TestBase.s3AccessKeyId, "url");
+        Assert.assertEquals(URLDecoder.decode(currDelimiter.getDelimiter(), encodingType),
+                URLDecoder.decode(delimiter, encodingType));
+        Assert.assertEquals(currDelimiter.getStatus(), "Normal");
         listObjectsAndCheckResult(delimiter, position);
         runSuccess = true;
     }
@@ -80,14 +91,14 @@ public class ListObjectsWithDelimiter18120 extends S3TestBase {
 
     private void listObjectsAndCheckResult(String delimiter, int position) throws IOException {
         List<String> expKeyList = new ArrayList<>(keyList);
-        ListObjectsV2Request request = new ListObjectsV2Request().withBucketName(bucketName).withEncodingType("url")
-                .withDelimiter(delimiter);
+        ListObjectsV2Request request = new ListObjectsV2Request().withBucketName(bucketName)
+                .withEncodingType("url")
+                .withDelimiter(URLDecoder.decode(delimiter, encodingType));
         ListObjectsV2Result result = s3Client.listObjectsV2(request);
         List<String> commonPrefixes = result.getCommonPrefixes();
         // matching delimiter displays only 1 record
         Assert.assertEquals(commonPrefixes.size(), 1);
-        Assert.assertEquals(commonPrefixes.get(0), delimiter);
-
+        Assert.assertEquals(commonPrefixes.get(0), URLDecoder.decode(delimiter, "utf-8"));
         // objects do not match delimiter are displayed in contents,num is 5
         List<S3ObjectSummary> objects = result.getObjectSummaries();
         int contentsNums = 5;
@@ -97,7 +108,6 @@ public class ListObjectsWithDelimiter18120 extends S3TestBase {
             String key = os.getKey();
             queryKeyList.add(key);
         }
-
         // check the keyName
         expKeyList.remove(position);
         Collections.sort(expKeyList);
@@ -114,5 +124,23 @@ public class ListObjectsWithDelimiter18120 extends S3TestBase {
             keyList.add(keyName);
         }
         return keyList;
+    }
+
+    public static void updateDelimiterSuccessAgain(String bucketName, String delimiter, String accessKeyId,
+                                                   String encodingType) throws Exception {
+        int interval = 10000;
+        int tryTimes = 300;
+        while (tryTimes-- > 0) {
+            try {
+                DelimiterUtils.putBucketDelimiterWithEncodingType(bucketName, delimiter, accessKeyId, encodingType);
+                return;
+            } catch (AmazonS3Exception e) {
+                if (e.getStatusCode() != 409) {
+                    throw e;
+                }
+                Thread.sleep(interval);
+            }
+        }
+        throw new Exception("update delimiter fail,delimiter = " + delimiter);
     }
 }
