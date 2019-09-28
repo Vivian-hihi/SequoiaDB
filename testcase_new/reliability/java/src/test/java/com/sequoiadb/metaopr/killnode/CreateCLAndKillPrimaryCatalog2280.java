7070f8d8cf6d9e2b46b781bfa8505e5ed0820cf7
luweikang <luweikang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-09-28 06:10:41 +0000

    检查创建cl设置replsize为0，确保cl已完全创建再进行检查
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43481 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/reliability/java/src/test/java/com/sequoiadb/metaopr/killnode/CreateCLAndKillPrimaryCatalog2280.java
+++ testcase_new/reliability/java/src/test/java/com/sequoiadb/metaopr/killnode/CreateCLAndKillPrimaryCatalog2280.java
@@ -1,20 +1,13 @@
 package com.sequoiadb.metaopr.killnode;
 
-import com.sequoiadb.base.CollectionSpace;
-import com.sequoiadb.base.DBCollection;
-import com.sequoiadb.base.DBCursor;
-import com.sequoiadb.base.Sequoiadb;
-import com.sequoiadb.commlib.GroupMgr;
-import com.sequoiadb.commlib.GroupWrapper;
-import com.sequoiadb.commlib.NodeWrapper;
-import com.sequoiadb.commlib.SdbTestBase;
-import com.sequoiadb.exception.BaseException;
-import com.sequoiadb.exception.ReliabilityException;
-import com.sequoiadb.fault.KillNode;
-import com.sequoiadb.metaopr.diskfull.Utils ;
-import com.sequoiadb.task.FaultMakeTask;
-import com.sequoiadb.task.OperateTask;
-import com.sequoiadb.task.TaskMgr;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
@@ -26,18 +19,31 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+import com.sequoiadb.base.CollectionSpace;
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.commlib.GroupMgr;
+import com.sequoiadb.commlib.GroupWrapper;
+import com.sequoiadb.commlib.NodeWrapper;
+import com.sequoiadb.commlib.SdbTestBase;
+import com.sequoiadb.exception.BaseException;
+import com.sequoiadb.exception.ReliabilityException;
+import com.sequoiadb.fault.KillNode;
+import com.sequoiadb.metaopr.diskfull.Utils;
+import com.sequoiadb.task.FaultMakeTask;
+import com.sequoiadb.task.OperateTask;
+import com.sequoiadb.task.TaskMgr;
 
 /**
-* FileName: CreateCLAndKillPrimaryCatalog2280.java
-* test content:when create cl(designated domain), kill -9  the catalog group master node             
-* testlink case:seqDB-2280
-* @author wuyan
-    * @Date    2017.4.24
-* @version 1.00
-*/
-
+ * FileName: CreateCLAndKillPrimaryCatalog2280.java test content:when create
+ * cl(designated domain), kill -9 the catalog group master node testlink
+ * case:seqDB-2280
+ * 
+ * @author wuyan
+ * @Date 2017.4.24
+ * @version 1.00
+ */
 
 public class CreateCLAndKillPrimaryCatalog2280 extends SdbTestBase {
     private GroupMgr groupMgr = null;
@@ -52,16 +58,16 @@ public class CreateCLAndKillPrimaryCatalog2280 extends SdbTestBase {
     private List<String> groupNames = null;
 
     @BeforeClass
-    public void setUp() {        
+    public void setUp() {
         try {
             System.out.println(this.getClass().getName() + " begin at:"
                     + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
-            
+
             groupMgr = GroupMgr.getInstance();
             if (!groupMgr.checkBusiness()) {
                 throw new SkipException("checkBusiness failed");
             }
-            
+
             sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
             domainName = "domain_2280";
             createDomain(domainName);
@@ -69,13 +75,13 @@ public class CreateCLAndKillPrimaryCatalog2280 extends SdbTestBase {
         } catch (ReliabilityException e) {
             Assert.fail(this.getClass().getName() + " setUp error, error description:" + e.getMessage() + "\r\n"
                     + Utils.getKeyStack(e, this));
-        } 
+        }
     }
 
     @Test
-    public void test() {        
+    public void test() {
         try {
-        	GroupWrapper cataGroup = groupMgr.getGroupByName("SYSCatalogGroup");
+            GroupWrapper cataGroup = groupMgr.getGroupByName("SYSCatalogGroup");
             NodeWrapper priNode = cataGroup.getMaster();
 
             FaultMakeTask faultTask = KillNode.getFaultMakeTask(priNode.hostName(), priNode.svcName(), 5);
@@ -83,181 +89,176 @@ public class CreateCLAndKillPrimaryCatalog2280 extends SdbTestBase {
             CreateCLTask cTask = new CreateCLTask();
             mgr.addTask(cTask);
             mgr.execute();
-            
-            //TaskMgr check if there is any exception
-            Assert.assertEquals(mgr.isAllSuccess(), true, mgr.getErrorMsg()); 
-            
+
+            // TaskMgr check if there is any exception
+            Assert.assertEquals(mgr.isAllSuccess(), true, mgr.getErrorMsg());
+
             Assert.assertEquals(groupMgr.checkBusinessWithLSN(600), true, "check LSN consistency fail");
-                        
-            //check result
+
+            // check result
             checkCreateCLResult();
-            Utils.checkConsistency(groupMgr);           
-           
-            //Normal operating environment
+            Utils.checkConsistency(groupMgr);
+
+            // Normal operating environment
             clearFlag = true;
         } catch (ReliabilityException e) {
             e.printStackTrace();
             Assert.fail(e.getMessage());
-        } 
+        }
     }
 
     @AfterClass
     public void tearDown() {
         try {
-        	if (clearFlag) {                
-        		sdb.dropCollectionSpace(csName);
-        		sdb.dropDomain(domainName);
-            }            
-        }catch (BaseException e) {
+            if (clearFlag) {
+                sdb.dropCollectionSpace(csName);
+                sdb.dropDomain(domainName);
+            }
+        } catch (BaseException e) {
             Assert.fail(e.getMessage() + "\r\n" + Utils.getKeyStack(e, this));
-        }finally {
-        	if (sdb != null) {
-        		sdb.close();
-        	}
-        	System.out.println(this.getClass().getName() + " end at:"
+        } finally {
+            if (sdb != null) {
+                sdb.close();
+            }
+            System.out.println(this.getClass().getName() + " end at:"
                     + new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS").format(new Date()));
         }
     }
 
-    
-    private class CreateCLTask extends OperateTask {    
+    private class CreateCLTask extends OperateTask {
         @Override
-        public void exec() throws Exception {       
-            try( Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {                
-                CollectionSpace commCS = db.getCollectionSpace(csName);                
-                BSONObject options = (BSONObject)JSON.parse("{ShardingKey:{no:1},ShardingType:'hash',Partition:4096}");
+        public void exec() throws Exception {
+            try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
+                CollectionSpace commCS = db.getCollectionSpace(csName);
+                BSONObject options = (BSONObject) JSON.parse("{ShardingKey:{no:1},ShardingType:'hash',Partition:4096}");
                 for (int i = 0; i < CL_NUM; i++) {
                     String clName = preCLName + "_" + i;
-                    commCS.createCollection(clName,options);                   
-                    count++;          
-                }                
+                    commCS.createCollection(clName, options);
+                    count++;
+                }
             } catch (BaseException e) {
-            	int successCLnums = count ;
-            	System.out.println("the create cl num is ="+successCLnums);  
-            } 
+                int successCLnums = count;
+                System.out.println("the create cl num is =" + successCLnums);
+            }
         }
     }
-    
-    private void createDomain(String doaminName){
-		try{
-			groupNames = groupMgr.getAllDataGroupName();
-			BSONObject option = new BasicBSONObject();
-            BSONObject groups = new BasicBSONList();           
+
+    private void createDomain(String doaminName) {
+        try {
+            groupNames = groupMgr.getAllDataGroupName();
+            BSONObject option = new BasicBSONObject();
+            BSONObject groups = new BasicBSONList();
             for (int i = 0; i < groupNames.size(); i++) {
                 groups.put("" + i, groupNames.get(i));
             }
             option.put("Groups", groups);
             option.put("AutoSplit", true);
-            sdb.createDomain(domainName, option);			
-		}catch(BaseException e){
-			Assert.fail("create domain failed, errMsg:" + e.getMessage());
-		}
-	}
-    
-    private void createCS(){
-		try{
-			BSONObject option = new BasicBSONObject();
-	        option.put("Domain", domainName);
-	        cs = sdb.createCollectionSpace(csName, option);			
-		}catch(BaseException e){
-			Assert.fail("create csName failed, errMsg:" + e.getMessage());
-		}
-	}
-    
-    
-    
+            sdb.createDomain(domainName, option);
+        } catch (BaseException e) {
+            Assert.fail("create domain failed, errMsg:" + e.getMessage());
+        }
+    }
+
+    private void createCS() {
+        try {
+            BSONObject option = new BasicBSONObject();
+            option.put("Domain", domainName);
+            cs = sdb.createCollectionSpace(csName, option);
+        } catch (BaseException e) {
+            Assert.fail("create csName failed, errMsg:" + e.getMessage());
+        }
+    }
+
     /**
-	* check the result of create cl
-	* the result: 1. to create cl success,create the same cl again failed
-	*             2. to create cl fail,create the same cl again successfully
-	*             3. to create cl fail,the actual cl created successfully did not return,
-	*                creating the same cl failed again
-	*/
+     * check the result of create cl the result: 1. to create cl success,create
+     * the same cl again failed 2. to create cl fail,create the same cl again
+     * successfully 3. to create cl fail,the actual cl created successfully did
+     * not return, creating the same cl failed again
+     */
     private void checkCreateCLResult() {
-        if ( CL_NUM == count ) {
-        	try {
-        		String sameCLName = preCLName + "_" + (count - 1);
-                cs.createCollection(sameCLName);               
-                Assert.fail("create the same cl should be fail");                
+        if (CL_NUM == count) {
+            try {
+                String sameCLName = preCLName + "_" + (count - 1);
+                cs.createCollection(sameCLName);
+                Assert.fail("create the same cl should be fail");
             } catch (BaseException e) {
-                // -22 SDB_DMS_EXIST  
+                // -22 SDB_DMS_EXIST
                 if (e.getErrorCode() != -22) {
-                	Assert.fail("the error not -22: "+e.getErrorCode()+e.getMessage());
+                    Assert.fail("the error not -22: " + e.getErrorCode() + e.getMessage());
                 }
-            } 
-        	
-        	try {
-        		String newclName = preCLName + "_" + count;
-                cs.createCollection(newclName);                                 
-            } catch (BaseException e) {                
-                Assert.fail("create new cl fail: "+e.getErrorCode()+e.getMessage());
-               
             }
-        	
-        }else{
-			//create cl fail,the count is not equals CL_NUM
-        	try {         	
-                cs.createCollection(preCLName + "_" + count);      
-            } catch (BaseException e) {            	
-                // -22 SDB_DMS_EXIST  
+
+            try {
+                String newclName = preCLName + "_" + count;
+                cs.createCollection(newclName, new BasicBSONObject("ReplSize", 0));
+            } catch (BaseException e) {
+                Assert.fail("create new cl fail: " + e.getErrorCode() + e.getMessage());
+
+            }
+
+        } else {
+            // create cl fail,the count is not equals CL_NUM
+            try {
+                cs.createCollection(preCLName + "_" + count, new BasicBSONObject("ReplSize", 0));
+            } catch (BaseException e) {
+                // -22 SDB_DMS_EXIST
                 if (e.getErrorCode() != -22) {
-                	Assert.fail("the error not -22: "+e.getErrorCode()+e.getErrorType());
+                    Assert.fail("the error not -22: " + e.getErrorCode() + e.getErrorType());
                 }
             }
         }
         checkListCL();
-    	insertByCL();         
-    }     
-    
-    /**
-	* randomly select a cl insert data,check the data split groups by domain
-	*/   
-    private void insertByCL() { 
-    	String clName = "";
-    	if (count != 0){
-    		Random random = new Random();    	
-        	clName = preCLName + "_" + random.nextInt(count);
-    	}else{
-    		clName = preCLName + "_" +count;
-    	}
-    	
-    	long insertNum = 1000;
-    	try{
-			 List<BSONObject>list = new ArrayList<BSONObject>();			 
-			 for ( long i = 0; i < insertNum; i++){
-				 BSONObject obj = new BasicBSONObject();
-				 ObjectId id = new ObjectId();
-				 obj.put("_id", id);				 		 
-				 obj.put("no", i);				
-				 obj.put("str", "test_" + String.valueOf(i));				 
-				 list.add(obj);				
-			 }			 
-			 DBCollection cl = sdb.getCollectionSpace(csName).getCollection(clName);
-		 	 cl.insert(list, DBCollection.FLG_INSERT_CONTONDUP);		
-		 }catch(BaseException e){
-			 Assert.assertTrue(false,"insert fail "+e.getErrorCode()+e.getMessage());
-		 }		
-    	
-    	//check data split the groups of domains
-    	long dataCount = 0;
-    	long recsNum = 0;
-    	for (int i = 0; i < groupNames.size(); i++) {
-    		String groupName = groupNames.get(i);
-    		try (Sequoiadb dataNode = sdb.getReplicaGroup(groupName).getMaster().connect() ){    			
-    			DBCollection cl = dataNode.getCollectionSpace(csName).getCollection(clName);
-    			dataCount = cl.getCount();
-    			recsNum += dataCount;
-    			if( dataCount == 0 ){
-    				Assert.fail("no data on group:"+groupName);
-    			}    				
-    		}catch (BaseException e) {                
-                Assert.fail(e.getMessage() + e.getErrorCode());
-            }    		
-        }
-    	Assert.assertEquals(recsNum, insertNum, "incorrect number of the cl,actnum="+recsNum );    	
+        insertByCL();
     }
-    
-    
+
+    /**
+     * randomly select a cl insert data,check the data split groups by domain
+     */
+    private void insertByCL() {
+        String clName = "";
+        if (count != 0) {
+            Random random = new Random();
+            clName = preCLName + "_" + random.nextInt(count);
+        } else {
+            clName = preCLName + "_" + count;
+        }
+
+        long insertNum = 1000;
+        try {
+            List<BSONObject> list = new ArrayList<BSONObject>();
+            for (long i = 0; i < insertNum; i++) {
+                BSONObject obj = new BasicBSONObject();
+                ObjectId id = new ObjectId();
+                obj.put("_id", id);
+                obj.put("no", i);
+                obj.put("str", "test_" + String.valueOf(i));
+                list.add(obj);
+            }
+            DBCollection cl = sdb.getCollectionSpace(csName).getCollection(clName);
+            cl.insert(list, DBCollection.FLG_INSERT_CONTONDUP);
+        } catch (BaseException e) {
+            Assert.assertTrue(false, "insert fail " + e.getErrorCode() + e.getMessage());
+        }
+
+        // check data split the groups of domains
+        long dataCount = 0;
+        long recsNum = 0;
+        for (int i = 0; i < groupNames.size(); i++) {
+            String groupName = groupNames.get(i);
+            try (Sequoiadb dataNode = sdb.getReplicaGroup(groupName).getMaster().connect()) {
+                DBCollection cl = dataNode.getCollectionSpace(csName).getCollection(clName);
+                dataCount = cl.getCount();
+                recsNum += dataCount;
+                if (dataCount == 0) {
+                    Assert.fail("no data on group:" + groupName);
+                }
+            } catch (BaseException e) {
+                Assert.fail(e.getMessage() + e.getErrorCode());
+            }
+        }
+        Assert.assertEquals(recsNum, insertNum, "incorrect number of the cl,actnum=" + recsNum);
+    }
+
     private void checkListCL() {
         // get expect cl name list
         List<BSONObject> expCLNames = new ArrayList<BSONObject>();
@@ -268,7 +269,7 @@ public class CreateCLAndKillPrimaryCatalog2280 extends SdbTestBase {
             nameBSON.put("Name", clFullName);
             expCLNames.add(nameBSON);
         }
-        
+
         // get actual cl name list
         DBCursor cursor = sdb.listCollections();
         List<BSONObject> actCLNames = new ArrayList<BSONObject>();
@@ -277,26 +278,26 @@ public class CreateCLAndKillPrimaryCatalog2280 extends SdbTestBase {
             actCLNames.add(result);
         }
         cursor.close();
-        
+
         // compare them
         sortByName(actCLNames);
         sortByName(expCLNames);
         if (!actCLNames.equals(expCLNames)) {
-            System.out.println("actCLNames="+actCLNames);
-            System.out.println("expCLNames="+expCLNames);
+            System.out.println("actCLNames=" + actCLNames);
+            System.out.println("expCLNames=" + expCLNames);
             Assert.fail("listCollections() is not the expected.");
         }
     }
-    
+
     private void sortByName(List<BSONObject> list) {
         Collections.sort(list, new Comparator<BSONObject>() {
+            @Override
             public int compare(BSONObject a, BSONObject b) {
-                String aName = (String)a.get("Name");
-                String bName = (String)b.get("Name");
+                String aName = (String) a.get("Name");
+                String bName = (String) b.get("Name");
                 return aName.compareTo(bName);
             }
         });
     }
-    
-    
+
 }
