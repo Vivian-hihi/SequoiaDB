88488ed3c08b1b5db6bd21717abfb2408bbc0b81
yinzhen <yinzhen@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-11 00:58:15 +0000

    修改用例为一个线程创建全文索引，一个线程删除全文索引，最后检查结果
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43632 6121f079-4a18-41d3-8c30-8870a322f2c2

--- testcase_new/story/java/src/test/java/com/sequoiadb/fulltext/parallel/Fulltext14414.java
+++ testcase_new/story/java/src/test/java/com/sequoiadb/fulltext/parallel/Fulltext14414.java
@@ -1,7 +1,5 @@
 package com.sequoiadb.fulltext.parallel;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,6 +9,7 @@ import com.sequoiadb.exception.BaseException;
 import com.sequoiadb.fulltext.utils.FullTextDBUtils;
 import com.sequoiadb.fulltext.utils.FullTextUtils;
 import com.sequoiadb.testcommon.FullTestBase;
+import com.sequoiadb.testcommon.SdbTestBase;
 import com.sequoiadb.threadexecutor.ThreadExecutor;
 import com.sequoiadb.threadexecutor.annotation.ExecuteOrder;
 
@@ -24,7 +23,7 @@ public class Fulltext14414 extends FullTestBase {
     private String fullIdxName = "idx14414";
     private String cappedCLName;
     private String esIndexName;
-    private AtomicInteger atoint = new AtomicInteger(0);
+    private boolean checkFlag = false;
     private int insertNum = 20000;
 
     @Override
@@ -41,42 +40,55 @@ public class Fulltext14414 extends FullTestBase {
     @Test
     public void test() throws Exception {
         ThreadExecutor thExecutor = new ThreadExecutor(FullTextUtils.THREAD_TIMEOUT);
-        for (int i = 0; i < 10; i++) {
-            thExecutor.addWorker(new CreateAndDropIdx());
-        }
+        thExecutor.addWorker(new CreateIdx());
+        thExecutor.addWorker(new DropIndex());
         thExecutor.run();
-        Assert.assertEquals(atoint.get(), 1);
 
         // 主备节点上索引信息一致，固定集合、索引信息、ES端数据一致
-        Assert.assertTrue(FullTextUtils.isIndexDeleted(sdb, esIndexName, cappedCLName));
-        Assert.assertTrue(FullTextUtils.isCLConsistency(cl));
-        Assert.assertTrue(FullTextUtils.isIndexConsistency(cl, fullIdxName));
+        if (cl.isIndexExist(fullIdxName)) {
+            Assert.assertTrue(FullTextUtils.isIndexCreated(cl, fullIdxName, insertNum));
+        } else {
+            if (checkFlag) {
+                Assert.assertTrue(FullTextUtils.isIndexDeleted(sdb, esIndexName, cappedCLName));
+            }
+        }
     }
 
     @Override
     protected void caseFini() throws Exception {
-        Assert.assertTrue(FullTextUtils.isIndexDeleted(sdb, esIndexName, cappedCLName));
+        if (checkFlag) {
+            Assert.assertTrue(FullTextUtils.isIndexDeleted(sdb, esIndexName, cappedCLName));
+        }
     }
 
-    private class CreateAndDropIdx {
+    private class CreateIdx {
         @ExecuteOrder(step = 1, desc = "多线程创建删除同一个全文索引")
         private void createFullIdx() {
-            Sequoiadb db = null;
-            try {
-                db = new Sequoiadb(coordUrl, "", "");
+            try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
                 DBCollection cl = db.getCollectionSpace(csName).getCollection(clName);
                 cl.createIndex(fullIdxName, "{'a':'text','b':'text','c':'text', 'd':'text', 'e':'text', 'f':'text'}",
                         false, false);
 
                 cappedCLName = FullTextDBUtils.getCappedName(cl, fullIdxName);
                 esIndexName = FullTextDBUtils.getESIndexName(cl, fullIdxName);
-                cl.dropIndex(fullIdxName);
-                atoint.incrementAndGet();
+                checkFlag = true;
             } catch (BaseException e) {
-                Assert.assertEquals(e.getErrorCode(), -42);
-            } finally {
-                if (db != null) {
-                    db.close();
+                if (e.getErrorCode() != -199) {
+                    throw e;
+                }
+            }
+        }
+    }
+
+    private class DropIndex {
+        @ExecuteOrder(step = 1, desc = "删除全文索引")
+        private void dropIndex() {
+            try (Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "")) {
+                DBCollection cl = db.getCollectionSpace(SdbTestBase.csName).getCollection(clName);
+                cl.dropIndex(fullIdxName);
+            } catch (BaseException e) {
+                if (e.getErrorCode() != -47) {
+                    throw e;
                 }
             }
         }
