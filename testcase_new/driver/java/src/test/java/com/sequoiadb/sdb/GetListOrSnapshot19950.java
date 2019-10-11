170332caa191b27558fed0e54b0a8d0b6a51d0e8
huangxiaoni <huangxiaoni@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-11 01:42:48 +0000

    SEQUOIADBMAINSTREAM-4999，新增
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43633 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/driver/java/src/test/java/com/sequoiadb/sdb/GetListOrSnapshot19950.java
@@ -0,0 +1,89 @@
+package com.sequoiadb.sdb;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.SdbTestBase;
+
+/**
+ * @Descreption seqDB-19950:getList新增hint/limit/skip参数，并新增list类型
+ * @Author huangxiaoni
+ * @Date 2019.10.10
+ */
+
+public class GetListOrSnapshot19950 extends SdbTestBase {
+    private int runSuccNum = 0;
+    private int expRunSuccNum = 2;
+    private Sequoiadb sdb;
+    private String clName = "cl19950";
+    private String fullCLName;
+
+    @BeforeClass
+    public void setUp() {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        sdb.getCollectionSpace(csName).createCollection(clName);
+        fullCLName = csName + "." + clName;
+    }
+
+    @Test
+    public void test_getList() {
+        // test hint / skip / limit
+        BSONObject query = new BasicBSONObject("Name", fullCLName);
+        BSONObject hint = new BasicBSONObject("", "test");
+        long skipRows = 0;
+        long returnRows = 1;
+        DBCursor cursor = sdb.getList(Sequoiadb.SDB_LIST_COLLECTIONS, query, null, null, hint, skipRows, returnRows);
+        int size = 0;
+        while (cursor.hasNext()) {
+            Object name = cursor.getNext().get("Name");
+            Assert.assertEquals(name, fullCLName);
+            size++;
+        }
+        Assert.assertEquals(size, returnRows);
+
+        // test listType: SDB_LIST_USERS, not need verify results
+        cursor = sdb.getList(Sequoiadb.SDB_LIST_USERS, null, null, null);
+        while (cursor.hasNext()) {
+            cursor.getNext().get("User");
+        }
+
+        // test listType: SDB_LIST_SVCTASKS, not need verify results
+        cursor = sdb.getList(Sequoiadb.SDB_LIST_SVCTASKS, null, null, null);
+        while (cursor.hasNext()) {
+            cursor.getNext();
+        }
+
+        runSuccNum++;
+    }
+
+    /*
+     * TestgetSnapshot15758 has covered hint/limit/skip of snapshot, the test
+     * only cover snapshotType: SDB_SNAP_SVCTASKS
+     */
+    @Test(enabled = false)
+    public void test_getSnapshot() {
+        // test snapshotType: SDB_SNAP_SVCTASKS, not need verify results
+        DBCursor cursor = sdb.getSnapshot(Sequoiadb.SDB_SNAP_SVCTASKS, null, null, null, null, 0, 1);
+        while (cursor.hasNext()) {
+            System.out.println(cursor.getNext().get("NodeName"));
+        }
+        runSuccNum++;
+    }
+
+    @AfterClass
+    public void tearDown() {
+        try {
+            if (runSuccNum < expRunSuccNum) {
+                sdb.getCollectionSpace(csName).dropCollection(clName);
+            }
+        } finally {
+            sdb.close();
+        }
+    }
+}
