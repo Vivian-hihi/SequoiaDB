bdaaf3bf496d8a454cd203ad0d4b992f2c949e31
zhaoyu <zhaoyu@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-10-12 02:00:43 +0000

    SEQUOIADBMAINSTREAM-5009新增事务自动化用例
    git-svn-id: http://192.168.20.11/sequoiadb/trunk@43681 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/uniqueIndex/Transaction20011.java
@@ -0,0 +1,97 @@
+package com.sequoiadb.transaction.uniqueIndex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.transaction.TransUtils;
+
+/**
+ * @FileName seqDB-20011 : 回滚多条记录，回滚第一条插入的记录与过程中插入的记录重复
+ * @Author zhaoyu
+ * @Date 2019年10月12日
+ */
+@Test(groups = { "rc", "ru", "rcuserbs" })
+public class Transaction20011 extends SdbTestBase {
+
+    private String clName = "transCL_20011";
+    private Sequoiadb sdb = null;
+    private DBCollection cl = null;
+    private DBCursor recordCur = null;
+    private List<BSONObject> expDataList = null;
+    private List<BSONObject> actDataList = null;
+
+    @BeforeClass
+    public void setUp() {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        cl = sdb.getCollectionSpace(csName).createCollection(clName);
+        cl.createIndex("a", "{a:1}", true, false);
+        expDataList = new ArrayList<BSONObject>();
+    }
+
+    @Test
+    public void test() {
+        Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+
+        BasicBSONObject r1 = new BasicBSONObject();
+        r1.put("a", 1);
+
+        BasicBSONObject r2 = new BasicBSONObject();
+        r2.put("a", 2);
+        try {
+            db.beginTransaction();
+            DBCollection cl1 = db.getCollectionSpace(csName).getCollection(clName);
+            cl1.insert(r1);
+            cl1.insert(r2);
+            cl1.delete(r1);
+            expDataList.add(r2);
+            recordCur = cl1.query(null, null, "{a:1}", "{'': null}");
+            actDataList = TransUtils.getReadActList(recordCur);
+            Assert.assertEquals(actDataList, expDataList);
+            actDataList.clear();
+            expDataList.clear();
+
+            cl.insert(r1);
+            expDataList.add(r1);
+            expDataList.add(r2);
+            recordCur = cl1.query(null, null, "{a:1}", "{'': null}");
+            actDataList = TransUtils.getReadActList(recordCur);
+            Assert.assertEquals(actDataList, expDataList);
+            actDataList.clear();
+            expDataList.clear();
+
+            db.rollback();
+            expDataList.add(r1);
+            recordCur = cl1.query(null, null, "{a:1}", "{'': null}");
+            actDataList = TransUtils.getReadActList(recordCur);
+            Assert.assertEquals(actDataList, expDataList);
+        } finally {
+            db.commit();
+            if (db != null) {
+                db.close();
+            }
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        sdb.getCollectionSpace(csName).dropCollection(clName);
+        if (recordCur != null) {
+            recordCur.close();
+        }
+        if (sdb != null) {
+            sdb.close();
+        }
+    }
+
+}
---
+++ testcase_new/story/java/src/test/java/com/sequoiadb/transaction/uniqueIndex/Transaction20013.java
@@ -0,0 +1,98 @@
+package com.sequoiadb.transaction.uniqueIndex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiadb.base.DBCollection;
+import com.sequoiadb.base.DBCursor;
+import com.sequoiadb.base.Sequoiadb;
+import com.sequoiadb.testcommon.SdbTestBase;
+import com.sequoiadb.transaction.TransUtils;
+
+/**
+ * @FileName seqDB-20013 : 回滚多条记录，回滚第一条更新的记录与过程中插入的记录重复
+ * @Author zhaoyu
+ * @Date 2019年10月12日
+ */
+@Test(groups = { "rc", "ru", "rcuserbs" })
+public class Transaction20013 extends SdbTestBase {
+
+    private String clName = "transCL_20013";
+    private Sequoiadb sdb = null;
+    private DBCollection cl = null;
+    private DBCursor recordCur = null;
+    private List<BSONObject> expDataList = null;
+    private List<BSONObject> actDataList = null;
+
+    @BeforeClass
+    public void setUp() {
+        sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+        cl = sdb.getCollectionSpace(csName).createCollection(clName);
+        cl.createIndex("a", "{a:1}", true, false);
+        expDataList = new ArrayList<BSONObject>();
+    }
+
+    @Test
+    public void test() {
+        Sequoiadb db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
+
+        String r1 = "{_id:1,a:1}";
+        String r2 = "{_id:2,a:2}";
+        String r3 = "{_id:1,a:3}";
+        String r4 = "{_id:3,a:1}";
+        try {
+            db.beginTransaction();
+            DBCollection cl1 = db.getCollectionSpace(csName).getCollection(clName);
+            cl1.insert(r1);
+            cl1.insert(r2);
+            cl1.update("{a:1}", "{$set:{a:3}}", null);
+            expDataList.add((BSONObject) JSON.parse(r2));
+            expDataList.add((BSONObject) JSON.parse(r3));
+            recordCur = cl1.query(null, null, "{a:1}", "{'': null}");
+            actDataList = TransUtils.getReadActList(recordCur);
+            Assert.assertEquals(actDataList, expDataList);
+            actDataList.clear();
+            expDataList.clear();
+
+            cl.insert(r4);
+            expDataList.add((BSONObject) JSON.parse(r4));
+            expDataList.add((BSONObject) JSON.parse(r2));
+            expDataList.add((BSONObject) JSON.parse(r3));
+            recordCur = cl1.query(null, null, "{a:1}", "{'': null}");
+            actDataList = TransUtils.getReadActList(recordCur);
+            Assert.assertEquals(actDataList, expDataList);
+            actDataList.clear();
+            expDataList.clear();
+
+            db.rollback();
+            expDataList.add((BSONObject) JSON.parse(r4));
+            recordCur = cl1.query(null, null, "{a:1}", "{'': null}");
+            actDataList = TransUtils.getReadActList(recordCur);
+            Assert.assertEquals(actDataList, expDataList);
+        } finally {
+            db.commit();
+            if (db != null) {
+                db.close();
+            }
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        sdb.getCollectionSpace(csName).dropCollection(clName);
+        if (recordCur != null) {
+            recordCur.close();
+        }
+        if (sdb != null) {
+            sdb.close();
+        }
+    }
+
+}
