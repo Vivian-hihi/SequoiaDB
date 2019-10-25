6ee98f2f7d901e4e990581650356139f66f90ef5
yinzhen <40-yinzhen@users.noreply.gitlab.example.com>
2019-10-25 15:11:56 +0800

    SEQUOIADBMAINSTREAM-5039 补充用例

---
+++ testcase_new/driver/python/monitor/test_list_svctasks_12504.py
@@ -0,0 +1,30 @@
+# @decription: test list svctasks
+# @testlink:   seqDB-12504
+# @interface:  get_list(self,list_type,kwargs)
+# @author:     yinzhen 2019-10-24
+
+import unittest
+from lib import testlib
+from pysequoiadb.client import (SDB_LIST_SVCTASKS)
+
+class TestListSvctasks12504(testlib.SdbTestBase):
+   def setUp(self):
+      pass
+
+   def test_list_backups_12504(self):
+
+      # get list with option
+      expect_result = {"Name":"Default", "TaskID":0}
+      cursor = self.db.get_list(SDB_LIST_SVCTASKS, condition={"Name":"Default"})
+      act_results = testlib.get_all_records(cursor)
+      act_result = act_results[0]
+      
+      # check list
+      self.check_list(expect_result, act_result)
+
+   def tearDown(self):
+      pass 
+     
+   def check_list(self, expect_result, act_result):
+      for field in expect_result:
+         self.assertEqual(expect_result[field], act_result[field])
