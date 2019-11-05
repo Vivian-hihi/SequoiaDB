2c24ac050100867feeb8fc446ecb31b28116f6b4
yinzhen <yinzhen@sequoiadb.com>
2019-11-05 13:35:05 +0800

    ci-#1221 失败用例修改

--- testcase_new/sdv/java/src/test/java/com/sequoiadb/crud/InsertAndDropCS11426.java
+++ testcase_new/sdv/java/src/test/java/com/sequoiadb/crud/InsertAndDropCS11426.java
@@ -19,16 +19,16 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
-* FileName: InsertAndDropCS11426.java
-* test content:concurrent insert, when drop cs  
-* testlink case:seqDB-11426
-* @author wuyan
-    * @Date    2017.11.9
-* @version 1.00
-*/
+ * FileName: InsertAndDropCS11426.java test content:concurrent insert, when drop
+ * cs testlink case:seqDB-11426
+ * 
+ * @author wuyan
+ * @Date 2017.11.9
+ * @version 1.00
+ */
 public class InsertAndDropCS11426 extends SdbTestBase {
-	private final String CLNAME = "cl_11426";
-	private final String CSNAME = "cs_11426";
+    private final String CLNAME = "cl_11426";
+    private final String CSNAME = "cs_11426";
     private Sequoiadb sdb = null;
     private CollectionSpace cs = null;
     private DBCollection dbcl;
@@ -36,103 +36,103 @@ public class InsertAndDropCS11426 extends SdbTestBase {
     @BeforeClass
     public void setup() {
         sdb = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-        if( sdb.isCollectionSpaceExist(CSNAME)){
-			sdb.dropCollectionSpace(CSNAME);
-		}
+        if (sdb.isCollectionSpaceExist(CSNAME)) {
+            sdb.dropCollectionSpace(CSNAME);
+        }
         cs = sdb.createCollectionSpace(CSNAME);
         dbcl = cs.createCollection(CLNAME);
     }
 
     @Test
     public void test() throws InterruptedException {
-    	InsertTask insertTask = new InsertTask();
+        InsertTask insertTask = new InsertTask();
         insertTask.start(20);
-       
-        //sleep random value to drop cs at different stages of insert
-        int time = new Random().nextInt(1000);
-		Thread.sleep(time);		
 
-		//drop cs fail is -147, repeat drop cs again
-        int eachSleepTime = 1000;
-        int maxSleetTime = 60000;
-   	 	int alreadySleepTime = 0;
-   	 	int errorNo = 0;
-   	 	do{
-   	 		errorNo = dropCS( sdb );   	 		
-   	 		Thread.sleep(eachSleepTime);
-   	 		alreadySleepTime += eachSleepTime;
-   	 		if( alreadySleepTime > maxSleetTime )
-   	 			Assert.fail("drop cs fail exceeds maximum waiting time:"+alreadySleepTime);
-   		 }while( errorNo == -147 || errorNo == -190);
-        
+        // sleep random value to drop cs at different stages of insert
+        int time = new Random().nextInt(1000);
+        Thread.sleep(time);
+
+        // drop cs fail is -147, repeat drop cs again
+        int eachSleepTime = 100;
+        int maxSleetTime = 90000;
+        int alreadySleepTime = 0;
+        int errorNo = 0;
+        do {
+            errorNo = dropCS(sdb);
+            Thread.sleep(eachSleepTime);
+            alreadySleepTime += eachSleepTime;
+            if (alreadySleepTime > maxSleetTime)
+                Assert.fail("drop cs fail exceeds maximum waiting time:" + alreadySleepTime);
+        } while (errorNo == -147 || errorNo == -190);
         Assert.assertTrue(insertTask.isSuccess(), insertTask.getErrorMsg());
-        
-        //check the result
+
+        // check the result
         Assert.assertFalse(sdb.isCollectionSpaceExist(CSNAME), "cs exist!! drop cs error e:" + errorNo);
-        //insert fail after drop cs 
-        try{
-        	dbcl.insert("{a:1}");
-        	Assert.fail("insert should be fail!");
-        }catch (BaseException e) {            	
-            if( e.getErrorCode() != -34 && e.getErrorCode() != -23 ){
-            	Assert.fail("insert should be fail! e:"+e.getErrorCode());
+        // insert fail after drop cs
+        try {
+            dbcl.insert("{a:1}");
+            Assert.fail("insert should be fail!");
+        } catch (BaseException e) {
+            if (e.getErrorCode() != -34 && e.getErrorCode() != -23) {
+                Assert.fail("insert should be fail! e:" + e.getErrorCode());
             }
-        } 
-       
+        }
+
     }
-    
+
     @AfterClass
     public void teardown() {
-    	try{
-    		if( sdb.isCollectionSpaceExist(CSNAME)){
-    			sdb.dropCollectionSpace(CSNAME);
-    		}    		
-    	}finally{
-    		if (sdb != null) {
+        try {
+            if (sdb.isCollectionSpaceExist(CSNAME)) {
+                sdb.dropCollectionSpace(CSNAME);
+            }
+        } finally {
+            if (sdb != null) {
                 sdb.close();
             }
-    	}
-        
-    }   
-    
-    private int dropCS(Sequoiadb sdb){
-    	 int errorNo = 0;
-    	 try{
-         	sdb.dropCollectionSpace(CSNAME);  
-         }catch (BaseException e) {  
-        	 errorNo = e.getErrorCode();         	
-         }    	 
-    	 return errorNo;    	 
+        }
+
     }
-    
-    private class InsertTask extends SdbThreadBase {    	
-		@Override
+
+    private int dropCS(Sequoiadb sdb) {
+        int errorNo = 0;
+        try {
+            sdb.dropCollectionSpace(CSNAME);
+        } catch (BaseException e) {
+            errorNo = e.getErrorCode();
+        }
+        return errorNo;
+    }
+
+    private class InsertTask extends SdbThreadBase {
+        @Override
         public void exec() throws Exception {
             Sequoiadb db = null;
             try {
                 db = new Sequoiadb(SdbTestBase.coordUrl, "", "");
-                DBCollection cl = db.getCollectionSpace(CSNAME).getCollection(CLNAME);                 
-                for ( int i = 0; i < 10000; i++){  	
-        			BSONObject obj = new BasicBSONObject();
-    				obj.put("a", i);         				
-    				obj.put("str", "test_" + String.valueOf(i));
-    				//insert the decimal type data
-    				String str = "32345.067891234567890123456789" + i;
-    				BSONDecimal decimal = new BSONDecimal(str);			
-    				obj.put("decimal",decimal);
-    				//the data type 
-    				Date now = new Date();
-    				obj.put("date",now);        			
-        			cl.insert(obj);        			
-        		}		              
-            } catch (BaseException e) {             	
-                if( e.getErrorCode() != -34 && e.getErrorCode() != -23&& e.getErrorCode() != -248 && e.getErrorCode() != -190&& e.getErrorCode() != -147){
-                	Assert.fail("insert fail!! e=" + e.getErrorCode() );
+                DBCollection cl = db.getCollectionSpace(CSNAME).getCollection(CLNAME);
+                for (int i = 0; i < 10000; i++) {
+                    BSONObject obj = new BasicBSONObject();
+                    obj.put("a", i);
+                    obj.put("str", "test_" + String.valueOf(i));
+                    // insert the decimal type data
+                    String str = "32345.067891234567890123456789" + i;
+                    BSONDecimal decimal = new BSONDecimal(str);
+                    obj.put("decimal", decimal);
+                    // the data type
+                    Date now = new Date();
+                    obj.put("date", now);
+                    cl.insert(obj);
+                }
+            } catch (BaseException e) {
+                if (e.getErrorCode() != -34 && e.getErrorCode() != -23 && e.getErrorCode() != -248
+                        && e.getErrorCode() != -190 && e.getErrorCode() != -147) {
+                    Assert.fail("insert fail!! e=" + e.getErrorCode());
                 }
             } finally {
                 if (db != null)
                     db.close();
             }
-        }    
-    } 
+        }
+    }
 }
