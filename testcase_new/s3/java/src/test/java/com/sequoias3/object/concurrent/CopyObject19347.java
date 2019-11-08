db53c5dc5a8005fbbcc995efd14ae8fe33d07026
fanyu <fanyu@sequoiadb.com>
2019-11-08 16:28:09 +0800

    ci#1264 并发下，用例使用int类型统计成功的test个数，线程不安全，导致数据残留

--- testcase_new/s3/java/src/test/java/com/sequoias3/object/concurrent/CopyObject19347.java
+++ testcase_new/s3/java/src/test/java/com/sequoias3/object/concurrent/CopyObject19347.java
@@ -1,19 +1,18 @@
 package com.sequoias3.object.concurrent;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
 import com.amazonaws.services.s3.AmazonS3;
 import com.sequoias3.testcommon.CommLib;
 import com.sequoias3.testcommon.S3TestBase;
 import com.sequoias3.testcommon.TestTools;
 import com.sequoias3.testcommon.s3utils.ObjectUtils;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 /**
  * @Description seqDB-19347:并发指定相同源对象复制对象，目标对象不同
@@ -22,22 +21,22 @@ import com.sequoias3.testcommon.s3utils.ObjectUtils;
  * @version 1.00
  */
 public class CopyObject19347 extends S3TestBase {
+
     @DataProvider(name = "copyObjectProvider", parallel = true)
     public Object[][] generateKeyName() {
-        return new Object[][] {
-                // the parameter is srcBucketName, destBucketName,destKeyName
-                // test a: destBucketName is the same as source bucketName
-                new Object[] { bucketNameA, bucketNameA, "/dest//aa%maa/bb%object19347" },
-                new Object[] { bucketNameA, bucketNameA, "/dest/object19347" },
-                new Object[] { bucketNameA, bucketNameA, "/dest/c/object19347" },
-                // test b: destBucketName is different from source bucketName
-                new Object[] { bucketNameA, bucketNameB, "/dest/aa/object19347" },
-                new Object[] { bucketNameA, bucketNameB, "/dest/bb/object19347" },
-                new Object[] { bucketNameA, bucketNameB, "/dest/cc/object19347" } };
+        return new Object[][]{
+            // the parameter is srcBucketName, destBucketName,destKeyName
+            // test a: destBucketName is the same as source bucketName
+            new Object[]{bucketNameA, bucketNameA, "/dest//aa%maa/bb%object19347"},
+            new Object[]{bucketNameA, bucketNameA, "/dest/object19347"},
+            new Object[]{bucketNameA, bucketNameA, "/dest/c/object19347"},
+            // test b: destBucketName is different from source bucketName
+            new Object[]{bucketNameA, bucketNameB, "/dest/aa/object19347"},
+            new Object[]{bucketNameA, bucketNameB, "/dest/bb/object19347"},
+            new Object[]{bucketNameA, bucketNameB, "/dest/cc/object19347"}};
 
     }
 
-    private boolean runSuccess = false;
     private String srcKeyName = "src/bb%object19347";
     private String bucketNameA = "bucket19347a";
     private String bucketNameB = "bucket19347b";
@@ -45,7 +44,7 @@ public class CopyObject19347 extends S3TestBase {
     private int fileSize = 1024 * 1024 * 10;
     private File localPath = null;
     private String filePath = null;
-    private int successNum = 0;
+    private AtomicInteger actSuccessTests = new AtomicInteger(0);
 
     @BeforeClass
     private void setUp() throws IOException {
@@ -65,21 +64,20 @@ public class CopyObject19347 extends S3TestBase {
     }
 
     @Test(dataProvider = "copyObjectProvider")
-    public void testCopyObject(String srcBucketName, String destBucketName, String destKeyName) throws Exception {
+    public void testCopyObject(String srcBucketName, String destBucketName, String destKeyName)
+        throws Exception {
         s3Client.copyObject(srcBucketName, srcKeyName, destBucketName, destKeyName);
-        checkObjectContent(destBucketName, destKeyName);
-
-        successNum++;
-        // all threads(6) execute successfully, then set runsuccess=true
-        if (successNum == 6) {
-            runSuccess = true;
-        }
+        //check result
+        String downfileMd5 = ObjectUtils
+            .getMd5OfObject(s3Client, localPath, destBucketName, destKeyName);
+        Assert.assertEquals(downfileMd5, TestTools.getMD5(filePath));
+        actSuccessTests.getAndIncrement();
     }
 
     @AfterClass
     private void tearDown() {
         try {
-            if (runSuccess) {
+            if (actSuccessTests.get() == generateKeyName().length) {
                 CommLib.clearBucket(s3Client, bucketNameA);
                 CommLib.clearBucket(s3Client, bucketNameB);
                 TestTools.LocalFile.removeFile(localPath);
@@ -87,10 +85,5 @@ public class CopyObject19347 extends S3TestBase {
         } finally {
             s3Client.shutdown();
         }
-    }
-
-    private void checkObjectContent(String bucketName, String keyName) throws Exception {
-        String downfileMd5 = ObjectUtils.getMd5OfObject(s3Client, localPath, bucketName, keyName);
-        Assert.assertEquals(downfileMd5, TestTools.getMD5(filePath));
     }
 }
