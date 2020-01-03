7b0dc67245a317368e4ac6d4b3e25ff820a4c517
yangqincheng <yangqincheng@sequoiadb.com>
2020-01-03 16:34:39 +0800

    SEQUOIADBMAINSTREAM-5330 修改 java 驱动使用中文密码连接 sequoiadb 报鉴权失败错误

--- driver/java/src/test/java/com/sequoiadb/base/TestUser.java
+++ driver/java/src/test/java/com/sequoiadb/base/TestUser.java
@@ -5,6 +5,8 @@ import com.sequoiadb.exception.SDBError;
 import com.sequoiadb.test.SingleCSCLTestCase;
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
 import static org.junit.Assert.fail;
 
 public class TestUser extends SingleCSCLTestCase {
@@ -19,6 +21,30 @@ public class TestUser extends SingleCSCLTestCase {
             if (e.getErrorCode() != SDBError.SDB_RTN_COORD_ONLY.getErrorCode()) {
                 fail(e.toString());
             }
+        }
+    }
+
+    @Test
+    public void testUserWithChinese(){
+
+        // case 1: UTF-8
+        try {
+            String user = "用户";
+            String password = new String("密码".getBytes("UTF-8"));
+            sdb.createUser(user, password);
+            sdb.removeUser(user, password);
+        }catch (BaseException | UnsupportedEncodingException e){
+            throw new BaseException(SDBError.SDB_INVALIDARG, e);
+        }
+
+        // case 2: GBK
+        try {
+            String user = "用户";
+            String password = new String("密码".getBytes("GBK"));
+            sdb.createUser(user, password);
+            sdb.removeUser(user, password);
+        }catch (BaseException | UnsupportedEncodingException e){
+            throw new BaseException(SDBError.SDB_INVALIDARG, e);
         }
     }
 }
