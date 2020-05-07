dc64bc1ed4e536af8661e978c12138bc87447ff3
chimanzhao <chimanzhao@sequoiadb.com>
2020-05-07 09:38:13 +0800

    优化执行计划公共方法

--- testcase_new/story/js/explain/commlib.js
+++ testcase_new/story/js/explain/commlib.js
@@ -1,8 +1,26 @@
 /************************************
-*@Description: seqDB-11353:seqDB-11353:rtnPredicate为[$minKey, $maxKey]的索引选择
-*@author:      chimanzhao
-*@createdate:  2020.4.25
-*@testlinkCase: seqDB-11353
+*@description : Public function for explain
+*@author : chimanzhao
+*@createdate:  2020.5.6
 **************************************/
 import( "../lib/basic_operation/sequoiadb.js" );
-import ("../lib/main.js");
+import( "../lib/main.js" );
+
+function testExplain( conds, dbcl, indexName, scanType )
+{
+   for ( var i = 0; i < conds.length; ++i )
+   {
+      checkExplain( dbcl, conds[i], indexName, scanType );
+   }
+}
+
+function checkExplain( dbcl, cond, expIndexName, expScanType )
+{
+   var explainObj = dbcl.find( cond ).explain().next().toObj();
+   var IndexName  = explainObj.IndexName;
+   var ScanType   = explainObj.ScanType;
+   if(expIndexName !== IndexName || expScanType !== ScanType)
+   {
+      throw new Error("索引选择错误！")
+   }
+}
