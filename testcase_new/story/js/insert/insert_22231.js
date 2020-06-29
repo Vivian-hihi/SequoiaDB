8ab85567e16f67b78d947413232481a2817e9c4e
chimanzhao <chimanzhao@sequoiadb.com>
2020-06-29 19:17:04 +0800

    补充自动化用例

---
+++ testcase_new/story/js/insert/insert_22231.js
@@ -0,0 +1,25 @@
+/************************************
+*@Description: seqDB-22231:插入错误$date参数报错_insert({a:{$date:null}})
+*@Author      : 2020.06.01:chimanzhao
+**************************************/
+import( "../lib/main.js" );
+testConf.clName = COMMCLNAME + "_22231";
+
+main( test )
+
+function test ( testPara )
+{
+   var dbcl = testPara.testCL;
+   try
+   {
+      dbcl.insert( { a: { $date: null } } )
+      assert.fail( "exp fail but act success!" )
+   }
+   catch( e )
+   {
+      if( e.message !== -6 )
+      {
+         throw new Error( e );
+      }
+   }
+}
