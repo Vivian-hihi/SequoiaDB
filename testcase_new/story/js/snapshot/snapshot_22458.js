51a3558648639862a377884342feefe1c43f794c
zhaoxiaoni <42-zhaoxiaoni@users.noreply.gitlab.example.com>
2020-07-16 18:34:53 +0800

    强制从主节点取快照

--- testcase_new/story/js/snapshot/snapshot_22458.js
+++ testcase_new/story/js/snapshot/snapshot_22458.js
@@ -10,7 +10,6 @@ main( test );
 function test()
 {
    var clNames = [];
-   var groupName = commGetGroups ( db )[0][0].GroupName;
    for( var i = 0; i < 10; i++ )
    {
       var clName = "cl_22458_" + i;
@@ -18,7 +17,7 @@ function test()
       var indexName = "index_22458_" + i;
       var indexDef = { "a.b.c": 1, "d.e.f": 1, "g.h.i": 1 };
       commDropCL ( db, COMMCSNAME, clName );
-      var cl = commCreateCL ( db, COMMCSNAME, clName, { "Group": groupName } );
+      var cl = commCreateCL ( db, COMMCSNAME, clName );
       commCreateIndex ( cl, indexName, indexDef );
 
       var records = [];
@@ -30,13 +29,15 @@ function test()
       db.analyze( { "Collection": COMMCSNAME + "." + clName, "SampleNum": 200 } );
    }
 
+   var expResult = [].concat( clNames );
    for( var i = 0; i < 10; i++ )
-   {
+   { 
       var actResult = [];
       commDropCL( db, COMMCSNAME, clNames[i] );
-      var cursor = db.snapshot( SDB_SNAP_INDEXSTATS );
+
+      var cursor = db.snapshot( SDB_SNAP_INDEXSTATS, { "NodeSelect": "master" } );
       while( cursor.next() )
-      {
+      {    
          var collection = cursor.current().toObj().Collection;
          var index = cursor.current().toObj().Index.toString();
          if( clNames.indexOf( collection ) !== -1 && index !== "$id" )
@@ -44,9 +45,11 @@ function test()
             actResult.push( collection );
          }
       }
-      if( actResult.length !== clNames.length - ( i + 1 ) )
-      {
-         throw new Error( "expResult: " + clNames.length - ( i + 1 ) + ", actResult: " + actResult );
+
+      expResult.shift();
+      if( actResult.length !== expResult.length )
+      { 
+         throw new Error( "expResult: " + expResult + ", actResult: " + actResult );
       }
    }
 }
