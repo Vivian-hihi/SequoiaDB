784fde7957a9a22d9f46f21715861d8184fa8810
liuli <liuli@sequoiadb.com>
2023-07-14 10:54:48 +0800

    SEQUOIADBMAINSTREAM-9615:数据恢复工具自动化用例

---
+++ testcase_new/story/js/sdbrevert_sync/sdbrevert_32300.js
@@ -0,0 +1,68 @@
+/******************************************************************************
+ * @Description   : seqDB-32300:使用--hostname和--svcname指定连接地址
+ * @Author        : liuli
+ * @CreateTime    : 2023.07.07
+ * @LastEditTime  : 2023.07.13
+ * @LastEditors   : liuli
+ ******************************************************************************/
+testConf.clName = COMMCLNAME + "_32300_" + generateRandomString( 5 );
+testConf.useSrcGroup = true;
+testConf.skipStandAlone = true;
+
+main( test );
+
+function test ( testPara )
+{
+   var srcGroupName = testPara.srcGroupName;
+   var dbcl = testPara.testCL;
+   var docs = { _id: 2, a: 2 };
+   dbcl.insert( docs );
+
+   // 删除数据
+   dbcl.remove();
+   db.sync( { GroupName: srcGroupName } );
+
+   // 获取主节点
+   var masterNode = db.getRG( srcGroupName ).getMaster();
+   var hostName = masterNode.getHostName();
+   var scvName = masterNode.getServiceName();
+   var masterNodeName = hostName + ":" + scvName;
+
+   // 获取同步日志路径
+   var replicalogPath = getReplicalogPath( db, masterNodeName );
+
+   var remoteObj = new Remote( hostName, CMSVCNAME );
+   var cmd = remoteObj.getCmd();
+
+   // 创建临时集合用于恢复数据
+   var clNameTmp = "cl_32300_tmp";
+   commDropCL( db, COMMCSNAME, clNameTmp );
+   var tmpCL = commCreateCL( db, COMMCSNAME, clNameTmp );
+
+   // --hostname和--svcname为正确地址
+   var installDir = commGetRemoteInstallPath( hostName, CMSVCNAME );
+   var command = installDir + "/bin/sdbrevert --targetcl " + COMMCSNAME + "." + testConf.clName + " --logpath "
+      + replicalogPath + " --output " + COMMCSNAME + "." + clNameTmp + " --hostname " + COORDHOSTNAME
+      + " --svcname " + COORDSVCNAME;
+
+   cmd.run( command );
+
+   // 校验临时集合数据，不校验lsn和source字段
+   var expResult = [{ entry: { _id: 2, a: 2 }, optype: "DOC_DELETE", "label": "" }];
+   var actResult = tmpCL.find( {}, { "lsn": { "$include": 0 }, "source": { "$include": 0 } } );
+   commCompareResults( actResult, expResult );
+
+   // --hostname和--svcname指定地址不可用
+   tmpCL.truncate();
+   var command = installDir + "/bin/sdbrevert --targetcl " + COMMCSNAME + "." + testConf.clName + " --logpath "
+      + replicalogPath + " --output " + COMMCSNAME + "." + clNameTmp + " --hostname " + "host_32300"
+      + " --svcname " + "32300";
+
+   assert.tryThrow( 135, function()
+   {
+      cmd.run( command );
+   } );
+
+   remoteObj.close();
+   commDropCL( db, COMMCSNAME, clNameTmp );
+}
