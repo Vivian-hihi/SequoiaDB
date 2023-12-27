ed7f243c3e41ee672aded7dd55cbc5d6c0ae4fae
chenzejia <chenzejia@sequoiadb.com>
2023-12-27 16:36:01 +0800

    SDB支持GB18030编码，移动dcl用例到串行目录

---
+++ testcase_new/story/js/charset_gb18030_sync/gb18030_33905.js
@@ -0,0 +1,72 @@
+/************************************
+*@Description: seqDB-33905 设置字符集为GB18030，执行dcl操作
+*@author:      chenzejia
+*@createDate:  2023.12.16
+**************************************/
+testConf.skipStandAlone = true;
+main( test );
+function test ()
+{
+   var roleName = "角色_33905";
+   var userName = "user_33905";
+   var passwd = "123456"
+   var csName = "cs_33905";
+   var authDB;
+
+   db.setCharsets( "GB18030" );
+   try
+   {
+      db.dropRole( roleName );
+   } catch( e )
+   {
+      if( e != SDB_AUTH_ROLE_NOT_EXIST )
+      {
+         throw e;
+      }
+   }
+   // create role
+   db.createRole( { Role: roleName } );
+   try
+   {
+      // grant privilege
+      db.grantPrivilegesToRole( roleName, [{ Resource: { cs: csName, cl: "" }, Actions: ["createCL", "dropCL"] }] )
+      // grant role
+      db.grantRolesToRole( roleName, ["_" + csName + ".admin"] );
+      var roleInfo = db.getRole( roleName ).toObj();
+      assert.equal( roleInfo.Role, roleName, "check role failed,roleInfo: " + JSON.stringify( roleInfo ) );
+      assert.equal( roleInfo.Roles, ["_" + csName + ".admin"], "check role failed,roleInfo: " + JSON.stringify( roleInfo ) );
+   } catch( e )
+   {
+      db.dropRole( roleName );
+      throw e;
+   }
+   // create user
+   db.createUsr( userName, passwd, { Role: "admin" } );
+   try
+   {
+      authDB = new Sdb( COORDHOSTNAME, COORDSVCNAME, userName, passwd );
+      authDB.setCharsets( "GB18030" );
+      // grant role
+      authDB.grantRolesToUser( userName, [roleName] );
+      // check user info
+      var userInfo = authDB.getUser( userName ).toObj();
+      assert.equal( userInfo.User, userName, "check user failed, userinfo: " + JSON.stringify( userInfo ) );
+      // SEQUOIADBMAINSTREAM-10053
+      //assert.equal( userInfo.Roles, ["_root", roleName], "check user failed, userinfo: " + JSON.stringify( userInfo ) );
+   } finally
+   {
+      // drop role
+      authDB.dropRole( roleName );
+      assert.tryThrow( SDB_AUTH_ROLE_NOT_EXIST, function()
+      {
+         authDB.getRole( roleName );
+      } );
+      // drop user
+      authDB.dropUsr( userName, passwd );
+      assert.tryThrow( SDB_AUTH_USER_NOT_EXIST, function()
+      {
+         authDB.getUser( userName );
+      } );
+      authDB.close();
+   }
+}
